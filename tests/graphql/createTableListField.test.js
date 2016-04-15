e7759b7bba4c9b613cfc55d9d0557ea6127a3c0c
calebmer <calebmeredith8@gmail.com>
2016-04-15 17:45:27 -0400

    refactor: split out resolve table list

--- tests/graphql/createTableListField.test.js
+++ tests/graphql/createTableListField.test.js
@@ -1,20 +1,9 @@
-import assert from 'assert'
 import expect from 'expect'
 import { GraphQLObjectType, GraphQLNonNull, GraphQLList } from 'graphql'
-import { getClient, TestTable } from '../helpers.js'
-import getCatalog from '#/postgres/getCatalog.js'
-import createTableType from '#/graphql/createTableType.js'
+import { TestTable } from '../helpers.js'
 import createTableListField from '#/graphql/createTableListField.js'
 
 describe('graphql/createTableListField', () => {
-  const testCreateTableListField = async tableName => {
-    const client = await getClient()
-    const catalog = await getCatalog(client)
-    const table = catalog.getTable('create_table_list_field', tableName)
-    const field = createTableListField(client, table, createTableType(client, table))
-    return field
-  }
-
   it('has an object type', async () => {
     const { type } = createTableListField(new TestTable())
     expect(type).toBeA(GraphQLObjectType)
@@ -69,93 +58,4 @@ describe('graphql/createTableListField', () => {
   })
 
   it.skip('implements a connection interface')
-
-  it.skip('does not allow `first` and `last` together', async () => {
-    const personList = await testCreateTableListField('person')
-
-    try {
-      await personList.resolve({}, { first: 2, last: 5 })
-      throw new Error('Error not thrown!')
-    }
-    catch (error) {
-      if (!/cannot define both a/i.test(error.message))
-        throw error
-    }
-  })
-
-  it.skip('will get the correct `hasNextPage` and `hasPreviousPage`', async () => {
-    const personList = await testCreateTableListField('person')
-
-    const expectPageInfo = async (pageInfo, hasNextPage, hasPreviousPage) => {
-      expect(await pageInfo.hasNextPage).toEqual(hasNextPage)
-      expect(await pageInfo.hasPreviousPage).toEqual(hasPreviousPage)
-    }
-
-    await Promise.all([
-      expectPageInfo(personList.resolve({}, { orderBy: 'id' }), false, false),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', first: 2 }), true, false),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', first: 3 }), true, false),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', first: 4 }), false, false),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', offset: 1 }), false, true),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', offset: 2 }), false, true),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', offset: 50 }), false, false),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', first: 1, offset: 1 }), true, true),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', first: 2, offset: 3 }), false, true),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', first: 2, offset: 2 }), false, true),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', first: 1, offset: 2 }), true, true),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', after: '2' }), false, true),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', after: '50' }), false, false),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', after: '50', before: '60' }), false, false),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', after: '1', before: '3' }), true, true),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', after: '1', first: 2 }), true, true),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', after: '1', first: 4 }), false, true),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', before: '1' }), false, false),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', before: '3' }), true, false),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', before: '4' }), true, false),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', before: '5' }), false, false),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', before: '5', last: 2 }), false, true),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', before: '4', last: 2 }), true, true),
-      expectPageInfo(personList.resolve({}, { orderBy: 'id', before: '4', first: 2 }), true, false),
-    ])
-  })
-
-  it.skip('will get the correct `totalLength`', async () => {
-    const personList = await testCreateTableListField('person')
-    expect(await personList.resolve({}, {}).totalCount).toEqual(4)
-  })
-
-  it.skip('will get the correct `list`', async () => {
-    const personList = await testCreateTableListField('person')
-    assert.deepEqual(await personList.resolve({}, { orderBy: 'id' }).list, [{ id: 1, name: 'Jane' }, { id: 2, name: 'Jim' }, { id: 3, name: 'Bob' }, { id: 4, name: 'Betsy' }])
-    assert.deepEqual(await personList.resolve({}, { orderBy: 'id', first: 1 }).list, [{ id: 1, name: 'Jane' }])
-    assert.deepEqual(await personList.resolve({}, { orderBy: 'id', first: 1, offset: 1 }).list, [{ id: 2, name: 'Jim' }])
-  })
-
-  it.skip('will correctly order with `orderBy` and `descending`', async () => {
-    const personList = await testCreateTableListField('person')
-    assert.deepEqual(await personList.resolve({}, { orderBy: 'name' }).list, [{ id: 4, name: 'Betsy' }, { id: 3, name: 'Bob' }, { id: 1, name: 'Jane' }, { id: 2, name: 'Jim' }])
-    assert.deepEqual(await personList.resolve({}, { orderBy: 'name', descending: true }).list, [{ id: 4, name: 'Betsy' }, { id: 3, name: 'Bob' }, { id: 1, name: 'Jane' }, { id: 2, name: 'Jim' }].reverse())
-    assert.deepEqual(await personList.resolve({}, { orderBy: 'name', descending: true, first: 2 }).list, [{ id: 2, name: 'Jim' }, { id: 1, name: 'Jane' }])
-  })
 })
-
-// before(setupDatabase(`
-// drop schema if exists create_table_list_field cascade;
-//
-// create schema create_table_list_field;
-//
-// set search_path = 'create_table_list_field';
-//
-// create table camel_case_me (noop int);
-//
-// create table person (
-//   id serial primary key,
-//   name varchar not null
-// );
-//
-// insert into person (id, name) values
-//   (1, 'Jane'),
-//   (2, 'Jim'),
-//   (3, 'Bob'),
-//   (4, 'Betsy');
-// `))
---
+++ tests/graphql/resolveTableListField.test.js
@@ -0,0 +1,34 @@
+import expect, { createSpy } from 'expect'
+import { noop } from 'lodash'
+import { TestTable } from '../helpers.js'
+import resolveTableListField from '#/graphql/resolveTableListField.js'
+
+// More in depth testing is done in integration tests.
+describe('graphql/resolveTableListField', () => {
+  it('does not allow `first` and `last` together', async () => {
+    const resolve = resolveTableListField(new TestTable())
+
+    try {
+      await resolve({}, { first: 2, last: 5 }, {})
+      throw new Error('Error not thrown!')
+    }
+    catch (error) {
+      if (!/cannot define both a/i.test(error.message))
+        throw error
+    }
+  })
+
+  it('will lazily get properties and memoize common data', async () => {
+    const resolve = resolveTableListField(new TestTable())
+    const client = { queryAsync: createSpy().andReturn(Promise.resolve({ rows: [] })) }
+    const result1 = await resolve({}, { orderBy: 'id' }, { client })
+    const result2 = await resolve({}, { orderBy: 'id', descending: true }, { client })
+    expect(client.queryAsync.calls.length).toEqual(0)
+    noop(await Promise.all([result1.hasNextPage, result1.hasPreviousPage, result2.hasNextPage, result2.hasPreviousPage]))
+    expect(client.queryAsync.calls.length).toEqual(2)
+    noop(await result2.list)
+    expect(client.queryAsync.calls.length).toEqual(2)
+    noop(await Promise.all([result2.list, result1.list, result1.edges, result2.edges]))
+    expect(client.queryAsync.calls.length).toEqual(2)
+  })
+})
