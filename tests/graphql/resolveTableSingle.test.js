3f51315a9075850734396888b0cb7dea8f93eb73
calebmer <calebmeredith8@gmail.com>
2016-04-16 10:58:31 -0400

    test: add test for resolveTableSingle

---
+++ tests/graphql/resolveTableSingle.test.js
@@ -0,0 +1,19 @@
+import expect, { createSpy } from 'expect'
+import { TestTable } from '../helpers.js'
+import resolveTableSingle from '#/graphql/resolveTableSingle.js'
+
+describe('resolveTableSingle', () => {
+  it('will only call the client once for many resolves', async () => {
+    const table = new TestTable()
+    const resolve = resolveTableSingle(table, [table.columns[0]], () => [1])
+    const client = { queryAsync: createSpy().andReturn(Promise.resolve({ rows: [] })) }
+    expect(client.queryAsync.calls.length).toEqual(0)
+    await Promise.all([
+      resolve({}, {}, { client }),
+      resolve({}, {}, { client }),
+      resolve({}, {}, { client }),
+      resolve({}, {}, { client }),
+    ])
+    expect(client.queryAsync.calls.length).toEqual(1)
+  })
+})
