dc0614bd78bc93e9d491feef642c6de65afee515
calebmer <calebmeredith8@gmail.com>
2016-04-30 09:18:20 -0400

    Use a session for request

--- src/createServer.js
+++ src/createServer.js
@@ -27,9 +27,25 @@ const createServer = async ({ graphqlSchema, pgConfig, route, development }) => 
   server.use(route || '/', graphqlHTTP(async req => {
     // Acquire a new client for every request.
     const client = await pg.connectAsync(pgConfig)
+
+    // Start a session for our client. Useful when setting roles.
+    await client.queryAsync('begin')
+
     // Make sure we release our client back to the pool once the response has
     // finished.
-    onFinished(req.res, () => client.end())
+    onFinished(req.res, () => {
+      // Try to end our session with a commit. If it succeeds, release the
+      // client back into the pool. If it fails, release the client back into
+      // the pool, but also report that it failed. We cannot report an error in
+      // the request at this point because it has finished.
+      client.queryAsync('commit')
+      .then(() => client.end())
+      .catch(error => {
+        console.error(error.stack) // eslint-disable-line no-console
+        client.end()
+      })
+    })
+
     return {
       schema: graphqlSchema,
       context: { client },
@@ -45,9 +61,7 @@ const createServer = async ({ graphqlSchema, pgConfig, route, development }) => 
 export default createServer
 
 const developmentFormatError = error => {
-  /* eslint-disable no-console */
-  console.error(error.stack)
-  /* eslint-enable no-console */
+  console.error(error.stack) // eslint-disable-line no-console
   return {
     message: error.message,
     locations: error.locations,
