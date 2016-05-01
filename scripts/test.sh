97f44e97b36d09f724c87ae417348c5910e8887b
calebmer <calebmeredith8@gmail.com>
2016-05-01 15:42:23 -0400

    Use min reporter for watching tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-mocha tests/**/*.test.js tests/**/**/*.test.js --require babel-register --require ./src/promisify.js $@
+mocha \
+  tests/**/*.test.js tests/**/**/*.test.js \
+  --require babel-register \
+  --require ./src/promisify.js \
+  $@
