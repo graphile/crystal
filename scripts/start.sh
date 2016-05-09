59dd4efd8abdb5ac74ce74291f25bf425abd98c9
calebmer <calebmeredith8@gmail.com>
2016-05-09 16:33:25 -0400

    Make start script take a schema argument

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 babel-watch --watch src \
-  src/main.js -- postgres://localhost:5432 --schema forum_example --development
+  src/main.js -- postgres://localhost:5432 \
+    --schema ${1-forum_example} \
+    --development
