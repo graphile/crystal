907d9eaab9329d119379be94b5b37e3dbb6abc0a
calebmer <calebmeredith8@gmail.com>
2016-05-11 17:31:03 -0400

    Add query viewer field

--- src/graphql/query/createQueryType.js
+++ src/graphql/query/createQueryType.js
@@ -1,8 +1,6 @@
-import { fromPairs, ary, assign } from 'lodash'
 import { GraphQLObjectType } from 'graphql'
-import createNodeQueryField from './createNodeQueryField.js'
-import createTableQueryFields from './createTableQueryFields.js'
-import createProcedureQueryField from './createProcedureQueryField.js'
+import createQueryFields from './createQueryFields.js'
+import createViewerField from './createViewerField.js'
 
 /**
  * Creates the Query type for the entire schema. To see the fields created for
@@ -16,23 +14,8 @@ const createQueryType = schema =>
     name: 'Query',
     description: schema.description || 'The entry type for GraphQL queries.',
     fields: {
-      // Add the node query field.
-      node: createNodeQueryField(schema),
-      // Add fields for procedures.
-      ...fromPairs(
-        schema
-        .getProcedures()
-        .filter(({ isMutation }) => !isMutation)
-        .filter(procedure => !procedure.hasTableArg())
-        .map(procedure => [procedure.getFieldName(), createProcedureQueryField(procedure)])
-      ),
-      // Add the table query fields.
-      ...(
-        schema
-        .getTables()
-        .map(table => createTableQueryFields(table))
-        .reduce(ary(assign, 2), {})
-      ),
+      ...createQueryFields(schema),
+      viewer: createViewerField(schema),
     },
   })
 
---
+++ src/graphql/query/createViewerField.js
@@ -0,0 +1,20 @@
+import { memoize } from 'lodash'
+import { GraphQLObjectType, GraphQLNonNull } from 'graphql'
+import createQueryFields from './createQueryFields.js'
+
+const createViewerField = memoize(schema => ({
+  type: new GraphQLNonNull(new GraphQLObjectType({
+    name: 'Viewer',
+    description: '',
+    fields: createQueryFields(schema),
+  })),
+
+  description:
+    'A single entry query for advanced data clients like Relay. Nothing ' +
+    'special at all, if you don’t know what this field is for, you probably ' +
+    'don’t need it.',
+
+  resolve: source => source || {},
+}))
+
+export default createViewerField
