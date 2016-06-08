4ebfaaaa9004f2de9ef3d5fca097b26f805774bb
Ferdinand Salis-Samaden <mail@ferdinandsalis.com>
2016-06-08 10:56:53 +0200

    Add react-router and react-router-relay

--- examples/forum-relay/src/components/App.js
+++ examples/forum-relay/src/components/App.js
@@ -1,53 +1,22 @@
 import React from 'react';
-import Relay from 'react-relay';
+import { Link } from 'react-router'
 
-class App extends React.Component {
+export default class App extends React.Component {
   render() {
     return (
-      <main>
-        <h1>Posts</h1>
-        <ul>
-          {this.props.viewer.posts.edges.map(({ node: post }) =>
-            <li key={post.id}>
-              <h2><a href={`#posts/${post.id}`}>{post.headline}</a></h2>
-              <p>by {post.author.fullName}</p>
-              <div><p>{post.summary}</p></div>
-              {post.topic && <p>Filed under <span>{post.topic}</span></p>}
-              <PostTimestamp date={post.createdAt}/>
-            </li>
-          )}
-        </ul>
-      </main>
+      <div>
+        <header>
+          <h1>Forum Example</h1>
+          <Link to="/">Home</Link>
+          <Link to="posts">Posts</Link>
+        </header>
+        <main>
+          {this.props.children}
+        </main>
+        <footer>
+          An example application for PostGraphQL and Relay
+        </footer>
+      </div>
     );
   }
 }
-
-function PostTimestamp({ date }) {
-  const timestamp = new Date(date)
-  return (
-    <p>Created on <time>{timestamp.toDateString()}</time></p>
-  )
-}
-
-export default Relay.createContainer(App, {
-  fragments: {
-    viewer: () => Relay.QL`
-      fragment on Viewer {
-        posts: postNodes(first: 10) {
-          edges {
-            node {
-              id
-              headline
-              createdAt
-              topic
-              summary
-              author: personByAuthorId {
-                fullName
-              }
-            },
-          },
-        },
-      }
-    `,
-  },
-});
