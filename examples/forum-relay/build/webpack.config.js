c4d91cf6faf92224575f0eb79231d904ab6fd8fe
Ferdinand Salis-Samaden <mail@ferdinandsalis.com>
2016-09-11 19:55:36 +0200

    Add sanitize.css and css modules

--- examples/forum-relay/build/webpack.config.js
+++ examples/forum-relay/build/webpack.config.js
@@ -2,12 +2,21 @@ import path from 'path'
 import { DefinePlugin } from 'webpack'
 
 export default {
-  entry: path.resolve(__dirname, '../src/main.js'),
+  entry: path.resolve(__dirname, '../src/app.js'),
   module: {
     loaders: [{
+      test: /\.css$/,
+      exclude: /node_modules/,
+      loaders: ['style', 'css'],
+      options: 'modules'
+    }, {
+      test: /\.css$/,
+      include: /node_modules/,
+      loaders: ['style', 'css'],
+    }, {
+      test: /\.js$/,
       include: [path.resolve(__dirname, '../src')],
       loader: 'babel',
-      test: /\.js$/,
     }]
   },
   output: {
@@ -19,4 +28,7 @@ export default {
       AUTH_URL: JSON.stringify('http://localhost:3000/authenticate'),
     })
   ],
+  resolve: {
+    modules: ['src', 'node_modules'],
+  },
 }
