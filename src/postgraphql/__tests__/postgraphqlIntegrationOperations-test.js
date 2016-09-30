8c7f6dc024b398884df0bacbe82b960b1ed21773
calebmer <calebmeredith8@gmail.com>
2016-09-30 18:44:12 -0400

    refactor(postgraphql): splits integration tests in two

--- src/postgraphql/__tests__/postgraphqlIntegrationOperations-test.js
+++ src/postgraphql/__tests__/postgraphqlIntegrationOperations-test.js
@@ -1,7 +1,7 @@
 import { resolve as resolvePath } from 'path'
 import { readFile, readdirSync } from 'fs'
-import { GraphQLSchema, printSchema, graphql } from 'graphql'
-import { Inventory, Context } from '../../interface'
+import { graphql } from 'graphql'
+import { Inventory } from '../../interface'
 import withPGClient from '../../postgres/__tests__/fixtures/withPGClient'
 import { introspectDatabase } from '../../postgres/introspection'
 import addPGCatalogToInventory from '../../postgres/inventory/addPGCatalogToInventory'
@@ -15,38 +15,15 @@ const kitchenSinkData = new Promise((resolve, reject) => {
   })
 })
 
-/**
- * @type {GraphQLSchema}
- */
-let schema1, schema2
-
-beforeAll(withPGClient(async client => {
-  try {
-    const catalog = await introspectDatabase(client, ['a', 'b', 'c'])
-
-    const inventory1 = new Inventory()
-    addPGCatalogToInventory(inventory1, catalog)
-    schema1 = createGraphqlSchema(inventory1)
-
-    const inventory2 = new Inventory()
-    addPGCatalogToInventory(inventory2, catalog, { renameIdToRowId: true })
-    schema2 = createGraphqlSchema(inventory2, { nodeIdFieldName: 'id' })
-  }
-  catch (error) {
-    console.error(error.stack)
-    throw error
-  }
-}))
-
-test('schema', async () => {
-  expect(printSchema(schema1)).toMatchSnapshot()
-  expect(printSchema(schema2)).toMatchSnapshot()
-})
-
 const queriesDir = resolvePath(__dirname, 'fixtures/queries')
 
 for (const file of readdirSync(queriesDir)) {
-  test(`query ${file}`, withPGClient(async client => {
+  test(`operation ${file}`, withPGClient(async client => {
+    const catalog = await introspectDatabase(client, ['a', 'b', 'c'])
+    const inventory = new Inventory()
+    addPGCatalogToInventory(inventory, catalog)
+    const schema = createGraphqlSchema(inventory)
+
     const query = await new Promise((resolve, reject) => {
       readFile(resolvePath(queriesDir, file), (error, data) => {
         if (error) reject(error)
@@ -57,7 +34,7 @@ for (const file of readdirSync(queriesDir)) {
     await client.query(await kitchenSinkData)
     const context = createPGContext(client)
 
-    const result = await graphql(schema1, query, null, context)
+    const result = await graphql(schema, query, null, context)
 
     // Log the errors in our result.
     if (result.errors)
