00c2baa160c98ea1b60b269096c18323bb241cdf
Ferdinand Salis-Samaden <mail@ferdinandsalis.com>
2016-10-02 16:04:45 +0200

    Add ContentEditable component

--- examples/forum-relay/src/containers/PostPage.js
+++ examples/forum-relay/src/containers/PostPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Relay from 'react-relay'
 import { Link, withRouter } from 'react-router'
 import { StyleSheet, css } from 'aphrodite'
-import UncontrolledContentEditable from './UncontrolledContentEditable'
+import ContentEditable from '../components/ContentEditable'
 import { UpdatePostMutation, DeletePostMutation } from '../mutations'
 
 class PostPage extends React.Component {
@@ -10,17 +10,49 @@ class PostPage extends React.Component {
     user: React.PropTypes.object,
   }
 
-  handleUpdate = (event) => {
+  render() {
+    const { post } = this.props
+    const { token, personId } = this.context.user
+    const authAndOwn = personId === post.authorId && !!token
+
+    return (
+      <article>
+        <Link to="/posts">back to Posts</Link>
+        <header>
+          <ContentEditable
+            editable={authAndOwn}
+            onChange={this.handleChange} 
+            tagName="h1"
+            text={post.headline}
+            data-prop="headline"
+          />
+          <p>by {post.author.fullName}</p>
+        </header>
+        <ContentEditable
+          editable={authAndOwn}
+          onChange={this.handleChange} 
+          tagName="div"
+          text={post.body}
+          data-prop="body"
+        />
+        <footer>
+          {authAndOwn &&
+            <button onClick={this.handleDelete}>Delete Post</button>
+          }
+        </footer>
+      </article>
+    )
+  }
+
+  handleChange = (event) => {
     const propName = event.target.dataset.prop
     const newValue = event.target.innerText
     const oldValue = this.props.post[propName]
 
-    if (newValue === oldValue)
-      return
+    if (newValue === oldValue) return
 
     // PostGrahpQL expects the prop names of the
     // new values to be prefixed with `new`
-
     this.props.relay.commitUpdate(
       new UpdatePostMutation({
         post: this.props.post,
@@ -33,8 +65,6 @@ class PostPage extends React.Component {
 
   handleDelete = (event) => {
     this.props.router.push('/posts')
-
-    // TODO: Use applyUpdate and commit once navigated?
     this.props.relay.commitUpdate(
       new DeletePostMutation({
         post: this.props.post,
@@ -43,30 +73,6 @@ class PostPage extends React.Component {
     )
   }
 
-  // TODO: get rid of warnings
-  // contenteditable is used here out of simplicity, for the moment
-  // react complains; I chose to ignore it
-  render() {
-    const { post } = this.props
-    const { token, personId } = this.context.user
-    const authAndOwn = personId === post.authorId && !!token
-
-    return (
-      <div>
-        <Link to="/posts">back to Posts</Link>
-        <header>
-          <h1 data-prop="headline" contentEditable={authAndOwn} onBlur={this.handleUpdate}>{post.headline}</h1>
-          <p>by {post.author.fullName}</p>
-        </header>
-        <p data-prop="body" contentEditable={authAndOwn} onBlur={this.handleUpdate}>{post.body}</p>
-        <aside>
-          {authAndOwn &&
-            <button onClick={this.handleDelete}>Delete Post</button>
-          }
-        </aside>
-      </div>
-    )
-  }
 }
 
 function capitalizeFirstLetter(string) {
