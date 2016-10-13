ff7be28e95f884b1c3fb3686b7a0d73097193c22
calebmer <calebmeredith8@gmail.com>
2016-10-13 13:37:58 -0400

    chore(package): add continuous integration

--- src/postgres/__tests__/fixtures/pgPool.ts
+++ src/postgres/__tests__/fixtures/pgPool.ts
@@ -1,10 +1,11 @@
 import { Pool } from 'pg'
+import { parse as parsePGConnectionString } from 'pg-connection-string'
 
-const pgPool = new Pool({
-  database: 'postgraphql_test',
-  port: 5432,
+const pgUrl = process.env.TEST_PG_URL || 'postgres://localhost:5432/postgraphql_test'
+
+const pgPool = new Pool(Object.assign({}, parsePGConnectionString(pgUrl), {
   max: 15,
   idleTimeoutMillis: 500,
-})
+}))
 
 export default pgPool
