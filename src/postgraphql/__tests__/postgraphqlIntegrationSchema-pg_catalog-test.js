de33952e356ade197853dcd4beaedb96cc6582dc
calebmer <calebmeredith8@gmail.com>
2016-10-13 20:14:47 -0400

    test(postgraphql): remove information_schema and pg_catalog tests
    These tests, while showing that PostGraphQL can serve a GraphQL schema for internal Postgres schemas are flaky when it comes to Postgres version. They vary, which is not something we want on CI. Perhaps in the future we may re-add these but pin them to run only on a specific version.

--- src/postgraphql/__tests__/postgraphqlIntegrationSchema-pg_catalog-test.js
+++
@@ -1,8 +0,0 @@
-import { printSchema } from 'graphql'
-import withPGClient from '../../postgres/__tests__/fixtures/withPGClient'
-import createPostGraphQLSchema from '../schema/createPostGraphQLSchema'
-
-test('prints the GraphQL schema for `pg_catalog`', withPGClient(async pgClient => {
-  const gqlSchema = await createPostGraphQLSchema(pgClient, ['pg_catalog'])
-  expect(printSchema(gqlSchema)).toMatchSnapshot()
-}))
