7f531333915de28bf58389f325554467c333137a
calebmer <calebmeredith8@gmail.com>
2016-10-15 14:49:19 -0400

    test(postgraphql): remove forum example schema test
    This test was flaky in development where users are global across databases.

--- src/postgraphql/__tests__/postgraphqlIntegrationSchema-test.js
+++ src/postgraphql/__tests__/postgraphqlIntegrationSchema-test.js
@@ -1,7 +1,6 @@
 // TODO: There may be some excessive waste, if we could somehow filter what
 // these guys see, that would be great 👍
 
-import { readFileSync } from 'fs'
 import { printSchema } from 'graphql'
 import withPgClient from '../../postgres/__tests__/fixtures/withPgClient'
 import createPostGraphQLSchema from '../schema/createPostGraphQLSchema'
@@ -22,12 +21,5 @@ test('prints a schema with Relay 1 style ids', withPgClient(async pgClient => {
 
 test('prints a schema with a JWT generating mutation', withPgClient(async pgClient => {
   const gqlSchema = await createPostGraphQLSchema(pgClient, 'b', { jwtSecret: 'secret', jwtPgTypeIdentifier: 'b.jwt_token' })
-  expect(printSchema(gqlSchema)).toMatchSnapshot()
-}))
-
-test('prints the forum example schema', withPgClient(async pgClient => {
-  await pgClient.query(readFileSync('examples/forum/schema-drop.sql', 'utf8').replace(/begin;|commit;/g, ''))
-  await pgClient.query(readFileSync('examples/forum/schema.sql', 'utf8').replace(/begin;|commit;/g, ''))
-  const gqlSchema = await createPostGraphQLSchema(pgClient, 'forum_example')
   expect(printSchema(gqlSchema)).toMatchSnapshot()
 }))
