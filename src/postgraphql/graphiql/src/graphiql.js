97a45f4ea3753973af7d495f2712d6e09c7be513
Ferdinand Salis-Samaden <mail@ferdinandsalis.com>
2016-10-24 21:40:49 +0200

    Refactor components and add basic tests

--- src/postgraphql/graphiql/src/graphiql.js
+++ src/postgraphql/graphiql/src/graphiql.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
 import EnhancedGraphiQL from './components/EnhancedGraphiQL'
+import setupEventSource from './setupEventSource'
 import './styles/global/index.css'
 
-function renderGraphiQL() {
+const renderGraphiQL = () => {
   const { GRAPHIQL_CONFIG } = window
+
   render(
-    <EnhancedGraphiQL config={GRAPHIQL_CONFIG}/>,
+    <EnhancedGraphiQL
+      config={GRAPHIQL_CONFIG}
+      setupEventSource={setupEventSource('/_postgraphql/stream')}
+    />,
     document.getElementById('root')
   )
 }
