a086f6fded68d76286c60168456c7c234b22969f
Benjie Gillam <benjie@jemjie.com>
2016-10-30 07:37:32 +0000

    fix(tests): make tests more reliable for everyone

--- src/postgraphql/__tests__/postgraphqlIntegrationSchema-test.js
+++ src/postgraphql/__tests__/postgraphqlIntegrationSchema-test.js
@@ -5,9 +5,8 @@ import { printSchema } from 'graphql'
 import withPgClient from '../../postgres/__tests__/fixtures/withPgClient'
 import createPostGraphQLSchema from '../schema/createPostGraphQLSchema'
 
-// This test suite can be flaky in CI. Increase it’s timeout.
-if (process.env.CI)
-  jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 10
+// This test suite can be flaky. Increase it’s timeout.
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 20
 
 test('prints a schema with the default options', withPgClient(async pgClient => {
   const gqlSchema = await createPostGraphQLSchema(pgClient, ['a', 'b', 'c'])
