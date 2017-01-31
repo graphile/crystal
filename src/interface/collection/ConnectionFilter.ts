a44794fa99e67f5b1559b869c9e8b011239e5799
G. Grau <ggrau@me.com>
2017-01-31 22:15:31 +0100

    Add inline documentation to the ConnectionFilter type

--- src/interface/collection/ConnectionFilter.ts
+++ src/interface/collection/ConnectionFilter.ts
@@ -1,17 +1,17 @@
 import { sql } from '../../postgres/utils'
 
 type ConnectionFilter = (
-  rawCondition: string,
-  sql: mixed,
+  rawCondition: string,  // raw GraphQL connection argument `condition`
+  sql: mixed,  // a set of SQL generation helper functions
   context: {
-    initialTable: string,
-    initialSchema: string,
+    initialTable: string,  // the name of the connection's base table
+    initialSchema: string,  // the schema for the connection's base table
     convertRowIdToId: boolean,
   }
 ) => {
-  conditionSql: sql.Sql,
-  fromSql: sql.Sql,
-  groupBySql: sql.Sql,
+  conditionSql: sql.Sql,  // part of the `where` clause
+  fromSql: sql.Sql,  // `from` clause; leave `undefined` to use the default
+  groupBySql: sql.Sql,  // `group by` clause; leave `undefined` to use the default (empty)
 }
 
 export default ConnectionFilter
