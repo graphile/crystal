b614cf3f2c4a8f0dc85bab08e18fc56608d615f5
Benjie Gillam <benjie@jemjie.com>
2017-02-02 13:04:13 +0000

    Args

--- src/postgres/inventory/paginator/getSelectFragment.js
+++ src/postgres/inventory/paginator/getSelectFragment.js
@@ -5,7 +5,7 @@ export default (resolveInfo, aliasIdentifier) => {
     console.error("This won't work much longer! Just a hack to keep the tests working")
     return sql.query`to_json(${sql.identifier(aliasIdentifier)})`
   }
-  const {parentType} = resolveInfo
+  const {parentType, variableValues} = resolveInfo
   const fieldNodes = resolveInfo.fieldNodes || resolveInfo.fieldASTs
   if (fieldNodes.length !== 1) throw new Error("FIXME! Field nodes doesn't have length 1?!")
   const [queryAST] = fieldNodes
@@ -22,7 +22,12 @@ export default (resolveInfo, aliasIdentifier) => {
           const fieldName = selectionQueryAST.name.value
           const field = nodeGqlType._fields[fieldName]
           const gqlType = stripNonNullType(field.type)
-          // XXX: get details from type
+          const args = {}
+          if (nodeQueryAST.arguments.length) {
+            for (let arg of nodeQueryASTNode.arguments) {
+              args[arg.name.value] = parseArgValue(arg.value, variableValues)
+            }
+          }
         }
       }
     );
@@ -42,6 +47,21 @@ function getNodeTypeFromRelayType(type, queryASTNode) {
     : {}
   return { nodeGqlType, nodeQueryAST }
 }
+
 function stripNonNullType(type) {
   return type.constructor.name === 'GraphQLNonNull' ? type.ofType : type
+}
+
+function parseArgValue(value, variableValues) {
+  if (value.kind === 'Variable') {
+    const variableName = value.name.value
+    return variableValues[variableName]
+  }
+
+  let primitive = value.value
+  // TODO parse other kinds of variables
+  if (value.kind === 'IntValue') {
+    primitive = parseInt(primitive)
+  }
+  return primitive
 }
