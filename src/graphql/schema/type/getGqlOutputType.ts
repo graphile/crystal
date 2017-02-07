880e77b89629c47e6d277cde4288cb34d026a554
Benjie Gillam <benjie@jemjie.com>
2017-02-07 19:47:59 +0000

    You almost escaped, you little rascal!

--- src/graphql/schema/type/getGqlOutputType.ts
+++ src/graphql/schema/type/getGqlOutputType.ts
@@ -161,6 +161,7 @@ const createGqlOutputType = <TValue>(buildToken: BuildToken, _type: Type<TValue>
                   value: {
                     description: field.description,
                     type: gqlType,
+                    externalFieldName: fieldName,
                     resolve: (value: TValue): mixed => intoGqlOutput(field.getValue(value)),
                   },
                 }
