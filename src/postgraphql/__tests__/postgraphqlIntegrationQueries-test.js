48741f1f24cb1dcd92f9b57859d70fc6629e70ba
Caleb Meredith <calebmeredith8@gmail.com>
2017-03-26 14:08:14 -0400

    fix integration queries with different configurations

--- src/postgraphql/__tests__/postgraphqlIntegrationQueries-test.js
+++ src/postgraphql/__tests__/postgraphqlIntegrationQueries-test.js
@@ -20,9 +20,25 @@ const queryFileNames = readdirSync(queriesDir)
 let queryResults = []
 
 beforeAll(() => {
-  // Get a GraphQL schema instance that we can query.
-  const gqlSchemaPromise = withPgClient(async pgClient => {
-    return await createPostGraphQLSchema(pgClient, ['a', 'b', 'c'])
+  // Get a few GraphQL schema instance that we can query.
+  const gqlSchemasPromise = withPgClient(async pgClient => {
+    // Different fixtures need different schemas with different configurations.
+    // Make all of the different schemas with different configurations that we
+    // need and wait for them to be created in parallel.
+    const [
+      normal,
+      classicIds,
+      dynamicJson,
+    ] = await Promise.all([
+      createPostGraphQLSchema(pgClient, ['a', 'b', 'c']),
+      createPostGraphQLSchema(pgClient, ['a', 'b', 'c'], { classicIds: true }),
+      createPostGraphQLSchema(pgClient, ['a', 'b', 'c'], { dynamicJson: true }),
+    ])
+    return {
+      normal,
+      classicIds,
+      dynamicJson,
+    }
   })()
 
   // Execute all of the queries in parallel. We will not wait for them to
@@ -32,7 +48,7 @@ beforeAll(() => {
   const queryResultsPromise = (async () => {
     // Wait for the schema to resolve. We need the schema to be introspected
     // before we can do anything else!
-    const gqlSchema = await gqlSchemaPromise
+    const gqlSchemas = await gqlSchemasPromise
     // Get a new Postgres client instance.
     return await withPgClient(async pgClient => {
       // Add data to the client instance we are using.
@@ -46,6 +62,13 @@ beforeAll(() => {
             else resolve(data)
           })
         })
+        // Get the appropriate GraphQL schema for this fixture. We want to test
+        // some specific fixtures against a schema configured slightly
+        // differently.
+        const gqlSchema =
+          fileName === 'classic-ids.graphql' ? gqlSchemas.classicIds :
+          fileName === 'dynamic-json.graphql' ? gqlSchemas.dynamicJson :
+          gqlSchemas.normal
         // Return the result of our GraphQL query.
         return await graphql(gqlSchema, query, null, { [$$pgClient]: pgClient })
       }))
