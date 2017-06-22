c94eeb2ae2850a5f2e5388b3c917dc62b1c3ce42
Benjie Gillam <benjie@jemjie.com>
2017-06-22 16:13:55 +0100

    feat(resolveInfo): can now process arguments

--- packages/graphql-build/parseResolveInfo.js
+++ packages/graphql-build/parseResolveInfo.js
@@ -1,11 +1,14 @@
 "use strict";
 const assert = require("assert");
 const defaults = require("lodash/defaults");
+const { getArgumentValues } = require("graphql/execution/values");
 
 // Based on https://github.com/tjmehta/graphql-parse-fields
 
-function parseFields(info, options = {}) {
-  const fieldNodes = info && (info.fieldASTs || info.fieldNodes);
+function parseFields(resolveInfo, options = {}) {
+  const fieldNodes =
+    resolveInfo && (resolveInfo.fieldASTs || resolveInfo.fieldNodes);
+  const { parentType } = resolveInfo;
   if (!fieldNodes) {
     throw new Error("No fieldNodes provided!");
   }
@@ -13,7 +16,13 @@ function parseFields(info, options = {}) {
     keepRoot: false,
     deep: true,
   });
-  let tree = fieldTreeFromAST(fieldNodes, info.fragments, undefined, options);
+  let tree = fieldTreeFromAST(
+    fieldNodes,
+    resolveInfo,
+    undefined,
+    options,
+    parentType
+  );
   if (!options.keepRoot) {
     const key = firstKey(tree);
     tree = tree[key];
@@ -21,7 +30,24 @@ function parseFields(info, options = {}) {
   return tree;
 }
 
-function fieldTreeFromAST(inASTs, fragments, init, options) {
+function getFieldFromAST(ast, parentType) {
+  if (ast.kind === "Field") {
+    const fieldName = ast.name.value;
+    return parentType._fields[fieldName];
+  }
+  return;
+}
+
+function stripNonNullType(type) {
+  return type.constructor.name === "GraphQLNonNull" ? type.ofType : type;
+}
+
+function stripListType(type) {
+  return type.constructor.name === "GraphQLList" ? type.ofType : type;
+}
+
+function fieldTreeFromAST(inASTs, resolveInfo, init, options, parentType) {
+  let { fragments, variableValues } = resolveInfo;
   fragments = fragments || {};
   init = init || {};
   options = options || {};
@@ -30,7 +56,11 @@ function fieldTreeFromAST(inASTs, fragments, init, options) {
     const kind = val.kind;
     const name = val.name && val.name.value;
     const alias = val.alias ? val.alias.value : name;
-    const args = val.arguments;
+    const field = getFieldFromAST(val, parentType);
+    const fieldGqlType = stripNonNullType(
+      stripListType(stripNonNullType(field.type))
+    );
+    const args = getArgumentValues(field, val, variableValues);
     if (kind === "Field") {
       if (!tree[alias]) {
         tree[alias] = {
@@ -43,9 +73,10 @@ function fieldTreeFromAST(inASTs, fragments, init, options) {
         if (val.selectionSet && options.deep) {
           fieldTreeFromAST(
             val.selectionSet.selections,
-            fragments,
+            resolveInfo,
             tree[alias].fields,
-            options
+            options,
+            fieldGqlType
           );
         } else {
           // No fields to add
@@ -56,19 +87,23 @@ function fieldTreeFromAST(inASTs, fragments, init, options) {
       assert(fragment, 'unknown fragment "' + name + '"');
       fieldTreeFromAST(
         fragment.selectionSet.selections,
-        fragments,
+        resolveInfo,
         tree,
-        options
+        options,
+        parentType
       );
     } else if (kind === "InlineFragment" && options.deep) {
       const fragment = val;
       fieldTreeFromAST(
         fragment.selectionSet.selections,
-        fragments,
+        resolveInfo,
         tree,
-        options
+        options,
+        parentType
       );
     } // else ignore
+    // XXX: need to process FragmentDefinition?
+    // Ref: https://github.com/postgraphql/postgraphql/pull/342/files#diff-d6702ec9fed755c88b9d70b430fda4d8R148
     return tree;
   }, init);
 }
