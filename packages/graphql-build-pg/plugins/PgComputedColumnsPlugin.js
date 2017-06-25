956c7cfc717c9788d3152d695e5ff33c0fae004d
Benjie Gillam <benjie@jemjie.com>
2017-06-25 17:09:23 +0100

    Move shared proc logic to a common file

--- packages/graphql-build-pg/plugins/PgComputedColumnsPlugin.js
+++ packages/graphql-build-pg/plugins/PgComputedColumnsPlugin.js
@@ -1,4 +1,5 @@
-const { GraphQLNonNull, GraphQLList, GraphQLString } = require("graphql");
+const makeProcField = require("./makeProcField");
+
 module.exports = function PgComputedColumnsPlugin(
   builder,
   { pgInflection: inflection, pgStrictFunctions: strictFunctions = false }
@@ -55,163 +56,23 @@ module.exports = function PgComputedColumnsPlugin(
             */
 
             const sliceAmount = 1;
-            const argNames = proc.argNames
-              .slice(sliceAmount)
-              .map((name, index) => name || `arg${index}`);
-            const argTypes = proc.argTypeIds
-              .slice(sliceAmount)
-              .map(typeId => introspectionResultsByKind.typeById[typeId]);
-            const requiredArgs = Math.max(
-              0,
-              proc.isStrict
-                ? proc.argNames.length - sliceAmount
-                : strictFunctions
-                  ? proc.argNames.length - sliceAmount - proc.argDefaultsNum
-                  : 0
-            );
-            const argGqlTypes = argTypes.map((type, idx) => {
-              const Type = gqlInputTypeByTypeId[type.id] || GraphQLString;
-              if (idx >= requiredArgs) {
-                return Type;
-              } else {
-                return new GraphQLNonNull(Type);
-              }
-            });
-
             const pseudoColumnName = proc.name.substr(table.name.length + 1);
             const fieldName = inflection.column(
               pseudoColumnName,
               table.name,
               table.namespace.name
             );
-            const schema = table.namespace;
-
-            const returnType =
-              introspectionResultsByKind.typeById[proc.returnTypeId];
-            const returnTypeTable =
-              introspectionResultsByKind.classById[returnType.classId];
-            if (!returnType) {
-              throw new Error(
-                `Could not determine return type for function '${proc.name}'`
-              );
-            }
-            const returnTypeTableAttributes =
-              returnTypeTable &&
-              introspectionResultsByKind.attribute.filter(
-                attr => attr.classId === returnTypeTable.id
-              );
-            const returnTypeTablePrimaryKeyConstraint =
-              returnTypeTable &&
-              introspectionResultsByKind.constraint
-                .filter(con => con.classId === returnTypeTable.id)
-                .filter(con => ["p"].includes(con.type))[0];
-            const returnTypeTablePrimaryKeys =
-              returnTypeTablePrimaryKeyConstraint &&
-              returnTypeTablePrimaryKeyConstraint.keyAttributeNums.map(
-                num =>
-                  returnTypeTableAttributes.filter(attr => attr.num === num)[0]
-              );
-
-            memo[
-              fieldName
-            ] = buildFieldWithHooks(
+            memo[fieldName] = buildFieldWithHooks(
               fieldName,
-              ({ addDataGenerator, getDataFromParsedResolveInfoFragment }) => {
-                addDataGenerator(() => {
-                  return {
-                    pgQuery: queryBuilder => {
-                      queryBuilder.select(() => {
-                        const resolveData = getDataFromParsedResolveInfoFragment(
-                          parsedResolveInfoFragment
-                        );
-                        const { args } = parsedResolveInfoFragment;
-                        const functionAlias = Symbol();
-                        const parentTableAlias = queryBuilder.getTableAlias();
-                        const argValues = argNames.map((argName, argIndex) => {
-                          const gqlArgName = inflection.argument(argName);
-                          return args[gqlArgName];
-                        });
-                        while (
-                          argValues.length > requiredArgs &&
-                          argValues[argValues.length - 1] == null
-                        ) {
-                          argValues.pop();
-                        }
-                        const query = queryFromResolveData(
-                          sql.fragment`${sql.identifier(
-                            proc.namespace.name,
-                            proc.name
-                          )}(${sql.identifier(
-                            parentTableAlias
-                          )}${argValues.length
-                            ? `, ${sql.join(argValues.map(sql.value), ", ")}`
-                            : ""})`,
-                          functionAlias,
-                          resolveData,
-                          { asJsonAggregate: true },
-                          innerQueryBuilder => {
-                            if (returnTypeTablePrimaryKeys) {
-                              innerQueryBuilder.beforeFinalize(() => {
-                                // append order by primary key to the list of orders
-                                returnTypeTablePrimaryKeys.forEach(key => {
-                                  innerQueryBuilder.orderBy(
-                                    sql.fragment`${sql.identifier(
-                                      functionAlias,
-                                      key.name
-                                    )}`,
-                                    true
-                                  );
-                                });
-                              });
-                            }
-                          }
-                        );
-                        return sql.fragment`(${query})`;
-                      }, parsedResolveInfoFragment.alias);
-                    },
-                  };
-                });
-                let type;
-                if (returnTypeTable) {
-                  const TableType = getTypeByName(
-                    inflection.tableType(
-                      returnTypeTable.name,
-                      returnTypeTable.namespace.name
-                    )
-                  );
-                  if (proc.returnsSet) {
-                    const ConnectionType = getTypeByName(
-                      inflection.connection(TableType.name)
-                    );
-                    type = new GraphQLNonNull(ConnectionType);
-                  } else {
-                    type = TableType;
-                  }
-                } else {
-                  const Type = gqlTypeByTypeId[returnType.id] || GraphQLString;
-                  if (proc.returnsSet) {
-                    type = new GraphQLList(new GraphQLNonNull(Type));
-                  } else {
-                    type = Type;
-                  }
-                }
-                return {
-                  type: type,
-                  args: argNames.reduce((memo, argName, argIndex) => {
-                    const gqlArgName = inflection.argument(argName);
-                    memo[gqlArgName] = {
-                      type: argGqlTypes[argIndex],
-                    };
-                    return memo;
-                  }, {}),
-                  resolve: (data, _args, _context, resolveInfo) => {
-                    const { alias } = parseResolveInfo(resolveInfo, {
-                      deep: false,
-                    });
-                    return data[alias];
-                  },
-                };
-              }
+              makeProcField(proc, {
+                computed: true,
+                introspectionResultsByKind,
+                strictFunctions,
+                gqlTypeByTypeId,
+                gqlInputTypeByTypeId,
+                getTypeByName,
+                inflection,
+              })
             );
             return memo;
           }, {})
---
+++ packages/graphql-build-pg/plugins/makeProcField.js
@@ -0,0 +1,177 @@
+const { GraphQLNonNull, GraphQLList, GraphQLString } = require("graphql");
+
+function makeQuery(
+  proc,
+  parsedResolveInfoFragment,
+  { implicitArgs, inflection }
+) {
+  const resolveData = getDataFromParsedResolveInfoFragment(
+    parsedResolveInfoFragment
+  );
+  const { args } = parsedResolveInfoFragment;
+  const argValues = argNames.map((argName, argIndex) => {
+    const gqlArgName = inflection.argument(argName);
+    return args[gqlArgName];
+  });
+  while (
+    argValues.length > requiredArgs &&
+    argValues[argValues.length - 1] == null
+  ) {
+    argValues.pop();
+  }
+  const functionAlias = Symbol();
+  return queryFromResolveData(
+    sql.fragment`${sql.identifier(proc.namespace.name, proc.name)}(${sql.join(
+      [...implicitArgs, ...argValues.map(sql.value)],
+      ","
+    )})`,
+    functionAlias,
+    resolveData,
+    { asJsonAggregate: true },
+    innerQueryBuilder => {
+      if (returnTypeTablePrimaryKeys) {
+        innerQueryBuilder.beforeFinalize(() => {
+          // append order by primary key to the list of orders
+          returnTypeTablePrimaryKeys.forEach(key => {
+            innerQueryBuilder.orderBy(
+              sql.fragment`${sql.identifier(functionAlias, key.name)}`,
+              true
+            );
+          });
+        });
+      }
+    }
+  );
+}
+module.exports = function makeProcField(
+  proc,
+  {
+    computed,
+    strictFunctions,
+    introspectionResultsByKind,
+    gqlTypeByTypeId,
+    gqlInputTypeByTypeId,
+    getTypeByName,
+    inflection,
+  }
+) {
+  const sliceAmount = computed ? 1 : 0;
+  const argNames = proc.argNames
+    .slice(sliceAmount)
+    .map((name, index) => name || `arg${index}`);
+  const argTypes = proc.argTypeIds
+    .slice(sliceAmount)
+    .map(typeId => introspectionResultsByKind.typeById[typeId]);
+  const requiredArgs = Math.max(
+    0,
+    proc.isStrict
+      ? proc.argNames.length - sliceAmount
+      : strictFunctions
+        ? proc.argNames.length - sliceAmount - proc.argDefaultsNum
+        : 0
+  );
+  const argGqlTypes = argTypes.map((type, idx) => {
+    const Type = gqlInputTypeByTypeId[type.id] || GraphQLString;
+    if (idx >= requiredArgs) {
+      return Type;
+    } else {
+      return new GraphQLNonNull(Type);
+    }
+  });
+
+  const returnType = introspectionResultsByKind.typeById[proc.returnTypeId];
+  const returnTypeTable =
+    introspectionResultsByKind.classById[returnType.classId];
+  if (!returnType) {
+    throw new Error(
+      `Could not determine return type for function '${proc.name}'`
+    );
+  }
+  const returnTypeTableAttributes =
+    returnTypeTable &&
+    introspectionResultsByKind.attribute.filter(
+      attr => attr.classId === returnTypeTable.id
+    );
+  const returnTypeTablePrimaryKeyConstraint =
+    returnTypeTable &&
+    introspectionResultsByKind.constraint
+      .filter(con => con.classId === returnTypeTable.id)
+      .filter(con => ["p"].includes(con.type))[0];
+  const returnTypeTablePrimaryKeys =
+    returnTypeTablePrimaryKeyConstraint &&
+    returnTypeTablePrimaryKeyConstraint.keyAttributeNums.map(
+      num => returnTypeTableAttributes.filter(attr => attr.num === num)[0]
+    );
+
+  return ({ addDataGenerator, getDataFromParsedResolveInfoFragment }) => {
+    if (computed) {
+      addDataGenerator(() => {
+        return {
+          pgQuery: queryBuilder => {
+            queryBuilder.select(() => {
+              const parentTableAlias = queryBuilder.getTableAlias();
+              const query = makeQuery(proc, parsedResolveInfoFragment, {
+                implicitArgs: [sql.identifier(parentTableAlias)],
+                inflection,
+              });
+              return sql.fragment`(${query})`;
+            }, parsedResolveInfoFragment.alias);
+          },
+        };
+      });
+    }
+    let type;
+    if (returnTypeTable) {
+      const TableType = getTypeByName(
+        inflection.tableType(
+          returnTypeTable.name,
+          returnTypeTable.namespace.name
+        )
+      );
+      if (proc.returnsSet) {
+        const ConnectionType = getTypeByName(
+          inflection.connection(TableType.name)
+        );
+        type = new GraphQLNonNull(ConnectionType);
+      } else {
+        type = TableType;
+      }
+    } else {
+      const Type = gqlTypeByTypeId[returnType.id] || GraphQLString;
+      if (proc.returnsSet) {
+        type = new GraphQLList(new GraphQLNonNull(Type));
+      } else {
+        type = Type;
+      }
+    }
+    return {
+      type: type,
+      args: argNames.reduce((memo, argName, argIndex) => {
+        const gqlArgName = inflection.argument(argName);
+        memo[gqlArgName] = {
+          type: argGqlTypes[argIndex],
+        };
+        return memo;
+      }, {}),
+      resolve: computed
+        ? (data, _args, _context, resolveInfo) => {
+            const { alias } = parseResolveInfo(resolveInfo, {
+              deep: false,
+            });
+            return data[alias];
+          }
+        : async (data, args, { pgClient }, resolveInfo) => {
+            const parsedResolveInfoFragment = parseResolveInfo(resolveInfo);
+            const query = makeQuery(proc, parsedResolveInfoFragment, {
+              implicitArgs: [sql.identifier(parentTableAlias)],
+              inflection,
+            });
+
+            const { text, values } = sql.compile(query);
+            console.log(require("sql-formatter").format(text));
+            const { rows } = await pgClient.query(text, values);
+            return rows || [];
+          },
+    };
+  };
+};
