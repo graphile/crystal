454a17a266a4af928c67a0e4c985b3833e5149fa
Benjie Gillam <benjie@jemjie.com>
2017-06-28 18:59:58 +0100

    BREAKING: parseResolveInfo now factors in type

--- packages/graphql-build/parseResolveInfo.js
+++ packages/graphql-build/parseResolveInfo.js
@@ -6,14 +6,10 @@ const { getNamedType } = require("graphql");
 
 // Based on https://github.com/tjmehta/graphql-parse-fields
 
-function parseFields(resolveInfo, options = {}, targetType = null) {
-  if (!targetType) {
-    throw new Error("You must specify a target type");
-  }
+function parseFields(resolveInfo, options = {}) {
   const fieldNodes =
     resolveInfo && (resolveInfo.fieldASTs || resolveInfo.fieldNodes);
   const { parentType } = resolveInfo;
-  console.log("Target Type", targetType);
   if (!fieldNodes) {
     throw new Error("No fieldNodes provided!");
   }
@@ -26,12 +22,13 @@ function parseFields(resolveInfo, options = {}, targetType = null) {
     resolveInfo,
     undefined,
     options,
-    parentType,
-    targetType
+    parentType
   );
   if (!options.keepRoot) {
-    const key = firstKey(tree);
-    tree = tree[key];
+    const typeKey = firstKey(tree);
+    tree = tree[typeKey];
+    const fieldKey = firstKey(tree);
+    tree = tree[fieldKey];
   }
   return tree;
 }
@@ -44,57 +41,57 @@ function getFieldFromAST(ast, parentType) {
   return;
 }
 
-function fieldTreeFromAST(
-  inASTs,
-  resolveInfo,
-  init,
-  options,
-  parentType,
-  targetType
-) {
+let iNum = 1;
+function fieldTreeFromAST(inASTs, resolveInfo, initTree, options, parentType) {
+  const instance = iNum++;
+  console.log(instance, "Field tree from AST");
   let { fragments, variableValues } = resolveInfo;
   fragments = fragments || {};
-  init = init || {};
+  initTree = initTree || {};
   options = options || {};
   const asts = Array.isArray(inASTs) ? inASTs : [inASTs];
-  return asts.reduce(function(tree, val) {
+  initTree[parentType.name] = initTree[parentType.name] || {};
+  return asts.reduce(function(tree, val, idx) {
+    console.log(instance, "AST", idx + 1, "/", asts.length);
     const kind = val.kind;
     const name = val.name && val.name.value;
     const isReserved = name && name.substr(0, 2) === "__";
     if (kind === "Field" && !isReserved) {
       const alias = val.alias ? val.alias.value : name;
+      console.log(instance, "Field", alias, name);
       const field = getFieldFromAST(val, parentType);
       const fieldGqlType = getNamedType(field.type);
       const args = getArgumentValues(field, val, variableValues) || {};
-      if (!tree[alias]) {
-        tree[alias] = {
+      if (!tree[parentType.name][alias]) {
+        tree[parentType.name][alias] = {
           ast: val,
           alias,
           name,
           args,
-          fields: {},
+          fieldsByTypeName: {
+            [fieldGqlType.name]: {},
+          },
         };
-      }
-      if (val.selectionSet && options.deep) {
-        fieldTreeFromAST(
-          val.selectionSet.selections,
-          resolveInfo,
-          tree[alias].fields,
-          options,
-          fieldGqlType,
-          targetType
-        );
-      } else {
-        // No fields to add
+        if (val.selectionSet && options.deep) {
+          fieldTreeFromAST(
+            val.selectionSet.selections,
+            resolveInfo,
+            tree[parentType.name][alias].fieldsByTypeName,
+            options,
+            fieldGqlType
+          );
+        } else {
+          // No fields to add
+          console.log(instance, "EXITING!", val);
+        }
       }
     } else if (kind === "FragmentSpread" && options.deep) {
+      console.log(instance, "Fragment spread", name);
       const fragment = fragments[name];
       assert(fragment, 'unknown fragment "' + name + '"');
       let fragmentType = parentType;
       if (fragment.typeCondition) {
-        fragmentType =
-          findCompatibleType(parentType, fragment.typeCondition) ||
-          findCompatibleType(targetType, fragment.typeCondition);
+        fragmentType = getType(resolveInfo, fragment.typeCondition);
       }
       if (fragmentType) {
         fieldTreeFromAST(
@@ -102,17 +99,14 @@ function fieldTreeFromAST(
           resolveInfo,
           tree,
           options,
-          fragmentType,
-          targetType
+          fragmentType
         );
       }
     } else if (kind === "InlineFragment" && options.deep) {
       const fragment = val;
       let fragmentType = parentType;
       if (fragment.typeCondition) {
-        fragmentType =
-          findCompatibleType(parentType, fragment.typeCondition) ||
-          findCompatibleType(targetType, fragment.typeCondition);
+        fragmentType = getType(resolveInfo, fragment.typeCondition);
       }
       if (fragmentType) {
         fieldTreeFromAST(
@@ -120,15 +114,14 @@ function fieldTreeFromAST(
           resolveInfo,
           tree,
           options,
-          fragmentType,
-          targetType
+          fragmentType
         );
       }
     } // else ignore
     // XXX: need to process FragmentDefinition?
     // Ref: https://github.com/postgraphql/postgraphql/pull/342/files#diff-d6702ec9fed755c88b9d70b430fda4d8R148
     return tree;
-  }, init);
+  }, initTree);
 }
 
 function firstKey(obj) {
@@ -150,6 +143,15 @@ function findCompatibleType(type, typeCondition) {
     }
   }
   return compatibleType;
+}
+
+function getType(resolveInfo, typeCondition) {
+  const { schema } = resolveInfo;
+  const { kind, name } = typeCondition;
+  if (kind === "NamedType") {
+    const typeName = name.value;
+    return schema.getType(typeName);
+  }
 }
 
 module.exports = parseFields;
