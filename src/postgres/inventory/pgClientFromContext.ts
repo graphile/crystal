a215f25f32fa41bf387a70d0cabfcc319e977316
Benjie Gillam <benjie@jemjie.com>
2017-07-05 13:20:22 +0100

    Schema generation provided by postgraphql-build

--- src/postgres/inventory/pgClientFromContext.ts
+++ src/postgres/inventory/pgClientFromContext.ts
@@ -2,7 +2,7 @@
 
 import { Client } from 'pg'
 
-export const $$pgClient = Symbol('postgres/client')
+export const $$pgClient = 'pgClient';
 
 /**
  * Retrieves a Postgres client from a context, throwing an error if such a
