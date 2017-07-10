221d459bb6a69b3c117c9e56cff52247ca223caf
Daniel Büchele <daniel@buechele.cc>
2017-07-10 10:10:39 +0200

    fix(postgres): fix linter errors

--- src/postgres/inventory/type/__tests__/transformValueIntoPgValue-test.ts
+++ src/postgres/inventory/type/__tests__/transformValueIntoPgValue-test.ts
@@ -5,23 +5,23 @@ import pgStringType from '../../type/scalar/pgStringType'
 import sql from '../../../utils/sql'
 
 test('will work with enum lists', () => {
-  const USStateType = new PgEnumType({
+  const uSStateType = new PgEnumType({
     name: 'us_states',
     description: 'The 50 U.S. states',
-    variants: new Set(['ALABAMA','ALASKA','ARIZONA','ARKANSAS','CALIFORNIA','COLORADO','CONNECTICUT','DELAWARE','FLORIDA','GEORGIA','HAWAII','IDAHO','ILLINOIS','INDIANA','IOWA','KANSAS','KENTUCKY','LOUISIANA','MAINE','MARYLAND','MASSACHUSETTS','MICHIGAN','MINNESOTA','MISSISSIPPI','MISSOURI','MONTANA','NEBRASKA','NEVADA','NEW HAMPSHIRE','NEW JERSEY','NEW MEXICO','NEW YORK','NORTH CAROLINA','NORTH DAKOTA','OHIO','OKLAHOMA','OREGON','PENNSYLVANIA','RHODE ISLAND','SOUTH CAROLINA','SOUTH DAKOTA','TENNESSEE','TEXAS','UTAH','VERMONT','VIRGINIA','WASHINGTON','WEST VIRGINIA','WISCONSIN','WYOMING'])
+    variants: new Set(['ALABAMA', 'ALASKA', 'ARIZONA', 'ARKANSAS', 'CALIFORNIA', 'COLORADO', 'CONNECTICUT', 'DELAWARE', 'FLORIDA', 'GEORGIA', 'HAWAII', 'IDAHO', 'ILLINOIS', 'INDIANA', 'IOWA', 'KANSAS', 'KENTUCKY', 'LOUISIANA', 'MAINE', 'MARYLAND', 'MASSACHUSETTS', 'MICHIGAN', 'MINNESOTA', 'MISSISSIPPI', 'MISSOURI', 'MONTANA', 'NEBRASKA', 'NEVADA', 'NEW HAMPSHIRE', 'NEW JERSEY', 'NEW MEXICO', 'NEW YORK', 'NORTH CAROLINA', 'NORTH DAKOTA', 'OHIO', 'OKLAHOMA', 'OREGON', 'PENNSYLVANIA', 'RHODE ISLAND', 'SOUTH CAROLINA', 'SOUTH DAKOTA', 'TENNESSEE', 'TEXAS', 'UTAH', 'VERMONT', 'VIRGINIA', 'WASHINGTON', 'WEST VIRGINIA', 'WISCONSIN', 'WYOMING']),
   })
 
-  const USStateListType = new PgListType(USStateType)
-  expect(sql.compile(USStateListType.transformValueIntoPgValue(['CALIFORNIA','OREGON'])).text).toEqual('array[$1, $2]::\"us_states\"[]')
+  const uSStateListType = new PgListType(uSStateType)
+  expect(sql.compile(uSStateListType.transformValueIntoPgValue(['CALIFORNIA', 'OREGON'])).text).toEqual('array[$1, $2]::\"us_states\"[]')
 
-  const NullableUSStateListType = new PgListType(new PgNullableType(USStateType))
-  expect(sql.compile(NullableUSStateListType.transformValueIntoPgValue(['CALIFORNIA','OREGON'])).text).toEqual('array[$1, $2]::\"us_states\"[]')
+  const nullableUSStateListType = new PgListType(new PgNullableType(uSStateType))
+  expect(sql.compile(nullableUSStateListType.transformValueIntoPgValue(['CALIFORNIA', 'OREGON'])).text).toEqual('array[$1, $2]::\"us_states\"[]')
 })
 
 test('will work with text lists', () => {
-  const StringListType = new PgListType(pgStringType)
-  expect(sql.compile(StringListType.transformValueIntoPgValue(['string1','string2'])).text).toEqual('array[$1, $2]::\"text\"[]')
+  const stringListType = new PgListType(pgStringType)
+  expect(sql.compile(stringListType.transformValueIntoPgValue(['string1', 'string2'])).text).toEqual('array[$1, $2]::\"text\"[]')
 
-  const NullableStringListType = new PgListType(new PgNullableType(pgStringType))
-  expect(sql.compile(NullableStringListType.transformValueIntoPgValue(['string1','string2'])).text).toEqual('array[$1, $2]::\"text\"[]')
+  const nullableStringListType = new PgListType(new PgNullableType(pgStringType))
+  expect(sql.compile(nullableStringListType.transformValueIntoPgValue(['string1', 'string2'])).text).toEqual('array[$1, $2]::\"text\"[]')
 })
