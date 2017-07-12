679c27e7de4264de4708dae8ef2cfcf4a02b967a
Benjie Gillam <benjie@jemjie.com>
2017-07-12 22:05:26 +0100

    Lint

--- src/__tests__/utils/printSchemaOrdered.js
+++ src/__tests__/utils/printSchemaOrdered.js
@@ -1,37 +1,37 @@
-import { parse, buildASTSchema } from "graphql";
-import { printSchema } from "graphql/utilities";
+import { parse, buildASTSchema } from 'graphql'
+import { printSchema } from 'graphql/utilities'
 
 export default function printSchemaOrdered(originalSchema) {
   // Clone schema so we don't damage anything
-  const schema = buildASTSchema(parse(printSchema(originalSchema)));
+  const schema = buildASTSchema(parse(printSchema(originalSchema)))
 
-  const typeMap = schema.getTypeMap();
+  const typeMap = schema.getTypeMap()
   Object.keys(typeMap).forEach(name => {
-    const Type = typeMap[name];
+    const gqlType = typeMap[name]
 
     // Object?
-    if (Type.getFields) {
-      const fields = Type.getFields();
-      const keys = Object.keys(fields).sort();
+    if (gqlType.getFields) {
+      const fields = gqlType.getFields()
+      const keys = Object.keys(fields).sort()
       keys.forEach(key => {
-        const value = fields[key];
+        const value = fields[key]
 
         // Move the key to the end of the object
-        delete fields[key];
-        fields[key] = value;
+        delete fields[key]
+        fields[key] = value
 
         // Sort args
         if (value.args) {
-          value.args.sort((a, b) => a.name.localeCompare(b.name));
+          value.args.sort((a, b) => a.name.localeCompare(b.name))
         }
-      });
+      })
     }
 
     // Enum?
-    if (Type.getValues) {
-      Type.getValues().sort((a, b) => a.name.localeCompare(b.name));
+    if (gqlType.getValues) {
+      gqlType.getValues().sort((a, b) => a.name.localeCompare(b.name))
     }
-  });
+  })
 
-  return printSchema(schema);
-};
+  return printSchema(schema)
+}
