9abec13a135f583a865db256aca2948a1b793a76
Benjie Gillam <benjie@jemjie.com>
2017-07-13 12:38:49 +0100

    Update the schema export test

--- src/postgraphql/__tests__/postgraphqlIntegrationSchemaExport-test.js
+++ src/postgraphql/__tests__/postgraphqlIntegrationSchemaExport-test.js
@@ -13,9 +13,6 @@ const gqlSchemaPromise = withPgClient(async pgClient => {
   return await createPostGraphQLSchema(pgClient, ['a', 'b', 'c'])
 })()
 
-/*
- * This test had to be disabled because jest baulked at the size of the diff.
-
 test('exports a schema as JSON', async () => {
   writeFile.mockClear()
   const gqlSchema = await gqlSchemaPromise
@@ -24,8 +21,6 @@ test('exports a schema as JSON', async () => {
   expect(writeFile.mock.calls[0][0]).toBe('/schema.json')
   expect(writeFile.mock.calls[0][1]).toMatchSnapshot()
 })
-
-*/
 
 test('exports a schema as GQL', async () => {
   writeFile.mockClear()
