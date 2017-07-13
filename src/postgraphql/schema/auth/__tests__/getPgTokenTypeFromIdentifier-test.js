22faf5108d027d20ca57990c8881593e0892be00
Fellman <fellmanj@pet-fellmanj-m.ds.ad.adp.com>
2017-07-12 17:38:34 -0700

    fix(postgraphql): clean up bad style

--- src/postgraphql/schema/auth/__tests__/getPgTokenTypeFromIdentifier-test.js
+++ src/postgraphql/schema/auth/__tests__/getPgTokenTypeFromIdentifier-test.js
@@ -3,13 +3,11 @@ import introspectDatabase from '../../../../postgres/introspection/introspectDat
 import getPgTokenTypeFromIdentifier from '../getPgTokenTypeFromIdentifier'
 
 // good names start with a letter or underscore and may also contain numbers after position 0
-let goodNames = [
-  'test', 'test_0', 'test_1', 'test_2', 'test_3', 'test_4', 'test_5', 'test_6', 'test_7', 'test_8', 'test_9', '_test'
+const goodNames = [
+  'test', 'test_0', 'test_1', 'test_2', 'test_3', 'test_4', 'test_5', 'test_6', 'test_7', 'test_8', 'test_9', '_test',
 ]
 
-for (var i in goodNames) {
-  let goodName = goodNames[i]
-
+goodNames.forEach(goodName => {
   test(`Good schema name ${goodName} should allow create without error`, withPgClient(async client => {
     await client.query(`create schema ${goodName}; create type ${goodName}.jwt_token as (role text, exp integer, a integer, b integer, c integer)`)
 
@@ -18,16 +16,14 @@ for (var i in goodNames) {
 
     expect(tokenType.name).toBe('jwt_token')
   })
-}
+})
 
 // schema, type, etc... names are not allowed to start with numbers
-let badNames = [
-  '0_test', '1_test', '2_test', '3_test', '4_test', '5_test', '6_test', '7_test', '8_test', '9_test'
+const badNames = [
+  '0_test', '1_test', '2_test', '3_test', '4_test', '5_test', '6_test', '7_test', '8_test', '9_test',
 ]
 
-for (var i in badNames) {
-  let badName = badNames[i]
-
+badNames.forEach(badName => {
   test(`Bad schema name ${badName} should throw a syntax error on create`, withPgClient(async client => {
     let badQuery = 'not an error'
 
