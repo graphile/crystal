d0e46c9b0c572acdabffa010c78b193fa69a8a98
Fellman <fellmanj@pet-fellmanj-m.ds.ad.adp.com>
2017-07-14 09:47:02 -0700

    fix(postgraphql) removes unnecessary tests and adds double-quote escaping to testing of names starting with a number

--- src/postgraphql/schema/auth/__tests__/getPgTokenTypeFromIdentifier-test.js
+++ src/postgraphql/schema/auth/__tests__/getPgTokenTypeFromIdentifier-test.js
@@ -3,37 +3,26 @@ import introspectDatabase from '../../../../postgres/introspection/introspectDat
 import getPgTokenTypeFromIdentifier from '../getPgTokenTypeFromIdentifier'
 
 // good names start with a letter or underscore and may also contain numbers after position 0
-const goodNames = [
-  'test', 'test_0', 'test_1', 'test_2', 'test_3', 'test_4', 'test_5', 'test_6', 'test_7', 'test_8', 'test_9', '_test',
+// note: names that start with a number must be escaped with double quotes
+const validNames = [
+  '_test', '0_test', 'test_0', '9_test', 'test_9',
 ]
 
-goodNames.forEach(goodName => {
-  test(`Good schema name ${goodName} should allow create without error`, withPgClient(async client => {
-    await client.query(`create schema ${goodName}; create type ${goodName}.jwt_token as (role text, exp integer, a integer, b integer, c integer)`)
-
-    let catalog   = await introspectDatabase(client, [goodName])
-    let tokenType = getPgTokenTypeFromIdentifier(catalog, `${goodName}.jwt_token`)
-
-    expect(tokenType.name).toBe('jwt_token')
-  }))
-})
-
-// schema, type, etc... names are not allowed to start with numbers
-const badNames = [
-  '0_test', '1_test', '2_test', '3_test', '4_test', '5_test', '6_test', '7_test', '8_test', '9_test',
-]
-
-badNames.forEach(badName => {
-  test(`Bad schema name ${badName} should throw a syntax error on create`, withPgClient(async client => {
-    let badQuery = 'not an error'
+validNames.forEach(validName => {
+  test(`Valid schema name ${validName} and its jwt_token type should create without error and pass validation`, withPgClient(async client => {
+    let query = 'not an error'
 
     try {
-      badQuery = await client.query(`create schema ${badName}; create type ${badName}.jwt_token as (role text, exp integer, a integer, b integer, c integer)`)
+      query = await client.query(`create schema "${validName}"; create type "${validName}"."jwt_token" as (role text, exp integer, a integer, b integer, c integer)`)
     } catch (e) {
-      badQuery = e
+      query = e
     }
 
-    // unfortunately we only get a generic error with the text "syntax error" from the client
-    expect(badQuery instanceof Error && /syntax/.test(badQuery)).toBe(true)
+    expect(query).not.toBeInstanceOf(Error)
+
+    let catalog   = await introspectDatabase(client, [validName])
+    let tokenType = getPgTokenTypeFromIdentifier(catalog, `"${validName}"."jwt_token"`)
+
+    expect(tokenType.name).toBe('jwt_token')
   }))
 })
