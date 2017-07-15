8711bb755705a385e87f1885b74868d86b43be55
Benjie Gillam <benjie@jemjie.com>
2017-07-15 22:13:47 +0100

    Add some tests for pg-sql2

---
+++ packages/pg-sql2/__tests__/general.test.js
@@ -0,0 +1,80 @@
+const sql = require("..");
+
+it("sql.value", () => {
+  const node = sql.value({ foo: { bar: 1 } });
+  expect(node).toEqual({ type: "VALUE", value: { foo: { bar: 1 } } });
+});
+
+describe("sql.identifier", () => {
+  it("one", () => {
+    const node = sql.identifier("foo");
+    expect(node).toEqual({ type: "IDENTIFIER", names: ["foo"] });
+  });
+
+  it("many", () => {
+    const node = sql.identifier("foo", "bar", 'b"z');
+    expect(node).toEqual({ type: "IDENTIFIER", names: ["foo", "bar", 'b"z'] });
+  });
+});
+
+describe("sql.query", () => {
+  it("simple", () => {
+    const node = sql.query`select 1`;
+    expect(node).toEqual([{ type: "RAW", text: "select 1" }]);
+  });
+
+  it("with values", () => {
+    const node = sql.query`select ${sql.value(1)}::integer`;
+    expect(node).toEqual([
+      { type: "RAW", text: "select " },
+      { type: "VALUE", value: 1 },
+      { type: "RAW", text: "::integer" },
+    ]);
+  });
+
+  it("with sub-sub-sub query", () => {
+    const node = sql.query`select ${sql.query`1 ${sql.query`from ${sql.query`foo`}`}`}`;
+    expect(node).toEqual([
+      { type: "RAW", text: "select " },
+      { type: "RAW", text: "1 " },
+      { type: "RAW", text: "from " },
+      { type: "RAW", text: "foo" },
+      { type: "RAW", text: "" },
+      { type: "RAW", text: "" },
+      { type: "RAW", text: "" },
+    ]);
+  });
+});
+
+describe("sql.compile", () => {
+  it("simple", () => {
+    const node = sql.query`select 1`;
+    expect(sql.compile(node)).toEqual({ text: "select 1", values: [] });
+  });
+
+  it("with values", () => {
+    const node = sql.query`select ${sql.value(1)}::integer`;
+    expect(sql.compile(node)).toEqual({
+      text: "select $1::integer",
+      values: [1],
+    });
+  });
+
+  it("with sub-sub-sub query", () => {
+    const node = sql.query`select ${sql.query`1 ${sql.query`from ${sql.query`foo`}`}`}`;
+    expect(sql.compile(node)).toEqual({
+      text: "select 1 from foo",
+      values: [],
+    });
+  });
+
+  it("more complex", () => {
+    const node = sql.query`select ${sql.query`${sql.value(
+      1
+    )} ${sql.query`from ${sql.identifier("foo", 'b"z')}`}`}`;
+    expect(sql.compile(node)).toEqual({
+      text: 'select $1 from "foo"."b""z"',
+      values: [1],
+    });
+  });
+});
