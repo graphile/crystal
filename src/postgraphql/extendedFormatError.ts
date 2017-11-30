206a775ddaf2767e1962b28acbf72a08c857d056
Benjie Gillam <benjie@jemjie.com>
2017-11-30 10:12:46 +0000

    feat(errors): extend errors further (#641)

--- src/postgraphql/extendedFormatError.ts
+++ src/postgraphql/extendedFormatError.ts
@@ -1,33 +1,45 @@
 import { GraphQLError } from 'graphql/error'
 
 /**
- * Given a GraphQLError, format it according to the rules described by the
- * Response Format, Errors section of the GraphQL Specification, plus a few
- * additional fields relevant to postgres errors, including HINT, DETAIL,
- * and ERRCODE.
+ * Extracts the requested fields from a pg error object, handling 'code' -> 'errcode' mapping.
  */
-export function extendedFormatError(error: GraphQLError, fields: Array<String>): GraphQLFormattedErrorExtended {
+function pickPgError(err: mixed, fields: Array<string>): {[s: string]: string | void} {
+  const result: mixed = {}
+  if (err && typeof err === 'object') {
+    fields.forEach((field: string) => {
+      // pg places 'errcode' on the 'code' property
+      const errField = field === 'errcode' ? 'code' : field
+      result[field] = err[errField] != null ? String(err[errField]) : err[errField]
+    })
+  }
+  return result
+}
+
+/**
+ * Given a GraphQLError, format it according to the rules described by the
+ * Response Format, Errors section of the GraphQL Specification, plus it can
+ * extract additional error codes from the postgres error, such as 'hint',
+ * 'detail', 'errcode', 'where', etc. - see `extendedErrors` option.
+ */
+export function extendedFormatError(error: GraphQLError, fields: Array<string>): GraphQLFormattedErrorExtended {
   if (!error) {
     throw new Error('Received null or undefined error.')
   }
   const originalError = error.originalError as GraphQLErrorExtended
   return {
+    ...(originalError && fields ? pickPgError(originalError, fields) : undefined),
     message: error.message,
     locations: error.locations,
     path: error.path,
-    hint: originalError && fields.indexOf('hint') > -1 ? originalError.hint : undefined,
-    detail: originalError && fields.indexOf('detail') > -1 ? originalError.detail : undefined,
-    errcode: originalError && fields.indexOf('errcode') > -1 ? originalError.code : undefined,
   }
 }
 
 export type GraphQLFormattedErrorExtended = {
+  // This is ugly, really I just want `string | void` but apparently TypeScript doesn't support that.
+  [s: string]: Array<GraphQLErrorLocation> | Array<string | number> | string | void,
   message: string,
   locations: Array<GraphQLErrorLocation> | void,
   path: Array<string | number> | void,
-  hint: string | void,
-  detail: string | void,
-  errcode: string | void,
 }
 
 export type GraphQLErrorLocation = {
