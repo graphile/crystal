6f223297a7ff9161555dfe4a2878d2638317a7ea
Benjie Gillam <benjie@jemjie.com>
2018-02-23 19:49:46 +0000

    chore: update minimum Node.js version to v8.6

--- packages/graphile-build-pg/index.js
+++
@@ -1,5 +0,0 @@
-// This script detects if you're running on Node v8 or above; if so it runs the
-// code directly, otherwise it falls back to the babel-compiled version
-
-const isNode8Plus = process.versions.node.match(/^([89]|[1-9][0-9]+)\./);
-module.exports = isNode8Plus ? require("./node8plus") : require("./node7minus");
