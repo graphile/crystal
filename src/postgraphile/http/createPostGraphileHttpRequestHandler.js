7e61b69b9c6ac7e869a6d1b2cfd5e1c5b2845b9e
Benjie Gillam <benjie@jemjie.com>
2018-05-01 10:57:46 +0100

    Add hook for validating paramsList

--- src/postgraphile/http/createPostGraphileHttpRequestHandler.js
+++ src/postgraphile/http/createPostGraphileHttpRequestHandler.js
@@ -483,6 +483,7 @@ export default function createPostGraphileHttpRequestHandler(options) {
       } else {
         paramsList = [paramsList]
       }
+      paramsList = pluginHook('postgraphile:httpParamsList', paramsList, { req, res, returnArray, httpError })
       const handleErrors = options.handleErrors || (errors => errors.map(formatError))
       results = await Promise.all(paramsList.map(async (params) => {
         let queryDocumentAst
