0681660f0596789f043b97fbb8cb791d37911c2c
Benjie Gillam <benjie@jemjie.com>
2018-05-16 17:33:35 +0100

    Add first/offset to simple collections

--- packages/graphile-build-pg/src/plugins/PgConnectionArgFirstLastBeforeAfter.js
+++ packages/graphile-build-pg/src/plugins/PgConnectionArgFirstLastBeforeAfter.js
@@ -10,14 +10,18 @@ export default (function PgConnectionArgs(builder) {
       args,
       { extend, getTypeByName, graphql: { GraphQLInt } },
       {
-        scope: { isPgFieldConnection, pgFieldIntrospection: source },
+        scope: {
+          isPgFieldConnection,
+          isPgFieldSimpleCollection,
+          pgFieldIntrospection: source,
+        },
         addArgDataGenerator,
         field,
         Self,
       }
     ) => {
       if (
-        !isPgFieldConnection ||
+        !(isPgFieldConnection || isPgFieldSimpleCollection) ||
         !source ||
         (source.kind !== "class" && source.kind !== "procedure")
       ) {
@@ -34,28 +38,32 @@ export default (function PgConnectionArgs(builder) {
       }) {
         return {
           pgQuery: queryBuilder => {
-            if (after != null) {
-              addCursorConstraint(after, true);
-            }
-            if (before != null) {
-              addCursorConstraint(before, false);
-            }
             if (first != null) {
               queryBuilder.first(first);
             }
             if (offset != null) {
               queryBuilder.offset(offset);
             }
-            if (last != null) {
-              if (first != null) {
-                throw new Error("We don't support setting both first and last");
+            if (isPgFieldConnection) {
+              if (after != null) {
+                addCursorConstraint(after, true);
               }
-              if (offset != null) {
-                throw new Error(
-                  "We don't support setting both offset and last"
-                );
+              if (before != null) {
+                addCursorConstraint(before, false);
               }
-              queryBuilder.last(last);
+              if (last != null) {
+                if (first != null) {
+                  throw new Error(
+                    "We don't support setting both first and last"
+                  );
+                }
+                if (offset != null) {
+                  throw new Error(
+                    "We don't support setting both offset and last"
+                  );
+                }
+                queryBuilder.last(last);
+              }
             }
 
             function addCursorConstraint(cursor, isAfter) {
@@ -73,29 +81,42 @@ export default (function PgConnectionArgs(builder) {
             description: "Only read the first `n` values of the set.",
             type: GraphQLInt,
           },
-          last: {
-            description: "Only read the last `n` values of the set.",
-            type: GraphQLInt,
-          },
+          ...(isPgFieldConnection
+            ? {
+                last: {
+                  description: "Only read the last `n` values of the set.",
+                  type: GraphQLInt,
+                },
+              }
+            : null),
           offset: {
-            description:
-              "Skip the first `n` values from our `after` cursor, an alternative to cursor based pagination. May not be used with `last`.",
+            description: isPgFieldConnection
+              ? "Skip the first `n` values from our `after` cursor, an alternative to cursor based pagination. May not be used with `last`."
+              : "Skip the first `n` values.",
             type: GraphQLInt,
           },
-          before: {
-            description:
-              "Read all values in the set before (above) this cursor.",
-            type: Cursor,
-          },
-          after: {
-            description:
-              "Read all values in the set after (below) this cursor.",
-            type: Cursor,
-          },
+          ...(isPgFieldConnection
+            ? {
+                before: {
+                  description:
+                    "Read all values in the set before (above) this cursor.",
+                  type: Cursor,
+                },
+                after: {
+                  description:
+                    "Read all values in the set after (below) this cursor.",
+                  type: Cursor,
+                },
+              }
+            : null),
         },
-        `Adding connection pagination args to field '${field.name}' of '${
-          Self.name
-        }'`
+        isPgFieldConnection
+          ? `Adding connection pagination args to field '${field.name}' of '${
+              Self.name
+            }'`
+          : `Adding simple collection args to field '${field.name}' of '${
+              Self.name
+            }'`
       );
     }
   );
