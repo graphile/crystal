00c86161373944797f2b6d054133726e95b0d2cc
Benjie Gillam <benjie@jemjie.com>
2019-03-15 11:48:56 +0000

    perf(connections): more efficient totalCount (#421)

--- packages/graphile-build-pg/src/plugins/PgConnectionTotalCount.js
+++ packages/graphile-build-pg/src/plugins/PgConnectionTotalCount.js
@@ -37,7 +37,7 @@ export default (function PgConnectionTotalCount(builder) {
                 return {
                   pgAggregateQuery: aggregateQueryBuilder => {
                     aggregateQueryBuilder.select(
-                      sql.fragment`count(*)`,
+                      sql.fragment`count(1)`,
                       "totalCount"
                     );
                   },
