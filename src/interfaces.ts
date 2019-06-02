3c4c3b2b72b538e2b055f78b3781d8761e483a38
michaelbridge <mbridge@gmail.com>
2019-06-02 09:51:55 -0400

    Fix comment ordering

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -105,15 +105,15 @@ export interface PostGraphileOptions<
   // recommend you start with it set to `false`.
   // The default is `true`, in v5 the default may change to `false`.
   ignoreIndexes?: boolean;
-  // By default, tables and functions that come from extensions are excluded
-  // from the generated GraphQL schema as general applications don't need them
-  // to be exposed to the end user. You can use this flag to include them in
-  // the generated schema (not recommended).
-  ignoreSchemas?: boolean;
   // Not recommended. By default, database objects within generated queries
   // consist of the PostgreSQL schema name and table name separated by a dot.
   // Set 'true' to omit the schema name, such that relevant table is determined
   // based on the current search_path.  Useful in multi-tenant applications.
+  ignoreSchemas?: boolean;
+  // By default, tables and functions that come from extensions are excluded
+  // from the generated GraphQL schema as general applications don't need them
+  // to be exposed to the end user. You can use this flag to include them in
+  // the generated schema (not recommended).
   includeExtensionResources?: boolean;
   // Enables adding a `stack` field to the error response.  Can be either the
   // boolean `true` (which results in a single stack string) or the string
