b552eecda5f36e07c49513670e7f114f40375d7f
Benjie Gillam <benjie@jemjie.com>
2019-09-03 15:42:25 +0100

    chore(types): remove @types/graphql (#1152)
    Thanks to @none23

--- rmlocal.sh
+++ rmlocal.sh
@@ -1,1 +1,1 @@
-rm -Rf node_modules/graphile-utils node_modules/graphile-build node_modules/graphile-build-pg node_modules/postgraphile-core/ node_modules/graphql-parse-resolve-info node_modules/graphql node_modules/pg-sql2 node_modules/@types/graphql node_modules/@types/pg node_modules/pg-sql2 node_modules/@graphile
+rm -Rf node_modules/graphile-utils node_modules/graphile-build node_modules/graphile-build-pg node_modules/postgraphile-core/ node_modules/graphql-parse-resolve-info node_modules/graphql node_modules/pg-sql2 node_modules/@types/pg node_modules/pg-sql2 node_modules/@graphile
