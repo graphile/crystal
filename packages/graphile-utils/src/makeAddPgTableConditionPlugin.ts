314fce122995ab7f6017e6c43910a2fec4171623
Benjie Gillam <benjie@jemjie.com>
2019-12-11 12:07:12 +0000

    fix(utils): only apply condition when specified (#572)

--- packages/graphile-utils/src/makeAddPgTableConditionPlugin.ts
+++ packages/graphile-utils/src/makeAddPgTableConditionPlugin.ts
@@ -107,7 +107,7 @@ export default function makeAddPgTableConditionPlugin(
         }: {
           condition: { [key: string]: any } | null;
         }) {
-          if (!condition) {
+          if (!condition || !(conditionFieldName in condition)) {
             return {};
           }
           const { [conditionFieldName]: conditionValue } = condition;
