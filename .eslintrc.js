481a7a04e32c7d04a71856043580904ead082c1c
Benjie Gillam <benjie@jemjie.com>
2020-05-20 16:31:13 +0100

    chore(lint): fix eslintrc typo

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,9 +26,12 @@ module.exports = {
     "@typescript-eslint/camelcase": "off",
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/no-namespace": ["error", {
-      "allowDeclarations": true
-    },
+    "@typescript-eslint/no-namespace": [
+      "error",
+      {
+        allowDeclarations: true,
+      },
+    ],
     "@typescript-eslint/no-var-requires": "off",
     "@typescript-eslint/no-unused-vars": [
       "error",
