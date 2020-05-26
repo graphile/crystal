375cf0f525fdc957893eb4b24744b0b0fcd3df21
Benjie Gillam <benjie@jemjie.com>
2020-05-26 17:32:10 +0100

    chore(tests): fix ts-jest moaning about package.json

--- jest.config.base.js
+++ jest.config.base.js
@@ -17,6 +17,15 @@ module.exports = (dir) => {
     rootDir: dir,
     name: packageJson.name,
     displayName: packageJson.name,
+    globals: {
+      "ts-jest": {
+        // Ref: https://github.com/votingworks/ballot-encoder/pull/13/files/b5bbb770c1cc0777c0aee1455b61497afd46cccd#r381505804
+        // ts-jest assumes it can walk up from its location on disk to find
+        // package.json, but this is not true when operating in the yarn PnP
+        // virtual file system. Instead, we provide it directly.
+        packageJson,
+      },
+    },
 
     setupFiles: [`${__dirname}/__tests__/setup-mock-fs.ts`],
   };
