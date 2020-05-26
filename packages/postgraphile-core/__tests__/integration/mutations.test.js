32fdf5f82af002ad4c479ffb79f2c8cd18e75b82
Benjie Gillam <benjie@jemjie.com>
2020-05-26 16:40:14 +0100

    chore(tests): neater mutation errors

--- packages/postgraphile-core/__tests__/integration/mutations.test.js
+++ packages/postgraphile-core/__tests__/integration/mutations.test.js
@@ -154,7 +154,19 @@ beforeAll(() => {
       );
       if (result.errors) {
         // eslint-disable-next-line no-console
-        console.log(result.errors.map((e) => e.originalError));
+        console.log(
+          `GraphQL mutation '${fileName}' had an error:\n  ` +
+            result.errors
+              .map((e) => {
+                const error = e.originalError || e;
+                let message = error.message || String(e);
+                if (e.locations && e.locations[0]) {
+                  message = `[${e.locations[0].line}:${e.locations[0].column}]: ${message}`;
+                }
+                return message;
+              })
+              .join("\n  "),
+        );
       }
       return result;
     });
