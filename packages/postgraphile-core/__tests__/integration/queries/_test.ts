aeae33ddae397d97913bf4a3ded306672d4954a9
Benjie Gillam <benjie@jemjie.com>
2020-06-09 12:43:02 +0100

    Newly split up tests now almost passing

---
+++ packages/postgraphile-core/__tests__/integration/queries/_test.ts
@@ -0,0 +1,139 @@
+import { createPostGraphileSchema } from "../../..";
+import { makeExtendSchemaPlugin, gql } from "graphile-utils";
+import { basename } from "path";
+import { withPgClient, getServerVersionNum } from "../../helpers";
+import { promises as fsp } from "fs";
+import { graphql } from "graphql";
+import JSON5 from "json5";
+import ToyCategoriesPlugin from "../ToyCategoriesPlugin";
+
+const QueryExtendedPlugin = makeExtendSchemaPlugin({
+  typeDefs: gql`
+    extend type Query {
+      extended: Boolean
+    }
+  `,
+  resolvers: {
+    Query: {
+      extended: () => true,
+    },
+  },
+});
+
+const PLUGINS: {
+  [key: string]: GraphileEngine.Plugin;
+} = {
+  QueryExtendedPlugin,
+  ToyCategoriesPlugin,
+};
+
+const kitchenSinkData = () =>
+  fsp.readFile(`${__dirname}/../../kitchen-sink-data.sql`, "utf8");
+
+const pg10Data = () => fsp.readFile(`${__dirname}/../../pg10-data.sql`, "utf8");
+
+const dSchemaComments = () =>
+  fsp.readFile(`${__dirname}/../../kitchen-sink-d-schema-comments.sql`, "utf8");
+
+export const testGraphQL = (query: string, path: string) => {
+  const fileName = basename(path);
+  const lines = query.split("\n");
+  const config: {
+    [key: string]: any;
+  } = {};
+  for (const line of lines) {
+    if (line.startsWith("#>")) {
+      const colon = line.indexOf(":");
+      if (colon < 0) {
+        throw new Error(
+          `Invalid query configuration '${line}' - expected colon.`,
+        );
+      }
+      const key = line.substr(2, colon - 2).trim();
+      const value = JSON5.parse(line.substr(colon + 1));
+      config[key] = value;
+    }
+  }
+
+  return withPgClient(async (pgClient) => {
+    const serverVersionNum = await getServerVersionNum(pgClient);
+    if (fileName.startsWith("pg10.")) {
+      if (serverVersionNum < 100000) {
+        console.log("Skipping test as PG version is less than 10");
+        return;
+      }
+    }
+
+    await pgClient.query(await dSchemaComments());
+    await pgClient.query(await kitchenSinkData());
+    if (serverVersionNum >= 100000) {
+      await pgClient.query(await pg10Data());
+    }
+
+    const {
+      rbac,
+      plugins: pluginNames = [],
+      schemas = ["a", "b", "c"],
+      options = {},
+      ...rest
+    } = config;
+    if (Object.keys(rest).length) {
+      throw new Error(
+        `Invalid query configuration - keys '${Object.keys(rest).join(
+          "', '",
+        )}' not understood.`,
+      );
+    }
+    const postgraphileOptions = {
+      subscriptions: true,
+      appendPlugins: [
+        QueryExtendedPlugin,
+        ...pluginNames.map((pluginName: string) => {
+          if (!PLUGINS[pluginName]) {
+            throw new Error(`Plugin '${pluginName}' not known`);
+          }
+          return PLUGINS[pluginName];
+        }),
+      ],
+      ...config.options,
+    };
+
+    const schema = await createPostGraphileSchema(
+      pgClient,
+      schemas,
+      postgraphileOptions,
+    );
+
+    await pgClient.query("savepoint test");
+    if (rbac) {
+      await pgClient.query(
+        "select set_config('role', 'postgraphile_test_visitor', true), set_config('jwt.claims.user_id', '3', true)",
+      );
+    }
+    try {
+      // Return the result of our GraphQL query.
+      const result = await graphql(schema, query, null, {
+        pgClient,
+      });
+      if (result.errors) {
+        // eslint-disable-next-line no-console
+        console.log(
+          `GraphQL query '${fileName}' had an error:\n  ` +
+            result.errors
+              .map((e) => {
+                const error = e.originalError || e;
+                let message = error.message || String(e);
+                if (e.locations && e.locations[0]) {
+                  message = `[${e.locations[0].line}:${e.locations[0].column}]: ${message}`;
+                }
+                return message;
+              })
+              .join("\n  "),
+        );
+      }
+      expect(result).toMatchSnapshot();
+    } finally {
+      await pgClient.query("rollback to savepoint test");
+    }
+  });
+};
