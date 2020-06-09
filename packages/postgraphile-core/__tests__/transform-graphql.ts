0345d51b543e3a79b58e484c851b89da10377314
Benjie Gillam <benjie@jemjie.com>
2020-06-09 13:54:09 +0100

    chore(tests): refactor integration tests (#1296)

---
+++ packages/postgraphile-core/__tests__/transform-graphql.ts
@@ -0,0 +1,14 @@
+// IMPORTANT: after editing this file, you must run `yarn jest --clearCache`
+// because the transformed code gets cached.
+const { basename } = require("path");
+
+exports.process = (src, path) => {
+  return `\
+const { testGraphQL } = require("./_test");
+
+const query = ${JSON.stringify(src)};
+const path = ${JSON.stringify(path)};
+
+it('execute', () => testGraphQL(query, path));
+`;
+};
