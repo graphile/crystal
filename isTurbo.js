28ce7dfdcf8803bd4e5755f950b8fb7cde26bb8e
Benjie Gillam <benjie@jemjie.com>
2020-12-07 12:33:37 +0000

    Prettier?

--- isTurbo.js
+++ isTurbo.js
@@ -1,7 +1,9 @@
 if (process.env.GRAPHILE_TURBO === '1') {
   const major = parseInt(process.version.replace(/\..*$/, ''), 10);
   if (major < 14) {
-    throw new Error('Turbo mode currently requires Node v14 or higher, please upgrade Node.js or remove the GRAPHILE_TURBO environmental variable.');
+    throw new Error(
+      'Turbo mode currently requires Node v14 or higher, please upgrade Node.js or remove the GRAPHILE_TURBO environmental variable.'
+    );
   }
   module.exports = true;
 } else {
