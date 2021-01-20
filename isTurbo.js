fd482787989449b5c0766fb3431cdc5672f948f2
Benjie Gillam <benjie@jemjie.com>
2021-01-20 14:13:57 +0000

    Lint fix

--- isTurbo.js
+++ isTurbo.js
@@ -2,7 +2,7 @@ if (process.env.GRAPHILE_TURBO === '1') {
   const major = parseInt(process.version.replace(/\..*$/, ''), 10);
   if (major < 14) {
     throw new Error(
-      'Turbo mode currently requires Node v14 or higher, please upgrade Node.js or remove the GRAPHILE_TURBO environmental variable.'
+      'Turbo mode currently requires Node v14 or higher, please upgrade Node.js or remove the GRAPHILE_TURBO environmental variable.',
     );
   }
   module.exports = true;
