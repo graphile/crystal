b5b324725953b8c13dab665ac207a5e235ca22c0
Benjie Gillam <benjie@jemjie.com>
2021-03-05 09:47:52 +0000

    When main is done, release the pool

--- packages/graphile-crystal/src/examples/example2.ts
+++ packages/graphile-crystal/src/examples/example2.ts
@@ -821,7 +821,9 @@ async function main() {
   }
 }
 
-main().catch((e) => {
-  console.error(e);
-  process.exit(1);
-});
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(() => testPool.end());
