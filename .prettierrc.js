42c18b0a3243f62c4d38cef699a1fc64114cfcd9
Benjie Gillam <benjie@jemjie.com>
2021-03-17 13:07:06 +0000

    Wider markdown printing

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   trailingComma: "all",
   proseWrap: "always",
+  overrides: [
+    {
+      files: "*.md",
+      options: {
+        printWidth: 120,
+      },
+    },
+  ],
 };
