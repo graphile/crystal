e79d0bdfce29dea15332ca7193f3ccfadb330145
Benjie Gillam <benjie@jemjie.com>
2021-04-01 11:07:54 +0100

    Start again

--- packages/graphile-crystal/src/crystalResult.ts
+++
@@ -1,5 +0,0 @@
-import { CrystalResult, $$batch } from "./interfaces";
-
-export function isCrystalResult(parent: unknown): parent is CrystalResult {
-  return (typeof parent === "object" && parent && $$batch in parent) || false;
-}
