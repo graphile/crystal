963a9c532e18f4c2a52e961ce0b51c706a89cecc
Paul Melnikow <email@paulmelnikow.com>
2021-05-04 21:41:21 -0400

    Declare shutdownActions in WithPostGraphileContextOptions

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -387,6 +387,7 @@ export interface HttpRequestHandler<
  */
 export interface WithPostGraphileContextOptions {
   pgPool: Pool;
+  shutdownActions: ShutdownActions;
   jwtToken?: string;
   jwtSecret?: jwt.Secret;
   jwtPublicKey?: jwt.Secret | jwt.GetPublicKeyOrSecret;
