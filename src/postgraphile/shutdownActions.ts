f8c3cd60bf06e2c13aad669ea62ea449dcdf51eb
Paul Melnikow <github@paulmelnikow.com>
2021-05-14 16:24:10 -0400

    ShutdownActions: Add benjie's comments
    Co-authored-by: Benjie Gillam <benjie@jemjie.com>

--- src/postgraphile/shutdownActions.ts
+++ src/postgraphile/shutdownActions.ts
@@ -1,9 +1,28 @@
 export type Action = () => void | Promise<void>;
 
+/**
+ * This class tracks actions that must be taken
+ * when PostGraphile is shut down (released) in
+ * order to make sure that all the resources it
+ * has consumed has been cleaned up.
+ *
+ * Since PostGraphile is extensible via plugins
+ * and plugins may have their own shutdown actions
+ * we use this generic system to handle them all.
+ * If your plugin sets up something that would
+ * keep running after PostGraphile is `.release()`d
+ * (such as a timer, interval, network connection,
+ * filesystem monitoring, or similar activity)
+ * then you should register an action via
+ * `shutdownActions` to clean it up.
+ */
 export class ShutdownActions {
   private actions: Action[] = [];
   private didInvoke = false;
 
+  /**
+   * Register a function to be called when PostGraphile is released.
+   */
   add(action: Action): void {
     if (this.didInvoke) {
       console.warn(
@@ -20,6 +39,12 @@ export class ShutdownActions {
     }
   }
 
+  /**
+   * If your action is no longer relevant (for example it has
+   * completed, or it was only relevant whilst in a particular
+   * mode) then be sure to remove it so it won't be called
+   * when PostGraphile is released.
+   */
   remove(action: Action): void {
     const index = this.actions.indexOf(action);
     if (index === -1) {
@@ -28,6 +53,12 @@ export class ShutdownActions {
     this.actions.splice(index, 1);
   }
 
+  /**
+   * Calls the release actions in parallel and returns
+   * the array of resulting promises/results. Will not
+   * throw unless the shutdown actions have already
+   * been invoked.
+   */
   invoke(): Array<Promise<void> | void> {
     if (this.didInvoke) {
       throw new Error('release() has already been called.');
@@ -46,6 +77,10 @@ export class ShutdownActions {
     });
   }
 
+  /**
+   * Calls all the release actions and resolves
+   * when complete (rejecting if an error occurred).
+   */
   async invokeAll(): Promise<void> {
     // TODO:v5: This would be better if it used `Promise.allSettled()` but we can't
     // use it yet.
