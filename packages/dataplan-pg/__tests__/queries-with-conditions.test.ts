d7c2503505de360e9c4f34f6468def74e15a6851
Benjie Gillam <benjie@jemjie.com>
2021-06-03 14:58:43 +0100

    Another test

--- packages/dataplan-pg/__tests__/queries-with-conditions.test.ts
+++ packages/dataplan-pg/__tests__/queries-with-conditions.test.ts
@@ -329,3 +329,97 @@ it("{forums{name messagesConnection(...){nodes{body author{...}} edges{cursor no
   `);
   expect(queries).toHaveLength(1);
 });
+
+it("EXCLUSIVELY/INHERIT", async () => {
+  const { data, queries } = await runTestQuery(/* GraphQL */ `
+    {
+      forums(includeArchived: EXCLUSIVELY) {
+        name
+        messagesConnection(includeArchived: INHERIT) {
+          nodes {
+            body
+            author {
+              username
+              gravatarUrl
+            }
+          }
+          edges {
+            cursor
+            node {
+              body
+              author {
+                username
+                gravatarUrl
+              }
+            }
+          }
+        }
+      }
+    }
+  `);
+  expect({ __: data }).toMatchInlineSnapshot(`
+    {
+      forums: [
+        {
+          name: "Dogs",
+          messagesConnection: {
+            nodes: [
+              { body: "Dogs = awesome -- Alice", author: { username: "Alice", gravatarUrl: null } },
+              { body: "Dogs = awesome -- Bob", author: { username: "Bob", gravatarUrl: null } },
+              { body: "Dogs = awesome -- Cecilia", author: { username: "Cecilia", gravatarUrl: null } },
+            ],
+            edges: [
+              {
+                cursor: "424242",
+                node: { body: "Dogs = awesome -- Alice", author: { username: "Alice", gravatarUrl: null } },
+              },
+              { cursor: "424242", node: { body: "Dogs = awesome -- Bob", author: { username: "Bob", gravatarUrl: null } } },
+              {
+                cursor: "424242",
+                node: { body: "Dogs = awesome -- Cecilia", author: { username: "Cecilia", gravatarUrl: null } },
+              },
+            ],
+          },
+        },
+      ],
+    }
+  `);
+  expect({
+    __: queries.map((q) => q.text).join("\n\n"),
+  }).toMatchInlineSnapshot(`
+    select 
+      __forums__."name"::text as "0",
+      array(
+        select array[
+          __messages__."body"::text,
+          __users__."username"::text,
+          __users__."gravatar_url"::text,
+          __messages__."author_id"::text,
+          424242 /* TODO: CURSOR */,
+          __users_2."username"::text,
+          __users_2."gravatar_url"::text
+        ]::text[]
+        from app_public.messages as __messages__
+        left outer join app_public.users as __users__
+        on ((__messages__."author_id"::"uuid" = __users__."id"))
+        left outer join app_public.users as __users_2
+        on ((__messages__."author_id"::"uuid" = __users_2."id"))
+        where (
+          (__messages__.archived_at is null) = (__forums__."archived_at" is null)
+        ) and (
+          __forums__."id"::"uuid" = __messages__."forum_id"
+        )
+        order by __messages__."id" asc
+      ) as "1",
+      __forums__."id"::text as "2",
+      __forums__."archived_at"::text as "3"
+    from app_public.forums as __forums__
+    where (
+      __forums__.archived_at is not null
+    ) and (
+      true /* authorization checks */
+    )
+    order by __forums__."id" asc
+  `);
+  expect(queries).toHaveLength(1);
+});
