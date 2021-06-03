b4226ae349f8ec5b49471533405851f85f972745
Benjie Gillam <benjie@jemjie.com>
2021-06-03 16:17:37 +0100

    New test system for dataplan

---
+++ packages/dataplan-pg/__tests__/queries/_test.ts
@@ -0,0 +1,64 @@
+import { promises as fsp } from "fs";
+import JSON5 from "json5";
+import prettier from "prettier";
+
+import type { PgClientQuery } from "../../src/datasource";
+import { runTestQuery } from "../helpers";
+
+const UPDATE_SNAPSHOTS = process.env.UPDATE_SNAPSHOTS === "1";
+
+async function exists(path: string) {
+  try {
+    await fsp.stat(path);
+  } catch (e) {
+    return false;
+  }
+}
+
+async function snapshot(actual: string, filePath: string) {
+  let expected: string | null = null;
+  try {
+    expected = await fsp.readFile(filePath, "utf8");
+  } catch (e) {
+    /* noop */
+  }
+  if (expected == null || UPDATE_SNAPSHOTS) {
+    if (expected !== actual) {
+      console.warn(`Updated snapshot in '${filePath}'`);
+      await fsp.writeFile(filePath, actual);
+    }
+  } else {
+    expect(actual).toEqual(expected);
+  }
+}
+
+export const testGraphQL = async ({
+  document,
+  path,
+  assertions,
+}: {
+  document: string;
+  path: string;
+  assertions: (result: {
+    data: any;
+    queries: PgClientQuery[];
+  }) => Promise<void>;
+  config: any;
+}): Promise<void> => {
+  const sqlFileName = path + ".sql";
+  const resultFileName = path + ".json5";
+
+  const { data, queries } = await runTestQuery(document);
+
+  await assertions({ data, queries });
+
+  const formattedData = prettier.format(JSON5.stringify(data), {
+    parser: "json5",
+    printWidth: 120,
+  });
+  await snapshot(formattedData, resultFileName);
+  const formattedQueries = queries.map((q) => q.text).join("\n\n");
+  await snapshot(formattedQueries, sqlFileName);
+
+  expect(queries).toHaveLength(1);
+};
---
+++ packages/dataplan-pg/__tests__/transform-graphql.js
@@ -0,0 +1,46 @@
+// IMPORTANT: after editing this file, you must run `yarn jest --clearCache`
+// because the transformed code gets cached.
+const { basename } = require("path");
+const JSON5 = require("json5");
+
+exports.process = (src, path) => {
+  const lines = src.split("\n");
+  const config = {};
+  const assertions = [];
+  const documentLines = [];
+  for (const line of lines) {
+    if (line.startsWith("#>")) {
+      const colon = line.indexOf(":");
+      if (colon < 0) {
+        throw new Error(
+          `Invalid query configuration '${line}' - expected colon.`,
+        );
+      }
+      const key = line.substr(2, colon - 2).trim();
+      const value = JSON5.parse(line.substr(colon + 1));
+      config[key] = value;
+    } else if (line.startsWith("#!")) {
+      assertions.push(line.substr(2));
+    } else {
+      documentLines.push(line);
+    }
+  }
+  const document = documentLines.join("\n");
+
+  return `\
+const { testGraphQL } = require("../_test");
+
+const document = ${JSON.stringify(document)};
+const path = ${JSON.stringify(path)};
+
+it('execute', () => testGraphQL({
+  document,
+  path,
+  assertions: async (result) => {
+    const { data, queries } = result;
+    ${assertions.join("\n    ")}
+  },
+  config: ${JSON.stringify(config)},
+}));
+`;
+};
