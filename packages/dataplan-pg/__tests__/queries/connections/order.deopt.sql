ab0be5e72e4b1756c2286159b2e18c28a90df186
Benjie Gillam <benjie@jemjie.com>
2021-07-17 12:50:17 +0100

    Testing ordering

---
+++ packages/dataplan-pg/__tests__/queries/connections/order.deopt.sql
@@ -0,0 +1,38 @@
+select
+  __author__.username::text as "0",
+  __messages__.body::text as "1",
+  __messages__."id"::text as "2",
+  __messages__."body"::text as "3",
+  __messages__."author_id"::text as "4"
+from app_public.messages as __messages__
+left outer join app_public.users as __author__
+on (__messages__."author_id" = __author__."id")
+where
+  (
+    __messages__.archived_at is null
+  ) and (
+    true /* authorization checks */
+  )
+order by __author__.username desc, __messages__.body asc, __messages__."id" asc
+
+select __users_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"uuid" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __users_identifiers__,
+lateral (
+  select
+    __users__."username"::text as "0",
+    __users__."gravatar_url"::text as "1",
+    __users_identifiers__.idx as "2"
+  from app_public.users as __users__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __users__."id" = __users_identifiers__."id0"
+    )
+  order by __users__."id" asc
+) as __users_result__
---
+++ packages/dataplan-pg/__tests__/queries/connections/order.sql
@@ -0,0 +1,20 @@
+select
+  __author__.username::text as "0",
+  __messages__.body::text as "1",
+  __messages__."id"::text as "2",
+  __messages__."body"::text as "3",
+  __users__."username"::text as "4",
+  __users__."gravatar_url"::text as "5",
+  __messages__."author_id"::text as "6"
+from app_public.messages as __messages__
+left outer join app_public.users as __author__
+on (__messages__."author_id" = __author__."id")
+left outer join app_public.users as __users__
+on (__messages__."author_id"::"uuid" = __users__."id")
+where
+  (
+    __messages__.archived_at is null
+  ) and (
+    true /* authorization checks */
+  )
+order by __author__.username desc, __messages__.body asc, __messages__."id" asc
