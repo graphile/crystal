6a8929677fbe1684ee1c3d6ed91aff245c433147
Benjie Gillam <benjie@jemjie.com>
2021-07-17 10:14:40 +0100

    Add missing reverse plan

---
+++ packages/graphile-crystal/src/plans/reverse.ts
@@ -0,0 +1,42 @@
+import type { CrystalResultsList, CrystalValuesList } from "../interfaces";
+import { ExecutablePlan } from "../plan";
+
+/**
+ * Returns a reversed copy of the list.
+ */
+export function reverseArray<TData = any>(list: readonly TData[]): TData[] {
+  if (!Array.isArray(list)) {
+    // @ts-ignore
+    return list;
+  }
+  const l = list.length;
+  const newList = new Array(l);
+  for (let i = 0; i < l; i++) {
+    newList[i] = list[l - i - 1];
+  }
+  return newList;
+}
+
+/**
+ * Reverses a list.
+ */
+export class ReversePlan<TData extends any> extends ExecutablePlan<TData[]> {
+  constructor(plan: ExecutablePlan<TData[]>) {
+    super();
+    this.addDependency(plan);
+  }
+
+  execute(values: CrystalValuesList<[TData[]]>): CrystalResultsList<TData[]> {
+    return values.map(([arr]) => reverseArray(arr));
+  }
+
+  deduplicate(peers: ReversePlan<TData>[]): ReversePlan<TData> {
+    return peers.length > 0 ? peers[0] : this;
+  }
+}
+
+export function reverse<TData extends any>(
+  plan: ExecutablePlan<TData[]>,
+): ReversePlan<TData> {
+  return new ReversePlan<TData>(plan);
+}
