91042405a8e09937e495198f1a7c1e4127a1e3cf
Benjie Gillam <benjie@jemjie.com>
2021-07-23 10:03:40 +0100

    Another super simple test

---
+++ packages/dataplan-pg/__tests__/queries/super-simple/single-record-via-arg.deopt.sql
@@ -0,0 +1,21 @@
+select __forums_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"uuid" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __forums_identifiers__,
+lateral (
+  select
+    __forums__."id"::text as "0",
+    __forums__."name"::text as "1",
+    __forums_identifiers__.idx as "2"
+  from app_public.forums as __forums__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __forums__."id" = __forums_identifiers__."id0"
+    )
+  order by __forums__."id" asc
+) as __forums_result__
