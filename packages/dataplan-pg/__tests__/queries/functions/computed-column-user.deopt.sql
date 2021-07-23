609d6a85ac7ffffcb388a583c90904d03a8ea84b
Benjie Gillam <benjie@jemjie.com>
2021-07-23 14:26:20 +0100

    Computed column record test

---
+++ packages/dataplan-pg/__tests__/queries/functions/computed-column-user.deopt.sql
@@ -0,0 +1,24 @@
+select __forums_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"uuid" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __forums_identifiers__,
+lateral (
+  select
+    __forums_random_user__."username"::text as "0",
+    __forums_random_user__."gravatar_url"::text as "1",
+    __forums__::text as "2",
+    __forums_identifiers__.idx as "3"
+  from app_public.forums as __forums__
+  left outer join app_public.forums_random_user(__forums__) as __forums_random_user__
+  on TRUE
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __forums__."id" = __forums_identifiers__."id0"
+    )
+  order by __forums__."id" asc
+) as __forums_result__
