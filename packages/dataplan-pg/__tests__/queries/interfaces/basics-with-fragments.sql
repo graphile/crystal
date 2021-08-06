0174cdf817b3132a379ee7bfdee751e97ae5260f
Benjie Gillam <benjie@jemjie.com>
2021-08-06 19:59:57 +0100

    Interfaces now able to query on typed inline fragments

---
+++ packages/dataplan-pg/__tests__/queries/interfaces/basics-with-fragments.sql
@@ -0,0 +1,31 @@
+select
+  __people__."username"::text as "0",
+  array(
+    select array[
+      __single_table_items__."type"::text,
+      __single_table_items__."id"::text,
+      __single_table_items__."position"::text,
+      __single_table_items__."created_at"::text,
+      __single_table_items__."updated_at"::text,
+      __single_table_items__."is_explicitly_archived"::text,
+      __single_table_items__."archived_at"::text,
+      __single_table_items__."title"::text,
+      __single_table_items__."description"::text,
+      __single_table_items__."note"::text,
+      __single_table_items__."color"::text
+    ]::text[]
+    from interfaces_and_unions.single_table_items as __single_table_items__
+    where
+      (
+        __people__."person_id"::"int4" = __single_table_items__."author_id"
+      ) and (
+        true /* authorization checks */
+      )
+    order by __single_table_items__."id" asc
+  ) as "1",
+  __people__."person_id"::text as "2"
+from interfaces_and_unions.people as __people__
+where (
+  true /* authorization checks */
+)
+order by __people__."person_id" asc
