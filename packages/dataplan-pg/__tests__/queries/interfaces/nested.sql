1fec0c7ad456214631892544ba48d88e2c33e015
Benjie Gillam <benjie@jemjie.com>
2021-08-25 14:09:30 +0100

    Nested interfaces test

---
+++ packages/dataplan-pg/__tests__/queries/interfaces/nested.sql
@@ -0,0 +1,27 @@
+select
+  __people__."username"::text as "0",
+  array(
+    select array[
+      __single_table_items_2."type"::text,
+      __single_table_items__."type"::text,
+      __single_table_items__."id"::text,
+      __single_table_items_2."parent_id"::text,
+      __single_table_items_2."id"::text
+    ]::text[]
+    from interfaces_and_unions.single_table_items as __single_table_items_2
+    left outer join interfaces_and_unions.single_table_items as __single_table_items__
+    on (__single_table_items_2."parent_id"::"int4" = __single_table_items__."id")
+    where
+      (
+        __people__."person_id"::"int4" = __single_table_items_2."author_id"
+      ) and (
+        true /* authorization checks */
+      )
+    order by __single_table_items_2."id" asc
+  ) as "1",
+  __people__."person_id"::text as "2"
+from interfaces_and_unions.people as __people__
+where (
+  true /* authorization checks */
+)
+order by __people__."person_id" asc
