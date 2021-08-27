2e7afdbe8db04b0e474dddff075bc5f96c036cfb
Benjie Gillam <benjie@jemjie.com>
2021-08-27 11:10:17 +0100

    Test for singleTableTopicById

---
+++ packages/dataplan-pg/__tests__/queries/interfaces-single-table/single-topic.deopt.sql
@@ -0,0 +1,30 @@
+select __single_table_items_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0",
+    (ids.value->>1)::interfaces_and_unions.item_type as "id1"
+  from json_array_elements($1::json) with ordinality as ids
+) as __single_table_items_identifiers__,
+lateral (
+  select
+    __single_table_items__."id"::text as "0",
+    __single_table_items__."type"::text as "1",
+    __single_table_items__."position"::text as "2",
+    __single_table_items__."created_at"::text as "3",
+    __single_table_items__."updated_at"::text as "4",
+    __single_table_items__."is_explicitly_archived"::text as "5",
+    __single_table_items__."archived_at"::text as "6",
+    __single_table_items__."title"::text as "7",
+    __single_table_items_identifiers__.idx as "8"
+  from interfaces_and_unions.single_table_items as __single_table_items__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __single_table_items__."id" = __single_table_items_identifiers__."id0"
+    ) and (
+      __single_table_items__."type" = __single_table_items_identifiers__."id1"
+    )
+  order by __single_table_items__."id" asc
+) as __single_table_items_result__
