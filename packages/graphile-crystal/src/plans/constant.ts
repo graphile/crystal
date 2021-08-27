45a3e364faaa7f49041abe75ca46f26408a24001
Benjie Gillam <benjie@jemjie.com>
2021-08-27 11:08:06 +0100

    ConstantPlan now has tagged template literal helper

--- packages/graphile-crystal/src/plans/constant.ts
+++ packages/graphile-crystal/src/plans/constant.ts
@@ -14,6 +14,29 @@ export class ConstantPlan<TData> extends ExecutablePlan<TData> {
   }
 }
 
-export function constant<TData>(data: TData): ConstantPlan<TData> {
+function isTemplateStringsArray(data: any): data is TemplateStringsArray {
+  return (
+    Array.isArray(data) &&
+    "raw" in data &&
+    Array.isArray((data as TemplateStringsArray).raw)
+  );
+}
+
+// Call this as a template string or as a function. Only intended for handling scalar values, not arrays/objects/etc.
+export function constant(
+  strings: TemplateStringsArray & [string],
+): ConstantPlan<string>;
+export function constant<TData>(data: TData): ConstantPlan<TData>;
+export function constant<TData>(
+  data: TData | (TemplateStringsArray & [TData]),
+): ConstantPlan<TData> {
+  if (isTemplateStringsArray(data)) {
+    if (data.length !== 1) {
+      throw new Error(
+        "constant`...` doesn't currently support placeholders; please use 'constant(`...`)' instead",
+      );
+    }
+    return new ConstantPlan<TData>(data[0]);
+  }
   return new ConstantPlan<TData>(data);
 }
