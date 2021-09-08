e1b29d491cd20cfa5f4442f33f01d4ca9863643d
Benjie Gillam <benjie@jemjie.com>
2021-09-08 17:20:22 +0100

    Refactor _test file to allow greater code reuse

--- packages/dataplan-pg/__tests__/helpers.ts
+++ packages/dataplan-pg/__tests__/helpers.ts
@@ -1,12 +1,16 @@
 Error.stackTraceLimit = Infinity;
+import { promises as fsp } from "fs";
 import type { BaseGraphQLContext } from "graphile-crystal";
 import { graphql } from "graphql";
+import JSON5 from "json5";
 import type { PoolClient } from "pg";
 import { Pool } from "pg";
-import { inspect } from "util";
+import prettier from "prettier";
 
 import type { PgClient, PgClientQuery, WithPgClient } from "../src";
 import { makeExampleSchema, schema as optimizedSchema } from "./exampleSchema";
+
+export const UPDATE_SNAPSHOTS = process.env.UPDATE_SNAPSHOTS === "1";
 
 const deoptimizedSchema = makeExampleSchema({ deoptimize: true });
 
@@ -163,3 +167,65 @@ export async function runTestQuery(
   expect(data).toBeTruthy();
   return { data, queries };
 }
+
+async function snapshot(actual: string, filePath: string) {
+  let expected: string | null = null;
+  try {
+    expected = await fsp.readFile(filePath, "utf8");
+  } catch (e) {
+    /* noop */
+  }
+  if (expected == null || UPDATE_SNAPSHOTS) {
+    if (expected !== actual) {
+      console.warn(`Updated snapshot in '${filePath}'`);
+      await fsp.writeFile(filePath, actual);
+    }
+  } else {
+    expect(actual).toEqual(expected);
+  }
+}
+
+export const assertSnapshotsMatch = async (
+  only: "sql" | "result",
+  props: {
+    result: ReturnType<typeof runTestQuery>;
+    document: string;
+    path: string;
+    config: any;
+    ext?: string;
+  },
+): Promise<void> => {
+  const { path, result, ext } = props;
+  const basePath = path.replace(/\.test\.graphql$/, "");
+  if (basePath === path) {
+    throw new Error(`Failed to trim .test.graphql from '${path}'`);
+  }
+
+  const { data, queries } = await result;
+
+  if (only === "result") {
+    const resultFileName = basePath + (ext || "") + ".json5";
+    const formattedData = prettier.format(JSON5.stringify(data), {
+      parser: "json5",
+      printWidth: 120,
+    });
+    await snapshot(formattedData, resultFileName);
+  } else if (only === "sql") {
+    const sqlFileName = basePath + (ext || "") + ".sql";
+    const formattedQueries = queries.map((q) => q.text).join("\n\n");
+    await snapshot(formattedQueries, sqlFileName);
+  } else {
+    throw new Error(
+      `Unexpected argument to assertSnapshotsMatch; expected result|sql, received '${only}'`,
+    );
+  }
+};
+
+export const assertResultsMatch = async (
+  result1: ReturnType<typeof runTestQuery>,
+  result2: ReturnType<typeof runTestQuery>,
+): Promise<void> => {
+  const { data: data1 } = await result1;
+  const { data: data2 } = await result2;
+  expect(data2).toEqual(data1);
+};
