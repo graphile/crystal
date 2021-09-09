c582ee54daac914fea174bf69ed3bd75234ea4ff
Benjie Gillam <benjie@jemjie.com>
2021-09-09 17:56:49 +0100

    Wider selection set for create-relational-post test

--- packages/dataplan-pg/__tests__/mutations/basics/create-relational-post.deopt.sql
+++ packages/dataplan-pg/__tests__/mutations/basics/create-relational-post.deopt.sql
@@ -15,13 +15,17 @@ from (
 ) as __relational_posts_identifiers__,
 lateral (
   select
-    __relational_posts__."id"::text as "0",
-    __relational_posts__."title"::text as "1",
-    __relational_posts__."description"::text as "2",
-    __relational_posts__."note"::text as "3",
-    __relational_posts_title_lower__.__relational_posts_title_lower__::text as "4",
-    __relational_posts_identifiers__.idx as "5"
+    __relational_items__."is_explicitly_archived"::text as "0",
+    __relational_items__."author_id"::text as "1",
+    __relational_posts__."id"::text as "2",
+    __relational_posts__."title"::text as "3",
+    __relational_posts__."description"::text as "4",
+    __relational_posts__."note"::text as "5",
+    __relational_posts_title_lower__.__relational_posts_title_lower__::text as "6",
+    __relational_posts_identifiers__.idx as "7"
   from interfaces_and_unions.relational_posts as __relational_posts__
+  left outer join interfaces_and_unions.relational_items as __relational_items__
+  on (__relational_posts__."id"::"int4" = __relational_items__."id")
   left outer join interfaces_and_unions.relational_posts_title_lower(__relational_posts__) as __relational_posts_title_lower__
   on TRUE
   where
@@ -32,3 +36,25 @@ lateral (
     )
   order by __relational_posts__."id" asc
 ) as __relational_posts_result__
+
+select __people_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __people_identifiers__,
+lateral (
+  select
+    __people__."person_id"::text as "0",
+    __people__."username"::text as "1",
+    __people_identifiers__.idx as "2"
+  from interfaces_and_unions.people as __people__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __people__."person_id" = __people_identifiers__."id0"
+    )
+  order by __people__."person_id" asc
+) as __people_result__
