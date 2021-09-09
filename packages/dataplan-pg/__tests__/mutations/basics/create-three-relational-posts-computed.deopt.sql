69ac2652d979e6910a62582f7f134d451f948e8a
Benjie Gillam <benjie@jemjie.com>
2021-09-09 18:43:01 +0100

    Test that mutations work right with pgSelect

---
+++ packages/dataplan-pg/__tests__/mutations/basics/create-three-relational-posts-computed.deopt.sql
@@ -0,0 +1,188 @@
+select __relational_posts_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0",
+    (ids.value->>1)::"text" as "id1"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_posts_identifiers__,
+lateral (
+  select
+    __relational_posts_identifiers__.idx as "0"
+  from interfaces_and_unions.insert_post(__relational_posts_identifiers__."id0", __relational_posts_identifiers__."id1") as __relational_posts__
+  where (
+    true /* authorization checks */
+  )
+  order by __relational_posts__."id" asc
+) as __relational_posts_result__
+
+select __relational_posts_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0",
+    (ids.value->>1)::"text" as "id1"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_posts_identifiers__,
+lateral (
+  select
+    __relational_posts_identifiers__.idx as "0"
+  from interfaces_and_unions.insert_post(__relational_posts_identifiers__."id0", __relational_posts_identifiers__."id1") as __relational_posts__
+  where (
+    true /* authorization checks */
+  )
+  order by __relational_posts__."id" asc
+) as __relational_posts_result__
+
+select __relational_posts_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0",
+    (ids.value->>1)::"text" as "id1"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_posts_identifiers__,
+lateral (
+  select
+    __relational_posts_identifiers__.idx as "0"
+  from interfaces_and_unions.insert_post(__relational_posts_identifiers__."id0", __relational_posts_identifiers__."id1") as __relational_posts__
+  where (
+    true /* authorization checks */
+  )
+  order by __relational_posts__."id" asc
+) as __relational_posts_result__
+
+select __relational_items_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_items_identifiers__,
+lateral (
+  select
+    __relational_items__."type"::text as "0",
+    __relational_items__."id"::text as "1",
+    __relational_items_identifiers__.idx as "2"
+  from interfaces_and_unions.relational_items as __relational_items__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __relational_items__."id" = __relational_items_identifiers__."id0"
+    )
+  order by __relational_items__."id" asc
+) as __relational_items_result__
+
+select __relational_items_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_items_identifiers__,
+lateral (
+  select
+    __relational_items__."type"::text as "0",
+    __relational_items__."id"::text as "1",
+    __relational_items_identifiers__.idx as "2"
+  from interfaces_and_unions.relational_items as __relational_items__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __relational_items__."id" = __relational_items_identifiers__."id0"
+    )
+  order by __relational_items__."id" asc
+) as __relational_items_result__
+
+select __relational_items_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_items_identifiers__,
+lateral (
+  select
+    __relational_items__."type"::text as "0",
+    __relational_items__."id"::text as "1",
+    __relational_items_identifiers__.idx as "2"
+  from interfaces_and_unions.relational_items as __relational_items__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __relational_items__."id" = __relational_items_identifiers__."id0"
+    )
+  order by __relational_items__."id" asc
+) as __relational_items_result__
+
+select __relational_posts_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_posts_identifiers__,
+lateral (
+  select
+    __relational_posts__."title"::text as "0",
+    __relational_posts__."description"::text as "1",
+    __relational_posts__."note"::text as "2",
+    __relational_posts_identifiers__.idx as "3"
+  from interfaces_and_unions.relational_posts as __relational_posts__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __relational_posts__."id" = __relational_posts_identifiers__."id0"
+    )
+  order by __relational_posts__."id" asc
+) as __relational_posts_result__
+
+select __relational_posts_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_posts_identifiers__,
+lateral (
+  select
+    __relational_posts__."title"::text as "0",
+    __relational_posts__."description"::text as "1",
+    __relational_posts__."note"::text as "2",
+    __relational_posts_identifiers__.idx as "3"
+  from interfaces_and_unions.relational_posts as __relational_posts__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __relational_posts__."id" = __relational_posts_identifiers__."id0"
+    )
+  order by __relational_posts__."id" asc
+) as __relational_posts_result__
+
+select __relational_posts_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_posts_identifiers__,
+lateral (
+  select
+    __relational_posts__."title"::text as "0",
+    __relational_posts__."description"::text as "1",
+    __relational_posts__."note"::text as "2",
+    __relational_posts_identifiers__.idx as "3"
+  from interfaces_and_unions.relational_posts as __relational_posts__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __relational_posts__."id" = __relational_posts_identifiers__."id0"
+    )
+  order by __relational_posts__."id" asc
+) as __relational_posts_result__
