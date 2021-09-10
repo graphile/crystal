73ac943f567d102499ee125bce7ba2d84d5121ab
Benjie Gillam <benjie@jemjie.com>
2021-09-10 17:36:37 +0100

    Tests passing for defer

---
+++ packages/dataplan-pg/__tests__/queries/conditions/archived-forum-inherited-messages.defer-4.sql
@@ -0,0 +1,51 @@
+select
+  __forums__."name"::text as "0",
+  array(
+    select array[
+      __messages__."body"::text,
+      __messages__."author_id"::text,
+      __messages__."id"::text,
+      __users__."username"::text,
+      __users__."gravatar_url"::text
+    ]::text[]
+    from app_public.messages as __messages__
+    left outer join app_public.users as __users__
+    on (__messages__."author_id"::"uuid" = __users__."id")
+    where
+      (
+        (__messages__.archived_at is null) = (__forums__."archived_at" is null)
+      ) and (
+        __forums__."id"::"uuid" = __messages__."forum_id"
+      )
+    order by __messages__."id" asc
+  ) as "1"
+from app_public.forums as __forums__
+where
+  (
+    __forums__.archived_at is not null
+  ) and (
+    true /* authorization checks */
+  )
+order by __forums__."id" asc
+
+select __users_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"uuid" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __users_identifiers__,
+lateral (
+  select
+    __users__."username"::text as "0",
+    __users__."gravatar_url"::text as "1",
+    __users_identifiers__.idx as "2"
+  from app_public.users as __users__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __users__."id" = __users_identifiers__."id0"
+    )
+  order by __users__."id" asc
+) as __users_result__
