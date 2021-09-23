1cac900496a489dd8ecf31e907c8ee49e3c88642
Benjie Gillam <benjie@jemjie.com>
2021-09-23 16:39:07 +0100

    Update SQL snapshots to honour new groupId determinations

--- packages/dataplan-pg/__tests__/queries/conditions/archived-forum-inherited-messages.stream-2.sql
+++ packages/dataplan-pg/__tests__/queries/conditions/archived-forum-inherited-messages.stream-2.sql
@@ -1,20 +1,7 @@
 select
   __forums__."name"::text as "0",
-  array(
-    select array[
-      __messages__."body"::text,
-      __messages__."author_id"::text,
-      __messages__."id"::text
-    ]::text[]
-    from app_public.messages as __messages__
-    where
-      (
-        (__messages__.archived_at is null) = (__forums__."archived_at" is null)
-      ) and (
-        __forums__."id"::"uuid" = __messages__."forum_id"
-      )
-    order by __messages__."id" asc
-  ) as "1"
+  __forums__."id"::text as "1",
+  __forums__."archived_at"::text as "2"
 from app_public.forums as __forums__
 where
   (
@@ -24,24 +11,33 @@ where
   )
 order by __forums__."id" asc
 
-select __users_result__.*
+select __messages_result__.*
 from (
   select
     ids.ordinality - 1 as idx,
-    (ids.value->>0)::"uuid" as "id0"
+    (ids.value->>0)::"uuid" as "id0",
+    (ids.value->>1)::timestamptz as "id1"
   from json_array_elements($1::json) with ordinality as ids
-) as __users_identifiers__,
+) as __messages_identifiers__,
 lateral (
   select
-    __users__."username"::text as "0",
-    __users__."gravatar_url"::text as "1",
-    __users_identifiers__.idx as "2"
-  from app_public.users as __users__
+    __messages__."body"::text as "0",
+    __users__."username"::text as "1",
+    __users__."gravatar_url"::text as "2",
+    __messages__."id"::text as "3",
+    __users_2."username"::text as "4",
+    __users_2."gravatar_url"::text as "5",
+    __messages_identifiers__.idx as "6"
+  from app_public.messages as __messages__
+  left outer join app_public.users as __users__
+  on (__messages__."author_id"::"uuid" = __users__."id")
+  left outer join app_public.users as __users_2
+  on (__messages__."author_id"::"uuid" = __users_2."id")
   where
     (
-      true /* authorization checks */
+      (__messages__.archived_at is null) = (__messages_identifiers__."id1" is null)
     ) and (
-      __users__."id" = __users_identifiers__."id0"
+      __messages__."forum_id" = __messages_identifiers__."id0"
     )
-  order by __users__."id" asc
-) as __users_result__
+  order by __messages__."id" asc
+) as __messages_result__
