23afdee8694d8e1df47f3e4cd825fb73707b7fec
Benjie Gillam <benjie@jemjie.com>
2021-10-01 17:19:59 +0100

    @stream collections cannot be merged (due to not caching async iterators)

--- packages/dataplan-pg/__tests__/queries/conditions/archived-forum-inherited-messages.stream-2.deopt.sql
+++ packages/dataplan-pg/__tests__/queries/conditions/archived-forum-inherited-messages.stream-2.deopt.sql
@@ -23,7 +23,30 @@ lateral (
   select
     __messages__."body"::text as "0",
     __messages__."author_id"::text as "1",
-    __messages__."id"::text as "2",
+    __messages_identifiers__.idx as "2"
+  from app_public.messages as __messages__
+  where
+    (
+      (__messages__.archived_at is null) = (__messages_identifiers__."id1" is null)
+    ) and (
+      __messages__."forum_id" = __messages_identifiers__."id0"
+    )
+  order by __messages__."id" asc
+) as __messages_result__
+
+select __messages_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"uuid" as "id0",
+    (ids.value->>1)::timestamptz as "id1"
+  from json_array_elements($1::json) with ordinality as ids
+) as __messages_identifiers__,
+lateral (
+  select
+    __messages__."id"::text as "0",
+    __messages__."body"::text as "1",
+    __messages__."author_id"::text as "2",
     __messages_identifiers__.idx as "3"
   from app_public.messages as __messages__
   where
--- packages/dataplan-pg/__tests__/queries/conditions/archived-forum-inherited-messages.stream-2.sql
+++ packages/dataplan-pg/__tests__/queries/conditions/archived-forum-inherited-messages.stream-2.sql
@@ -24,15 +24,37 @@ lateral (
     __messages__."body"::text as "0",
     __users__."username"::text as "1",
     __users__."gravatar_url"::text as "2",
-    __messages__."id"::text as "3",
-    __users_2."username"::text as "4",
-    __users_2."gravatar_url"::text as "5",
-    __messages_identifiers__.idx as "6"
+    __messages_identifiers__.idx as "3"
   from app_public.messages as __messages__
   left outer join app_public.users as __users__
   on (__messages__."author_id"::"uuid" = __users__."id")
-  left outer join app_public.users as __users_2
-  on (__messages__."author_id"::"uuid" = __users_2."id")
+  where
+    (
+      (__messages__.archived_at is null) = (__messages_identifiers__."id1" is null)
+    ) and (
+      __messages__."forum_id" = __messages_identifiers__."id0"
+    )
+  order by __messages__."id" asc
+) as __messages_result__
+
+select __messages_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"uuid" as "id0",
+    (ids.value->>1)::timestamptz as "id1"
+  from json_array_elements($1::json) with ordinality as ids
+) as __messages_identifiers__,
+lateral (
+  select
+    __messages__."id"::text as "0",
+    __messages__."body"::text as "1",
+    __users__."username"::text as "2",
+    __users__."gravatar_url"::text as "3",
+    __messages_identifiers__.idx as "4"
+  from app_public.messages as __messages__
+  left outer join app_public.users as __users__
+  on (__messages__."author_id"::"uuid" = __users__."id")
   where
     (
       (__messages__.archived_at is null) = (__messages_identifiers__."id1" is null)
