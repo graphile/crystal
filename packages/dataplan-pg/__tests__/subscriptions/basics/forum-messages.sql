bd00c6343660abc4c16d1bfe7d1a2266091b6ad4
Benjie Gillam <benjie@jemjie.com>
2021-10-08 18:07:03 +0100

    Subscriptions test

---
+++ packages/dataplan-pg/__tests__/subscriptions/basics/forum-messages.sql
@@ -0,0 +1,65 @@
+select __messages_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"uuid" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __messages_identifiers__,
+lateral (
+  select
+    __messages__."id"::text as "0",
+    __messages__."featured"::text as "1",
+    __messages__."body"::text as "2",
+    (__messages__.archived_at is not null)::text as "3",
+    __forums__."name"::text as "4",
+    (__forums__.archived_at is not null)::text as "5",
+    __messages__."forum_id"::text as "6",
+    __users__."username"::text as "7",
+    __users__."gravatar_url"::text as "8",
+    __messages_identifiers__.idx as "9"
+  from app_public.messages as __messages__
+  left outer join app_public.forums as __forums__
+  on (__messages__."forum_id"::"uuid" = __forums__."id")
+  left outer join app_public.users as __users__
+  on (__messages__."author_id"::"uuid" = __users__."id")
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __messages__."id" = __messages_identifiers__."id0"
+    )
+  order by __messages__."id" asc
+) as __messages_result__
+
+select __messages_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"uuid" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __messages_identifiers__,
+lateral (
+  select
+    __messages__."id"::text as "0",
+    __messages__."featured"::text as "1",
+    __messages__."body"::text as "2",
+    (__messages__.archived_at is not null)::text as "3",
+    __forums__."name"::text as "4",
+    (__forums__.archived_at is not null)::text as "5",
+    __messages__."forum_id"::text as "6",
+    __users__."username"::text as "7",
+    __users__."gravatar_url"::text as "8",
+    __messages_identifiers__.idx as "9"
+  from app_public.messages as __messages__
+  left outer join app_public.forums as __forums__
+  on (__messages__."forum_id"::"uuid" = __forums__."id")
+  left outer join app_public.users as __users__
+  on (__messages__."author_id"::"uuid" = __users__."id")
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __messages__."id" = __messages_identifiers__."id0"
+    )
+  order by __messages__."id" asc
+) as __messages_result__
