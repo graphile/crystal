8a56c3972b6d878ced57a353eac608844c1694cf
Benjie Gillam <benjie@jemjie.com>
2021-10-12 17:40:07 +0100

    Snapshots for test that now passes

---
+++ packages/dataplan-pg/__tests__/queries/interfaces-relational/commentables-simple.deopt.sql
@@ -0,0 +1,33 @@
+select
+  __relational_commentables__."type"::text as "0",
+  __relational_commentables__."id"::text as "1"
+from interfaces_and_unions.relational_commentables as __relational_commentables__
+where (
+  true /* authorization checks */
+)
+order by __relational_commentables__.id asc
+limit 1
+
+select __relational_posts_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_posts_identifiers__,
+lateral (
+  select
+    __relational_items__."type"::text as "0",
+    __relational_items__."position"::text as "1",
+    __relational_posts_identifiers__.idx as "2"
+  from interfaces_and_unions.relational_posts as __relational_posts__
+  left outer join interfaces_and_unions.relational_items as __relational_items__
+  on (__relational_posts__."id"::"int4" = __relational_items__."id")
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __relational_posts__."id" = __relational_posts_identifiers__."id0"
+    )
+  order by __relational_posts__."id" asc
+) as __relational_posts_result__
