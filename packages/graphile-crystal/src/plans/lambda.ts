fac9964bebd1ebf934d34c4621402356158ad6cc
Benjie Gillam <benjie@jemjie.com>
2021-11-10 14:46:50 +0000

    Allow lambda to be passed null/undefined for a root-less plan

--- packages/graphile-crystal/src/plans/lambda.ts
+++ packages/graphile-crystal/src/plans/lambda.ts
@@ -2,19 +2,27 @@ import type { CrystalResultsList, CrystalValuesList } from "../interfaces";
 import { ExecutablePlan } from "../plan";
 
 export class LambdaPlan<TIn, TOut> extends ExecutablePlan<TOut> {
-  private planId: number;
-  constructor($plan: ExecutablePlan<TIn>, private fn: (value: TIn) => TOut) {
+  private planId: number | null;
+  constructor(
+    $plan: ExecutablePlan<TIn> | null | undefined,
+    private fn: (value: TIn) => TOut,
+  ) {
     super();
-    this.planId = this.addDependency($plan);
+    this.planId = $plan != null ? this.addDependency($plan) : null;
   }
 
   execute(values: CrystalValuesList<[TIn]>): CrystalResultsList<TOut> {
-    return values.map((v) => this.fn(v[this.planId]));
+    const { planId } = this;
+    if (planId != null) {
+      return values.map((v) => this.fn(v[planId]));
+    } else {
+      return values.map(() => this.fn(undefined as any));
+    }
   }
 }
 
 export function lambda<TIn, TOut>(
-  $plan: ExecutablePlan<TIn>,
+  $plan: ExecutablePlan<TIn> | null | undefined,
   fn: (value: TIn) => TOut,
 ): LambdaPlan<TIn, TOut> {
   return new LambdaPlan<TIn, TOut>($plan, fn);
