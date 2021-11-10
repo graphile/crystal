2b8dcbb8e32829a6d5921cba6e09bd905122b5c3
Benjie Gillam <benjie@jemjie.com>
2021-11-10 15:01:27 +0000

    Example schema running plans through crystal

---
+++ packages/graphile-build/examples/README-1.ts
@@ -0,0 +1,80 @@
+import { lambda } from "graphile-crystal";
+import { graphql, printSchema } from "graphql";
+
+import { buildSchema, defaultPlugins } from "../src";
+
+declare global {
+  namespace GraphileEngine {
+    interface GraphileBuildOptions {
+      myDefaultMin?: number;
+      myDefaultMax?: number;
+    }
+  }
+}
+
+// Create a simple plugin that adds a random field to every GraphQLObject
+const MyRandomFieldPlugin: GraphileEngine.Plugin = (
+  builder,
+  { myDefaultMin = 1, myDefaultMax = 100 },
+) => {
+  builder.hook("GraphQLObjectType:fields", (fields, build, context) => {
+    const {
+      extend,
+      graphql: { GraphQLInt },
+    } = build;
+    const { Self } = context;
+    return extend<typeof fields, typeof fields>(
+      fields,
+      {
+        random: {
+          type: GraphQLInt,
+          args: {
+            sides: {
+              type: GraphQLInt,
+            },
+          },
+          plan(_$parent, args) {
+            return lambda(
+              args.sides,
+              (sides = myDefaultMax) =>
+                Math.floor(Math.random() * (sides + 1 - myDefaultMin)) +
+                myDefaultMin,
+            );
+          },
+        },
+      },
+      `adding 'random' field to ${Self.name}`,
+    );
+  });
+};
+
+(async function () {
+  // Create our GraphQL schema by applying all the plugins
+  const schema = await buildSchema([...defaultPlugins, MyRandomFieldPlugin], {
+    // ... options
+    myDefaultMin: 1,
+    myDefaultMax: 6,
+  });
+
+  // Output our schema
+  console.log(printSchema(schema));
+  console.log();
+  console.log();
+  console.log();
+
+  // Run our query
+  const result = await graphql({
+    schema,
+    source: `
+      query {
+        random
+      }
+    `,
+    rootValue: null,
+    variableValues: {},
+  });
+  console.log(result); // { data: { random: 4 } }
+})().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
