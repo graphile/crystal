7a3b8cb29de6c3c1cf95306faa1b7f89d1188c4e
Benjie Gillam <benjie@jemjie.com>
2021-11-11 20:49:17 +0000

    moduleResolution: 'node12' enables native import without transpiling

--- packages/dataplan-pg/__tests__/exportExampleSchema.ts
+++ packages/dataplan-pg/__tests__/exportExampleSchema.ts
@@ -1,7 +1,5 @@
 import { makeExampleSchema } from "../src/examples/exampleSchema";
 
-(
-  eval("import('graphile-build')") as Promise<typeof import("graphile-build")>
-).then(({ exportSchema }) => {
+import("graphile-build").then(({ exportSchema }) => {
   exportSchema(makeExampleSchema(), `${__dirname}/../exampleSchemaExport.ts`);
 });
