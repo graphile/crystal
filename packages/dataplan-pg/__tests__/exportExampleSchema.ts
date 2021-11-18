5d7e6e3614f96aaf3b33c804ec2c4e8e465c2851
Benjie Gillam <benjie@jemjie.com>
2021-11-18 16:33:10 +0000

    Simplify import script

--- packages/dataplan-pg/__tests__/exportExampleSchema.ts
+++ packages/dataplan-pg/__tests__/exportExampleSchema.ts
@@ -1,5 +1,5 @@
+import { exportSchema } from "graphile-exporter";
+
 import { makeExampleSchema } from "../src/examples/exampleSchema";
 
-import("graphile-exporter").then(({ exportSchema }) => {
-  exportSchema(makeExampleSchema(), `${__dirname}/../exampleSchemaExport.ts`);
-});
+exportSchema(makeExampleSchema(), `${__dirname}/../exampleSchemaExport.ts`);
