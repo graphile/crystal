4a524bddfb42c0f76c29a47276fd7bbfc81775a4
Benjie Gillam <benjie@jemjie.com>
2021-11-19 18:43:37 +0000

    Move functions to common

--- packages/eslint-plugin-graphile-exporter/src/ExhaustiveDeps.ts
+++ packages/eslint-plugin-graphile-exporter/src/ExhaustiveDeps.ts
@@ -22,7 +22,7 @@ import type {
   Super as ESTreeSuper,
 } from "estree";
 
-import { reportProblem } from "./common";
+import { fastFindReferenceWithParent, reportProblem } from "./common";
 
 interface CommonOptions {
   disableAutofix: boolean;
@@ -768,60 +768,6 @@ function getScopesCallbackIndex(
   }
 }
 
-/**
- * ESLint won't assign node.parent to references from context.getScope()
- *
- * So instead we search for the node from an ancestor assigning node.parent
- * as we go. This mutates the AST.
- *
- * This traversal is:
- * - optimized by only searching nodes with a range surrounding our target node
- * - agnostic to AST node types, it looks for `{ type: string, ... }`
- */
-function fastFindReferenceWithParent(
-  start: Node & Rule.NodeParentExtension,
-  target: Node,
-) {
-  const queue: Array<Node & Rule.NodeParentExtension> = [start];
-  let item: (Node & Rule.NodeParentExtension) | null = null;
-
-  while (queue.length) {
-    item = queue.shift()!;
-    const definitelyItem = item;
-
-    if (isSameIdentifier(definitelyItem, target)) {
-      return definitelyItem;
-    }
-
-    if (!isAncestorNodeOf(definitelyItem, target)) {
-      continue;
-    }
-
-    for (const [key, value] of Object.entries(definitelyItem)) {
-      if (key === "parent") {
-        continue;
-      }
-      if (isNodeLike(value)) {
-        const valueWithParent = Object.assign(value, {
-          parent: definitelyItem,
-        });
-        queue.push(valueWithParent);
-      } else if (Array.isArray(value)) {
-        value.forEach((val) => {
-          if (isNodeLike(val)) {
-            const valWithParent = Object.assign(val, {
-              parent: definitelyItem,
-            });
-            queue.push(valWithParent);
-          }
-        });
-      }
-    }
-  }
-
-  return null;
-}
-
 function joinEnglish(arr: string[]) {
   let s = "";
   for (let i = 0; i < arr.length; i++) {
@@ -835,27 +781,4 @@ function joinEnglish(arr: string[]) {
     }
   }
   return s;
-}
-
-function isNodeLike(val: unknown): val is Node {
-  return (
-    typeof val === "object" &&
-    val !== null &&
-    !Array.isArray(val) &&
-    typeof (val as any).type === "string"
-  );
-}
-
-function isSameIdentifier(a: any, b: any): a is typeof b {
-  return (
-    (a.type === "Identifier" || a.type === "JSXIdentifier") &&
-    a.type === b.type &&
-    a.name === b.name &&
-    a.range[0] === b.range[0] &&
-    a.range[1] === b.range[1]
-  );
-}
-
-function isAncestorNodeOf(a: Node & Rule.NodeParentExtension, b: Node) {
-  return a.range![0] <= b.range![0] && a.range![1] >= b.range![1];
 }
--- packages/eslint-plugin-graphile-exporter/src/common.ts
+++ packages/eslint-plugin-graphile-exporter/src/common.ts
@@ -1,4 +1,4 @@
-import type { Expression } from "@babel/types";
+import type { Expression, Node } from "@babel/types";
 import type { Rule } from "eslint";
 import type { Expression as ESTreeExpression } from "estree";
 
@@ -26,4 +26,81 @@ export function reportProblem(
     }
   }
   context.report(problem);
+}
+
+/**
+ * ESLint won't assign node.parent to references from context.getScope()
+ *
+ * So instead we search for the node from an ancestor assigning node.parent
+ * as we go. This mutates the AST.
+ *
+ * This traversal is:
+ * - optimized by only searching nodes with a range surrounding our target node
+ * - agnostic to AST node types, it looks for `{ type: string, ... }`
+ */
+export function fastFindReferenceWithParent(
+  start: Node & Rule.NodeParentExtension,
+  target: Node,
+) {
+  const queue: Array<Node & Rule.NodeParentExtension> = [start];
+  let item: (Node & Rule.NodeParentExtension) | null = null;
+
+  while (queue.length) {
+    item = queue.shift()!;
+    const definitelyItem = item;
+
+    if (isSameIdentifier(definitelyItem, target)) {
+      return definitelyItem;
+    }
+
+    if (!isAncestorNodeOf(definitelyItem, target)) {
+      continue;
+    }
+
+    for (const [key, value] of Object.entries(definitelyItem)) {
+      if (key === "parent") {
+        continue;
+      }
+      if (isNodeLike(value)) {
+        const valueWithParent = Object.assign(value, {
+          parent: definitelyItem,
+        });
+        queue.push(valueWithParent);
+      } else if (Array.isArray(value)) {
+        value.forEach((val) => {
+          if (isNodeLike(val)) {
+            const valWithParent = Object.assign(val, {
+              parent: definitelyItem,
+            });
+            queue.push(valWithParent);
+          }
+        });
+      }
+    }
+  }
+
+  return null;
+}
+
+function isSameIdentifier(a: any, b: any): a is typeof b {
+  return (
+    (a.type === "Identifier" || a.type === "JSXIdentifier") &&
+    a.type === b.type &&
+    a.name === b.name &&
+    a.range[0] === b.range[0] &&
+    a.range[1] === b.range[1]
+  );
+}
+
+function isNodeLike(val: unknown): val is Node {
+  return (
+    typeof val === "object" &&
+    val !== null &&
+    !Array.isArray(val) &&
+    typeof (val as any).type === "string"
+  );
+}
+
+function isAncestorNodeOf(a: Node & Rule.NodeParentExtension, b: Node) {
+  return a.range![0] <= b.range![0] && a.range![1] >= b.range![1];
 }
