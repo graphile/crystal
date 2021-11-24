6bb15a5f56ea48ad9f5f34f4e1f8395337629820
Benjie Gillam <benjie@jemjie.com>
2021-11-24 14:17:39 +0000

    Apply fixes from ansi-regex

--- packages/graphile-crystal/src/stripAnsi.ts
+++ packages/graphile-crystal/src/stripAnsi.ts
@@ -3,7 +3,7 @@
 // Copyright (c) Benjie (https://twitter.com/benjie)
 export const ANSI_REGEXP =
   // eslint-disable-next-line no-control-regex, no-useless-escape
-  /[\u001B\u009B][[\]()#;?]*(?:(?:(?:[a-zA-Z\d]*(?:;[-a-zA-Z\d\/#&.:=?%@~_]*)*)?\u0007)|(?:(?:\d{1,4}(?:;\d{0,4})*)?[\dA-PR-TZcf-ntqry=><~]))/g;
+  /[\u001B\u009B][[\]()#;?]*(?:(?:(?:(?:;[-a-zA-Z\d\/#&.:=?%@~_]+)*|[a-zA-Z\d]+(?:;[-a-zA-Z\d\/#&.:=?%@~_]*)*)?\u0007)|(?:(?:\d{1,4}(?:;\d{0,4})*)?[\dA-PR-TZcf-nq-uy=><~]))/g;
 export function stripAnsi(str: string): string {
   return str.replace(ANSI_REGEXP, "");
 }
