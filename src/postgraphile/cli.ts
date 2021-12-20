d8401c6e143a2680c092d8ad077f8469f2f02625
Max Smolin <maximsmol@gmail.com>
2021-12-20 03:11:05 -0800

    fix(cli): handling of SSL options from the connection string (#1564)
    Co-authored-by: Benjie Gillam <benjie@jemjie.com>

--- src/postgraphile/cli.ts
+++ src/postgraphile/cli.ts
@@ -572,7 +572,12 @@ const coerce = (o: ReturnType<typeof parsePgConnectionString>): PoolConfig => {
   return {
     ...o,
     application_name: o['application_name'] || undefined,
-    ssl: o.ssl != null ? !!o.ssl : undefined,
+    ssl:
+      o.ssl == null
+        ? undefined
+        : (o.ssl as any).rejectUnauthorized == null
+        ? !!o.ssl
+        : (o.ssl as any),
     user: typeof o.user === 'string' ? o.user : undefined,
     database: typeof o.database === 'string' ? o.database : undefined,
     password: typeof o.password === 'string' ? o.password : undefined,
