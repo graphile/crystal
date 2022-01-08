a686b695aaa45e23759a127bee76623dad36d770
Benjie Gillam <benjie@jemjie.com>
2022-01-08 15:20:10 +0000

    Test before/last works even with no fetching of the nodes

---
+++ packages/dataplan-pg/__tests__/queries/connections/pagination-before-last-pagination-only.sql
@@ -0,0 +1,31 @@
+select
+  (count(*))::text as "0"
+from app_public.messages as __messages__
+where
+  (
+    __messages__.archived_at is null
+  ) and (
+    true /* authorization checks */
+  )
+
+
+select __messages_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"uuid" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __messages_identifiers__,
+lateral (
+  select
+    __messages_identifiers__.idx as "0"
+  from app_public.messages as __messages__
+  where
+    (
+      __messages__.archived_at is null
+    ) and (__messages__."id" < __messages_identifiers__."id0") and (
+      true /* authorization checks */
+    )
+  order by __messages__."id" desc
+  limit 4
+) as __messages_result__
