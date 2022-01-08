f66e89b28f547ef10bd8314f80a0302badfd872b
Benjie Gillam <benjie@jemjie.com>
2022-01-08 18:17:30 +0000

    Refactor

--- packages/dataplan-pg/src/plans/pgSelect.ts
+++ packages/dataplan-pg/src/plans/pgSelect.ts
@@ -2276,20 +2276,18 @@ lateral (${sql.indent(wrappedInnerQuery)}) as ${wrapperAlias}`;
           const limit = this.fetchOneExtra ? this.first ?? this.last : null;
           const rowsPlan = access<any[]>(parent2, [selfIndex]);
           const shouldReverse = this.shouldReverseOrder();
-          const orderedPlan = shouldReverse ? reverse(rowsPlan) : rowsPlan;
           if (this.fetchOneExtra && limit != null) {
-            return lambda(orderedPlan, (rows) => {
+            return lambda(rowsPlan, (rows) => {
               const hasMore = rows.length > limit;
-              const slicedRows =
-                shouldReverse && hasMore
-                  ? rows.slice(1)
-                  : hasMore
-                  ? rows.slice(0, limit)
-                  : rows;
-              Object.defineProperty(slicedRows, "hasMore", { value: hasMore });
-              return slicedRows;
+              const slicedRows = hasMore ? rows.slice(0, limit) : rows;
+              const orderedRows = shouldReverse
+                ? slicedRows.reverse()
+                : slicedRows;
+              Object.defineProperty(orderedRows, "hasMore", { value: hasMore });
+              return orderedRows;
             });
           } else {
+            const orderedPlan = shouldReverse ? reverse(rowsPlan) : rowsPlan;
             return orderedPlan;
           }
         }
