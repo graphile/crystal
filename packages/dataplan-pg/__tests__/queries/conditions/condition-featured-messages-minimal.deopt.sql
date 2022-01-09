8144cdca333b845afad342aff7de62d94ef38f28
Benjie Gillam <benjie@jemjie.com>
2022-01-09 17:06:08 +0000

    Tests for minimal reproduction of duplicate identifier (solved)

---
+++ packages/dataplan-pg/__tests__/queries/conditions/condition-featured-messages-minimal.deopt.sql
@@ -0,0 +1,60 @@
+select
+  __forums__."id"::text as "0",
+  __forums__."archived_at"::text as "1"
+from app_public.forums as __forums__
+where
+  (
+    __forums__.archived_at is null
+  ) and (
+    true /* authorization checks */
+  )
+order by __forums__."id" asc
+
+select __messages_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"uuid" as "id0",
+    (ids.value->>1)::boolean as "id1",
+    (ids.value->>2)::timestamptz as "id2"
+  from json_array_elements($1::json) with ordinality as ids
+) as __messages_identifiers__,
+lateral (
+  select
+    (count(*))::text as "0",
+    __messages_identifiers__.idx as "1"
+  from app_public.messages as __messages__
+  where
+    (
+      (__messages__.archived_at is null) = (__messages_identifiers__."id2" is null)
+    ) and (
+      __messages__.featured = __messages_identifiers__."id1"
+    ) and (
+      __messages__."forum_id" = __messages_identifiers__."id0"
+    )
+) as __messages_result__
+
+select __messages_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"uuid" as "id0",
+    (ids.value->>1)::boolean as "id1",
+    (ids.value->>2)::timestamptz as "id2"
+  from json_array_elements($1::json) with ordinality as ids
+) as __messages_identifiers__,
+lateral (
+  select
+    __messages_identifiers__.idx as "0"
+  from app_public.messages as __messages__
+  where
+    (
+      (__messages__.archived_at is null) = (__messages_identifiers__."id2" is null)
+    ) and (
+      __messages__.featured = __messages_identifiers__."id1"
+    ) and (
+      __messages__."forum_id" = __messages_identifiers__."id0"
+    )
+  order by __messages__."id" asc
+  limit 6
+) as __messages_result__
---
+++ packages/dataplan-pg/__tests__/queries/conditions/condition-featured-messages-minimal.sql
@@ -0,0 +1,47 @@
+select __forums_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::boolean as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __forums_identifiers__,
+lateral (
+  select
+    array(
+      select array['' /* NOTHING?! */]::text[]
+      from app_public.messages as __messages__
+      where
+        (
+          (__messages__.archived_at is null) = (__forums__."archived_at" is null)
+        ) and (
+          __messages__.featured = __forums_identifiers__."id0"
+        ) and (
+          __forums__."id"::"uuid" = __messages__."forum_id"
+        )
+      order by __messages__."id" asc
+      limit 6
+    ) as "0",
+    array(
+      select array[
+        (count(*))::text
+      ]::text[]
+      from app_public.messages as __messages__
+      where
+        (
+          (__messages__.archived_at is null) = (__forums__."archived_at" is null)
+        ) and (
+          __messages__.featured = __forums_identifiers__."id0"
+        ) and (
+          __forums__."id"::"uuid" = __messages__."forum_id"
+        )
+    ) as "1",
+    __forums_identifiers__.idx as "2"
+  from app_public.forums as __forums__
+  where
+    (
+      __forums__.archived_at is null
+    ) and (
+      true /* authorization checks */
+    )
+  order by __forums__."id" asc
+) as __forums_result__
