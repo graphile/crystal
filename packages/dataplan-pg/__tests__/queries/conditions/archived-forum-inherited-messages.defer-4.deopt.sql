1b7aae29a5dbb584772387ce8ea2946a9daeb513
Benjie Gillam <benjie@jemjie.com>
2022-02-18 18:21:39 +0000

    Fewer queries needed for deferred test

--- packages/dataplan-pg/__tests__/queries/conditions/archived-forum-inherited-messages.defer-4.deopt.sql
+++ packages/dataplan-pg/__tests__/queries/conditions/archived-forum-inherited-messages.defer-4.deopt.sql
@@ -35,6 +35,27 @@ lateral (
   order by __messages__."id" asc
 ) as __messages_result__
 
+select __messages_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"uuid" as "id0",
+    (ids.value->>1)::"timestamptz" as "id1"
+  from json_array_elements($1::json) with ordinality as ids
+) as __messages_identifiers__,
+lateral (
+  select
+    (count(*))::text as "0",
+    __messages_identifiers__.idx as "1"
+  from app_public.messages as __messages__
+  where
+    (
+      (__messages__.archived_at is null) = (__messages_identifiers__."id1" is null)
+    ) and (
+      __messages__."forum_id" = __messages_identifiers__."id0"
+    )
+) as __messages_result__
+
 select __users_result__.*
 from (
   select
@@ -56,24 +77,3 @@ lateral (
     )
   order by __users__."id" asc
 ) as __users_result__
-
-select __messages_result__.*
-from (
-  select
-    ids.ordinality - 1 as idx,
-    (ids.value->>0)::"uuid" as "id0",
-    (ids.value->>1)::"timestamptz" as "id1"
-  from json_array_elements($1::json) with ordinality as ids
-) as __messages_identifiers__,
-lateral (
-  select
-    (count(*))::text as "0",
-    __messages_identifiers__.idx as "1"
-  from app_public.messages as __messages__
-  where
-    (
-      (__messages__.archived_at is null) = (__messages_identifiers__."id1" is null)
-    ) and (
-      __messages__."forum_id" = __messages_identifiers__."id0"
-    )
-) as __messages_result__
--- packages/dataplan-pg/__tests__/queries/conditions/archived-forum-inherited-messages.defer-4.sql
+++ packages/dataplan-pg/__tests__/queries/conditions/archived-forum-inherited-messages.defer-4.sql
@@ -3,10 +3,9 @@ select
   (select json_agg(_._) from (
     select json_build_array(
       __messages__."body",
-      __messages__."author_id",
-      __messages__."id",
       __users__."username",
-      __users__."gravatar_url"
+      __users__."gravatar_url",
+      __messages__."id"
     ) as _
     from app_public.messages as __messages__
     left outer join app_public.users as __users__
@@ -39,25 +38,3 @@ where
     true /* authorization checks */
   )
 order by __forums__."id" asc
-
-select __users_result__.*
-from (
-  select
-    ids.ordinality - 1 as idx,
-    (ids.value->>0)::"uuid" as "id0"
-  from json_array_elements($1::json) with ordinality as ids
-) as __users_identifiers__,
-lateral (
-  select
-    __users__."username" as "0",
-    __users__."gravatar_url" as "1",
-    __users_identifiers__.idx as "2"
-  from app_public.users as __users__
-  where
-    (
-      true /* authorization checks */
-    ) and (
-      __users__."id" = __users_identifiers__."id0"
-    )
-  order by __users__."id" asc
-) as __users_result__
