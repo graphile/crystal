ab9efc931333e5ec903ffa0f8f667c2e50b56f65
Benjie Gillam <benjie@jemjie.com>
2022-02-22 10:20:39 +0000

    Add test for transforms after defer

---
+++ packages/dataplan-pg/__tests__/queries/functions/computed-column-forums-messages-with-many-transforms.defer.deopt.sql
@@ -0,0 +1,22 @@
+select
+  __forums__."name" as "0",
+  __forums__."id" as "1"
+from app_public.forums as __forums__
+where
+  (
+    __forums__.archived_at is null
+  ) and (
+    true /* authorization checks */
+  )
+order by __forums__."id" asc
+
+select
+  __messages__."body" as "0",
+  __messages__."featured"::text as "1",
+  __messages__."id" as "2",
+  __messages__."forum_id" as "3"
+from app_public.messages as __messages__
+where (
+  true /* authorization checks */
+)
+order by __messages__."id" asc
