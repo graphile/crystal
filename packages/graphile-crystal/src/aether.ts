d5f9b7823bab634757f6ca11e2a3a31c3a2cfc39
Benjie Gillam <benjie@jemjie.com>
2022-02-24 18:03:46 +0000

    Hoist CrystalError class

--- packages/graphile-crystal/src/aether.ts
+++ packages/graphile-crystal/src/aether.ts
@@ -358,6 +358,24 @@ interface BucketDefinition {
   groupId?: number;
 }
 
+// IMPORTANT: this WILL NOT WORK when compiled down to ES5. It requires ES6+
+// native class support.
+/**
+ * When an error occurs during plan execution we wrap it in a CrystalError so
+ * that we can pass it around as a value.  It gets unwrapped and thrown in the
+ * crystal resolver.
+ *
+ * @internal
+ */
+export class CrystalError extends Error {
+  public readonly originalError: Error;
+  constructor(originalError: Error) {
+    const message = originalError?.message;
+    super(message ? `CrystalError: ${message}` : `CrystalError`);
+    this.originalError = originalError;
+  }
+}
+
 interface GroupAndChildren extends Group {
   id: number;
   parent: GroupAndChildren | null;
@@ -5205,24 +5223,6 @@ export function populateValuePlan(
 
 function isNotNullish<T>(a: T | null | undefined): a is T {
   return a != null;
-}
-
-// IMPORTANT: this WILL NOT WORK when compiled down to ES5. It requires ES6+
-// native class support.
-/**
- * When an error occurs during plan execution we wrap it in a CrystalError so
- * that we can pass it around as a value.  It gets unwrapped and thrown in the
- * crystal resolver.
- *
- * @internal
- */
-export class CrystalError extends Error {
-  public readonly originalError: Error;
-  constructor(originalError: Error) {
-    const message = originalError?.message;
-    super(message ? `CrystalError: ${message}` : `CrystalError`);
-    this.originalError = originalError;
-  }
 }
 
 function treeNodePath(
