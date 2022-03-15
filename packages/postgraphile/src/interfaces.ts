512cda46c8822e5d208bab574f3480dc9227e102
Benjie Gillam <benjie@jemjie.com>
2022-03-15 16:44:15 +0000

    pgSettings server configuration

--- packages/postgraphile/src/interfaces.ts
+++ packages/postgraphile/src/interfaces.ts
@@ -1,5 +1,6 @@
 import type { ResolvedPreset } from "graphile-plugin";
 import type { GraphQLSchema } from "graphql";
+import type { IncomingMessage } from "http";
 
 export type ContextCallback = () => object;
 
@@ -12,6 +13,9 @@ export interface SchemaResult {
 export interface ServerOptions {
   /** Port number to listen on */
   port?: number;
+
+  /** Return settings to set in the session */
+  pgSettings?: (req: IncomingMessage) => object;
 }
 
 declare module "graphile-plugin" {
