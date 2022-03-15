d3c478c00fc97ec4e3d5c1baaa43f16bd0b92aae
Benjie Gillam <benjie@jemjie.com>
2022-03-15 13:30:31 +0000

    Export plugins (and thus global interface extensions)

--- packages/graphile-build-pg/src/index.ts
+++ packages/graphile-build-pg/src/index.ts
@@ -1,3 +1,29 @@
 export { defaultPreset } from "./preset";
-
 export const version = require("../package.json").version;
+
+export { PgAllRowsPlugin } from "./plugins/PgAllRowsPlugin";
+export { PgBasicsPlugin } from "./plugins/PgBasicsPlugin";
+export { PgCodecsPlugin } from "./plugins/PgCodecsPlugin";
+export { PgColumnDeprecationPlugin } from "./plugins/PgColumnDeprecationPlugin";
+export { PgColumnsPlugin } from "./plugins/PgColumnsPlugin";
+export { PgConditionArgumentPlugin } from "./plugins/PgConditionArgumentPlugin";
+export { PgConditionCustomFieldsPlugin } from "./plugins/PgConditionCustomFieldsPlugin";
+export { PgConnectionArgFirstLastBeforeAfterPlugin } from "./plugins/PgConnectionArgFirstLastBeforeAfterPlugin";
+export { PgConnectionArgOrderByDefaultValuePlugin } from "./plugins/PgConnectionArgOrderByDefaultValuePlugin";
+export { PgConnectionArgOrderByPlugin } from "./plugins/PgConnectionArgOrderByPlugin";
+export { PgConnectionTotalCountPlugin } from "./plugins/PgConnectionTotalCountPlugin";
+export { PgCustomTypeFieldPlugin } from "./plugins/PgCustomTypeFieldPlugin";
+export { PgIntrospectionPlugin } from "./plugins/PgIntrospectionPlugin";
+export { PgJWTPlugin } from "./plugins/PgJWTPlugin";
+export { PgMutationCreatePlugin } from "./plugins/PgMutationCreatePlugin";
+export { PgMutationPayloadEdgePlugin } from "./plugins/PgMutationPayloadEdgePlugin";
+export { PgMutationUpdateDeletePlugin } from "./plugins/PgMutationUpdateDeletePlugin";
+export { PgOrderAllColumnsPlugin } from "./plugins/PgOrderAllColumnsPlugin";
+export { PgOrderByPrimaryKeyPlugin } from "./plugins/PgOrderByPrimaryKeyPlugin";
+export { PgOrderCustomFieldsPlugin } from "./plugins/PgOrderCustomFieldsPlugin";
+export { PgProceduresPlugin } from "./plugins/PgProceduresPlugin";
+export { PgRelationsPlugin } from "./plugins/PgRelationsPlugin";
+export { PgRowByUniquePlugin } from "./plugins/PgRowByUniquePlugin";
+export { PgTableNodePlugin } from "./plugins/PgTableNodePlugin";
+export { PgTablesPlugin } from "./plugins/PgTablesPlugin";
+export { PgTypesPlugin } from "./plugins/PgTypesPlugin";
