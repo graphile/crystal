bddc3988ab34e16b1684bf1db744f3235c14f3dd
Benjie Gillam <benjie@jemjie.com>
2022-03-16 10:35:29 +0000

    Disable Postgres' jit

--- packages/dataplan-pg/src/adaptors/node-postgres.ts
+++ packages/dataplan-pg/src/adaptors/node-postgres.ts
@@ -12,9 +12,41 @@ const cacheSizeFromEnv = process.env.DATAPLAN_PG_PREPARED_STATEMENT_CACHE_SIZE
 const PREPARED_STATEMENT_CACHE_SIZE =
   !!cacheSizeFromEnv || cacheSizeFromEnv === 0 ? cacheSizeFromEnv : 100;
 
+const $$isSetup = Symbol("isConfiguredForDataplanPg");
+
+const DONT_DISABLE_JIT = process.env.DATAPLAN_PG_DONT_DISABLE_JIT === "1";
+
 export function makeNodePostgresWithPgClient(pool: Pool): WithPgClient {
   return async (pgSettings, callback) => {
     const pgClient = await pool.connect();
+    if (!pgClient[$$isSetup]) {
+      pgClient[$$isSetup] = true;
+      if (!DONT_DISABLE_JIT) {
+        /*
+         * > JIT compilation is beneficial primarily for long-running CPU-bound
+         * > queries. Frequently these will be analytical queries. For short
+         * > queries the added overhead of performing JIT compilation will
+         * > often be higher than the time it can save.
+         * -- https://www.postgresql.org/docs/14/jit-decision.html
+         *
+         * PostGraphile is designed for extremely fast queries, but sometimes
+         * user code can make Postgres think the cost of the query is going to
+         * be very high (this is especially the case when lots of computed
+         * column functions are used), and thus enables JIT. In testing we've
+         * seen queries that would take 50ms with `jit=off` take 8200ms with
+         * jit on. As such we've made the decision to disable jit for all
+         * queries.
+         *
+         * If you don't agree with our decision, disable this by setting the
+         * environmental variable `DATAPLAN_PG_DONT_DISABLE_JIT=1`.
+         */
+        pgClient.query("set jit = off;").catch((e) => {
+          console.error(
+            `Error occurred applying @dataplan/pg global Postgres settings`,
+          );
+        });
+      }
+    }
     /** Transaction level; 0 = no transaction; 1 = begin; 2,... = savepoint */
     let txLevel = 0;
     const pgSettingsEntries = pgSettings
