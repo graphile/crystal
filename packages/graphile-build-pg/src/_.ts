d8aa0f6a64395cfb3ac73d3af8aecff161a80b72
Benjie Gillam <benjie@jemjie.com>
2022-04-01 17:39:47 +0100

    Move comment

--- packages/graphile-build-pg/src/_.ts
+++ packages/graphile-build-pg/src/_.ts
@@ -1,8 +1,8 @@
+// TODO: benchmark this; might be faster to use `Array.from()` or an explicit
+// for loop with set existance check
 /**
  * Returns a new array containing only the unique elements of `list`.
  */
 export function uniq<T>(list: T[]): T[] {
   return [...new Set(list)];
-  // TODO: benchmark this; might be faster to use `Array.from()` or an explicit
-  // for loop with set existance check
 }
