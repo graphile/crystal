2ba4cbd38df5cfd5c06c3681240ba9f919b4562b
Benjie Gillam <benjie@jemjie.com>
2022-04-05 12:04:14 +0100

    Remove unused tests

--- packages/graphile-build/__tests__/emptyMutation.test.ts
+++
@@ -1,6 +0,0 @@
-import { buildSchema, defaultPlugins, QueryQueryPlugin } from "../src";
-
-test("generates empty schema (with no Mutation type)", async () => {
-  const schema = await buildSchema([...defaultPlugins, QueryQueryPlugin]);
-  expect(schema).toMatchSnapshot();
-});
