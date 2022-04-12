0f5ac9f096c4b8967801cf80884335320d4fa3c6
Benjie Gillam <benjie@jemjie.com>
2022-04-12 15:48:44 +0100

    Turn no-unused-vars back on

--- .eslintrc.js
+++ .eslintrc.js
@@ -56,7 +56,7 @@ module.exports = {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-unused-vars": [
-      "off",
+      "error",
       {
         argsIgnorePattern: "^_",
         varsIgnorePattern: "^_",
