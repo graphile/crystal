08bd3262bb7da070906ca02fdd6dbe504d9ab99a
Benjie Gillam <benjie@jemjie.com>
2022-04-26 12:38:53 +0100

    Explicit mermaid options

--- packages/graphile-inspect/src/components/Mermaid.tsx
+++ packages/graphile-inspect/src/components/Mermaid.tsx
@@ -1,6 +1,11 @@
 import mermaid from "mermaid";
 import { FC, useEffect } from "react";
 
+(mermaid as any).initialize({
+  startOnLoad: true,
+  maxTextSize: 1000000,
+});
+
 export const Mermaid: FC<{ diagram: string }> = ({ diagram }) => {
   useEffect(() => {
     if (diagram) {
