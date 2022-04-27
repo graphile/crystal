9ad8edb1417831c7690c858e57585c2db81f8063
Benjie Gillam <benjie@jemjie.com>
2022-04-27 18:06:20 +0100

    Fix permissions and TypeScript woes

--- packages/postgraphile/src/schema.ts
+++ packages/postgraphile/src/schema.ts
@@ -6,7 +6,7 @@ import type { Preset } from "graphile-plugin";
 import { resolvePresets } from "graphile-plugin";
 import * as pg from "pg";
 
-const Pool = pg.Pool || pg.default.Pool;
+const Pool = pg.Pool || (pg as any).default?.Pool;
 
 import type { ContextCallback, SchemaResult } from "./interfaces.js";
 import { defaultPreset as postgraphilePreset } from "./preset.js";
