6616696f272b966a662e400f5abb0c0114bcca30
Benjie Gillam <benjie@jemjie.com>
2022-05-16 11:12:37 +0100

    Don't attempt to mark something as exportable twice

--- packages/graphile-export/src/helpers.ts
+++ packages/graphile-export/src/helpers.ts
@@ -3,9 +3,11 @@ export function EXPORTABLE<T, TScope extends any[]>(
   args: [...TScope],
 ): T {
   const fn: T = factory(...args);
-  Object.defineProperties(fn, {
-    $exporter$args: { value: args },
-    $exporter$factory: { value: factory },
-  });
+  if (!("$exporter$factory" in fn)) {
+    Object.defineProperties(fn, {
+      $exporter$args: { value: args },
+      $exporter$factory: { value: factory },
+    });
+  }
   return fn;
 }
