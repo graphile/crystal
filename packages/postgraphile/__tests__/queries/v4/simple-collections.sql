90f281b65da7f80f329b25ba1d8e4c9db8d08a6c
Benjie Gillam <benjie@jemjie.com>
2022-05-27 15:38:56 +0100

    Update plans and SQL snapshots

--- packages/postgraphile/__tests__/queries/v4/simple-collections.sql
+++ packages/postgraphile/__tests__/queries/v4/simple-collections.sql
@@ -1,152 +1,101 @@
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name",
-to_json((__local_0__."email")) as "email"
-from (
-  select __local_0__.*
-  from "c"."person" as __local_0__
-  where (TRUE) and (TRUE)
-  order by __local_0__."id" ASC
-) __local_0__
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2"
+from "c"."person" as __person__
+order by __person__."id" asc
 
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name",
-to_json((__local_0__."email")) as "email"
-from (
-  select __local_0__.*
-  from "c"."person" as __local_0__
-  where (TRUE) and (TRUE)
-  order by __local_0__."id" ASC
-  limit 2
-) __local_0__
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2"
+from "c"."person" as __person__
+order by __person__."id" asc
+limit 2
 
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name",
-to_json((__local_0__."email")) as "email"
-from (
-  select __local_0__.*
-  from "c"."person" as __local_0__
-  where (TRUE) and (TRUE)
-  order by __local_0__."person_full_name" ASC,
-  __local_0__."id" ASC
-) __local_0__
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2"
+from "c"."person" as __person__
+order by __person__."person_full_name" asc, __person__."id" asc
 
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name",
-to_json((__local_0__."email")) as "email"
-from (
-  select __local_0__.*
-  from "c"."person" as __local_0__
-  where (TRUE) and (TRUE)
-  order by __local_0__."person_full_name" DESC,
-  __local_0__."id" ASC
-) __local_0__
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2"
+from "c"."person" as __person__
+order by __person__."person_full_name" desc, __person__."id" asc
 
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."headline")) as "headline",
-to_json((__local_0__."author_id")) as "authorId"
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2"
+from "c"."person" as __person__
+order by __person__."id" asc
+limit 3
+offset 1
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2"
+from "c"."person" as __person__
+order by __person__."id" asc
+limit 0
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2"
+from "c"."person" as __person__
+where (
+  __person__."about" is null
+)
+order by __person__."id" asc
+
+select
+  __post__."headline" as "0",
+  __post__."author_id"::text as "1"
+from "a"."post" as __post__
+order by __post__."author_id" desc, __post__."headline" desc, __post__."id" asc
+limit 3
+
+select __post_result__.*
 from (
-  select __local_0__.*
-  from "a"."post" as __local_0__
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __post_identifiers__,
+lateral (
+  select
+    __post__."headline" as "0",
+    __post__."author_id"::text as "1",
+    __post_identifiers__.idx as "2"
+  from "a"."post" as __post__
   where (
-    __local_0__."author_id" = $1
-  ) and (TRUE) and (TRUE)
-  order by __local_0__."id" ASC
-) __local_0__
+    __post__."author_id" = __post_identifiers__."id0"
+  )
+  order by __post__."id" asc
+) as __post_result__
 
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."headline")) as "headline",
-to_json((__local_0__."author_id")) as "authorId"
+select __post_result__.*
 from (
-  select __local_0__.*
-  from "a"."post" as __local_0__
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __post_identifiers__,
+lateral (
+  select
+    __post__."headline" as "0",
+    __post__."author_id"::text as "1",
+    __post_identifiers__.idx as "2"
+  from "a"."post" as __post__
   where (
-    __local_0__."author_id" = $1
-  ) and (TRUE) and (TRUE)
-  order by __local_0__."id" ASC
+    __post__."author_id" = __post_identifiers__."id0"
+  )
+  order by __post__."id" asc
   limit 2
-) __local_0__
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name",
-to_json((__local_0__."email")) as "email"
-from (
-  select __local_0__.*
-  from "c"."person" as __local_0__
-  where (TRUE) and (TRUE)
-  order by __local_0__."id" ASC
-  limit 3 offset 1
-) __local_0__
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name",
-to_json((__local_0__."email")) as "email"
-from (
-  select __local_0__.*
-  from "c"."person" as __local_0__
-  where (TRUE) and (TRUE)
-  order by __local_0__."id" ASC
-  limit 0
-) __local_0__
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name",
-to_json((__local_0__."email")) as "email"
-from (
-  select __local_0__.*
-  from "c"."person" as __local_0__
-  where (TRUE) and (TRUE)
-  order by __local_0__."id" ASC
-) __local_0__
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name",
-to_json((__local_0__."email")) as "email"
-from (
-  select __local_0__.*
-  from "c"."person" as __local_0__
-  where (__local_0__."about" IS NULL) and (TRUE) and (TRUE)
-  order by __local_0__."id" ASC
-) __local_0__
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."headline")) as "headline",
-to_json((__local_0__."author_id")) as "authorId"
-from (
-  select __local_0__.*
-  from "a"."post" as __local_0__
-  where (TRUE) and (TRUE)
-  order by __local_0__."author_id" DESC,
-  __local_0__."headline" DESC,
-  __local_0__."id" ASC
-  limit 3
-) __local_0__
+) as __post_result__
