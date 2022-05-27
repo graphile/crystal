d51534e359a995fd27bc19c5686f9dd7d0e0751c
Benjie Gillam <benjie@jemjie.com>
2022-05-27 14:56:44 +0100

    Add basic support for classicIds setting

--- packages/postgraphile/src/presets/v4.ts
+++ packages/postgraphile/src/presets/v4.ts
@@ -5,16 +5,38 @@ import { PgV4SmartTagsPlugin } from "../plugins/PgV4SmartTagsPlugin.js";
 
 export interface V4Options {
   simpleCollections?: "both" | "only" | "omit";
+  classicIds?: boolean;
 }
 
 function isNotNullish<T>(arg: T | undefined | null): arg is T {
   return arg != null;
 }
 
-const makeV4Plugin = (options: V4Options): GraphileConfig.Plugin => {
+const makeV4Plugin = (options: V4Options = {}): GraphileConfig.Plugin => {
+  const { classicIds = false } = options;
   return {
     name: "PostGraphileV4CompatibilityPlugin",
     version: "0.0.0",
+    inflection: {
+      replace: {
+        ...(classicIds
+          ? null
+          : {
+              // Rename GraphQL Global Object Identification 'id' to 'nodeId'
+              // TODO: this will be better as `_id` in general, but V4 uses `nodeId`
+              nodeIdFieldName() {
+                return "nodeId";
+              },
+              // Don't rename 'id' to 'rowId'
+              column(previous, options, details) {
+                const columnFieldName = this.camelCase(
+                  this._columnName(details),
+                );
+                return columnFieldName;
+              },
+            }),
+      },
+    },
     schema: {
       hooks: {
         build: {
@@ -46,7 +68,7 @@ export const makeV4Preset = (options?: V4Options): GraphileConfig.Preset => {
     plugins: [
       PgV4InflectionPlugin,
       PgV4SmartTagsPlugin,
-      options ? makeV4Plugin(options) : null,
+      makeV4Plugin(options),
     ].filter(isNotNullish),
   };
 };
