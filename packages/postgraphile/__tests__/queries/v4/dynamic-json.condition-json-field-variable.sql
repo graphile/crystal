41fb21b693418871d669e11fba4dfd4e3980d77d
Benjie Gillam <benjie@jemjie.com>
2022-05-31 17:32:20 +0100

    Update snapshots for V5

--- packages/postgraphile/__tests__/queries/v4/dynamic-json.condition-json-field-variable.sql
+++ packages/postgraphile/__tests__/queries/v4/dynamic-json.condition-json-field-variable.sql
@@ -1,107 +1,37 @@
-with __local_0__ as (
-  select to_json(
-    (
-      json_build_object(
-        '__identifiers'::text,
-        json_build_array(__local_1__."id"),
-        'id'::text,
-        (__local_1__."id"),
-        'jsonData'::text,
-        (__local_1__."json_data")
-      )
-    )
-  ) as "@nodes"
-  from (
-    select __local_1__.*
-    from "c"."my_table" as __local_1__
-    where (
-      __local_1__."json_data" = $1
-    ) and (TRUE) and (TRUE)
-    order by __local_1__."id" ASC
-  ) __local_1__
-),
-__local_2__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
-  (
-    select __local_2__.data
-    from __local_2__
-  ),
-  '[]'::json
-) as "data"
+select __my_table_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"jsonb" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __my_table_identifiers__,
+lateral (
+  select
+    __my_table__."id"::text as "0",
+    __my_table__."json_data"::text as "1",
+    __my_table_identifiers__.idx as "2"
+  from "c"."my_table" as __my_table__
+  where (
+    __my_table__."json_data" = __my_table_identifiers__."id0"
+  )
+  order by __my_table__."id" asc
+) as __my_table_result__
 
-with __local_0__ as (
-  select to_json(
-    (
-      json_build_object(
-        '__identifiers'::text,
-        json_build_array(__local_1__."id"),
-        'id'::text,
-        (__local_1__."id"),
-        'jsonData'::text,
-        (__local_1__."json_data")
-      )
-    )
-  ) as "@nodes"
-  from (
-    select __local_1__.*
-    from "c"."my_table" as __local_1__
-    where (
-      __local_1__."json_data" = $1
-    ) and (TRUE) and (TRUE)
-    order by __local_1__."id" ASC
-  ) __local_1__
-),
-__local_2__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
-  (
-    select __local_2__.data
-    from __local_2__
-  ),
-  '[]'::json
-) as "data"
-
-with __local_0__ as (
-  select to_json(
-    (
-      json_build_object(
-        '__identifiers'::text,
-        json_build_array(__local_1__."id"),
-        'id'::text,
-        (__local_1__."id"),
-        'jsonData'::text,
-        (__local_1__."json_data")
-      )
-    )
-  ) as "@nodes"
-  from (
-    select __local_1__.*
-    from "c"."my_table" as __local_1__
-    where (
-      __local_1__."json_data" = $1
-    ) and (TRUE) and (TRUE)
-    order by __local_1__."id" ASC
-  ) __local_1__
-),
-__local_2__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
-  (
-    select __local_2__.data
-    from __local_2__
-  ),
-  '[]'::json
-) as "data"
+select __my_table_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"jsonb" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __my_table_identifiers__,
+lateral (
+  select
+    __my_table__."id"::text as "0",
+    __my_table__."json_data"::text as "1",
+    __my_table_identifiers__.idx as "2"
+  from "c"."my_table" as __my_table__
+  where (
+    __my_table__."json_data" = __my_table_identifiers__."id0"
+  )
+  order by __my_table__."id" asc
+) as __my_table_result__
