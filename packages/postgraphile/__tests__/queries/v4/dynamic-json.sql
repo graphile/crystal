b52f550414ef5ad423ded6a3a6780a2d99abcca4
Benjie Gillam <benjie@jemjie.com>
2022-05-31 17:33:39 +0100

    Update snapshots for V5

--- packages/postgraphile/__tests__/queries/v4/dynamic-json.sql
+++ packages/postgraphile/__tests__/queries/v4/dynamic-json.sql
@@ -1,105 +1,173 @@
-select to_json(__local_0__) as "value"
-from "c"."json_identity"(
-  $1
-) as __local_0__
-where (TRUE) and (TRUE)
+select __json_identity_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"json" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __json_identity_identifiers__,
+lateral (
+  select
+    __json_identity__.__json_identity__::text as "0",
+    __json_identity_identifiers__.idx as "1"
+  from "c"."json_identity"(__json_identity_identifiers__."id0") as __json_identity__
+) as __json_identity_result__
 
-select to_json(__local_0__) as "value"
-from "c"."json_identity"(
-  $1
-) as __local_0__
-where (TRUE) and (TRUE)
+select __json_identity_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"json" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __json_identity_identifiers__,
+lateral (
+  select
+    __json_identity__.__json_identity__::text as "0",
+    __json_identity_identifiers__.idx as "1"
+  from "c"."json_identity"(__json_identity_identifiers__."id0") as __json_identity__
+) as __json_identity_result__
 
-select to_json(__local_0__) as "value"
-from "c"."json_identity"(
-  $1
-) as __local_0__
-where (TRUE) and (TRUE)
+select __json_identity_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"json" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __json_identity_identifiers__,
+lateral (
+  select
+    __json_identity__.__json_identity__::text as "0",
+    __json_identity_identifiers__.idx as "1"
+  from "c"."json_identity"(__json_identity_identifiers__."id0") as __json_identity__
+) as __json_identity_result__
 
-select to_json(__local_0__) as "value"
-from "c"."json_identity"(
-  $1
-) as __local_0__
-where (TRUE) and (TRUE)
+select __json_identity_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"json" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __json_identity_identifiers__,
+lateral (
+  select
+    __json_identity__.__json_identity__::text as "0",
+    __json_identity_identifiers__.idx as "1"
+  from "c"."json_identity"(__json_identity_identifiers__."id0") as __json_identity__
+) as __json_identity_result__
 
-select to_json(__local_0__) as "value"
-from "c"."json_identity"(
-  $1
-) as __local_0__
-where (TRUE) and (TRUE)
+select __json_identity_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"json" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __json_identity_identifiers__,
+lateral (
+  select
+    __json_identity__.__json_identity__::text as "0",
+    __json_identity_identifiers__.idx as "1"
+  from "c"."json_identity"(__json_identity_identifiers__."id0") as __json_identity__
+) as __json_identity_result__
 
-select to_json(__local_0__) as "value"
-from "c"."json_identity"(
-  $1
-) as __local_0__
-where (TRUE) and (TRUE)
+select __json_identity_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"json" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __json_identity_identifiers__,
+lateral (
+  select
+    __json_identity__.__json_identity__::text as "0",
+    __json_identity_identifiers__.idx as "1"
+  from "c"."json_identity"(__json_identity_identifiers__."id0") as __json_identity__
+) as __json_identity_result__
 
-select to_json(__local_0__) as "value"
-from "c"."json_identity"(
-  $1
-) as __local_0__
-where (TRUE) and (TRUE)
+select __json_identity_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"json" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __json_identity_identifiers__,
+lateral (
+  select
+    __json_identity__.__json_identity__::text as "0",
+    __json_identity_identifiers__.idx as "1"
+  from "c"."json_identity"(__json_identity_identifiers__."id0") as __json_identity__
+) as __json_identity_result__
 
-select to_json(__local_0__) as "value"
-from "c"."json_identity"(
-  $1
-) as __local_0__
-where (TRUE) and (TRUE)
+select __json_identity_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"json" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __json_identity_identifiers__,
+lateral (
+  select
+    __json_identity__.__json_identity__::text as "0",
+    __json_identity_identifiers__.idx as "1"
+  from "c"."json_identity"(__json_identity_identifiers__."id0") as __json_identity__
+) as __json_identity_result__
 
-select to_json(__local_0__) as "value"
-from "c"."json_identity"(
-  $1
-) as __local_0__
-where (TRUE) and (TRUE)
+select __json_identity_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"json" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __json_identity_identifiers__,
+lateral (
+  select
+    __json_identity__.__json_identity__::text as "0",
+    __json_identity_identifiers__.idx as "1"
+  from "c"."json_identity"(__json_identity_identifiers__."id0") as __json_identity__
+) as __json_identity_result__
 
-select to_json(__local_0__) as "value"
-from "c"."json_identity"(
-  $1
-) as __local_0__
-where (TRUE) and (TRUE)
+select __json_identity_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"json" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __json_identity_identifiers__,
+lateral (
+  select
+    __json_identity__.__json_identity__::text as "0",
+    __json_identity_identifiers__.idx as "1"
+  from "c"."json_identity"(__json_identity_identifiers__."id0") as __json_identity__
+) as __json_identity_result__
 
-select to_json(__local_0__) as "value"
-from "c"."jsonb_identity"(
-  $1
-) as __local_0__
-where (TRUE) and (TRUE)
+select __jsonb_identity_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"jsonb" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __jsonb_identity_identifiers__,
+lateral (
+  select
+    __jsonb_identity__.__jsonb_identity__::text as "0",
+    __jsonb_identity_identifiers__.idx as "1"
+  from "c"."jsonb_identity"(__jsonb_identity_identifiers__."id0") as __jsonb_identity__
+) as __jsonb_identity_result__
 
-select to_json(__local_0__) as "value"
-from "c"."jsonb_identity"(
-  $1
-) as __local_0__
-where (TRUE) and (TRUE)
+select __jsonb_identity_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"jsonb" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __jsonb_identity_identifiers__,
+lateral (
+  select
+    __jsonb_identity__.__jsonb_identity__::text as "0",
+    __jsonb_identity_identifiers__.idx as "1"
+  from "c"."jsonb_identity"(__jsonb_identity_identifiers__."id0") as __jsonb_identity__
+) as __jsonb_identity_result__
 
-with __local_0__ as (
-  select to_json(
-    (
-      json_build_object(
-        '__identifiers'::text,
-        json_build_array(__local_1__."id"),
-        'json'::text,
-        (__local_1__."json"),
-        'jsonb'::text,
-        (__local_1__."jsonb")
-      )
-    )
-  ) as "@nodes"
-  from (
-    select __local_1__.*
-    from "b"."types" as __local_1__
-    where (TRUE) and (TRUE)
-    order by __local_1__."id" ASC
-  ) __local_1__
-),
-__local_2__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
-  (
-    select __local_2__.data
-    from __local_2__
-  ),
-  '[]'::json
-) as "data"
+select
+  __types__."json"::text as "0",
+  __types__."jsonb"::text as "1"
+from "b"."types" as __types__
+order by __types__."id" asc
