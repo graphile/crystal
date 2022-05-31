e42c435f16e2f7c48e05601230c6ac0747c6fab8
Benjie Gillam <benjie@jemjie.com>
2022-05-31 17:35:21 +0100

    Update snapshots for V5

--- packages/postgraphile/__tests__/queries/v4/empty-array.sql
+++ packages/postgraphile/__tests__/queries/v4/empty-array.sql
@@ -1,47 +1,7 @@
-with __local_0__ as (
-  select to_json(
-    (
-      json_build_object(
-        '__identifiers'::text,
-        json_build_array(__local_1__."id"),
-        'id'::text,
-        (__local_1__."id"),
-        'name'::text,
-        (__local_1__."person_full_name"),
-        'aliases'::text,
-        (
-          case when (__local_1__."aliases") is null then null when coalesce(
-            array_length(
-              (__local_1__."aliases"),
-              1
-            ),
-            0
-          ) = 0 then '[]'::json else (
-            select json_agg(__local_2__)
-            from unnest((__local_1__."aliases")) as __local_2__
-          ) end
-        )
-      )
-    )
-  ) as "@nodes"
-  from (
-    select __local_1__.*
-    from "c"."person" as __local_1__
-    where (TRUE) and (TRUE)
-    order by __local_1__."id" ASC
-    limit 1
-  ) __local_1__
-),
-__local_3__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
-  (
-    select __local_3__.data
-    from __local_3__
-  ),
-  '[]'::json
-) as "data"
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."aliases"::text as "2"
+from "c"."person" as __person__
+order by __person__."id" asc
+limit 1
