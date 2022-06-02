92855e2260aaa8d11192a58d9e188336a3be6083
Benjie Gillam <benjie@jemjie.com>
2022-06-02 09:23:58 +0100

    Update snapshots for V5

--- packages/postgraphile/__tests__/queries/v4/enum_tables.queries2.sql
+++ packages/postgraphile/__tests__/queries/v4/enum_tables.queries2.sql
@@ -1,37 +1,7 @@
-with __local_0__ as (
-  select to_json(
-    (
-      json_build_object(
-        '__identifiers'::text,
-        json_build_array(__local_1__."id"),
-        'id'::text,
-        (__local_1__."id"),
-        'enum1'::text,
-        (__local_1__."enum_1"),
-        'enum2'::text,
-        (__local_1__."enum_2"),
-        'enum3'::text,
-        (__local_1__."enum_3")
-      )
-    )
-  ) as "@nodes"
-  from (
-    select __local_1__.*
-    from "enum_tables"."referencing_table" as __local_1__
-    where (TRUE) and (TRUE)
-    order by __local_1__."id" ASC
-  ) __local_1__
-),
-__local_2__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
-  (
-    select __local_2__.data
-    from __local_2__
-  ),
-  '[]'::json
-) as "data"
+select
+  __referencing_table__."id"::text as "0",
+  __referencing_table__."enum_1"::text as "1",
+  __referencing_table__."enum_2"::text as "2",
+  __referencing_table__."enum_3"::text as "3"
+from "enum_tables"."referencing_table" as __referencing_table__
+order by __referencing_table__."id" asc
