afa256bcf6117027209eb0d0478686a0b2f6f935
Benjie Gillam <benjie@jemjie.com>
2022-06-10 11:34:22 +0100

    Update snapshots for V5

--- packages/postgraphile/__tests__/queries/v4/nonexistant-record-from-function.sql
+++ packages/postgraphile/__tests__/queries/v4/nonexistant-record-from-function.sql
@@ -1,11 +1,14 @@
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."headline")) as "headline"
-from "c"."table_query"(
-  $1
-) as __local_0__
-where (
-  not (__local_0__ is null)
-) and (TRUE) and (TRUE)
+select __table_query_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __table_query_identifiers__,
+lateral (
+  select
+    __table_query__."id"::text as "0",
+    __table_query__."headline" as "1",
+    __table_query_identifiers__.idx as "2"
+  from "c"."table_query"(__table_query_identifiers__."id0") as __table_query__
+) as __table_query_result__
