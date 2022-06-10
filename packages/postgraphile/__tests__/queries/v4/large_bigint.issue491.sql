a51ffbfe86f46f14977d4724313e93db970b3f52
Benjie Gillam <benjie@jemjie.com>
2022-06-10 10:43:08 +0100

    Update snapshots

--- packages/postgraphile/__tests__/queries/v4/large_bigint.issue491.sql
+++ packages/postgraphile/__tests__/queries/v4/large_bigint.issue491.sql
@@ -1,69 +1,43 @@
-with __local_0__ as (
-  select to_json(
-    (
-      json_build_object(
-        '__identifiers'::text,
-        json_build_array(
-          (
-            (__local_1__."id")::numeric
-          )::text
-        ),
-        'id'::text,
-        ((__local_1__."id"))::text,
-        'text'::text,
-        (__local_1__."text")
-      )
-    )
-  ) as "@nodes"
-  from (
-    select __local_1__.*
-    from "large_bigint"."large_node_id" as __local_1__
-    where (TRUE) and (TRUE)
-    order by __local_1__."id" ASC
-  ) __local_1__
-),
-__local_2__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
-  (
-    select __local_2__.data
-    from __local_2__
-  ),
-  '[]'::json
-) as "data"
+select
+  __large_node_id__."id"::text as "0",
+  __large_node_id__."text" as "1"
+from "large_bigint"."large_node_id" as __large_node_id__
+order by __large_node_id__."id" asc
 
-select to_json(
-  json_build_array(
-    (
-      (__local_0__."id")::numeric
-    )::text
+select __large_node_id_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int8" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __large_node_id_identifiers__,
+lateral (
+  select
+    __large_node_id__."id"::text as "0",
+    __large_node_id__."text" as "1",
+    __large_node_id_identifiers__.idx as "2"
+  from "large_bigint"."large_node_id" as __large_node_id__
+  where (
+    __large_node_id__."id" = __large_node_id_identifiers__."id0"
   )
-) as "__identifiers",
-to_json(
-  ((__local_0__."id"))::text
-) as "id",
-to_json((__local_0__."text")) as "text"
-from "large_bigint"."large_node_id" as __local_0__
-where (
-  __local_0__."id" = $1
-) and (TRUE) and (TRUE)
+  order by __large_node_id__."id" asc
+) as __large_node_id_result__
 
-select to_json(
-  json_build_array(
-    (
-      (__local_0__."id")::numeric
-    )::text
+select __large_node_id_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int8" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __large_node_id_identifiers__,
+lateral (
+  select
+    __large_node_id__."id"::text as "0",
+    __large_node_id__."text" as "1",
+    __large_node_id_identifiers__.idx as "2"
+  from "large_bigint"."large_node_id" as __large_node_id__
+  where (
+    __large_node_id__."id" = __large_node_id_identifiers__."id0"
   )
-) as "__identifiers",
-to_json(
-  ((__local_0__."id"))::text
-) as "id",
-to_json((__local_0__."text")) as "text"
-from "large_bigint"."large_node_id" as __local_0__
-where (
-  __local_0__."id" = $1
-) and (TRUE) and (TRUE)
+  order by __large_node_id__."id" asc
+) as __large_node_id_result__
