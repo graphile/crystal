6f74792869cd345361c7e9e54f04c0767328cbd9
Benjie Gillam <benjie@jemjie.com>
2022-06-10 13:55:45 +0100

    Update snapshots for V5

--- packages/postgraphile/__tests__/queries/v4/pg11.types.sql
+++ packages/postgraphile/__tests__/queries/v4/pg11.types.sql
@@ -1,84 +1,21 @@
-with __local_0__ as (
-  select to_json(
-    (
-      json_build_object(
-        'id'::text,
-        (__local_1__."id"),
-        'regrole'::text,
-        (__local_1__."regrole"),
-        'regnamespace'::text,
-        (__local_1__."regnamespace"),
-        'bigintDomainArrayDomain'::text,
-        (
-          case when (__local_1__."bigint_domain_array_domain") is null then null else array(
-            select (val)::text
-            from unnest((__local_1__."bigint_domain_array_domain")) as unnest(val)
-          ) end
-        ),
-        'domainConstrainedCompoundType'::text,
-        (__local_1__."domain_constrained_compound_type")
-      )
-    )
-  ) as "@nodes",
-  to_json(
-    (
-      json_build_object(
-        '@node'::text,
-        (
-          json_build_object(
-            'id'::text,
-            (__local_1__."id"),
-            'regrole'::text,
-            (__local_1__."regrole"),
-            'regnamespace'::text,
-            (__local_1__."regnamespace"),
-            'bigintDomainArrayDomain'::text,
-            (
-              case when (__local_1__."bigint_domain_array_domain") is null then null else array(
-                select (val)::text
-                from unnest((__local_1__."bigint_domain_array_domain")) as unnest(val)
-              ) end
-            ),
-            'domainConstrainedCompoundType'::text,
-            (__local_1__."domain_constrained_compound_type")
-          )
-        )
-      )
-    )
-  ) as "@edges",
-  to_json(
-    json_build_array(
-      'primary_key_asc',
-      json_build_array(__local_1__."id")
-    )
-  ) as "__cursor"
-  from (
-    select __local_1__.*
-    from "pg11"."types" as __local_1__
-    where (TRUE) and (TRUE)
-    order by __local_1__."id" ASC
-  ) __local_1__
-),
-__local_2__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
-  (
-    select __local_2__.data
-    from __local_2__
-  ),
-  '[]'::json
-) as "data",
-FALSE as "hasNextPage",
-FALSE as "hasPreviousPage",
-(
-  select json_build_object(
-    'totalCount'::text,
-    count(1)
-  )
-  from "pg11"."types" as __local_1__
-  where 1 = 1
-) as "aggregates"
+select
+  __types__."id"::text as "0",
+  __types__."regrole"::text as "1",
+  __types__."regnamespace"::text as "2",
+  __types__."bigint_domain_array_domain"::text as "3",
+  __temporary_source1__."a"::text as "4",
+  __temporary_source1__."b" as "5",
+  __temporary_source1__."c"::text as "6",
+  __temporary_source1__."d" as "7",
+  __temporary_source1__."e"::text as "8",
+  __temporary_source1__."f"::text as "9",
+  __temporary_source1__."foo_bar"::text as "10",
+  (not (__temporary_source1__ is null))::text as "11"
+from "pg11"."types" as __types__
+left outer join lateral (select (__types__."domain_constrained_compound_type").*) as __temporary_source1__
+on TRUE
+order by __types__."id" asc
+
+select
+  (count(*))::text as "0"
+from "pg11"."types" as __types__
