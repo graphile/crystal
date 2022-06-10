0ab207b65dc222cb980257c302f417e923b49ff1
Benjie Gillam <benjie@jemjie.com>
2022-06-10 11:34:54 +0100

    Update snapshots for V5

--- packages/postgraphile/__tests__/queries/v4/one-to-one-backward.sql
+++ packages/postgraphile/__tests__/queries/v4/one-to-one-backward.sql
@@ -1,128 +1,25 @@
-with __local_0__ as (
-  select to_json(
-    (
-      json_build_object(
-        '__identifiers'::text,
-        json_build_array(__local_1__."id"),
-        '@node'::text,
-        (
-          json_build_object(
-            '__identifiers'::text,
-            json_build_array(__local_1__."id"),
-            'id'::text,
-            (__local_1__."id"),
-            'name'::text,
-            (__local_1__."person_full_name"),
-            '@firstName'::text,
-            (
-              select to_json(__local_2__) as "value"
-              from "c"."person_first_name"(__local_1__) as __local_2__
-              where (TRUE) and (TRUE)
-            ),
-            '@leftArm'::text,
-            (
-              select (
-                case when (__local_3__ is null) then null else json_build_object(
-                  '__identifiers'::text,
-                  json_build_array(__local_3__."id"),
-                  'id'::text,
-                  (__local_3__."id"),
-                  'personId'::text,
-                  (__local_3__."person_id"),
-                  '@personByPersonId'::text,
-                  (
-                    select json_build_object(
-                      '__identifiers'::text,
-                      json_build_array(__local_4__."id"),
-                      'id'::text,
-                      (__local_4__."id"),
-                      'name'::text,
-                      (__local_4__."person_full_name"),
-                      '@firstName'::text,
-                      (
-                        select to_json(__local_5__) as "value"
-                        from "c"."person_first_name"(__local_4__) as __local_5__
-                        where (TRUE) and (TRUE)
-                      )
-                    ) as object
-                    from "c"."person" as __local_4__
-                    where (__local_3__."person_id" = __local_4__."id") and (TRUE) and (TRUE)
-                  ),
-                  'lengthInMetres'::text,
-                  (__local_3__."length_in_metres")
-                ) end
-              ) as object
-              from "c"."left_arm" as __local_3__
-              where (
-                not (__local_3__ is null)
-              )
-              and (__local_3__."person_id" = __local_1__."id") and (TRUE) and (TRUE)
-            ),
-            '@secret'::text,
-            (
-              select (
-                case when (__local_6__ is null) then null else json_build_object(
-                  '__identifiers'::text,
-                  json_build_array(__local_6__."person_id"),
-                  'personId'::text,
-                  (__local_6__."person_id"),
-                  '@personByPersonId'::text,
-                  (
-                    select json_build_object(
-                      '__identifiers'::text,
-                      json_build_array(__local_7__."id"),
-                      'id'::text,
-                      (__local_7__."id"),
-                      'name'::text,
-                      (__local_7__."person_full_name"),
-                      '@firstName'::text,
-                      (
-                        select to_json(__local_8__) as "value"
-                        from "c"."person_first_name"(__local_7__) as __local_8__
-                        where (TRUE) and (TRUE)
-                      )
-                    ) as object
-                    from "c"."person" as __local_7__
-                    where (__local_6__."person_id" = __local_7__."id") and (TRUE) and (TRUE)
-                  ),
-                  'secret'::text,
-                  (__local_6__."sekrit")
-                ) end
-              ) as object
-              from "c"."person_secret" as __local_6__
-              where (
-                not (__local_6__ is null)
-              )
-              and (__local_6__."person_id" = __local_1__."id") and (TRUE) and (TRUE)
-            )
-          )
-        )
-      )
-    )
-  ) as "@edges",
-  to_json(
-    json_build_array(
-      'primary_key_asc',
-      json_build_array(__local_1__."id")
-    )
-  ) as "__cursor"
-  from (
-    select __local_1__.*
-    from "c"."person" as __local_1__
-    where (TRUE) and (TRUE)
-    order by __local_1__."id" ASC
-  ) __local_1__
-),
-__local_9__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
-  (
-    select __local_9__.data
-    from __local_9__
-  ),
-  '[]'::json
-) as "data"
+select
+  __left_arm__."id"::text as "0",
+  __person__."id"::text as "1",
+  __person__."person_full_name" as "2",
+  "c"."person_first_name"(__person__) as "3",
+  __left_arm__."person_id"::text as "4",
+  __left_arm__."length_in_metres"::text as "5",
+  __person_2."id"::text as "6",
+  __person_2."person_full_name" as "7",
+  "c"."person_first_name"(__person_2) as "8",
+  __person_secret__."person_id"::text as "9",
+  __person_secret__."sekrit" as "10",
+  __person_3."id"::text as "11",
+  __person_3."person_full_name" as "12",
+  "c"."person_first_name"(__person_3) as "13"
+from "c"."person" as __person_3
+left outer join "c"."left_arm" as __left_arm__
+on (__person_3."id"::"int4" = __left_arm__."person_id")
+left outer join "c"."person" as __person__
+on (__left_arm__."person_id"::"int4" = __person__."id")
+left outer join "c"."person_secret" as __person_secret__
+on (__person_3."id"::"int4" = __person_secret__."person_id")
+left outer join "c"."person" as __person_2
+on (__person_secret__."person_id"::"int4" = __person_2."id")
+order by __person_3."id" asc
