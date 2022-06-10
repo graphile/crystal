066989b391bb72e3f57487c05eb07e11baf368df
Benjie Gillam <benjie@jemjie.com>
2022-06-10 10:26:40 +0100

    Update snapshots

--- packages/postgraphile/__tests__/queries/v4/geometry.queries.sql
+++ packages/postgraphile/__tests__/queries/v4/geometry.queries.sql
@@ -1,47 +1,12 @@
-with __local_0__ as (
-  select to_json(
-    (
-      json_build_object(
-        '__identifiers'::text,
-        json_build_array(__local_1__."id"),
-        'id'::text,
-        (__local_1__."id"),
-        'point'::text,
-        (__local_1__."point"),
-        'line'::text,
-        (__local_1__."line"),
-        'lseg'::text,
-        (__local_1__."lseg"),
-        'box'::text,
-        (__local_1__."box"),
-        'openPath'::text,
-        (__local_1__."open_path"),
-        'closedPath'::text,
-        (__local_1__."closed_path"),
-        'polygon'::text,
-        (__local_1__."polygon"),
-        'circle'::text,
-        (__local_1__."circle")
-      )
-    )
-  ) as "@nodes"
-  from (
-    select __local_1__.*
-    from "geometry"."geom" as __local_1__
-    where (TRUE) and (TRUE)
-    order by __local_1__."id" ASC
-  ) __local_1__
-),
-__local_2__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
-  (
-    select __local_2__.data
-    from __local_2__
-  ),
-  '[]'::json
-) as "data"
+select
+  __geom__."id"::text as "0",
+  __geom__."point"::text as "1",
+  __geom__."line"::text as "2",
+  __geom__."lseg"::text as "3",
+  __geom__."box"::text as "4",
+  __geom__."open_path"::text as "5",
+  __geom__."closed_path"::text as "6",
+  __geom__."polygon"::text as "7",
+  __geom__."circle"::text as "8"
+from "geometry"."geom" as __geom__
+order by __geom__."id" asc
