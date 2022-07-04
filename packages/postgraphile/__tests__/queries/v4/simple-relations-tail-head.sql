7141ca6f276e4afdcec133078adbecab9eefe03d
Benjie Gillam <benjie@jemjie.com>
2022-07-04 15:44:14 +0100

    Update snapshots

--- packages/postgraphile/__tests__/queries/v4/simple-relations-tail-head.sql
+++ packages/postgraphile/__tests__/queries/v4/simple-relations-tail-head.sql
@@ -1,91 +1,36 @@
-select to_json(
-  json_build_array(
-    __local_0__."person_id_1",
-    __local_0__."person_id_2"
-  )
-) as "__identifiers",
-to_json((__local_0__."person_id_1")) as "personId1",
-to_json((__local_0__."person_id_2")) as "personId2",
-to_json((__local_0__."extra")) as "extra",
-to_json(
-  (
-    select json_build_object(
-      '__identifiers'::text,
-      json_build_array(__local_1__."id"),
-      'name'::text,
-      (__local_1__."person_full_name"),
-      'email'::text,
-      (__local_1__."email")
-    ) as object
-    from "c"."person" as __local_1__
-    where (__local_0__."person_id_1" = __local_1__."id") and (TRUE) and (TRUE)
-  )
-) as "@personByPersonId1",
-to_json(
-  (
-    select json_build_object(
-      '__identifiers'::text,
-      json_build_array(__local_2__."id"),
-      'name'::text,
-      (__local_2__."person_full_name"),
-      'email'::text,
-      (__local_2__."email")
-    ) as object
-    from "c"."person" as __local_2__
-    where (__local_0__."person_id_2" = __local_2__."id") and (TRUE) and (TRUE)
-  )
-) as "@personByPersonId2"
-from (
-  select __local_0__.*
-  from "c"."compound_key" as __local_0__
-  where (TRUE) and (TRUE)
-  order by __local_0__."person_id_1" ASC,
-  __local_0__."person_id_2" ASC
-) __local_0__
+select
+  __person__."person_full_name" as "0",
+  __person__."email" as "1",
+  __compound_key__."person_id_1"::text as "2",
+  __person_2."person_full_name" as "3",
+  __person_2."email" as "4",
+  __compound_key__."person_id_2"::text as "5",
+  __compound_key__."extra"::text as "6"
+from "c"."compound_key" as __compound_key__
+left outer join "c"."person" as __person__
+on (__compound_key__."person_id_1"::"int4" = __person__."id")
+left outer join "c"."person" as __person_2
+on (__compound_key__."person_id_2"::"int4" = __person_2."id")
+order by __compound_key__."person_id_1" asc, __compound_key__."person_id_2" asc
 
-select to_json((__local_0__."person_id")) as "personId",
-to_json((__local_0__."compound_key_1")) as "compoundKey1",
-to_json((__local_0__."compound_key_2")) as "compoundKey2",
-to_json(
+select
+  __person__."person_full_name" as "0",
+  __person__."email" as "1",
+  __foreign_key__."person_id"::text as "2",
+  __compound_key__."person_id_1"::text as "3",
+  __compound_key__."person_id_2"::text as "4",
+  __compound_key__."extra"::text as "5",
+  __foreign_key__."compound_key_1"::text as "6",
+  __foreign_key__."compound_key_2"::text as "7",
+  (not (__foreign_key__ is null))::text as "8"
+from "a"."foreign_key" as __foreign_key__
+left outer join "c"."person" as __person__
+on (__foreign_key__."person_id"::"int4" = __person__."id")
+left outer join "c"."compound_key" as __compound_key__
+on (
   (
-    select json_build_object(
-      '__identifiers'::text,
-      json_build_array(__local_1__."id"),
-      'name'::text,
-      (__local_1__."person_full_name"),
-      'email'::text,
-      (__local_1__."email")
-    ) as object
-    from "c"."person" as __local_1__
-    where (__local_0__."person_id" = __local_1__."id") and (TRUE) and (TRUE)
+    __foreign_key__."compound_key_1"::"int4" = __compound_key__."person_id_1"
+  ) and (
+    __foreign_key__."compound_key_2"::"int4" = __compound_key__."person_id_2"
   )
-) as "@personByPersonId",
-to_json(
-  (
-    select json_build_object(
-      '__identifiers'::text,
-      json_build_array(
-        __local_2__."person_id_1",
-        __local_2__."person_id_2"
-      ),
-      'personId1'::text,
-      (__local_2__."person_id_1"),
-      'personId2'::text,
-      (__local_2__."person_id_2"),
-      'extra'::text,
-      (__local_2__."extra")
-    ) as object
-    from "c"."compound_key" as __local_2__
-    where (
-      __local_0__."compound_key_1" = __local_2__."person_id_1"
-    )
-    and (
-      __local_0__."compound_key_2" = __local_2__."person_id_2"
-    ) and (TRUE) and (TRUE)
-  )
-) as "@compoundKeyByCompoundKey1AndCompoundKey2"
-from (
-  select __local_0__.*
-  from "a"."foreign_key" as __local_0__
-  where (TRUE) and (TRUE)
-) __local_0__
+)
