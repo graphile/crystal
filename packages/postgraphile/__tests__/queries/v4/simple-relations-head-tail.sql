1584728aa220cf5bf71b642d1681cc47819ef821
Benjie Gillam <benjie@jemjie.com>
2022-07-04 15:43:56 +0100

    Update snapshots

--- packages/postgraphile/__tests__/queries/v4/simple-relations-head-tail.sql
+++ packages/postgraphile/__tests__/queries/v4/simple-relations-head-tail.sql
@@ -1,143 +1,65 @@
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name",
-to_json(
-  (
-    select coalesce(
-      (
-        select json_agg(__local_1__."object")
-        from (
-          select json_build_object(
-            '__identifiers'::text,
-            json_build_array(__local_2__."id"),
-            'headline'::text,
-            (__local_2__."headline"),
-            'authorId'::text,
-            (__local_2__."author_id")
-          ) as object
-          from (
-            select __local_2__.*
-            from "a"."post" as __local_2__
-            where (__local_2__."author_id" = __local_0__."id") and (TRUE) and (TRUE)
-            order by __local_2__."id" ASC
-            limit 2
-          ) __local_2__
-        ) as __local_1__
-      ),
-      '[]'::json
-    )
-  )
-) as "@postsByAuthorIdList",
-to_json(
-  (
-    select coalesce(
-      (
-        select json_agg(__local_3__."object")
-        from (
-          select json_build_object(
-            '__identifiers'::text,
-            json_build_array(__local_4__."id"),
-            'headline'::text,
-            (__local_4__."headline"),
-            'authorId'::text,
-            (__local_4__."author_id")
-          ) as object
-          from (
-            select __local_4__.*
-            from "a"."post" as __local_4__
-            where (__local_4__."author_id" = __local_0__."id")
-            and (
-              __local_4__."headline" = $1
-            ) and (TRUE) and (TRUE)
-            order by __local_4__."id" ASC
-          ) __local_4__
-        ) as __local_3__
-      ),
-      '[]'::json
-    )
-  )
-) as "@roundOnePost",
-to_json(
-  (
-    select coalesce(
-      (
-        select json_agg(__local_5__."object")
-        from (
-          select json_build_object(
-            '__identifiers'::text,
-            json_build_array(
-              __local_6__."person_id_1",
-              __local_6__."person_id_2"
-            ),
-            'personId1'::text,
-            (__local_6__."person_id_1"),
-            'personId2'::text,
-            (__local_6__."person_id_2")
-          ) as object
-          from (
-            select __local_6__.*
-            from "c"."compound_key" as __local_6__
-            where (__local_6__."person_id_1" = __local_0__."id") and (TRUE) and (TRUE)
-            order by __local_6__."person_id_1" ASC,
-            __local_6__."person_id_2" ASC
-          ) __local_6__
-        ) as __local_5__
-      ),
-      '[]'::json
-    )
-  )
-) as "@compoundKeysByPersonId1List",
-to_json(
-  (
-    select coalesce(
-      (
-        select json_agg(__local_7__."object")
-        from (
-          select json_build_object(
-            '__identifiers'::text,
-            json_build_array(
-              __local_8__."person_id_1",
-              __local_8__."person_id_2"
-            ),
-            'personId1'::text,
-            (__local_8__."person_id_1"),
-            'personId2'::text,
-            (__local_8__."person_id_2")
-          ) as object
-          from (
-            select __local_8__.*
-            from "c"."compound_key" as __local_8__
-            where (__local_8__."person_id_2" = __local_0__."id") and (TRUE) and (TRUE)
-            order by __local_8__."person_id_1" ASC,
-            __local_8__."person_id_2" ASC
-          ) __local_8__
-        ) as __local_7__
-      ),
-      '[]'::json
-    )
-  )
-) as "@compoundKeysByPersonId2List"
+select __person_result__.*
 from (
-  select __local_0__.*
-  from "c"."person" as __local_0__
-  where (TRUE) and (TRUE)
-  order by __local_0__."id" ASC
-) __local_0__
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"text" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __person_identifiers__,
+lateral (
+  select
+    (select json_agg(_) from (
+      select
+        __post__."headline" as "0",
+        __post__."author_id"::text as "1"
+      from "a"."post" as __post__
+      where (
+        __person__."id"::"int4" = __post__."author_id"
+      )
+      order by __post__."id" asc
+      limit 2
+    ) _) as "0",
+    (select json_agg(_) from (
+      select
+        __post_2."headline" as "0",
+        __post_2."author_id"::text as "1"
+      from "a"."post" as __post_2
+      where
+        (
+          __post_2."headline" = __person_identifiers__."id0"
+        ) and (
+          __person__."id"::"int4" = __post_2."author_id"
+        )
+      order by __post_2."id" asc
+    ) _) as "1",
+    (select json_agg(_) from (
+      select
+        __compound_key__."person_id_1"::text as "0",
+        __compound_key__."person_id_2"::text as "1"
+      from "c"."compound_key" as __compound_key__
+      where (
+        __person__."id"::"int4" = __compound_key__."person_id_1"
+      )
+      order by __compound_key__."person_id_1" asc, __compound_key__."person_id_2" asc
+    ) _) as "2",
+    (select json_agg(_) from (
+      select
+        __compound_key_2."person_id_1"::text as "0",
+        __compound_key_2."person_id_2"::text as "1"
+      from "c"."compound_key" as __compound_key_2
+      where (
+        __person__."id"::"int4" = __compound_key_2."person_id_2"
+      )
+      order by __compound_key_2."person_id_1" asc, __compound_key_2."person_id_2" asc
+    ) _) as "3",
+    __person__."id"::text as "4",
+    __person__."person_full_name" as "5",
+    __person_identifiers__.idx as "6"
+  from "c"."person" as __person__
+  order by __person__."id" asc
+) as __person_result__
 
-select to_json(
-  json_build_array(
-    __local_0__."person_id_1",
-    __local_0__."person_id_2"
-  )
-) as "__identifiers",
-to_json((__local_0__."person_id_1")) as "personId1",
-to_json((__local_0__."person_id_2")) as "personId2"
-from (
-  select __local_0__.*
-  from "c"."compound_key" as __local_0__
-  where (TRUE) and (TRUE)
-  order by __local_0__."person_id_1" ASC,
-  __local_0__."person_id_2" ASC
-) __local_0__
+select
+  __compound_key__."person_id_1"::text as "0",
+  __compound_key__."person_id_2"::text as "1"
+from "c"."compound_key" as __compound_key__
+order by __compound_key__."person_id_1" asc, __compound_key__."person_id_2" asc
