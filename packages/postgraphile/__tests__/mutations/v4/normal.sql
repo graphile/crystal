9cf6d0b43af4e0be5691645b970c854ed7f85c36
Benjie Gillam <benjie@jemjie.com>
2022-07-05 10:53:50 +0100

    Update snapshots

--- packages/postgraphile/__tests__/mutations/v4/normal.sql
+++ packages/postgraphile/__tests__/mutations/v4/normal.sql
@@ -1,49 +1,24 @@
-SAVEPOINT graphql_mutation
-
-with __local_0__ as (
-  select __local_1__.*
+select __authenticate_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0",
+    (ids.value->>1)::"numeric" as "id1",
+    (ids.value->>2)::"int8" as "id2"
+  from json_array_elements($1::json) with ordinality as ids
+) as __authenticate_identifiers__,
+lateral (
+  select
+    __authenticate__."role" as "0",
+    __authenticate__."exp"::text as "1",
+    __authenticate__."a"::text as "2",
+    __authenticate__."b"::text as "3",
+    __authenticate__."c"::text as "4",
+    (not (__authenticate__ is null))::text as "5",
+    __authenticate_identifiers__.idx as "6"
   from "b"."authenticate"(
-    $1,
-    $2,
-    $3
-  ) __local_1__
-)
-select (
-  (
-    case when __local_0__ is null then null else __local_0__ end
-  )
-)::text
-from __local_0__
-
-with __local_0__ as (
-  select (
-    str::"b"."jwt_token"
-  ).*
-  from unnest(
-    (
-      $1
-    )::text[]
-  ) str
-)
-select to_json(
-  (
-    json_build_object(
-      'role'::text,
-      (__local_0__."role"),
-      'exp'::text,
-      ((__local_0__."exp"))::text,
-      'a'::text,
-      (__local_0__."a"),
-      'b'::text,
-      ((__local_0__."b"))::text,
-      'c'::text,
-      ((__local_0__."c"))::text
-    )
-  )
-) as "@jwtToken"
-from __local_0__ as __local_0__
-where (
-  not (__local_0__ is null)
-) and (TRUE) and (TRUE)
-
-RELEASE SAVEPOINT graphql_mutation
+    __authenticate_identifiers__."id0",
+    __authenticate_identifiers__."id1",
+    __authenticate_identifiers__."id2"
+  ) as __authenticate__
+) as __authenticate_result__
