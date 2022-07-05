6123fde183222452400ed48be536631c19d476d1
Benjie Gillam <benjie@jemjie.com>
2022-07-05 09:19:24 +0100

    Update snapshots

--- packages/postgraphile/__tests__/queries/v4/unique-foreign-keys.sql
+++ packages/postgraphile/__tests__/queries/v4/unique-foreign-keys.sql
@@ -1,100 +1,37 @@
-with __local_0__ as (
-  select to_json(
-    (
-      json_build_object(
-        '__identifiers'::text,
-        json_build_array(
-          __local_1__."person_id_1",
-          __local_1__."person_id_2"
-        ),
-        'personId1'::text,
-        (__local_1__."person_id_1"),
-        'personId2'::text,
-        (__local_1__."person_id_2"),
-        '@uniqueForeignKeyByCompoundKey1AndCompoundKey2'::text,
-        (
-          select (
-            case when (__local_2__ is null) then null else json_build_object(
-              'compoundKey1'::text,
-              (__local_2__."compound_key_1"),
-              'compoundKey2'::text,
-              (__local_2__."compound_key_2"),
-              '@compoundKeyByCompoundKey1AndCompoundKey2'::text,
-              (
-                select json_build_object(
-                  '__identifiers'::text,
-                  json_build_array(
-                    __local_3__."person_id_1",
-                    __local_3__."person_id_2"
-                  ),
-                  'personId1'::text,
-                  (__local_3__."person_id_1"),
-                  'personId2'::text,
-                  (__local_3__."person_id_2"),
-                  '@uniqueForeignKeyByCompoundKey1AndCompoundKey2'::text,
-                  (
-                    select (
-                      case when (__local_4__ is null) then null else json_build_object(
-                        'compoundKey1'::text,
-                        (__local_4__."compound_key_1"),
-                        'compoundKey2'::text,
-                        (__local_4__."compound_key_2")
-                      ) end
-                    ) as object
-                    from "a"."unique_foreign_key" as __local_4__
-                    where (
-                      not (__local_4__ is null)
-                    )
-                    and (
-                      __local_4__."compound_key_1" = __local_3__."person_id_1"
-                    )
-                    and (
-                      __local_4__."compound_key_2" = __local_3__."person_id_2"
-                    ) and (TRUE) and (TRUE)
-                  )
-                ) as object
-                from "c"."compound_key" as __local_3__
-                where (
-                  __local_2__."compound_key_1" = __local_3__."person_id_1"
-                )
-                and (
-                  __local_2__."compound_key_2" = __local_3__."person_id_2"
-                ) and (TRUE) and (TRUE)
-              )
-            ) end
-          ) as object
-          from "a"."unique_foreign_key" as __local_2__
-          where (
-            not (__local_2__ is null)
-          )
-          and (
-            __local_2__."compound_key_1" = __local_1__."person_id_1"
-          )
-          and (
-            __local_2__."compound_key_2" = __local_1__."person_id_2"
-          ) and (TRUE) and (TRUE)
-        )
-      )
-    )
-  ) as "@nodes"
-  from (
-    select __local_1__.*
-    from "c"."compound_key" as __local_1__
-    where (TRUE) and (TRUE)
-    order by __local_1__."person_id_1" ASC,
-    __local_1__."person_id_2" ASC
-  ) __local_1__
-),
-__local_5__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
+select
+  __unique_foreign_key__."compound_key_1"::text as "0",
+  __unique_foreign_key__."compound_key_2"::text as "1",
+  (not (__unique_foreign_key__ is null))::text as "2",
+  __compound_key__."person_id_1"::text as "3",
+  __compound_key__."person_id_2"::text as "4",
+  __unique_foreign_key_2."compound_key_1"::text as "5",
+  __unique_foreign_key_2."compound_key_2"::text as "6",
+  (not (__unique_foreign_key_2 is null))::text as "7",
+  __compound_key_2."person_id_1"::text as "8",
+  __compound_key_2."person_id_2"::text as "9"
+from "c"."compound_key" as __compound_key_2
+left outer join "a"."unique_foreign_key" as __unique_foreign_key_2
+on (
   (
-    select __local_5__.data
-    from __local_5__
-  ),
-  '[]'::json
-) as "data"
+    __compound_key_2."person_id_1"::"int4" = __unique_foreign_key_2."compound_key_1"
+  ) and (
+    __compound_key_2."person_id_2"::"int4" = __unique_foreign_key_2."compound_key_2"
+  )
+)
+left outer join "c"."compound_key" as __compound_key__
+on (
+  (
+    __unique_foreign_key_2."compound_key_1"::"int4" = __compound_key__."person_id_1"
+  ) and (
+    __unique_foreign_key_2."compound_key_2"::"int4" = __compound_key__."person_id_2"
+  )
+)
+left outer join "a"."unique_foreign_key" as __unique_foreign_key__
+on (
+  (
+    __compound_key__."person_id_1"::"int4" = __unique_foreign_key__."compound_key_1"
+  ) and (
+    __compound_key__."person_id_2"::"int4" = __unique_foreign_key__."compound_key_2"
+  )
+)
+order by __compound_key_2."person_id_1" asc, __compound_key_2."person_id_2" asc
