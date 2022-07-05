eecdf4aa26a1cee8218e88bfd21a6bd9b2cc56aa
Benjie Gillam <benjie@jemjie.com>
2022-07-05 11:10:33 +0100

    Update snapshots

--- packages/postgraphile/__tests__/mutations/v4/pgJwtTypeIdentifier.sql
+++ packages/postgraphile/__tests__/mutations/v4/pgJwtTypeIdentifier.sql
@@ -9,8 +9,13 @@ from (
 ) as __authenticate_identifiers__,
 lateral (
   select
-    (not (__authenticate__ is null))::text as "0",
-    __authenticate_identifiers__.idx as "1"
+    __authenticate__."role" as "0",
+    __authenticate__."exp"::text as "1",
+    __authenticate__."a"::text as "2",
+    __authenticate__."b"::text as "3",
+    __authenticate__."c"::text as "4",
+    (not (__authenticate__ is null))::text as "5",
+    __authenticate_identifiers__.idx as "6"
   from "b"."authenticate"(
     __authenticate_identifiers__."id0",
     __authenticate_identifiers__."id1",
