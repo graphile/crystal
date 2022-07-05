106a2a4bd757a7559f893b7888e8ecad92ccb2cb
Benjie Gillam <benjie@jemjie.com>
2022-07-05 09:19:53 +0100

    Update snapshots (including new cursors)

--- packages/postgraphile/__tests__/queries/v4/view.sql
+++ packages/postgraphile/__tests__/queries/v4/view.sql
@@ -1,93 +1,15 @@
-with __local_0__ as (
-  select to_json(
-    (
-      json_build_object(
-        '@node'::text,
-        (
-          json_build_object(
-            'testviewid'::text,
-            (__local_1__."testviewid"),
-            'col1'::text,
-            (__local_1__."col1"),
-            'col2'::text,
-            (__local_1__."col2")
-          )
-        )
-      )
-    )
-  ) as "@edges",
-  to_json(
-    json_build_array(
-      'view_unique_key_asc',
-      json_build_array(__local_1__."testviewid")
-    )
-  ) as "__cursor"
-  from (
-    select __local_1__.*
-    from "a"."testview" as __local_1__
-    where (TRUE) and (TRUE)
-    order by __local_1__."testviewid" ASC
-  ) __local_1__
-),
-__local_2__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
-  (
-    select __local_2__.data
-    from __local_2__
-  ),
-  '[]'::json
-) as "data"
+select
+  (row_number() over (partition by 1))::text as "0",
+  __testview__."testviewid"::text as "1",
+  __testview__."col1"::text as "2",
+  __testview__."col2"::text as "3",
+  (not (__testview__ is null))::text as "4"
+from "a"."testview" as __testview__
 
-with __local_0__ as (
-  select to_json(
-    (
-      json_build_object(
-        '@node'::text,
-        (
-          json_build_object(
-            'testviewid'::text,
-            (__local_1__."testviewid"),
-            'col1'::text,
-            (__local_1__."col1"),
-            'col2'::text,
-            (__local_1__."col2")
-          )
-        )
-      )
-    )
-  ) as "@edges",
-  to_json(
-    json_build_array(
-      'col1_desc',
-      json_build_array(
-        __local_1__."col1",
-        __local_1__."testviewid"
-      )
-    )
-  ) as "__cursor"
-  from (
-    select __local_1__.*
-    from "a"."testview" as __local_1__
-    where (TRUE) and (TRUE)
-    order by __local_1__."col1" DESC,
-    __local_1__."testviewid" ASC
-  ) __local_1__
-),
-__local_2__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
-  (
-    select __local_2__.data
-    from __local_2__
-  ),
-  '[]'::json
-) as "data"
+select
+  __testview__."col1"::text as "0",
+  __testview__."testviewid"::text as "1",
+  __testview__."col2"::text as "2",
+  (not (__testview__ is null))::text as "3"
+from "a"."testview" as __testview__
+order by __testview__."col1" desc
