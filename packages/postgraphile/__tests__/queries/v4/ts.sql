f1cc4d574e1c2365b64a419c704d2f2d14bdad38
Benjie Gillam <benjie@jemjie.com>
2022-07-05 09:48:30 +0100

    Update snapshots

--- packages/postgraphile/__tests__/queries/v4/ts.sql
+++ packages/postgraphile/__tests__/queries/v4/ts.sql
@@ -1,22 +1,23 @@
-select to_json(
-  case when ((__local_0__."ts")) is null then null else json_build_object(
-    'start',
-    case when lower((__local_0__."ts")) is null then null else json_build_object(
-      'value',
-      lower((__local_0__."ts")),
-      'inclusive',
-      lower_inc((__local_0__."ts"))
-    ) end,
-    'end',
-    case when upper((__local_0__."ts")) is null then null else json_build_object(
-      'value',
-      upper((__local_0__."ts")),
-      'inclusive',
-      upper_inc((__local_0__."ts"))
-    ) end
-  ) end
-) as "ts"
-from "ranges"."range_test" as __local_0__
-where (
-  __local_0__."id" = $1
-) and (TRUE) and (TRUE)
+select __range_test_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __range_test_identifiers__,
+lateral (
+  select
+    json_build_array(
+      lower_inc(__range_test__."ts"),
+      to_char(lower(__range_test__."ts"), 'YYYY-MM-DD"T"HH24:MI:SS.US'::text),
+      to_char(upper(__range_test__."ts"), 'YYYY-MM-DD"T"HH24:MI:SS.US'::text),
+      upper_inc(__range_test__."ts")
+    )::text as "0",
+    __range_test__."id"::text as "1",
+    __range_test_identifiers__.idx as "2"
+  from "ranges"."range_test" as __range_test__
+  where (
+    __range_test__."id" = __range_test_identifiers__."id0"
+  )
+  order by __range_test__."id" asc
+) as __range_test_result__
