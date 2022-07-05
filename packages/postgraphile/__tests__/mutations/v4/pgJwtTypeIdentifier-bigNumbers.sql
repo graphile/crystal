b50798ec742d60cd729d6df1a1bc7f1266824bdc
Benjie Gillam <benjie@jemjie.com>
2022-07-05 12:17:19 +0100

    Import v4 test

---
+++ packages/postgraphile/__tests__/mutations/v4/pgJwtTypeIdentifier-bigNumbers.sql
@@ -0,0 +1,75 @@
+SAVEPOINT graphql_mutation
+
+with __local_0__ as (
+  select __local_1__.*
+  from "b"."authenticate"(
+    $1,
+    $2,
+    $3
+  ) __local_1__
+)
+select (
+  (
+    case when __local_0__ is null then null else __local_0__ end
+  )
+)::text
+from __local_0__
+
+with __local_0__ as (
+  select (
+    str::"b"."jwt_token"
+  ).*
+  from unnest(
+    (
+      $1
+    )::text[]
+  ) str
+)
+select to_json(
+  json_build_object(
+    'role'::text,
+    (__local_0__)."role",
+    'exp'::text,
+    (
+      (__local_0__)."exp"
+    )::text,
+    'a'::text,
+    (__local_0__)."a",
+    'b'::text,
+    (
+      (__local_0__)."b"
+    )::text,
+    'c'::text,
+    (
+      (__local_0__)."c"
+    )::text
+  )
+) as "value",
+to_json(
+  (
+    to_json(
+      json_build_object(
+        'role'::text,
+        (__local_0__)."role",
+        'exp'::text,
+        (
+          (__local_0__)."exp"
+        )::text,
+        'a'::text,
+        (__local_0__)."a",
+        'b'::text,
+        (
+          (__local_0__)."b"
+        )::text,
+        'c'::text,
+        (
+          (__local_0__)."c"
+        )::text
+      )
+    )
+  )
+) as "@jwtToken"
+from __local_0__ as __local_0__
+where (TRUE) and (TRUE)
+
+RELEASE SAVEPOINT graphql_mutation
