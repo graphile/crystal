83604a79f56a11f23cb30be3044aa1d7741193a4
Benjie Gillam <benjie@jemjie.com>
2022-07-05 15:13:55 +0100

    Update snapshots

--- packages/postgraphile/__tests__/mutations/v4/enum_tables.mutations.sql
+++ packages/postgraphile/__tests__/mutations/v4/enum_tables.mutations.sql
@@ -1,128 +1,26 @@
-SAVEPOINT graphql_mutation
+delete from "enum_tables"."letter_descriptions" as __letter_descriptions__ where (__letter_descriptions__."letter" = $1::"text") returning
+  __letter_descriptions__."id"::text as "0",
+  __letter_descriptions__."letter"::text as "1",
+  __letter_descriptions__."letter_via_view"::text as "2"
 
-with __local_0__ as (
-  delete
-  from "enum_tables"."letter_descriptions"
-  where (
-    "letter" = $1
-  ) returning *
-)
-select (
-  (
-    case when __local_0__ is null then null else __local_0__ end
-  )
-)::text
-from __local_0__
 
-with __local_0__ as (
-  select (
-    str::"enum_tables"."letter_descriptions"
-  ).*
-  from unnest(
-    (
-      $1
-    )::text[]
-  ) str
-)
-select to_json(
-  (
-    json_build_object(
-      'id'::text,
-      (__local_0__."id"),
-      'letter'::text,
-      (__local_0__."letter"),
-      'letterViaView'::text,
-      (__local_0__."letter_via_view")
-    )
-  )
-) as "@letterDescription"
-from __local_0__ as __local_0__
-where (TRUE) and (TRUE)
+insert into "enum_tables"."letter_descriptions" as __letter_descriptions__ ("letter", "letter_via_view", "description") values ($1::"text", $2::"text", $3::"text") returning
+  __letter_descriptions__."id"::text as "0",
+  __letter_descriptions__."letter"::text as "1",
+  __letter_descriptions__."letter_via_view"::text as "2",
+  __letter_descriptions__."description" as "3"
 
-RELEASE SAVEPOINT graphql_mutation
 
-SAVEPOINT graphql_mutation
-
-with __local_0__ as (
-  insert into "enum_tables"."letter_descriptions" (
-    "letter",
-    "letter_via_view",
-    "description"
-  ) values(
-    $1,
-    $2,
-    $3
-  ) returning *
-)
-select (
-  (
-    case when __local_0__ is null then null else __local_0__ end
-  )
-)::text
-from __local_0__
-
-with __local_0__ as (
-  select (
-    str::"enum_tables"."letter_descriptions"
-  ).*
-  from unnest(
-    (
-      $1
-    )::text[]
-  ) str
-)
-select to_json(
-  (
-    json_build_object(
-      'id'::text,
-      (__local_0__."id"),
-      'letter'::text,
-      (__local_0__."letter"),
-      'letterViaView'::text,
-      (__local_0__."letter_via_view"),
-      'description'::text,
-      (__local_0__."description")
-    )
-  )
-) as "@letterDescription"
-from __local_0__ as __local_0__
-where (TRUE) and (TRUE)
-
-RELEASE SAVEPOINT graphql_mutation
-
-SAVEPOINT graphql_mutation
-
-with __local_0__ as (
-  select __local_1__ as __local_0__
-  from "enum_tables"."referencing_table_mutation"(
-    row(
-      NULL,
-      $1::"unknown",
-      $2::"unknown",
-      $3::"unknown"
-    )::"enum_tables"."referencing_table"
-  ) __local_1__
-)
-select (
-  (__local_0__.__local_0__)::"pg_catalog"."int4"
-)::text
-from __local_0__
-
-with __local_0__ as (
-  select str::"pg_catalog"."int4" as __local_0__
-  from unnest(
-    (
-      $1
-    )::text[]
-  ) str
-)
-select to_json(__local_0__) as "value",
-to_json(
-  (
-    to_json(__local_0__)
-  )
-) as "@integer"
-from __local_0__ as __local_0__
-where (TRUE) and (TRUE)
-
-RELEASE SAVEPOINT graphql_mutation
+select __referencing_table_mutation_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"enum_tables"."referencing_table" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __referencing_table_mutation_identifiers__,
+lateral (
+  select
+    __referencing_table_mutation__.v::text as "0",
+    __referencing_table_mutation_identifiers__.idx as "1"
+  from "enum_tables"."referencing_table_mutation"(__referencing_table_mutation_identifiers__."id0") as __referencing_table_mutation__(v)
+) as __referencing_table_mutation_result__
