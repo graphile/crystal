22e2936941c5af023d9d6cb3a4d83caaf7b99946
Benjie Gillam <benjie@jemjie.com>
2022-07-05 09:18:58 +0100

    Update snapshots

--- packages/postgraphile/__tests__/queries/v4/unique-constraints.sql
+++ packages/postgraphile/__tests__/queries/v4/unique-constraints.sql
@@ -1,67 +1,106 @@
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."person_full_name")) as "name",
-to_json((__local_0__."email")) as "email"
-from "c"."person" as __local_0__
-where (
-  __local_0__."email" = $1
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."person_full_name")) as "name",
-to_json((__local_0__."email")) as "email"
-from "c"."person" as __local_0__
-where (
-  __local_0__."email" = $1
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(
-    __local_0__."person_id_1",
-    __local_0__."person_id_2"
+select __person_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"b"."email" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __person_identifiers__,
+lateral (
+  select
+    __person__."person_full_name" as "0",
+    __person__."email" as "1",
+    __person_identifiers__.idx as "2"
+  from "c"."person" as __person__
+  where (
+    __person__."email" = __person_identifiers__."id0"
   )
-) as "__identifiers",
-to_json((__local_0__."person_id_1")) as "personId1",
-to_json((__local_0__."person_id_2")) as "personId2"
-from "c"."compound_key" as __local_0__
-where (
-  __local_0__."person_id_1" = $1
-)
-and (
-  __local_0__."person_id_2" = $2
-) and (TRUE) and (TRUE)
+  order by __person__."id" asc
+) as __person_result__
 
-select to_json(
-  json_build_array(
-    __local_0__."person_id_1",
-    __local_0__."person_id_2"
+select __person_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"b"."email" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __person_identifiers__,
+lateral (
+  select
+    __person__."person_full_name" as "0",
+    __person__."email" as "1",
+    __person_identifiers__.idx as "2"
+  from "c"."person" as __person__
+  where (
+    __person__."email" = __person_identifiers__."id0"
   )
-) as "__identifiers",
-to_json((__local_0__."person_id_1")) as "personId1",
-to_json((__local_0__."person_id_2")) as "personId2"
-from "c"."compound_key" as __local_0__
-where (
-  __local_0__."person_id_1" = $1
-)
-and (
-  __local_0__."person_id_2" = $2
-) and (TRUE) and (TRUE)
+  order by __person__."id" asc
+) as __person_result__
 
-select to_json(
-  json_build_array(
-    __local_0__."person_id_1",
-    __local_0__."person_id_2"
-  )
-) as "__identifiers",
-to_json((__local_0__."person_id_1")) as "personId1",
-to_json((__local_0__."person_id_2")) as "personId2"
-from "c"."compound_key" as __local_0__
-where (
-  __local_0__."person_id_1" = $1
-)
-and (
-  __local_0__."person_id_2" = $2
-) and (TRUE) and (TRUE)
+select __compound_key_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0",
+    (ids.value->>1)::"int4" as "id1"
+  from json_array_elements($1::json) with ordinality as ids
+) as __compound_key_identifiers__,
+lateral (
+  select
+    __compound_key__."person_id_1"::text as "0",
+    __compound_key__."person_id_2"::text as "1",
+    __compound_key_identifiers__.idx as "2"
+  from "c"."compound_key" as __compound_key__
+  where
+    (
+      __compound_key__."person_id_1" = __compound_key_identifiers__."id0"
+    ) and (
+      __compound_key__."person_id_2" = __compound_key_identifiers__."id1"
+    )
+  order by __compound_key__."person_id_1" asc, __compound_key__."person_id_2" asc
+) as __compound_key_result__
+
+select __compound_key_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0",
+    (ids.value->>1)::"int4" as "id1"
+  from json_array_elements($1::json) with ordinality as ids
+) as __compound_key_identifiers__,
+lateral (
+  select
+    __compound_key__."person_id_1"::text as "0",
+    __compound_key__."person_id_2"::text as "1",
+    __compound_key_identifiers__.idx as "2"
+  from "c"."compound_key" as __compound_key__
+  where
+    (
+      __compound_key__."person_id_1" = __compound_key_identifiers__."id0"
+    ) and (
+      __compound_key__."person_id_2" = __compound_key_identifiers__."id1"
+    )
+  order by __compound_key__."person_id_1" asc, __compound_key__."person_id_2" asc
+) as __compound_key_result__
+
+select __compound_key_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0",
+    (ids.value->>1)::"int4" as "id1"
+  from json_array_elements($1::json) with ordinality as ids
+) as __compound_key_identifiers__,
+lateral (
+  select
+    __compound_key__."person_id_1"::text as "0",
+    __compound_key__."person_id_2"::text as "1",
+    __compound_key_identifiers__.idx as "2"
+  from "c"."compound_key" as __compound_key__
+  where
+    (
+      __compound_key__."person_id_1" = __compound_key_identifiers__."id0"
+    ) and (
+      __compound_key__."person_id_2" = __compound_key_identifiers__."id1"
+    )
+  order by __compound_key__."person_id_1" asc, __compound_key__."person_id_2" asc
+) as __compound_key_result__
