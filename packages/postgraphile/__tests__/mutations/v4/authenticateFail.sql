121bf277cb00a39cc8dc086136711fb0ee066828
Benjie Gillam <benjie@jemjie.com>
2022-07-05 11:22:56 +0100

    This is better

--- packages/postgraphile/__tests__/mutations/v4/authenticateFail.sql
+++ packages/postgraphile/__tests__/mutations/v4/authenticateFail.sql
@@ -1,3 +1,4 @@
 select
-  (not (__authenticate_fail__ is null))::text as "0"
+  __authenticate_fail__::text as "0",
+  (not (__authenticate_fail__ is null))::text as "1"
 from "b"."authenticate_fail"() as __authenticate_fail__
