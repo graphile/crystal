73ddff343f8b8485fd09e27cd442186561a08a38
Benjie Gillam <benjie@jemjie.com>
2022-07-05 10:52:51 +0100

    This is clearly wrong...

--- packages/postgraphile/__tests__/mutations/v4/authenticateFail.sql
+++ packages/postgraphile/__tests__/mutations/v4/authenticateFail.sql
@@ -1,14 +1,3 @@
-SAVEPOINT graphql_mutation
-
-with __local_0__ as (
-  select __local_1__.*
-  from "b"."authenticate_fail"( ) __local_1__
-)
-select (
-  (
-    case when __local_0__ is null then null else __local_0__ end
-  )
-)::text
-from __local_0__
-
-RELEASE SAVEPOINT graphql_mutation
+select
+  (not (__authenticate_fail__ is null))::text as "0"
+from "b"."authenticate_fail"() as __authenticate_fail__
