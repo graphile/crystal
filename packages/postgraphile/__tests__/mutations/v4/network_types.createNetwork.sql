3307834693d01d4f80a550a5b4f45536fadb2bc6
Benjie Gillam <benjie@jemjie.com>
2022-07-06 15:23:24 +0100

    Update snapshots

--- packages/postgraphile/__tests__/mutations/v4/network_types.createNetwork.sql
+++ packages/postgraphile/__tests__/mutations/v4/network_types.createNetwork.sql
@@ -1,50 +1,5 @@
-SAVEPOINT graphql_mutation
-
-with __local_0__ as (
-  insert into "network_types"."network" (
-    "inet",
-    "cidr",
-    "macaddr"
-  ) values(
-    $1,
-    $2,
-    $3
-  ) returning *
-)
-select (
-  (
-    case when __local_0__ is null then null else __local_0__ end
-  )
-)::text
-from __local_0__
-
-with __local_0__ as (
-  select (
-    str::"network_types"."network"
-  ).*
-  from unnest(
-    (
-      $1
-    )::text[]
-  ) str
-)
-select to_json(
-  (
-    json_build_object(
-      '__identifiers'::text,
-      json_build_array(__local_0__."id"),
-      'id'::text,
-      (__local_0__."id"),
-      'inet'::text,
-      (__local_0__."inet"),
-      'cidr'::text,
-      (__local_0__."cidr"),
-      'macaddr'::text,
-      (__local_0__."macaddr")
-    )
-  )
-) as "@network"
-from __local_0__ as __local_0__
-where (TRUE) and (TRUE)
-
-RELEASE SAVEPOINT graphql_mutation
+insert into "network_types"."network" as __network__ ("inet", "cidr", "macaddr") values ($1::"inet", $2::"cidr", $3::"macaddr") returning
+  __network__."id"::text as "0",
+  __network__."inet"::text as "1",
+  __network__."cidr"::text as "2",
+  __network__."macaddr"::text as "3"
