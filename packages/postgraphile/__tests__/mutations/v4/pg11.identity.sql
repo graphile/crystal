ff236b0a4b2d4588bf3d16c458c99046499994bc
Benjie Gillam <benjie@jemjie.com>
2022-07-06 15:35:05 +0100

    More resetting

--- packages/postgraphile/__tests__/mutations/v4/pg11.identity.sql
+++ packages/postgraphile/__tests__/mutations/v4/pg11.identity.sql
@@ -1,120 +1,13 @@
-SAVEPOINT graphql_mutation
+insert into "pg11"."always_as_identity" as __always_as_identity__ ("t") values ($1::"text") returning
+  __always_as_identity__."id"::text as "0",
+  __always_as_identity__."t" as "1"
 
-with __local_0__ as (
-  insert into "pg11"."always_as_identity" ("t") values(
-    $1
-  ) returning *
-)
-select (
-  (
-    case when __local_0__ is null then null else __local_0__ end
-  )
-)::text
-from __local_0__
 
-with __local_0__ as (
-  select (
-    str::"pg11"."always_as_identity"
-  ).*
-  from unnest(
-    (
-      $1
-    )::text[]
-  ) str
-)
-select to_json(
-  (
-    json_build_object(
-      'id'::text,
-      (__local_0__."id"),
-      't'::text,
-      (__local_0__."t")
-    )
-  )
-) as "@alwaysAsIdentity"
-from __local_0__ as __local_0__
-where (TRUE) and (TRUE)
+insert into "pg11"."by_default_as_identity" as __by_default_as_identity__ ("t") values ($1::"text") returning
+  __by_default_as_identity__."id"::text as "0",
+  __by_default_as_identity__."t" as "1"
 
-RELEASE SAVEPOINT graphql_mutation
 
-SAVEPOINT graphql_mutation
-
-with __local_0__ as (
-  insert into "pg11"."by_default_as_identity" ("t") values(
-    $1
-  ) returning *
-)
-select (
-  (
-    case when __local_0__ is null then null else __local_0__ end
-  )
-)::text
-from __local_0__
-
-with __local_0__ as (
-  select (
-    str::"pg11"."by_default_as_identity"
-  ).*
-  from unnest(
-    (
-      $1
-    )::text[]
-  ) str
-)
-select to_json(
-  (
-    json_build_object(
-      'id'::text,
-      (__local_0__."id"),
-      't'::text,
-      (__local_0__."t")
-    )
-  )
-) as "@byDefaultAsIdentity"
-from __local_0__ as __local_0__
-where (TRUE) and (TRUE)
-
-RELEASE SAVEPOINT graphql_mutation
-
-SAVEPOINT graphql_mutation
-
-with __local_0__ as (
-  insert into "pg11"."by_default_as_identity" (
-    "id",
-    "t"
-  ) values(
-    $1,
-    $2
-  ) returning *
-)
-select (
-  (
-    case when __local_0__ is null then null else __local_0__ end
-  )
-)::text
-from __local_0__
-
-with __local_0__ as (
-  select (
-    str::"pg11"."by_default_as_identity"
-  ).*
-  from unnest(
-    (
-      $1
-    )::text[]
-  ) str
-)
-select to_json(
-  (
-    json_build_object(
-      'id'::text,
-      (__local_0__."id"),
-      't'::text,
-      (__local_0__."t")
-    )
-  )
-) as "@byDefaultAsIdentity"
-from __local_0__ as __local_0__
-where (TRUE) and (TRUE)
-
-RELEASE SAVEPOINT graphql_mutation
+insert into "pg11"."by_default_as_identity" as __by_default_as_identity__ ("id", "t") values ($1::"int4", $2::"text") returning
+  __by_default_as_identity__."id"::text as "0",
+  __by_default_as_identity__."t" as "1"
