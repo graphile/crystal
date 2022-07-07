2bf43bbf3f1d44893e9399aa60bb357275d2b3ea
Benjie Gillam <benjie@jemjie.com>
2022-07-07 16:34:49 +0100

    Update snapshots

--- packages/postgraphile/__tests__/mutations/v4/rbac.leftArmIdentity.sql
+++ packages/postgraphile/__tests__/mutations/v4/rbac.leftArmIdentity.sql
@@ -1,52 +1,16 @@
-SAVEPOINT graphql_mutation
-
-with __local_0__ as (
-  select __local_1__.*
-  from "c"."left_arm_identity"(
-    row(
-      $1::"pg_catalog"."int4",
-      $2::"pg_catalog"."int4",
-      $3::"pg_catalog"."float8",
-      $4::"pg_catalog"."text"
-    )::"c"."left_arm"
-  ) __local_1__
-)
-select (
-  (
-    case when __local_0__ is null then null else __local_0__ end
-  )
-)::text
-from __local_0__
-
-with __local_0__ as (
-  select (
-    str::"c"."left_arm"
-  ).*
-  from unnest(
-    (
-      $1
-    )::text[]
-  ) str
-)
-select to_json(
-  (
-    json_build_object(
-      '__identifiers'::text,
-      json_build_array(__local_0__."id"),
-      'id'::text,
-      (__local_0__."id"),
-      'personId'::text,
-      (__local_0__."person_id"),
-      'lengthInMetres'::text,
-      (__local_0__."length_in_metres"),
-      'mood'::text,
-      (__local_0__."mood")
-    )
-  )
-) as "@leftArm"
-from __local_0__ as __local_0__
-where (
-  not (__local_0__ is null)
-) and (TRUE) and (TRUE)
-
-RELEASE SAVEPOINT graphql_mutation
+select __left_arm_identity_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"c"."left_arm" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __left_arm_identity_identifiers__,
+lateral (
+  select
+    __left_arm_identity__."id"::text as "0",
+    __left_arm_identity__."person_id"::text as "1",
+    __left_arm_identity__."length_in_metres"::text as "2",
+    __left_arm_identity__."mood" as "3",
+    __left_arm_identity_identifiers__.idx as "4"
+  from "c"."left_arm_identity"(__left_arm_identity_identifiers__."id0") as __left_arm_identity__
+) as __left_arm_identity_result__
