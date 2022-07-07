15f835bfcb1a1ca3ebd8c77319553d26e5893620
Benjie Gillam <benjie@jemjie.com>
2022-07-07 16:35:03 +0100

    Import v4 test

---
+++ packages/postgraphile/__tests__/mutations/v4/rbac.updateLeftArm.sql
@@ -0,0 +1,45 @@
+SAVEPOINT graphql_mutation
+
+with __local_0__ as (
+  update "c"."left_arm" set "mood" = $1
+  where (
+    "id" = $2
+  ) returning *
+)
+select (
+  (
+    case when __local_0__ is null then null else __local_0__ end
+  )
+)::text
+from __local_0__
+
+with __local_0__ as (
+  select (
+    str::"c"."left_arm"
+  ).*
+  from unnest(
+    (
+      $1
+    )::text[]
+  ) str
+)
+select to_json(
+  (
+    json_build_object(
+      '__identifiers'::text,
+      json_build_array(__local_0__."id"),
+      'id'::text,
+      (__local_0__."id"),
+      'personId'::text,
+      (__local_0__."person_id"),
+      'lengthInMetres'::text,
+      (__local_0__."length_in_metres"),
+      'mood'::text,
+      (__local_0__."mood")
+    )
+  )
+) as "@leftArm"
+from __local_0__ as __local_0__
+where (TRUE) and (TRUE)
+
+RELEASE SAVEPOINT graphql_mutation
