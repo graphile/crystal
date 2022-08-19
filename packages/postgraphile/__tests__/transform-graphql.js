76d8719f3a1a28a82461ba0bcee27c935f0193fa
Benjie Gillam <benjie@jemjie.com>
2022-08-19 09:26:33 +0100

    TODO

--- packages/postgraphile/__tests__/transform-graphql.js
+++ packages/postgraphile/__tests__/transform-graphql.js
@@ -3,6 +3,7 @@ import transform from "../../dataplan-pg/__tests__/transform-graphql.js";
 export default {
   process: transform.makeProcess({
     includeDeoptimize: false,
+    // TODO: change includeUnprepared to 'true'
     includeUnprepared: false,
   }),
 };
