e101638168bf339cf83c4bcdc2de4f3250647665
Maël Nison <nison.mael@gmail.com>
2022-08-25 18:32:42 +0200

    Exports `extendedFormatError`

--- src/postgraphile/index.ts
+++ src/postgraphile/index.ts
@@ -3,3 +3,4 @@ export { default as withPostGraphileContext, debugPgClient } from './withPostGra
 export { createPostGraphileSchema, watchPostGraphileSchema } from 'postgraphile-core';
 export { PostGraphilePlugin } from './pluginHook';
 export { enhanceHttpServerWithWebSockets } from './http/subscriptions';
+export { extendedFormatError } from './extendedFormatError';
