2036714c367bb4f80eb69fbccfe515c8dc91fdb7
Benjie Gillam <benjie@jemjie.com>
2022-08-26 17:33:50 +0100

    Re-enable the node test

--- packages/postgraphile/__tests__/queries/v4/node.sql
+++ packages/postgraphile/__tests__/queries/v4/node.sql
@@ -1,275 +1,179 @@
-with __local_0__ as (
-  select to_json(
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1"
+from "c"."person" as __person__
+order by __person__."id" asc
+
+select
+  __compound_key__."person_id_2"::text as "0",
+  __compound_key__."person_id_1"::text as "1"
+from "c"."compound_key" as __compound_key__
+order by __compound_key__."person_id_1" asc, __compound_key__."person_id_2" asc
+
+select __person_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __person_identifiers__,
+lateral (
+  select
+    __person__."id"::text as "0",
+    __person__."person_full_name" as "1",
+    __person_identifiers__.idx as "2"
+  from "c"."person" as __person__
+  where (
+    __person__."id" = __person_identifiers__."id0"
+  )
+  order by __person__."id" asc
+) as __person_result__
+
+select __person_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __person_identifiers__,
+lateral (
+  select
+    __person__."id"::text as "0",
+    __person__."person_full_name" as "1",
+    __person_identifiers__.idx as "2"
+  from "c"."person" as __person__
+  where (
+    __person__."id" = __person_identifiers__."id0"
+  )
+  order by __person__."id" asc
+) as __person_result__
+
+select __person_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __person_identifiers__,
+lateral (
+  select
+    __person__."id"::text as "0",
+    __person__."person_full_name" as "1",
+    __person_identifiers__.idx as "2"
+  from "c"."person" as __person__
+  where (
+    __person__."id" = __person_identifiers__."id0"
+  )
+  order by __person__."id" asc
+) as __person_result__
+
+select __compound_key_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0",
+    (ids.value->>1)::"int4" as "id1"
+  from json_array_elements($1::json) with ordinality as ids
+) as __compound_key_identifiers__,
+lateral (
+  select
+    __compound_key__."person_id_2"::text as "0",
+    __compound_key__."person_id_1"::text as "1",
+    __compound_key_identifiers__.idx as "2"
+  from "c"."compound_key" as __compound_key__
+  where
     (
-      json_build_object(
-        '__identifiers'::text,
-        json_build_array(__local_1__."id"),
-        'id'::text,
-        (__local_1__."id"),
-        'name'::text,
-        (__local_1__."person_full_name")
-      )
+      __compound_key__."person_id_1" = __compound_key_identifiers__."id0"
+    ) and (
+      __compound_key__."person_id_2" = __compound_key_identifiers__."id1"
     )
-  ) as "@nodes"
-  from (
-    select __local_1__.*
-    from "c"."person" as __local_1__
-    where (TRUE) and (TRUE)
-    order by __local_1__."id" ASC
-  ) __local_1__
-),
-__local_2__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
-  (
-    select __local_2__.data
-    from __local_2__
-  ),
-  '[]'::json
-) as "data"
+  order by __compound_key__."person_id_1" asc, __compound_key__."person_id_2" asc
+) as __compound_key_result__
 
-with __local_0__ as (
-  select to_json(
+select __compound_key_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0",
+    (ids.value->>1)::"int4" as "id1"
+  from json_array_elements($1::json) with ordinality as ids
+) as __compound_key_identifiers__,
+lateral (
+  select
+    __compound_key__."person_id_2"::text as "0",
+    __compound_key__."person_id_1"::text as "1",
+    __compound_key_identifiers__.idx as "2"
+  from "c"."compound_key" as __compound_key__
+  where
     (
-      json_build_object(
-        '__identifiers'::text,
-        json_build_array(
-          __local_1__."person_id_1",
-          __local_1__."person_id_2"
-        ),
-        'personId1'::text,
-        (__local_1__."person_id_1"),
-        'personId2'::text,
-        (__local_1__."person_id_2")
-      )
+      __compound_key__."person_id_1" = __compound_key_identifiers__."id0"
+    ) and (
+      __compound_key__."person_id_2" = __compound_key_identifiers__."id1"
     )
-  ) as "@nodes"
-  from (
-    select __local_1__.*
-    from "c"."compound_key" as __local_1__
-    where (TRUE) and (TRUE)
-    order by __local_1__."person_id_1" ASC,
-    __local_1__."person_id_2" ASC
-  ) __local_1__
-),
-__local_2__ as (
-  select json_agg(
-    to_json(__local_0__)
-  ) as data
-  from __local_0__
-)
-select coalesce(
-  (
-    select __local_2__.data
-    from __local_2__
-  ),
-  '[]'::json
-) as "data"
+  order by __compound_key__."person_id_1" asc, __compound_key__."person_id_2" asc
+) as __compound_key_result__
 
-select to_json(
-  json_build_array(
-    __local_0__."person_id_1",
-    __local_0__."person_id_2"
+select __compound_key_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0",
+    (ids.value->>1)::"int4" as "id1"
+  from json_array_elements($1::json) with ordinality as ids
+) as __compound_key_identifiers__,
+lateral (
+  select
+    __compound_key__."person_id_2"::text as "0",
+    __compound_key__."person_id_1"::text as "1",
+    __compound_key_identifiers__.idx as "2"
+  from "c"."compound_key" as __compound_key__
+  where
+    (
+      __compound_key__."person_id_1" = __compound_key_identifiers__."id0"
+    ) and (
+      __compound_key__."person_id_2" = __compound_key_identifiers__."id1"
+    )
+  order by __compound_key__."person_id_1" asc, __compound_key__."person_id_2" asc
+) as __compound_key_result__
+
+select __similar_table1_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __similar_table1_identifiers__,
+lateral (
+  select
+    __similar_table1__."id"::text as "0",
+    __similar_table1__."col1"::text as "1",
+    __similar_table1__."col2"::text as "2",
+    __similar_table1__."col3"::text as "3",
+    __similar_table1_identifiers__.idx as "4"
+  from "a"."similar_table_1" as __similar_table1__
+  where (
+    __similar_table1__."id" = __similar_table1_identifiers__."id0"
   )
-) as "__identifiers",
-to_json((__local_0__."person_id_1")) as "personId1",
-to_json((__local_0__."person_id_2")) as "personId2"
-from "c"."compound_key" as __local_0__
-where (
-  __local_0__."person_id_1" = $1
-)
-and (
-  __local_0__."person_id_2" = $2
-) and (TRUE) and (TRUE)
+  order by __similar_table1__."id" asc
+) as __similar_table1_result__
 
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name"
-from "c"."person" as __local_0__
-where (
-  __local_0__."id" = $1
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(
-    __local_0__."person_id_1",
-    __local_0__."person_id_2"
+select __similar_table2_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __similar_table2_identifiers__,
+lateral (
+  select
+    __similar_table2__."id"::text as "0",
+    __similar_table2__."col3"::text as "1",
+    __similar_table2__."col4"::text as "2",
+    __similar_table2__."col5"::text as "3",
+    __similar_table2_identifiers__.idx as "4"
+  from "a"."similar_table_2" as __similar_table2__
+  where (
+    __similar_table2__."id" = __similar_table2_identifiers__."id0"
   )
-) as "__identifiers",
-to_json((__local_0__."person_id_1")) as "personId1",
-to_json((__local_0__."person_id_2")) as "personId2"
-from "c"."compound_key" as __local_0__
-where (
-  __local_0__."person_id_1" = $1
-)
-and (
-  __local_0__."person_id_2" = $2
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name"
-from "c"."person" as __local_0__
-where (
-  __local_0__."id" = $1
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name"
-from "c"."person" as __local_0__
-where (
-  __local_0__."id" = $1
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(
-    __local_0__."person_id_1",
-    __local_0__."person_id_2"
-  )
-) as "__identifiers",
-to_json((__local_0__."person_id_1")) as "personId1",
-to_json((__local_0__."person_id_2")) as "personId2"
-from "c"."compound_key" as __local_0__
-where (
-  __local_0__."person_id_1" = $1
-)
-and (
-  __local_0__."person_id_2" = $2
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name"
-from "c"."person" as __local_0__
-where (
-  __local_0__."id" = $1
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name"
-from "c"."person" as __local_0__
-where (
-  __local_0__."id" = $1
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."person_full_name")) as "name"
-from "c"."person" as __local_0__
-where (
-  __local_0__."id" = $1
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(
-    __local_0__."person_id_1",
-    __local_0__."person_id_2"
-  )
-) as "__identifiers",
-to_json((__local_0__."person_id_1")) as "personId1",
-to_json((__local_0__."person_id_2")) as "personId2"
-from "c"."compound_key" as __local_0__
-where (
-  __local_0__."person_id_1" = $1
-)
-and (
-  __local_0__."person_id_2" = $2
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(
-    __local_0__."person_id_1",
-    __local_0__."person_id_2"
-  )
-) as "__identifiers",
-to_json((__local_0__."person_id_1")) as "personId1",
-to_json((__local_0__."person_id_2")) as "personId2"
-from "c"."compound_key" as __local_0__
-where (
-  __local_0__."person_id_1" = $1
-)
-and (
-  __local_0__."person_id_2" = $2
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(
-    __local_0__."person_id_1",
-    __local_0__."person_id_2"
-  )
-) as "__identifiers",
-to_json((__local_0__."person_id_1")) as "personId1",
-to_json((__local_0__."person_id_2")) as "personId2"
-from "c"."compound_key" as __local_0__
-where (
-  __local_0__."person_id_1" = $1
-)
-and (
-  __local_0__."person_id_2" = $2
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."col1")) as "col1",
-to_json((__local_0__."col2")) as "col2",
-to_json((__local_0__."col3")) as "col3"
-from "a"."similar_table_1" as __local_0__
-where (
-  __local_0__."id" = $1
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."col3")) as "col3",
-to_json((__local_0__."col4")) as "col4",
-to_json((__local_0__."col5")) as "col5"
-from "a"."similar_table_2" as __local_0__
-where (
-  __local_0__."id" = $1
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."col1")) as "col1",
-to_json((__local_0__."col2")) as "col2",
-to_json((__local_0__."col3")) as "col3"
-from "a"."similar_table_1" as __local_0__
-where (
-  __local_0__."id" = $1
-) and (TRUE) and (TRUE)
-
-select to_json(
-  json_build_array(__local_0__."id")
-) as "__identifiers",
-to_json((__local_0__."id")) as "id",
-to_json((__local_0__."col3")) as "col3",
-to_json((__local_0__."col4")) as "col4",
-to_json((__local_0__."col5")) as "col5"
-from "a"."similar_table_2" as __local_0__
-where (
-  __local_0__."id" = $1
-) and (TRUE) and (TRUE)
+  order by __similar_table2__."id" asc
+) as __similar_table2_result__
