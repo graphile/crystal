7b28df784443cca8d7b0c073a8c61594834afb27
Benjie Gillam <benjie@jemjie.com>
2022-08-26 16:11:56 +0100

    Remove more code we don't use

--- packages/dataplanner/src/graphqlMergeSelectionSets.ts
+++ packages/dataplanner/src/graphqlMergeSelectionSets.ts
@@ -1,6 +1,5 @@
 import type {
   FieldNode,
-  GraphQLInterfaceType,
   GraphQLObjectType,
   NamedTypeNode,
   SelectionNode,
@@ -9,51 +8,6 @@ import { Kind } from "graphql";
 import { inspect } from "util";
 
 import type { OperationPlan } from "./index.js";
-
-/**
- * Given a list of object types and a list of selections, returns only the
- * types that may be queried in that selection (e.g. because there's a type
- * fragment on them or on one of the interfaces they implement).
- */
-function _typesUsedInSelections(
-  operationPlan: OperationPlan,
-  types: ReadonlyArray<GraphQLObjectType>,
-  selections: ReadonlyArray<SelectionNode>,
-): GraphQLObjectType[] {
-  const typeNamesMap: { [typeName: string]: true } = Object.create(null);
-  for (let i = 0, l = selections.length; i < l; i++) {
-    const selection = selections[i];
-    switch (selection.kind) {
-      case "FragmentSpread": {
-        // Assumed to exist because query passed validation.
-        const fragment = operationPlan.fragments[selection.name.value];
-        const typeCondition = fragment.typeCondition;
-        typeNamesMap[typeCondition.name.value] = true;
-        break;
-      }
-      case "InlineFragment": {
-        const typeCondition = selection.typeCondition;
-        if (typeCondition) {
-          typeNamesMap[typeCondition.name.value] = true;
-        }
-        break;
-      }
-      default: {
-        /* noop */
-      }
-    }
-  }
-  // Return types with matching names, or where they have an interface with a
-  // matching name, or where they belong to a union with a matching name
-  return types.filter(
-    (t) =>
-      typeNamesMap[t.name] ||
-      t.getInterfaces().some((i) => typeNamesMap[i.name]) ||
-      operationPlan.unionsContainingObjectType[t.name].some(
-        (u) => typeNamesMap[u.name],
-      ),
-  );
-}
 
 // TODO:perf: this is incredibly inefficient
 function typeMatchesCondition(
@@ -134,75 +88,4 @@ export function fieldSelectionsForType(
     }
   }
   return result;
-}
-
-/**
- * Given an interface type and a list of selections, returns true if any
- * non-introspection fields are queried on the interface (or any of the
- * interfaces it implements), false otherwise.
- */
-function _interfaceTypeHasNonIntrospectionFieldQueriedInSelections(
-  operationPlan: OperationPlan,
-  interfaceType: GraphQLInterfaceType,
-  selections: readonly SelectionNode[],
-): boolean {
-  const types = [interfaceType, ...interfaceType.getInterfaces()];
-  for (let i = 0, l = selections.length; i < l; i++) {
-    const selection = selections[i];
-    switch (selection.kind) {
-      case Kind.FRAGMENT_SPREAD: {
-        // Assumed to exist because query passed validation.
-        const fragment = operationPlan.fragments[selection.name.value];
-        const typeCondition = fragment.typeCondition;
-        const matchingInterfaceType = types.find(
-          (t) => t.name === typeCondition.name.value,
-        );
-        if (matchingInterfaceType && fragment.selectionSet.selections) {
-          const result =
-            _interfaceTypeHasNonIntrospectionFieldQueriedInSelections(
-              operationPlan,
-              matchingInterfaceType,
-              fragment.selectionSet.selections,
-            );
-          if (result) {
-            return true;
-          }
-        }
-        break;
-      }
-      case Kind.INLINE_FRAGMENT: {
-        const typeCondition = selection.typeCondition;
-        if (typeCondition) {
-          const matchingInterfaceType = types.find(
-            (t) => t.name === typeCondition.name.value,
-          );
-          if (matchingInterfaceType && selection.selectionSet.selections) {
-            const result =
-              _interfaceTypeHasNonIntrospectionFieldQueriedInSelections(
-                operationPlan,
-                matchingInterfaceType,
-                selection.selectionSet.selections,
-              );
-            if (result) {
-              return true;
-            }
-          }
-        }
-        break;
-      }
-      case Kind.FIELD: {
-        const fieldName = selection.name.value;
-        if (!fieldName.startsWith("__")) {
-          return true;
-        }
-        break;
-      }
-      default: {
-        const never: never = selection;
-        console.error(`Unexpected invalid selection: ${inspect(never)}`);
-        throw new Error("Unexpected invalid selection");
-      }
-    }
-  }
-  return false;
 }
