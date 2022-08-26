9fbc19ad86683666f59cf2effc47a10452029208
Benjie Gillam <benjie@jemjie.com>
2022-08-26 11:33:26 +0100

    Update SQL queries

--- packages/dataplan-pg/__tests__/mutations/basics/create-relational-post-no-query.sql
+++ packages/dataplan-pg/__tests__/mutations/basics/create-relational-post-no-query.sql
@@ -24,27 +24,9 @@ from (
 lateral (
   select
     __relational_items__."type"::text as "0",
-    __relational_topics__."id"::text as "1",
-    __relational_dividers__."id"::text as "2",
-    __relational_checklists__."id"::text as "3",
-    __relational_checklist_items__."id"::text as "4",
-    __relational_posts__."title" as "5",
-    __relational_posts__."description" as "6",
-    __relational_posts__."note" as "7",
-    __relational_posts__."id"::text as "8",
-    __relational_items__."id"::text as "9",
-    __relational_items_identifiers__.idx as "10"
+    __relational_items__."id"::text as "1",
+    __relational_items_identifiers__.idx as "2"
   from interfaces_and_unions.relational_items as __relational_items__
-  left outer join interfaces_and_unions.relational_topics as __relational_topics__
-  on (__relational_items__."id"::"int4" = __relational_topics__."id")
-  left outer join interfaces_and_unions.relational_dividers as __relational_dividers__
-  on (__relational_items__."id"::"int4" = __relational_dividers__."id")
-  left outer join interfaces_and_unions.relational_checklists as __relational_checklists__
-  on (__relational_items__."id"::"int4" = __relational_checklists__."id")
-  left outer join interfaces_and_unions.relational_checklist_items as __relational_checklist_items__
-  on (__relational_items__."id"::"int4" = __relational_checklist_items__."id")
-  left outer join interfaces_and_unions.relational_posts as __relational_posts__
-  on (__relational_items__."id"::"int4" = __relational_posts__."id")
   where
     (
       true /* authorization checks */
@@ -64,27 +46,9 @@ from (
 lateral (
   select
     __relational_items__."type"::text as "0",
-    __relational_topics__."id"::text as "1",
-    __relational_dividers__."id"::text as "2",
-    __relational_checklists__."id"::text as "3",
-    __relational_checklist_items__."id"::text as "4",
-    __relational_posts__."title" as "5",
-    __relational_posts__."description" as "6",
-    __relational_posts__."note" as "7",
-    __relational_posts__."id"::text as "8",
-    __relational_items__."id"::text as "9",
-    __relational_items_identifiers__.idx as "10"
+    __relational_items__."id"::text as "1",
+    __relational_items_identifiers__.idx as "2"
   from interfaces_and_unions.relational_items as __relational_items__
-  left outer join interfaces_and_unions.relational_topics as __relational_topics__
-  on (__relational_items__."id"::"int4" = __relational_topics__."id")
-  left outer join interfaces_and_unions.relational_dividers as __relational_dividers__
-  on (__relational_items__."id"::"int4" = __relational_dividers__."id")
-  left outer join interfaces_and_unions.relational_checklists as __relational_checklists__
-  on (__relational_items__."id"::"int4" = __relational_checklists__."id")
-  left outer join interfaces_and_unions.relational_checklist_items as __relational_checklist_items__
-  on (__relational_items__."id"::"int4" = __relational_checklist_items__."id")
-  left outer join interfaces_and_unions.relational_posts as __relational_posts__
-  on (__relational_items__."id"::"int4" = __relational_posts__."id")
   where
     (
       true /* authorization checks */
@@ -104,27 +68,9 @@ from (
 lateral (
   select
     __relational_items__."type"::text as "0",
-    __relational_topics__."id"::text as "1",
-    __relational_dividers__."id"::text as "2",
-    __relational_checklists__."id"::text as "3",
-    __relational_checklist_items__."id"::text as "4",
-    __relational_posts__."title" as "5",
-    __relational_posts__."description" as "6",
-    __relational_posts__."note" as "7",
-    __relational_posts__."id"::text as "8",
-    __relational_items__."id"::text as "9",
-    __relational_items_identifiers__.idx as "10"
+    __relational_items__."id"::text as "1",
+    __relational_items_identifiers__.idx as "2"
   from interfaces_and_unions.relational_items as __relational_items__
-  left outer join interfaces_and_unions.relational_topics as __relational_topics__
-  on (__relational_items__."id"::"int4" = __relational_topics__."id")
-  left outer join interfaces_and_unions.relational_dividers as __relational_dividers__
-  on (__relational_items__."id"::"int4" = __relational_dividers__."id")
-  left outer join interfaces_and_unions.relational_checklists as __relational_checklists__
-  on (__relational_items__."id"::"int4" = __relational_checklists__."id")
-  left outer join interfaces_and_unions.relational_checklist_items as __relational_checklist_items__
-  on (__relational_items__."id"::"int4" = __relational_checklist_items__."id")
-  left outer join interfaces_and_unions.relational_posts as __relational_posts__
-  on (__relational_items__."id"::"int4" = __relational_posts__."id")
   where
     (
       true /* authorization checks */
@@ -133,6 +79,54 @@ lateral (
     )
   order by __relational_items__."id" asc
 ) as __relational_items_result__
+
+select __relational_posts_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_posts_identifiers__,
+lateral (
+  select
+    __relational_posts__."title" as "0",
+    __relational_posts__."description" as "1",
+    __relational_posts__."note" as "2",
+    __relational_posts__."id"::text as "3",
+    __relational_posts_identifiers__.idx as "4"
+  from interfaces_and_unions.relational_posts as __relational_posts__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __relational_posts__."id" = __relational_posts_identifiers__."id0"
+    )
+  order by __relational_posts__."id" asc
+) as __relational_posts_result__
+
+select __relational_posts_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_posts_identifiers__,
+lateral (
+  select
+    __relational_posts__."title" as "0",
+    __relational_posts__."description" as "1",
+    __relational_posts__."note" as "2",
+    __relational_posts__."id"::text as "3",
+    __relational_posts_identifiers__.idx as "4"
+  from interfaces_and_unions.relational_posts as __relational_posts__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __relational_posts__."id" = __relational_posts_identifiers__."id0"
+    )
+  order by __relational_posts__."id" asc
+) as __relational_posts_result__
 
 insert into interfaces_and_unions.relational_items as __relational_items__ ("type", "author_id") values ($1::interfaces_and_unions.item_type, $2::"int4") returning
   __relational_items__."id"::text as "0"
@@ -151,28 +145,10 @@ from (
 ) as __relational_items_identifiers__,
 lateral (
   select
-    __relational_posts__."note" as "0",
-    __relational_posts__."description" as "1",
-    __relational_posts__."title" as "2",
-    __relational_posts__."id"::text as "3",
-    __relational_checklist_items__."id"::text as "4",
-    __relational_checklists__."id"::text as "5",
-    __relational_dividers__."id"::text as "6",
-    __relational_topics__."id"::text as "7",
-    __relational_items__."type"::text as "8",
-    __relational_items__."id"::text as "9",
-    __relational_items_identifiers__.idx as "10"
+    __relational_items__."type"::text as "0",
+    __relational_items__."id"::text as "1",
+    __relational_items_identifiers__.idx as "2"
   from interfaces_and_unions.relational_items as __relational_items__
-  left outer join interfaces_and_unions.relational_posts as __relational_posts__
-  on (__relational_items__."id"::"int4" = __relational_posts__."id")
-  left outer join interfaces_and_unions.relational_checklist_items as __relational_checklist_items__
-  on (__relational_items__."id"::"int4" = __relational_checklist_items__."id")
-  left outer join interfaces_and_unions.relational_checklists as __relational_checklists__
-  on (__relational_items__."id"::"int4" = __relational_checklists__."id")
-  left outer join interfaces_and_unions.relational_dividers as __relational_dividers__
-  on (__relational_items__."id"::"int4" = __relational_dividers__."id")
-  left outer join interfaces_and_unions.relational_topics as __relational_topics__
-  on (__relational_items__."id"::"int4" = __relational_topics__."id")
   where
     (
       true /* authorization checks */
@@ -191,28 +167,10 @@ from (
 ) as __relational_items_identifiers__,
 lateral (
   select
-    __relational_posts__."note" as "0",
-    __relational_posts__."description" as "1",
-    __relational_posts__."title" as "2",
-    __relational_posts__."id"::text as "3",
-    __relational_checklist_items__."id"::text as "4",
-    __relational_checklists__."id"::text as "5",
-    __relational_dividers__."id"::text as "6",
-    __relational_topics__."id"::text as "7",
-    __relational_items__."id"::text as "8",
-    __relational_items__."type"::text as "9",
-    __relational_items_identifiers__.idx as "10"
+    __relational_items__."type"::text as "0",
+    __relational_items__."id"::text as "1",
+    __relational_items_identifiers__.idx as "2"
   from interfaces_and_unions.relational_items as __relational_items__
-  left outer join interfaces_and_unions.relational_posts as __relational_posts__
-  on (__relational_items__."id"::"int4" = __relational_posts__."id")
-  left outer join interfaces_and_unions.relational_checklist_items as __relational_checklist_items__
-  on (__relational_items__."id"::"int4" = __relational_checklist_items__."id")
-  left outer join interfaces_and_unions.relational_checklists as __relational_checklists__
-  on (__relational_items__."id"::"int4" = __relational_checklists__."id")
-  left outer join interfaces_and_unions.relational_dividers as __relational_dividers__
-  on (__relational_items__."id"::"int4" = __relational_dividers__."id")
-  left outer join interfaces_and_unions.relational_topics as __relational_topics__
-  on (__relational_items__."id"::"int4" = __relational_topics__."id")
   where
     (
       true /* authorization checks */
@@ -231,28 +189,10 @@ from (
 ) as __relational_items_identifiers__,
 lateral (
   select
-    __relational_posts__."note" as "0",
-    __relational_posts__."description" as "1",
-    __relational_posts__."title" as "2",
-    __relational_posts__."id"::text as "3",
-    __relational_checklist_items__."id"::text as "4",
-    __relational_checklists__."id"::text as "5",
-    __relational_dividers__."id"::text as "6",
-    __relational_topics__."id"::text as "7",
-    __relational_items__."id"::text as "8",
-    __relational_items__."type"::text as "9",
-    __relational_items_identifiers__.idx as "10"
+    __relational_items__."type"::text as "0",
+    __relational_items__."id"::text as "1",
+    __relational_items_identifiers__.idx as "2"
   from interfaces_and_unions.relational_items as __relational_items__
-  left outer join interfaces_and_unions.relational_posts as __relational_posts__
-  on (__relational_items__."id"::"int4" = __relational_posts__."id")
-  left outer join interfaces_and_unions.relational_checklist_items as __relational_checklist_items__
-  on (__relational_items__."id"::"int4" = __relational_checklist_items__."id")
-  left outer join interfaces_and_unions.relational_checklists as __relational_checklists__
-  on (__relational_items__."id"::"int4" = __relational_checklists__."id")
-  left outer join interfaces_and_unions.relational_dividers as __relational_dividers__
-  on (__relational_items__."id"::"int4" = __relational_dividers__."id")
-  left outer join interfaces_and_unions.relational_topics as __relational_topics__
-  on (__relational_items__."id"::"int4" = __relational_topics__."id")
   where
     (
       true /* authorization checks */
@@ -261,3 +201,75 @@ lateral (
     )
   order by __relational_items__."id" asc
 ) as __relational_items_result__
+
+select __relational_posts_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_posts_identifiers__,
+lateral (
+  select
+    __relational_posts__."title" as "0",
+    __relational_posts__."description" as "1",
+    __relational_posts__."note" as "2",
+    __relational_posts__."id"::text as "3",
+    __relational_posts_identifiers__.idx as "4"
+  from interfaces_and_unions.relational_posts as __relational_posts__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __relational_posts__."id" = __relational_posts_identifiers__."id0"
+    )
+  order by __relational_posts__."id" asc
+) as __relational_posts_result__
+
+select __relational_posts_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_posts_identifiers__,
+lateral (
+  select
+    __relational_posts__."title" as "0",
+    __relational_posts__."description" as "1",
+    __relational_posts__."note" as "2",
+    __relational_posts__."id"::text as "3",
+    __relational_posts_identifiers__.idx as "4"
+  from interfaces_and_unions.relational_posts as __relational_posts__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __relational_posts__."id" = __relational_posts_identifiers__."id0"
+    )
+  order by __relational_posts__."id" asc
+) as __relational_posts_result__
+
+select __relational_posts_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __relational_posts_identifiers__,
+lateral (
+  select
+    __relational_posts__."title" as "0",
+    __relational_posts__."description" as "1",
+    __relational_posts__."note" as "2",
+    __relational_posts__."id"::text as "3",
+    __relational_posts_identifiers__.idx as "4"
+  from interfaces_and_unions.relational_posts as __relational_posts__
+  where
+    (
+      true /* authorization checks */
+    ) and (
+      __relational_posts__."id" = __relational_posts_identifiers__."id0"
+    )
+  order by __relational_posts__."id" asc
+) as __relational_posts_result__
