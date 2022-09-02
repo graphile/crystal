73f0b337e4e573c3fe15f18a1f117041d1b03467
Benjie Gillam <benjie@jemjie.com>
2022-09-02 18:23:50 +0100

    Temporary graphile config

---
+++ packages/postgraphile/graphile.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-unresolved */
+import "graphile-config";
+
+import { StreamDeferPlugin } from "graphile-build";
+import postgraphilePresetAmber from "postgraphile/presets/amber";
+import { makeV4Preset } from "postgraphile/presets/v4";
+
+/** @type {GraphileConfig.Preset} */
+const preset = {
+  plugins: [StreamDeferPlugin],
+  extends: [
+    postgraphilePresetAmber,
+    makeV4Preset({
+      simpleCollections: "both",
+      jwtPgTypeIdentifier: '"b"."jwt_token"',
+    }),
+  ],
+};
+
+export default preset;
