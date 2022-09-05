graphile/crystal

commit 72d208e37538e5e9b294c7c0752a1dcac7721060

Standardize behavior handling

packages/graphile-build-pg/src/plugins/PgProceduresPlugin.ts
@@ -18,6 +18,7 @@ import type { PgProc } from "pg-introspection";
 import sql from "pg-sql2";
 
 import { version } from "../index.js";
+import { addBehaviorToTags } from "../utils.js";
 
 // TODO: these should be used, surely?
 interface _ComputedColumnDetails {
@@ -406,18 +407,10 @@ export const PgProceduresPlugin: GraphileConfig.Plugin = {
             [namespaceName, procName, sql, sqlFromArgDigests],
           );
 
-          const behavior = Array.isArray(tags.behavior)
-            ? [...(tags.behavior as string[])]
-            : typeof tags.behavior === "string"
-            ? [tags.behavior]
-            : [];
-          behavior.push("-filter -order");
+          addBehaviorToTags(tags, "-filter -order");
 
           const extensions: PgSourceExtensions = {
-            tags: {
-              ...tags,
-              behavior,
-            },
+            tags,
           };
 
           if (outOrInoutOrTableArgModes.length === 1) {

packages/graphile-build-pg/src/utils.ts
@@ -1,3 +1,5 @@
+import type { PgSmartTagsDict } from "pg-introspection";
+
 export function tagToString(
   str: undefined | null | boolean | string | (string | boolean)[],
 ): string | undefined {
@@ -5,4 +7,24 @@ export function tagToString(
     return undefined;
   }
   return Array.isArray(str) ? str.join("\n") : str === true ? " " : str;
+}
+
+export function addBehaviorToTags(
+  tags: PgSmartTagsDict,
+  behavior: string,
+  prepend = false,
+): void {
+  if (Array.isArray(tags.behavior)) {
+    if (prepend) {
+      tags.behavior.unshift(behavior);
+    } else {
+      tags.behavior.push(behavior);
+    }
+  } else if (typeof tags.behavior === "string") {
+    tags.behavior = prepend
+      ? [behavior, tags.behavior]
+      : [tags.behavior, behavior];
+  } else {
+    tags.behavior = behavior;
+  }
 }

packages/graphile-build-pg/src/plugins/PgEnumTablesPlugin.ts
@@ -10,6 +10,7 @@ import { sql } from "pg-sql2";
 
 import { version } from "../index.js";
 import { withPgClientFromPgSource } from "../pgSources.js";
+import { addBehaviorToTags } from "../utils.js";
 
 declare global {
   namespace GraphileConfig {
@@ -162,15 +163,8 @@ Original error: ${e.message}
             tags.enum === true || typeof tags.enum === "string";
 
           if (isEnumTable) {
-            if (Array.isArray(tags.behavior)) {
-              // no action
-            } else if (typeof tags.behavior === "string") {
-              tags.behavior = [tags.behavior];
-            } else {
-              tags.behavior = [];
-            }
             // Prevent the table being recognised as a table
-            tags.behavior.unshift("-*");
+            addBehaviorToTags(tags, "-*", true);
           }
 
           // By this point, even views should have "fake" constraints we can use

packages/graphile-build-pg/src/plugins/PgRemoveExtensionResourcesPlugin.ts
@@ -1,8 +1,7 @@
 import "graphile-config";
 
-import type { PgSmartTagsDict } from "pg-introspection";
-
 import { version } from "../index.js";
+import { addBehaviorToTags } from "../utils.js";
 
 declare global {
   namespace GraphileConfig {
@@ -80,13 +79,3 @@ export const PgRemoveExtensionResourcesPlugin: GraphileConfig.Plugin = {
     Cache
   >,
 };
-
-function addBehaviorToTags(tags: PgSmartTagsDict, behavior: string): void {
-  if (Array.isArray(tags.behavior)) {
-    tags.behavior.push(behavior);
-  } else if (typeof tags.behavior === "string") {
-    tags.behavior = [tags.behavior, behavior];
-  } else {
-    tags.behavior = behavior;
-  }
-}

packages/graphile-build-pg/src/plugins/PgTablesPlugin.ts
@@ -16,6 +16,7 @@ import type { PgClass, PgConstraint, PgNamespace } from "pg-introspection";
 
 import { getBehavior } from "../behavior.js";
 import { version } from "../index.js";
+import { addBehaviorToTags } from "../utils.js";
 
 type PgSourceBuilderOptions = Omit<
   PgSourceOptions<any, any, any, any>,
@@ -472,23 +473,18 @@ export const PgTablesPlugin: GraphileConfig.Plugin = {
 
           const { tags } = pgClass.getTagsAndDescription();
 
-          const behavior: string[] = Array.isArray(tags.behavior)
-            ? [...(tags.behavior as string[])]
-            : typeof tags.behavior === "string"
-            ? [tags.behavior]
-            : [];
           const mask = pgClass.updatable_mask ?? 2 ** 8 - 1;
           const isInsertable = mask & (1 << 3);
           const isUpdatable = mask & (1 << 2);
           const isDeletable = mask & (1 << 4);
           if (!isInsertable) {
-            behavior.push("-insert");
+            addBehaviorToTags(tags, "-insert");
           }
           if (!isUpdatable) {
-            behavior.push("-update");
+            addBehaviorToTags(tags, "-update");
           }
           if (!isDeletable) {
-            behavior.push("-delete");
+            addBehaviorToTags(tags, "-delete");
           }
 
           const options: PgSourceBuilderOptions = {
@@ -502,7 +498,6 @@ export const PgTablesPlugin: GraphileConfig.Plugin = {
             extensions: {
               tags: {
                 ...tags,
-                ...(behavior.length > 0 ? { behavior } : {}),
                 originalName: pgClass.relname,
               },
             },

commit 3febab573fd35cb083cb936fac12e92c42ade8a3

Fix bug introduced in 72d208e37538e5e9b294c7c0752a1dcac7721060

packages/graphile-build-pg/src/plugins/PgProceduresPlugin.ts
@@ -327,13 +327,13 @@ export const PgProceduresPlugin: GraphileConfig.Plugin = {
             isStrict || info.options.pgStrictFunctions === true;
           const numberOfRequiredArguments =
             numberOfArguments - numberOfArgumentsWithDefaults;
-          const { tags, description } = pgProc.getTagsAndDescription();
+          const { tags: rawTags, description } = pgProc.getTagsAndDescription();
           for (let i = 0, l = numberOfArguments; i < l; i++) {
             const argType = allArgTypes[i];
             const argName = pgProc.proargnames?.[i] ?? null;
 
             // TODO: smart tag should allow changing the modifier
-            const tag = tags[`arg${i}variant`];
+            const tag = rawTags[`arg${i}variant`];
             const variant = typeof tag === "string" ? tag : undefined;
 
             // i for IN arguments, o for OUT arguments, b for INOUT arguments,
@@ -407,10 +407,14 @@ export const PgProceduresPlugin: GraphileConfig.Plugin = {
             [namespaceName, procName, sql, sqlFromArgDigests],
           );
 
+          const tags = { ...rawTags };
+
           addBehaviorToTags(tags, "-filter -order");
 
           const extensions: PgSourceExtensions = {
-            tags,
+            tags: {
+              ...tags,
+            },
           };
 
           if (outOrInoutOrTableArgModes.length === 1) {

packages/graphile-build-pg/src/utils.ts
@@ -16,15 +16,15 @@ export function addBehaviorToTags(
 ): void {
   if (Array.isArray(tags.behavior)) {
     if (prepend) {
-      tags.behavior.unshift(behavior);
+      tags.behavior = [behavior, ...tags.behavior];
     } else {
-      tags.behavior.push(behavior);
+      tags.behavior = [...tags.behavior, behavior];
     }
   } else if (typeof tags.behavior === "string") {
     tags.behavior = prepend
       ? [behavior, tags.behavior]
       : [tags.behavior, behavior];
   } else {
-    tags.behavior = behavior;
+    tags.behavior = [behavior];
   }
 }