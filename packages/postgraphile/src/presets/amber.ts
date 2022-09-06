633c5144aa13b0fb2b2c4d544b62ee4f15bc127a
Benjie Gillam <benjie@jemjie.com>
2022-09-06 18:23:02 +0100

    Ordered schema is more similar to V4 schema now

--- packages/postgraphile/src/presets/amber.ts
+++ packages/postgraphile/src/presets/amber.ts
@@ -1,15 +1,54 @@
 import "graphile-config";
 
 import {
+  AddNodeInterfaceToSuitableTypesPlugin,
   defaultPreset as graphileBuildPreset,
+  MutationPayloadQueryPlugin,
+  NodeAccessorPlugin,
+  NodePlugin,
   QueryQueryPlugin,
   SwallowErrorsPlugin,
 } from "graphile-build";
-import { defaultPreset as graphileBuildPgPreset } from "graphile-build-pg";
+import {
+  defaultPreset as graphileBuildPgPreset,
+  PgAllRowsPlugin,
+  PgColumnsPlugin,
+  PgCustomTypeFieldPlugin,
+  PgMutationCreatePlugin,
+  PgMutationPayloadEdgePlugin,
+  PgMutationUpdateDeletePlugin,
+  PgOrderAllColumnsPlugin,
+  PgOrderByPrimaryKeyPlugin,
+  PgOrderCustomFieldsPlugin,
+  PgRelationsPlugin,
+  PgRowByUniquePlugin,
+} from "graphile-build-pg";
+
+// Shunts the plugins into an order that's more compatible with PostGraphile V4.
+export const orderedPlugins: GraphileConfig.Preset = {
+  plugins: [
+    QueryQueryPlugin,
+    AddNodeInterfaceToSuitableTypesPlugin,
+    NodePlugin,
+    PgAllRowsPlugin,
+    PgRowByUniquePlugin,
+    PgColumnsPlugin,
+    MutationPayloadQueryPlugin,
+    PgRelationsPlugin,
+    PgMutationCreatePlugin,
+    PgMutationUpdateDeletePlugin,
+    PgCustomTypeFieldPlugin,
+    NodeAccessorPlugin,
+    PgOrderAllColumnsPlugin,
+    PgOrderCustomFieldsPlugin,
+    PgOrderByPrimaryKeyPlugin,
+    PgMutationPayloadEdgePlugin,
+  ],
+};
 
 export const postgraphilePresetAmber: GraphileConfig.Preset = {
-  extends: [graphileBuildPreset, graphileBuildPgPreset],
-  plugins: [QueryQueryPlugin, SwallowErrorsPlugin],
+  extends: [orderedPlugins, graphileBuildPreset, graphileBuildPgPreset],
+  plugins: [SwallowErrorsPlugin],
 };
 
 export default postgraphilePresetAmber;
