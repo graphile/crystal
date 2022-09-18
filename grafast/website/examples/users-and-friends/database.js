77b08930db06024f943b6d7ddb994291c324ed47
Benjie Gillam <benjie@jemjie.com>
2022-09-18 11:27:36 +0100

    Add a few columns to the database

--- grafast/website/examples/users-and-friends/database.js
+++ grafast/website/examples/users-and-friends/database.js
@@ -16,11 +16,19 @@ const NAMES = [
   "Mandy",
 ];
 
+function makeBio(key) {
+  return `Hi, my name is ${NAMES[key]} but you can call me Dr. ${NAMES[key][0]}; I enjoy long walks in the park and dancing in the moonlight. Opinions are my own. #blessed`;
+}
+
 db.serialize(() => {
   db.run(`\
 CREATE TABLE users (
   id int primary key,
-  full_name text not null
+  full_name text not null,
+  username text not null,
+  bio text,
+  created_at int not null default CURRENT_TIMESTAMP,
+  updated_at int
 );
 `);
   db.run(
@@ -28,21 +36,32 @@ CREATE TABLE users (
 CREATE TABLE friendships (
   id int primary key,
   user_id int not null,
-  friend_id int not null
+  friend_id int not null,
+  created_at int not null default CURRENT_TIMESTAMP,
+  updated_at int
 );
 `,
   );
 
   {
-    const stmt = db.prepare("INSERT INTO users VALUES (?, ?)");
+    const stmt = db.prepare(
+      "INSERT INTO users (id, full_name, username, bio) VALUES (?, ?, ?, ?)",
+    );
     for (let nameIndex = 0; nameIndex < NAMES.length; nameIndex++) {
-      stmt.run(nameIndex + 1, NAMES[nameIndex]);
+      stmt.run(
+        nameIndex + 1,
+        NAMES[nameIndex],
+        NAMES[nameIndex].toLowerCase(),
+        makeBio(nameIndex),
+      );
     }
     stmt.finalize();
   }
 
   {
-    const stmt = db.prepare("INSERT INTO friendships VALUES (?, ?, ?)");
+    const stmt = db.prepare(
+      "INSERT INTO friendships (id, user_id, friend_id) VALUES (?, ?, ?)",
+    );
     let id = 0;
     for (let nameIndex = 0; nameIndex < NAMES.length; nameIndex++) {
       // A stable list of friendIds
