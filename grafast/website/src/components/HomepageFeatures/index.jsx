168fca0119753b5af787620fa078da314690220c
Benjie Gillam <benjie@jemjie.com>
2022-09-19 10:07:16 +0100

    Rename jsx files to .jsx

--- grafast/website/src/components/HomepageFeatures/index.jsx
+++ grafast/website/src/components/HomepageFeatures/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import clsx from "clsx";
 import styles from "./styles.module.css";
-import Grafast from "../Grafast/index.js";
+import Grafast from "../Grafast/index.jsx";
 
 const FeatureList = [
   {
