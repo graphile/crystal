9cb8208da1452f6c987d5dfa26d4202b0465591a
Benjie Gillam <benjie@jemjie.com>
2022-09-24 17:25:38 +0100

    Update snapshots

--- grafast/dataplan-pg/__tests__/queries/connections/pagination-before-end.sql
+++ grafast/dataplan-pg/__tests__/queries/connections/pagination-before-end.sql
@@ -1,14 +1,3 @@
-select
-  (count(*))::text as "0"
-from app_public.messages as __messages__
-where
-  (
-    __messages__.archived_at is null
-  ) and (
-    true /* authorization checks */
-  )
-
-
 select __messages_result__.*
 from (
   select
@@ -36,3 +25,13 @@ lateral (
   order by __messages__."id" asc
   limit 4
 ) as __messages_result__
+
+select
+  (count(*))::text as "0"
+from app_public.messages as __messages__
+where
+  (
+    __messages__.archived_at is null
+  ) and (
+    true /* authorization checks */
+  )
