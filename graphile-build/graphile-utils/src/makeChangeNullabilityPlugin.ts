6a38ebf0d53b5e1c49e47de67978e713755265ce
Benjie Gillam <benjie@jemjie.com>
2022-10-05 09:09:03 +0100

    Refactoring

--- graphile-build/graphile-utils/src/makeChangeNullabilityPlugin.ts
+++ graphile-build/graphile-utils/src/makeChangeNullabilityPlugin.ts
@@ -1,9 +1,10 @@
-import type { GraphQLFieldConfig, GraphQLInputFieldConfig } from "graphql";
+import type { GraphQLInputType, GraphQLOutputType, GraphQLType } from "graphql";
 
+interface ChangeNullabilityTypeRules {
+  [fieldName: string]: boolean;
+}
 interface ChangeNullabilityRules {
-  [typeName: string]: {
-    [fieldName: string]: boolean;
-  };
+  [typeName: string]: ChangeNullabilityTypeRules;
 }
 
 let counter = 0;
@@ -11,55 +12,59 @@ let counter = 0;
 export function makeChangeNullabilityPlugin(
   rules: ChangeNullabilityRules,
 ): GraphileConfig.Plugin {
-  function changeNullability(
-    field: GraphQLInputFieldConfig,
-    build: Build,
-    context: Context<GraphQLInputFieldConfig>,
-  ): typeof field;
-  function changeNullability<TSource, TContext>(
-    field: GraphQLFieldConfig<TSource, TContext>,
-    build: Build,
-    context: Context<GraphQLFieldConfig<TSource, TContext>>,
-  ): typeof field;
-  function changeNullability<TSource, TContext>(
-    field: GraphQLInputFieldConfig | GraphQLFieldConfig<TSource, TContext>,
-    build: Build,
-    context: Context<
-      GraphQLInputFieldConfig | GraphQLFieldConfig<TSource, TContext>
-    >,
-  ): typeof field {
-    const {
-      Self,
-      scope: { fieldName },
-    } = context;
-    const typeRules = rules[Self.name];
-    if (!typeRules) {
-      return field;
-    }
+  function applyNullability<T extends GraphQLInputType | GraphQLOutputType>(
+    type: GraphQLType,
+    fieldName: string,
+    typeRules: ChangeNullabilityTypeRules,
+    build: GraphileBuild.Build,
+  ): T {
     const shouldBeNullable = typeRules[fieldName];
     if (shouldBeNullable == null) {
-      return field;
+      return type as any;
     }
     const {
       graphql: { getNullableType, GraphQLNonNull },
     } = build;
-    const nullableType = getNullableType(field.type);
-    return {
-      ...field,
-      type: shouldBeNullable
-        ? nullableType
-        : nullableType === field.type
-        ? new GraphQLNonNull(field.type)
-        : field.type, // Optimisation if it's already non-null
-    };
+    const nullableType = getNullableType(type);
+    return shouldBeNullable
+      ? nullableType
+      : nullableType === type
+      ? new GraphQLNonNull(type)
+      : (type as any); // Optimisation if it's already non-null
   }
   return {
     name: `ChangeNullabilityPlugin_${++counter}`,
     version: "0.0.0",
     schema: {
       hooks: {
-        GraphQLInputObjectType_fields_field: { callback: changeNullability },
-        GraphQLObjectType_fields_field: { callback: changeNullability },
+        GraphQLInputObjectType_fields_field(field, build, context) {
+          const { Self, scope } = context;
+          const typeRules = rules[Self.name];
+          if (!typeRules) {
+            return field;
+          }
+          field.type = applyNullability<GraphQLInputType>(
+            field.type,
+            scope.fieldName,
+            typeRules,
+            build,
+          );
+          return field;
+        },
+        GraphQLObjectType_fields_field(field, build, context) {
+          const { Self, scope } = context;
+          const typeRules = rules[Self.name];
+          if (!typeRules) {
+            return field;
+          }
+          field.type = applyNullability<GraphQLOutputType>(
+            field.type,
+            scope.fieldName,
+            typeRules,
+            build,
+          );
+          return field;
+        },
       },
     },
   };
