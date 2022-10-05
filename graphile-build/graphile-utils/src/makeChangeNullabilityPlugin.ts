242fde638158762be05538e63aa1fed8c79e1d52
Benjie Gillam <benjie@jemjie.com>
2022-10-05 08:30:03 +0100

    Superficial direct port of makeChangeNullabilityPlugin

---
+++ graphile-build/graphile-utils/src/makeChangeNullabilityPlugin.ts
@@ -0,0 +1,66 @@
+import type { GraphQLFieldConfig, GraphQLInputFieldConfig } from "graphql";
+
+interface ChangeNullabilityRules {
+  [typeName: string]: {
+    [fieldName: string]: boolean;
+  };
+}
+
+let counter = 0;
+
+export function makeChangeNullabilityPlugin(
+  rules: ChangeNullabilityRules,
+): GraphileConfig.Plugin {
+  function changeNullability(
+    field: GraphQLInputFieldConfig,
+    build: Build,
+    context: Context<GraphQLInputFieldConfig>,
+  ): typeof field;
+  function changeNullability<TSource, TContext>(
+    field: GraphQLFieldConfig<TSource, TContext>,
+    build: Build,
+    context: Context<GraphQLFieldConfig<TSource, TContext>>,
+  ): typeof field;
+  function changeNullability<TSource, TContext>(
+    field: GraphQLInputFieldConfig | GraphQLFieldConfig<TSource, TContext>,
+    build: Build,
+    context: Context<
+      GraphQLInputFieldConfig | GraphQLFieldConfig<TSource, TContext>
+    >,
+  ): typeof field {
+    const {
+      Self,
+      scope: { fieldName },
+    } = context;
+    const typeRules = rules[Self.name];
+    if (!typeRules) {
+      return field;
+    }
+    const shouldBeNullable = typeRules[fieldName];
+    if (shouldBeNullable == null) {
+      return field;
+    }
+    const {
+      graphql: { getNullableType, GraphQLNonNull },
+    } = build;
+    const nullableType = getNullableType(field.type);
+    return {
+      ...field,
+      type: shouldBeNullable
+        ? nullableType
+        : nullableType === field.type
+        ? new GraphQLNonNull(field.type)
+        : field.type, // Optimisation if it's already non-null
+    };
+  }
+  return {
+    name: `ChangeNullabilityPlugin_${++counter}`,
+    version: "0.0.0",
+    schema: {
+      hooks: {
+        GraphQLInputObjectType_fields_field: { callback: changeNullability },
+        GraphQLObjectType_fields_field: { callback: changeNullability },
+      },
+    },
+  };
+}
