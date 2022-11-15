00f5e9784ba61fde515f01114e7c90cf3ecb7c5f
Benjie Gillam <benjie@jemjie.com>
2022-11-15 15:17:45 +0000

    Factor step ID into polymorphic path

--- grafast/grafast/src/engine/OperationPlan.ts
+++ grafast/grafast/src/engine/OperationPlan.ts
@@ -1417,7 +1417,8 @@ export class OperationPlan {
     $step: ExecutableStep,
     allPossibleObjectTypes: readonly GraphQLObjectType[],
   ): LayerPlan<LayerPlanReasonPolymorphic> {
-    const pathString = path.join("|");
+    // TODO: I added the $step.id to pathString to fix a planning issue; but maybe we can do this without branching?
+    const pathString = `${path.join("|")}!${$step.id}`;
     const polymorphicLayerPlanByPath =
       this.polymorphicLayerPlanByPathByLayerPlan.get(parentLayerPlan) ??
       new Map();
@@ -1432,7 +1433,9 @@ export class OperationPlan {
       const { stepId, layerPlan } = prev;
       if (this.steps[stepId] !== this.steps[$step.id]) {
         throw new Error(
-          "GraphileInternalError<e01bdc40-7c89-41c6-8d84-56efa22c872a>: unexpected inconsistency when determining the polymorphic LayerPlan to use",
+          `GraphileInternalError<e01bdc40-7c89-41c6-8d84-56efa22c872a>: unexpected inconsistency when determining the polymorphic LayerPlan to use (pathString = ${pathString}, ${
+            this.steps[stepId]
+          } (${stepId}) != ${this.steps[$step.id]} (${$step.id}))`,
         );
       }
       for (const t of allPossibleObjectTypes) {
