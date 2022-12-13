f5bc32ade695ceabc320c2b24e27e1866ccfdf49
Benjie Gillam <benjie@jemjie.com>
2022-12-13 11:16:16 +0000

    Tree shaking in step tracker

--- grafast/grafast/src/engine/StepTracker.ts
+++ grafast/grafast/src/engine/StepTracker.ts
@@ -1,4 +1,5 @@
 import type { ExecutableStep } from "../step";
+import type { LayerPlan } from "./LayerPlan";
 import type { OutputPlan } from "./OutputPlan";
 
 /**
@@ -15,18 +16,26 @@ export class StepTracker {
   private dependenciesByStep = new Map<ExecutableStep, ExecutableStep[]>();
   private dependentsByStep = new Map<
     ExecutableStep,
-    Array<{ step: ExecutableStep; dependencyIndex: number }>
+    Set<{ step: ExecutableStep; dependencyIndex: number }>
   >();
 
   private rootStepByOutputPlan = new Map<OutputPlan, ExecutableStep>();
   private outputPlansByRootStep = new Map<ExecutableStep, OutputPlan[]>();
 
+  private rootStepByLayerPlan = new Map<LayerPlan, ExecutableStep>();
+  private layerPlansByRootStep = new Map<ExecutableStep, LayerPlan[]>();
+
+  private parentStepByLayerPlan = new Map<LayerPlan, ExecutableStep>();
+  private layerPlansByParentStep = new Map<ExecutableStep, LayerPlan[]>();
+
   public addStep($step: ExecutableStep): number {
     const stepId = this.stepCount++;
     this.steps.add($step);
     this.dependenciesByStep.set($step, []);
-    this.dependentsByStep.set($step, []);
+    this.dependentsByStep.set($step, new Set());
     this.outputPlansByRootStep.set($step, []);
+    this.layerPlansByRootStep.set($step, []);
+    this.layerPlansByParentStep.set($step, []);
     return stepId;
   }
 
@@ -38,7 +47,7 @@ export class StepTracker {
       this.dependenciesByStep.get($dependent)!.push($dependency) - 1;
     this.dependentsByStep
       .get($dependency)!
-      .push({ step: $dependent, dependencyIndex });
+      .add({ step: $dependent, dependencyIndex });
     return dependencyIndex;
   }
 
@@ -71,23 +80,60 @@ export class StepTracker {
   ): void {
     // Transfer step dependents of $original to $replacement
     const dependents = this.dependentsByStep.get($original)!;
-    this.dependentsByStep.delete($original);
     const replacementDependents = this.dependentsByStep.get($replacement)!;
     for (const { step: $dependent, dependencyIndex } of dependents) {
       this.dependenciesByStep.get($dependent)![dependencyIndex] = $replacement;
-      replacementDependents.push({ step: $dependent, dependencyIndex });
+      replacementDependents.add({ step: $dependent, dependencyIndex });
     }
-    // Since this step has been replaced, it no longer has any dependencies
-    this.dependenciesByStep.delete($original);
 
     // Transfer output plan dependents of $original to $replacement
     const outputPlans = this.outputPlansByRootStep.get($original)!;
-    this.outputPlansByRootStep.delete($original);
     const outputPlansByReplacementStep =
       this.outputPlansByRootStep.get($replacement)!;
     for (const outputPlan of outputPlans) {
       this.rootStepByOutputPlan.set(outputPlan, $replacement);
       outputPlansByReplacementStep.push(outputPlan);
     }
+
+    // Remove this step (and perform localized tree-shaking)
+    this.eradicate($original);
+  }
+
+  /**
+   * ONLY CALL THIS IF NOTHING DEPENDS ON $original! It's intended to be called
+   * from `replaceStep` or from itself.
+   *
+   * This method removes $original from the various maps/sets/lists, and also
+   * removes the fact that it was dependent on other steps. If these other
+   * steps no longer have any dependents (steps, layer plans or output plans)
+   * then they can also be eradicated.
+   */
+  private eradicate($original: ExecutableStep) {
+    // Since this step is being removed, it doesn't need its dependencies any more
+    const oldDependencies = this.dependenciesByStep.get($original)!;
+    this.dependenciesByStep.delete($original);
+    for (const $dependency of oldDependencies) {
+      // $dependency is no longer a dependent of $original, since we're getting
+      // rid of $original
+      const dependents = this.dependentsByStep.get($dependency)!;
+      for (const dependent of dependents) {
+        if (dependent.step === $original) {
+          dependents.delete(dependent);
+        }
+      }
+      if (
+        dependents.size === 0 &&
+        !$dependency.hasSideEffects &&
+        this.outputPlansByRootStep.get($dependency)?.length === 0
+        // TODO: && noLayerPlans
+      ) {
+        // No-one depends on $dependency and it has no side effects - we can get rid of it!
+        this.eradicate($dependency);
+      }
+    }
+
+    this.outputPlansByRootStep.delete($original);
+    this.dependentsByStep.delete($original);
+    this.steps.delete($original);
   }
 }
