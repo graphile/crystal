d4a116ac0b05e36c314b2ad4d8b0a48387125894
Benjie Gillam <benjie@jemjie.com>
2022-12-16 13:21:21 +0000

    Don't bundle node modules

--- grafast/dataplan-pg/webpack.config.js
+++ grafast/dataplan-pg/webpack.config.js
@@ -1,3 +1,4 @@
+const nodeExternals = require("webpack-node-externals");
 const webpack = require("webpack");
 const path = require("path");
 
@@ -23,76 +24,17 @@ module.exports = {
       "process.env.GRAPHILE_ENV": JSON.stringify("production"),
     }),
   ],
-  externals: {
-    // graphql -> external
-    graphql: {
-      commonjs: "graphql",
-      commonjs2: "graphql",
-      amd: "graphql",
-      root: "graphql",
-    },
-    // grafast -> external
-    grafast: {
-      commonjs: "grafast",
-      commonjs2: "grafast",
-      amd: "grafast",
-      root: "grafast",
-    },
-    // @dataplan/json -> external
-    "@dataplan/json": {
-      commonjs: "@dataplan/json",
-      commonjs2: "@dataplan/json",
-      amd: "@dataplan/json",
-      root: "@dataplan/json",
-    },
-    // pg -> external
-    pg: {
-      commonjs: "pg",
-      commonjs2: "pg",
-      amd: "pg",
-      root: "pg",
-    },
-    // postgres-array -> external
-    "postgres-array": {
-      commonjs: "postgres-array",
-      commonjs2: "postgres-array",
-      amd: "postgres-array",
-      root: "postgres-array",
-    },
-    // postgres-range -> external
-    "postgres-range": {
-      commonjs: "postgres-range",
-      commonjs2: "postgres-range",
-      amd: "postgres-range",
-      root: "postgres-range",
-    },
-    // pg-sql2 -> external
-    "pg-sql2": {
-      commonjs: "pg-sql2",
-      commonjs2: "pg-sql2",
-      amd: "pg-sql2",
-      root: "pg-sql2",
-    },
-    // crypto.createHash -> external
-    crypto: {
-      commonjs: "crypto",
-      commonjs2: "crypto",
-      amd: "crypto",
-      root: "crypto",
-    },
-    // util.inspect -> external, optional
-    util: {
-      commonjs: "util",
-      commonjs2: "util",
-      amd: "util",
-      root: "util",
-    },
-    // @graphile/lru -> bundle
-    // chalk -> bundle
-    // debug -> bundle
-    // events.EventEmitter -> replaced with eventemitter3 -> bundle
-    // tslib -> bundle
+  target: "node", // use require() & use NodeJs CommonJS style
+  externalsPresets: {
+    node: true, // in order to ignore built-in modules like path, fs, etc.
   },
+  externals: [
+    // in order to ignore all modules in node_modules folder
+    nodeExternals(),
+    nodeExternals({
+      modulesDir: path.resolve(__dirname, "../../node_modules"),
+    }),
+  ],
   performance: {
     maxEntrypointSize: 215000,
   },
