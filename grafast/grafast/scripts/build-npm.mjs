8291aca042c8d3e077313c27482f6a6082482a77
Benjie Gillam <benjie@jemjie.com>
2023-01-11 15:19:57 +0000

    Copy npmignore into dist folders

--- grafast/grafast/scripts/build-npm.mjs
+++ grafast/grafast/scripts/build-npm.mjs
@@ -14,6 +14,7 @@ await $`tsc -b`;
 await $`webpack --mode=production`;
 await $`find dist -type d -exec mkdir -p release/{} \\;`;
 await $`find dist -type f -name '*.d.ts' -exec cp {} release/{} \\;`;
+await $`cp src/.npmignore release/dist/.npmignore`;
 await $`cp README.npm.md release/README.md`;
 
 await writePrereleaseLicense(__dirname + "/../release/LICENSE.md");
