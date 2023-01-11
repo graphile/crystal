6eef9b301b70cd405bb11fea1fc722b516ac0b0a
Benjie Gillam <benjie@jemjie.com>
2023-01-11 15:36:22 +0000

    Publish from outside repository

--- publish.sh
+++ publish.sh
@@ -1,43 +1,45 @@
 #!/usr/bin/env bash
 set -e -x
-tar tzf builds/graphile-config.tgz
-tar tzf builds/ruru.tgz
-tar tzf builds/graphile__lru.tgz
-tar tzf builds/grafast.tgz
-tar tzf builds/grafserv.tgz
-tar tzf builds/dataplan__json.tgz
-tar tzf builds/eslint-plugin-graphile-export.tgz
-tar tzf builds/graphile-utils.tgz
-tar tzf builds/graphile-export.tgz
-tar tzf builds/jest-serializer-graphql-schema.tgz
-tar tzf builds/jest-serializer-simple.tgz
-tar tzf builds/graphile-build.tgz
-tar tzf builds/pg-introspection.tgz
-tar tzf builds/pg-sql2.tgz
-tar tzf builds/dataplan__pg.tgz
-tar tzf builds/graphile-build-pg.tgz
-tar tzf builds/postgraphile.tgz
+
+cd ..
+tar tzf postgraphile-private/builds/graphile-config.tgz
+tar tzf postgraphile-private/builds/ruru.tgz
+tar tzf postgraphile-private/builds/graphile__lru.tgz
+tar tzf postgraphile-private/builds/grafast.tgz
+tar tzf postgraphile-private/builds/grafserv.tgz
+tar tzf postgraphile-private/builds/dataplan__json.tgz
+tar tzf postgraphile-private/builds/eslint-plugin-graphile-export.tgz
+tar tzf postgraphile-private/builds/graphile-utils.tgz
+tar tzf postgraphile-private/builds/graphile-export.tgz
+tar tzf postgraphile-private/builds/jest-serializer-graphql-schema.tgz
+tar tzf postgraphile-private/builds/jest-serializer-simple.tgz
+tar tzf postgraphile-private/builds/graphile-build.tgz
+tar tzf postgraphile-private/builds/pg-introspection.tgz
+tar tzf postgraphile-private/builds/pg-sql2.tgz
+tar tzf postgraphile-private/builds/dataplan__pg.tgz
+tar tzf postgraphile-private/builds/graphile-build-pg.tgz
+tar tzf postgraphile-private/builds/postgraphile.tgz
 
 read -n1 -p "Publish? [y,n]" doit
 case $doit in
   y|Y)
-    npm publish --access=public --tag=prealpha builds/graphile-config.tgz
-    npm publish --access=public --tag=prealpha builds/ruru.tgz
-    npm publish --access=public --tag=prealpha builds/graphile__lru.tgz
-    npm publish --access=public --tag=prealpha builds/grafast.tgz
-    npm publish --access=public --tag=prealpha builds/grafserv.tgz
-    npm publish --access=public --tag=prealpha builds/dataplan__json.tgz
-    npm publish --access=public --tag=prealpha builds/eslint-plugin-graphile-export.tgz
-    npm publish --access=public --tag=prealpha builds/graphile-utils.tgz
-    npm publish --access=public --tag=prealpha builds/graphile-export.tgz
-    npm publish --access=public --tag=prealpha builds/jest-serializer-graphql-schema.tgz
-    npm publish --access=public --tag=prealpha builds/jest-serializer-simple.tgz
-    npm publish --access=public --tag=prealpha builds/graphile-build.tgz
-    npm publish --access=public --tag=prealpha builds/pg-introspection.tgz
-    npm publish --access=public --tag=prealpha builds/pg-sql2.tgz
-    npm publish --access=public --tag=prealpha builds/dataplan__pg.tgz
-    npm publish --access=public --tag=prealpha builds/graphile-build-pg.tgz
-    npm publish --access=public --tag=prealpha builds/postgraphile.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/graphile-config.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/ruru.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/graphile__lru.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/grafast.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/grafserv.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/dataplan__json.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/eslint-plugin-graphile-export.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/graphile-utils.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/graphile-export.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/jest-serializer-graphql-schema.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/jest-serializer-simple.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/graphile-build.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/pg-introspection.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/pg-sql2.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/dataplan__pg.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/graphile-build-pg.tgz
+    npm publish --access=public --tag=prealpha postgraphile-private/builds/postgraphile.tgz
     ;;
   *) echo 'Not publishing' ;;
 esac
