b110b11817c609cf17ea6ae25e76ce03087f37ab
Benjie Gillam <benjie@jemjie.com>
2023-01-17 15:32:17 +0000

    More DYK tests

---
+++ utils/devil-you-know/__tests__/set.test.ts
@@ -0,0 +1,42 @@
+import dyk from "../dist";
+
+const symbol = Symbol("symbol");
+
+const testCases: Array<
+  [string | symbol | number, ReturnType<typeof dyk.compile>]
+> = [
+  ["foo", { refs: {}, string: "obj.foo" }],
+  ["1foo", { refs: {}, string: `obj["1foo"]` }],
+  [
+    '1foo"]; process.exit(1); //',
+    { refs: {}, string: `obj["1foo\\"]; process.exit(1); //"]` },
+  ],
+  ["__proto__", { refs: {}, string: "obj.__proto__" }],
+  [1, { refs: {}, string: `obj[1]` }],
+  [symbol, { refs: { _$$_ref_1: symbol }, string: `obj[_$$_ref_1]` }],
+];
+
+it.each(testCases)("set on Object.create(null)", (val, expected) => {
+  expect(dyk.compile(dyk`obj${dyk.set(val, true)}`)).toEqual(expected);
+});
+
+it("works on 'frogs' if non-null prototype", () => {
+  expect(dyk.compile(dyk`obj${dyk.set("frogs")}`)).toEqual({
+    refs: {},
+    string: "obj.frogs",
+  });
+});
+it("throws on '__proto__' if non-null prototype", () => {
+  expect(() =>
+    dyk.compile(dyk`obj${dyk.set("__proto__")}`),
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"Attempted to set '__proto__' on an object that isn't declared as having a null prototype. This could be unsafe."`,
+  );
+});
+it("throws on 'constructor' if non-null prototype", () => {
+  expect(() =>
+    dyk.compile(dyk`obj${dyk.set("constructor")}`),
+  ).toThrowErrorMatchingInlineSnapshot(
+    `"Attempted to set 'constructor' on an object that isn't declared as having a null prototype. This could be unsafe."`,
+  );
+});
