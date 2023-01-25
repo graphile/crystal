61d6ec7bf3acebcd3863734e1c4a4730077b4050
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2023-01-25 14:11:26 +0000

    tests for reserved words as table names

--- postgraphile/postgraphile/__tests__/queries/v4/js-reserved-table-names.sql
+++ postgraphile/postgraphile/__tests__/queries/v4/js-reserved-table-names.sql
@@ -1,13 +1,6 @@
 select
-  __yield__."crop" as "0",
-  __yield__."export" as "1",
-  __yield__."id"::text as "2"
-from "js_reserved"."yield" as __yield__
-order by __yield__."id" asc;
-
-select
-  __constructor__."export" as "0",
-  __constructor__."name" as "1",
+  __constructor__."name" as "0",
+  __constructor__."export" as "1",
   __constructor__."id"::text as "2"
 from "js_reserved"."constructor" as __constructor__
 order by __constructor__."id" asc;
@@ -40,8 +33,8 @@ from (
 ) as __constructor_identifiers__,
 lateral (
   select
-    __constructor__."name" as "0",
-    __constructor__."export" as "1",
+    __constructor__."export" as "0",
+    __constructor__."name" as "1",
     __constructor__."id"::text as "2",
     __constructor_identifiers__.idx as "3"
   from "js_reserved"."constructor" as __constructor__
@@ -70,6 +63,13 @@ lateral (
   order by __constructor__."id" asc
 ) as __constructor_result__;
 
+select
+  __yield__."crop" as "0",
+  __yield__."export" as "1",
+  __yield__."id"::text as "2"
+from "js_reserved"."yield" as __yield__
+order by __yield__."id" asc;
+
 select __yield_result__.*
 from (
   select
@@ -79,7 +79,7 @@ from (
 ) as __yield_identifiers__,
 lateral (
   select
-    __yield__."export" as "0",
+    __yield__."crop" as "0",
     __yield__."id"::text as "1",
     __yield_identifiers__.idx as "2"
   from "js_reserved"."yield" as __yield__
@@ -108,3 +108,114 @@ lateral (
   )
   order by __yield__."id" asc
 ) as __yield_result__;
+
+select
+  __proto__."id"::text as "0",
+  __proto__."name" as "1",
+  __proto__."brand" as "2"
+from "js_reserved"."__proto__" as __proto__
+order by __proto__."id" asc;
+
+select __proto_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __proto_identifiers__,
+lateral (
+  select
+    __proto__."brand" as "0",
+    __proto__."name" as "1",
+    __proto__."id"::text as "2",
+    __proto_identifiers__.idx as "3"
+  from "js_reserved"."__proto__" as __proto__
+  where (
+    __proto__."id" = __proto_identifiers__."id0"
+  )
+  order by __proto__."id" asc
+) as __proto_result__;
+
+select __proto_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"text" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __proto_identifiers__,
+lateral (
+  select
+    __proto__."brand" as "0",
+    __proto__."id"::text as "1",
+    __proto_identifiers__.idx as "2"
+  from "js_reserved"."__proto__" as __proto__
+  where (
+    __proto__."name" = __proto_identifiers__."id0"
+  )
+  order by __proto__."id" asc
+) as __proto_result__;
+
+select
+  __null__."break" as "0",
+  __null__."hasownproperty" as "1",
+  __null__."id"::text as "2"
+from "js_reserved"."null" as __null__
+order by __null__."id" asc;
+
+select __null_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"text" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __null_identifiers__,
+lateral (
+  select
+    __null__."hasownproperty" as "0",
+    __null__."id"::text as "1",
+    __null_identifiers__.idx as "2"
+  from "js_reserved"."null" as __null__
+  where (
+    __null__."break" = __null_identifiers__."id0"
+  )
+  order by __null__."id" asc
+) as __null_result__;
+
+select __null_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"text" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __null_identifiers__,
+lateral (
+  select
+    __null__."break" as "0",
+    __null__."id"::text as "1",
+    __null_identifiers__.idx as "2"
+  from "js_reserved"."null" as __null__
+  where (
+    __null__."hasownproperty" = __null_identifiers__."id0"
+  )
+  order by __null__."id" asc
+) as __null_result__;
+
+select __null_result__.*
+from (
+  select
+    ids.ordinality - 1 as idx,
+    (ids.value->>0)::"int4" as "id0"
+  from json_array_elements($1::json) with ordinality as ids
+) as __null_identifiers__,
+lateral (
+  select
+    __null__."break" as "0",
+    __null__."hasownproperty" as "1",
+    __null__."id"::text as "2",
+    __null_identifiers__.idx as "3"
+  from "js_reserved"."null" as __null__
+  where (
+    __null__."id" = __null_identifiers__."id0"
+  )
+  order by __null__."id" asc
+) as __null_result__;
