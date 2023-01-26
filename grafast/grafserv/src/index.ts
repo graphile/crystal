e51a05287c6ac0a28ef0abaccbc58fd4fc027cd9
Benjie Gillam <benjie@jemjie.com>
2023-01-26 15:11:03 +0000

    Remove old exports

--- grafast/grafserv/src/index.ts
+++ grafast/grafserv/src/index.ts
@@ -1,5 +1,2 @@
-// export { ServerParams } from "./interfaces.js";
-// export { Grafserv, grafserv } from "./middleware/index.js";
-
 export { GrafservBase } from "./core/base.js";
 export { GrafservConfig } from "./interfaces.js";
