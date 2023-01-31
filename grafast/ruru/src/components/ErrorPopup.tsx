ee66ce966e0898a3323d57b4e700e660ab77ee9c
Benjie Gillam <benjie@jemjie.com>
2023-01-31 08:59:08 +0000

    Move from inline styles to CSS

--- grafast/ruru/src/components/ErrorPopup.tsx
+++ grafast/ruru/src/components/ErrorPopup.tsx
@@ -6,30 +6,10 @@ export const ErrorPopup: FC<{ error: Error; onClose: () => void }> = ({
   error,
   onClose,
 }) => (
-  <div
-    style={{
-      position: "absolute",
-      top: 10,
-      right: 10,
-      backgroundColor: "#ff9999",
-      border: "1px solid black",
-      borderRadius: 7,
-      width: "20rem",
-      maxWidth: "90vw",
-    }}
-  >
-    <button
-      style={{
-        float: "right",
-        background: "transparent",
-        border: "none",
-        marginLeft: 10,
-        marginBottom: 10,
-      }}
-      onClick={onClose}
-    >
+  <div className="errorPopup">
+    <button className="errorPopupClose" onClick={onClose}>
       🗙
     </button>
-    <div style={{ padding: 10 }}>{String(error)}</div>
+    <div className="errorPopupError">{String(error)}</div>
   </div>
 );
