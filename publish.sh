ad0b995d39341fd7cc92da79f712be671e8fd9c0
Benjie Gillam <benjie@jemjie.com>
2023-02-06 21:37:11 +0000

    Tidier

--- publish.sh
+++ publish.sh
@@ -1,51 +1,53 @@
 #!/usr/bin/env bash
 set -e -x
 
+BUILD_DIR=postgraphile-private/builds
+
 cd ..
-tar tzf postgraphile-private/builds/graphile__lru.tgz
-tar tzf postgraphile-private/builds/tamedevil.tgz
-tar tzf postgraphile-private/builds/graphile-config.tgz
-tar tzf postgraphile-private/builds/ruru-components.tgz
-tar tzf postgraphile-private/builds/ruru.tgz
-tar tzf postgraphile-private/builds/grafast.tgz
-tar tzf postgraphile-private/builds/grafserv.tgz
-tar tzf postgraphile-private/builds/dataplan__json.tgz
-tar tzf postgraphile-private/builds/eslint-plugin-graphile-export.tgz
-tar tzf postgraphile-private/builds/graphile-utils.tgz
-tar tzf postgraphile-private/builds/graphile-export.tgz
-tar tzf postgraphile-private/builds/jest-serializer-graphql-schema.tgz
-tar tzf postgraphile-private/builds/jest-serializer-simple.tgz
-tar tzf postgraphile-private/builds/graphile-build.tgz
-tar tzf postgraphile-private/builds/pg-introspection.tgz
-tar tzf postgraphile-private/builds/pg-sql2.tgz
-tar tzf postgraphile-private/builds/dataplan__pg.tgz
-tar tzf postgraphile-private/builds/graphile-build-pg.tgz
-tar tzf postgraphile-private/builds/postgraphile.tgz
-tar tzf postgraphile-private/builds/graphile__simplify-inflection.tgz
+tar tzf $BUILD_DIR/graphile__lru.tgz
+tar tzf $BUILD_DIR/tamedevil.tgz
+tar tzf $BUILD_DIR/graphile-config.tgz
+tar tzf $BUILD_DIR/ruru-components.tgz
+tar tzf $BUILD_DIR/ruru.tgz
+tar tzf $BUILD_DIR/grafast.tgz
+tar tzf $BUILD_DIR/grafserv.tgz
+tar tzf $BUILD_DIR/dataplan__json.tgz
+tar tzf $BUILD_DIR/eslint-plugin-graphile-export.tgz
+tar tzf $BUILD_DIR/graphile-utils.tgz
+tar tzf $BUILD_DIR/graphile-export.tgz
+tar tzf $BUILD_DIR/jest-serializer-graphql-schema.tgz
+tar tzf $BUILD_DIR/jest-serializer-simple.tgz
+tar tzf $BUILD_DIR/graphile-build.tgz
+tar tzf $BUILD_DIR/pg-introspection.tgz
+tar tzf $BUILD_DIR/pg-sql2.tgz
+tar tzf $BUILD_DIR/dataplan__pg.tgz
+tar tzf $BUILD_DIR/graphile-build-pg.tgz
+tar tzf $BUILD_DIR/postgraphile.tgz
+tar tzf $BUILD_DIR/graphile__simplify-inflection.tgz
 
 read -n1 -p "Publish? [y,n]" doit
 case $doit in
   y|Y)
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/graphile__lru.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/tamedevil.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/graphile-config.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/ruru-components.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/ruru.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/grafast.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/grafserv.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/dataplan__json.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/eslint-plugin-graphile-export.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/graphile-utils.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/graphile-export.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/jest-serializer-graphql-schema.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/jest-serializer-simple.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/graphile-build.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/pg-introspection.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/pg-sql2.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/dataplan__pg.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/graphile-build-pg.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/postgraphile.tgz
-    npm publish --access=public --tag=prealpha postgraphile-private/builds/graphile__simplify-inflection.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile__lru.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/tamedevil.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile-config.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/ruru-components.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/ruru.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/grafast.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/grafserv.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/dataplan__json.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/eslint-plugin-graphile-export.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile-utils.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile-export.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/jest-serializer-graphql-schema.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/jest-serializer-simple.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile-build.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/pg-introspection.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/pg-sql2.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/dataplan__pg.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile-build-pg.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/postgraphile.tgz
+    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile__simplify-inflection.tgz
     ;;
   *) echo 'Not publishing' ;;
 esac
