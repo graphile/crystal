2dca23a73d2715ef743cf94e4331e6e68aedfabe
Benjie Gillam <benjie@jemjie.com>
2023-02-21 12:33:11 +0000

    Update snapshots

--- grafast/dataplan-pg/__tests__/queries/connections/pagination-before-last-pagination-only.sql
+++ grafast/dataplan-pg/__tests__/queries/connections/pagination-before-last-pagination-only.sql
@@ -1,13 +1,3 @@
-select
-  (count(*))::text as "0"
-from app_public.messages as __messages__
-where
-  (
-    __messages__.archived_at is null
-  ) and (
-    true /* authorization checks */
-  );
-
 select __messages_result__.*
 from (
   select
@@ -30,3 +20,13 @@ lateral (
   order by __messages__."id" desc
   limit 4
 ) as __messages_result__;
+
+select
+  (count(*))::text as "0"
+from app_public.messages as __messages__
+where
+  (
+    __messages__.archived_at is null
+  ) and (
+    true /* authorization checks */
+  );
