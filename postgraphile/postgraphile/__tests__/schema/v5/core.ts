e1df41a5d2300d4eb699da4808d7047f7339c4ec
Benjie Gillam <benjie@jemjie.com>
2023-02-27 17:37:29 +0000

    Add a test of skipping NodePlugin with V5 presets

---
+++ postgraphile/postgraphile/__tests__/schema/v5/core.ts
@@ -0,0 +1,70 @@
+import type { PromiseOrDirect } from "grafast";
+import { makeSchema } from "graphile-build";
+import type { GraphQLSchema } from "graphql";
+import { lexicographicSortSchema, printSchema } from "graphql";
+import type { PoolClient } from "pg";
+
+import AmberPreset from "../../../src/presets/amber.js";
+import {
+  connectionString,
+  snapshot,
+  withPoolClientTransaction,
+} from "../../helpers.js";
+
+let countByPath = Object.create(null);
+
+export const test =
+  (
+    testPath: string,
+    schemata: string | string[],
+    additionalPreset: GraphileConfig.Preset = {},
+    setup?: string | ((pgClient: PoolClient) => PromiseOrDirect<unknown>),
+    finalCheck?: (schema: GraphQLSchema) => PromiseOrDirect<unknown>,
+    sort = true,
+  ) =>
+  () =>
+    withPoolClientTransaction(async (client) => {
+      if (setup) {
+        if (typeof setup === "function") {
+          await setup(client);
+        } else {
+          await client.query(setup);
+        }
+      }
+      const schemas = Array.isArray(schemata) ? schemata : [schemata];
+      const graphileBuildOptions = {};
+      const preset: GraphileConfig.Preset = {
+        extends: [AmberPreset, additionalPreset],
+        pgConfigs: [
+          {
+            adaptor: "@dataplan/pg/adaptors/pg",
+            name: "main",
+            withPgClientKey: "withPgClient",
+            pgSettingsKey: "pgSettings",
+            pgSettingsForIntrospection: null,
+            pgSettings: undefined,
+            schemas: schemas,
+            adaptorSettings: {
+              poolClient: client,
+            },
+          } as any, //GraphileConfig.PgDatabaseConfiguration<"@dataplan/pg/adaptors/pg">,
+        ],
+        schema: {
+          ...graphileBuildOptions,
+        },
+      };
+      const { schema, resolvedPreset: _resolvedPreset } = await makeSchema(
+        preset,
+      );
+      const i = testPath in countByPath ? countByPath[testPath] + 1 : 1;
+      countByPath[testPath] = i;
+      const sorted = sort ? lexicographicSortSchema(schema) : schema;
+      const printed = printSchema(sorted);
+      const filePath = `${testPath.replace(/\.test\.[jt]s$/, "")}${
+        sort || i > 1 ? `.${i}` : ""
+      }.graphql`;
+      await snapshot(printed + "\n", filePath);
+      if (finalCheck) {
+        await finalCheck(schema);
+      }
+    });
