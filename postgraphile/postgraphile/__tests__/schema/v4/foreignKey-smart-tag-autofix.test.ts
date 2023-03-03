f2cee11180da16499cd301a74e8ba6690e11bbf0
Benjie Gillam <benjie@jemjie.com>
2023-03-03 12:44:10 +0000

    foreignKey autofix option

--- postgraphile/postgraphile/__tests__/schema/v4/foreignKey-smart-tag-autofix.test.ts
+++ postgraphile/postgraphile/__tests__/schema/v4/foreignKey-smart-tag-autofix.test.ts
@@ -1,24 +1,24 @@
 import * as core from "./core.js";
 
-const ForeignKeyUniqueAutofixPlugin: GraphileConfig.Plugin = {
-  name: "ForeignKeyUniqueAutofixPlugin",
-  version: "0.0.0",
-};
-
 test(
   "autofix plugin automatically adds uniques where needed",
   core.test(
     __filename,
     ["c"],
-    {
-      appendPlugins: [ForeignKeyUniqueAutofixPlugin],
-    },
+    {},
     (pgClient) => {
       return pgClient.query(
         `\
 comment on table c.person_secret is E'@foreignKey (sekrit) references c.person (about)\\n@deprecated This is deprecated (comment on table c.person_secret).\\nTracks the person''s secret';
 `,
       );
+    },
+    undefined,
+    undefined,
+    {
+      gather: {
+        pgFakeConstraintsAutofixForeignKeyUniqueness: true,
+      },
     },
   ),
 );
