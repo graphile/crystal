fd0a2963df2cfe274e038825081ef2059ef961f4
Benjie Gillam <benjie@jemjie.com>
2023-03-07 17:05:49 +0000

    Import and convert test from V4

---
+++ graphile-build/graphile-utils/__tests__/makeAddPgTableConditionPlugin.test.ts
@@ -0,0 +1,156 @@
+import { postgraphilePresetAmber } from "postgraphile/presets/amber";
+import { makeV4Preset } from "postgraphile/presets/v4";
+import { makeSchema } from "postgraphile";
+import pg, { Pool } from "pg";
+import { ExecutionResult } from "graphql";
+import { makeAddPgTableConditionPlugin } from "../";
+import { TYPES } from "@dataplan/pg";
+import { makePgConfig } from "@dataplan/pg/adaptors/pg";
+import { grafast } from "grafast";
+
+const clean = (data: any) => {
+  if (Array.isArray(data)) {
+    return data.map(clean);
+  } else if (data && typeof data === "object") {
+    return Object.keys(data).reduce((memo, key) => {
+      const value = data[key];
+      if (key === "id" && typeof value === "number") {
+        memo[key] = "[id]";
+      } else if (key === "nodeId" && typeof value === "string") {
+        memo[key] = "[nodeId]";
+      } else {
+        memo[key] = clean(value);
+      }
+      return memo;
+    }, {});
+  } else {
+    return data;
+  }
+};
+
+let pgPool: Pool | null = null;
+
+beforeAll(() => {
+  pgPool = new pg.Pool({
+    connectionString: process.env.TEST_DATABASE_URL,
+  });
+});
+
+afterAll(() => {
+  if (pgPool) {
+    pgPool.end();
+    pgPool = null;
+  }
+});
+
+const PetsCountPlugin = makeAddPgTableConditionPlugin(
+  { schemaName: "graphile_utils", tableName: "users" },
+  "petCountAtLeast",
+  (build) => ({
+    description: "Filters users to those that have at least this many pets",
+    type: build.graphql.GraphQLInt,
+  }),
+  (value, helpers) => {
+    expect(helpers.build.graphql).toBeTruthy();
+    const { sqlTableAlias, sql, $condition } = helpers;
+    const val = value.get();
+    return sql.fragment`(select count(*) from graphile_utils.pets where pets.user_id = ${sqlTableAlias}.id) >= ${$condition.placeholder(
+      val,
+      TYPES.int,
+    )}`;
+  },
+);
+
+it("allows adding a condition to a Relay connection", async () => {
+  const preset: GraphileConfig.Preset = {
+    extends: [
+      postgraphilePresetAmber,
+      makeV4Preset({
+        disableDefaultMutations: true,
+      }),
+    ],
+    plugins: [PetsCountPlugin],
+    pgConfigs: [
+      makePgConfig({
+        pool: pgPool!,
+        schemas: ["graphile_utils"],
+      }),
+    ],
+  };
+  const { schema, resolvedPreset } = await makeSchema(preset);
+  expect(schema).toMatchSnapshot();
+  const result = await grafast(
+    {
+      schema,
+      source: /* GraphQL */ `
+        query {
+          allUsers(condition: { petCountAtLeast: 3 }) {
+            nodes {
+              nodeId
+              id
+              name
+              email
+              bio
+            }
+          }
+        }
+      `,
+    },
+    resolvedPreset,
+    {},
+  );
+  const { data, errors } = result as ExecutionResult;
+  expect(errors).toBeFalsy();
+  expect(data).toBeTruthy();
+  expect(data!.allUsers).toBeTruthy();
+  expect((data!.allUsers as any).nodes).toBeTruthy();
+  expect((data!.allUsers as any).nodes.length).toEqual(1);
+  expect((data!.allUsers as any).nodes[0].email).toEqual(
+    "caroline@example.com",
+  );
+});
+
+it("allows adding a condition to a simple collection", async () => {
+  const preset: GraphileConfig.Preset = {
+    extends: [
+      postgraphilePresetAmber,
+      makeV4Preset({
+        simpleCollections: "both",
+        disableDefaultMutations: true,
+      }),
+    ],
+    plugins: [PetsCountPlugin],
+    pgConfigs: [
+      makePgConfig({
+        pool: pgPool!,
+        schemas: ["graphile_utils"],
+      }),
+    ],
+  };
+  const { schema, resolvedPreset } = await makeSchema(preset);
+  expect(schema).toMatchSnapshot();
+  const result = await grafast(
+    {
+      schema,
+      source: `
+      query {
+        allUsersList(condition: { petCountAtLeast: 3 }) {
+          nodeId
+          id
+          name
+          email
+          bio
+        }
+      }
+    `,
+    },
+    resolvedPreset,
+    {},
+  );
+  const { data, errors } = result as ExecutionResult;
+  expect(errors).toBeFalsy();
+  expect(data).toBeTruthy();
+  expect(data!.allUsersList as any).toBeTruthy();
+  expect((data!.allUsersList as any).length).toEqual(1);
+  expect((data!.allUsersList as any)[0].email).toEqual("caroline@example.com");
+});
