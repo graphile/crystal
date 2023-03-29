306279176ceda1c90a37eac2a4c0d70263e85aed
Benjie Gillam <benjie@jemjie.com>
2023-03-29 09:47:23 +0100

    Update references

--- graphile-build/graphile-build-pg/src/examples/benjies-test-script.ts
+++ graphile-build/graphile-build-pg/src/examples/benjies-test-script.ts
@@ -95,7 +95,7 @@ pool.on("error", (e) => {
   console.log("Sources:");
   console.log(
     "  " +
-      input.pgSources
+      input.pgRegistry.pgSources
         .map(
           (s) => grafastPrint((s as any).name),
           // + ` => ${(s as any).extensions?.tags?.name}`,
