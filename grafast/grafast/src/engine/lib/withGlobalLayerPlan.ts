e96ce016dffa7070fca7a580cbb8a58987a7c695
Benjie Gillam <benjie@jemjie.com>
2023-05-09 13:01:52 +0100

    Avoid object allocation for global data

--- grafast/grafast/src/engine/lib/withGlobalLayerPlan.ts
+++ grafast/grafast/src/engine/lib/withGlobalLayerPlan.ts
@@ -1,44 +1,46 @@
 import type { LayerPlan } from "../LayerPlan";
 
-let globalData: {
-  layerPlan: LayerPlan;
-  polymorphicPaths: ReadonlySet<string>;
-} | null = null;
+let globalData_layerPlan: LayerPlan | null = null;
+let globalData_polymorphicPaths: ReadonlySet<string> | null = null;
 
 export function withGlobalLayerPlan<T>(
   layerPlan: LayerPlan,
   polymorphicPaths: ReadonlySet<string>,
   callback: () => T,
 ): T {
-  const oldGlobalData = globalData;
-  globalData = {
-    layerPlan,
-    polymorphicPaths,
-  };
+  const oldGlobalData = globalData_layerPlan
+    ? {
+        layerPlan: globalData_layerPlan,
+        polymorphicPaths: globalData_polymorphicPaths,
+      }
+    : null;
+  globalData_layerPlan = layerPlan;
+  globalData_polymorphicPaths = polymorphicPaths;
   try {
     return callback();
   } finally {
-    globalData = oldGlobalData;
+    if (oldGlobalData !== null) {
+      globalData_layerPlan = oldGlobalData.layerPlan;
+      globalData_polymorphicPaths = oldGlobalData.polymorphicPaths;
+    }
   }
 }
 
 export function currentLayerPlan(): LayerPlan {
-  const lp = globalData?.layerPlan;
-  if (!lp) {
+  if (!globalData_layerPlan) {
     throw new Error(
       // Must only be called from inside `withGlobalLayerPlan`!
       "Now is not a valid time to call `currentLayerPlan`. This error typically occurs when you attempt to call a Grafast step function from outside of the planning lifecycle - it's important to note that Grafast plans must be resolved synchronously, so check for 'async' or 'setTimeout' or any location where a step function is called outside of a plan resolver. For more information, read about plan resolvers: https://grafast.org/grafast/plan-resolvers",
     );
   }
-  return lp;
+  return globalData_layerPlan;
 }
 
 export function currentPolymorphicPaths(): ReadonlySet<string> {
-  const pp = globalData?.polymorphicPaths;
-  if (!pp) {
+  if (!globalData_polymorphicPaths) {
     throw new Error(
       "GrafastInternalError<b0b05743-8b21-42c6-9b53-925013d88bd1>: currentPolymorphicPaths called out of turn; must only called within a withGlobalLayerPlan callback",
     );
   }
-  return pp;
+  return globalData_polymorphicPaths;
 }
