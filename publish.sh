a98953a5c6771591e742310610e4a8648d7df777
Benjie Gillam <benjie@jemjie.com>
2023-05-17 12:11:52 +0100

    Make it easy to change the tag we publish with

--- publish.sh
+++ publish.sh
@@ -2,6 +2,7 @@
 set -e -x
 
 BUILD_DIR=postgraphile-private/builds
+TAG=alpha
 
 cd ..
 tar tzf $BUILD_DIR/graphile__lru.tgz
@@ -30,28 +31,28 @@ tar tzf $BUILD_DIR/graphile.tgz
 read -n1 -p "Publish? [y,n]" doit
 case $doit in
   y|Y)
-    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile__lru.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/tamedevil.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile-config.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/ruru-components.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/ruru.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/grafast.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/grafserv.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/dataplan__json.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/eslint-plugin-graphile-export.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile-utils.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile-export.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/jest-serializer-graphql-schema.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/jest-serializer-simple.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile-build.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/pg-introspection.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/pg-sql2.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/dataplan__pg.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile-build-pg.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/postgraphile.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile__simplify-inflection.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/grafserv__persisted.tgz
-    npm publish --access=public --tag=prealpha $BUILD_DIR/graphile.tgz
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/graphile__lru.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/tamedevil.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/graphile-config.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/ruru-components.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/ruru.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/grafast.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/grafserv.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/dataplan__json.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/eslint-plugin-graphile-export.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/graphile-utils.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/graphile-export.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/jest-serializer-graphql-schema.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/jest-serializer-simple.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/graphile-build.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/pg-introspection.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/pg-sql2.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/dataplan__pg.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/graphile-build-pg.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/postgraphile.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/graphile__simplify-inflection.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/grafserv__persisted.tgz"
+    npm publish --access=public --tag="$TAG" "$BUILD_DIR/graphile.tgz"
     ;;
   *) echo 'Not publishing' ;;
 esac
