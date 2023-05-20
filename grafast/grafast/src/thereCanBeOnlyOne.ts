c78af257566bcb988b970fc479ceca8ac712beff
Benjie Gillam <benjie@jemjie.com>
2023-05-20 21:04:18 +0100

    Fix error for duplicate grafast

--- grafast/grafast/src/thereCanBeOnlyOne.ts
+++ grafast/grafast/src/thereCanBeOnlyOne.ts
@@ -4,10 +4,10 @@ const globalAny = globalThis as any;
 if (globalAny[$$grafast] === true) {
   // Already warned
 } else if (globalAny[$$grafast]) {
-  globalAny[$$grafast] = true;
   console.trace(
-    `WARNING: more than one version of the 'grafast' module has been instantiated - you must ensure there is exactly one grafast (e.g. using yarn "resolutions" or similar techniques). Try clearing your package lockfile and reinstalling. ${globalAny[$$grafast]}`,
+    `WARNING: more than one version of the 'grafast' module has been instantiated - you must ensure there is exactly one grafast (e.g. using yarn "resolutions" or similar techniques). Try clearing your package lockfile and reinstalling. ${globalAny[$$grafast].stack}`,
   );
+  globalAny[$$grafast] = true;
 } else {
   try {
     // Capture the stack trace
