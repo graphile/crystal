16eba6b6fb30d3843115cdc3efbd87e3bddc8e22
Benjie Gillam <benjie@jemjie.com>
2023-05-23 16:15:45 +0100

    Test can resolve null for empty interface

---
+++ grafast/grafast/__tests__/interface-zero-test.ts
@@ -0,0 +1,44 @@
+import { expect } from "chai";
+import type { ExecutionResult } from "graphql";
+import { it } from "mocha";
+
+import { constant, grafast, makeGrafastSchema } from "../dist/index.js";
+
+const schema = makeGrafastSchema({
+  typeDefs: /* GraphQL */ `
+    interface ZeroImplementations {
+      nobody: Int
+      implements: String
+    }
+    type Query {
+      interface: ZeroImplementations
+    }
+  `,
+  plans: {
+    Query: {
+      interface() {
+        return constant(null);
+      },
+    },
+  },
+});
+
+it("planning timeout works", async () => {
+  const source = /* GraphQL */ `
+    {
+      interface {
+        nobody
+        implements
+      }
+    }
+  `;
+  const result = (await grafast({
+    schema,
+    source,
+  })) as ExecutionResult;
+  expect(result.errors).to.equal(undefined);
+  expect(JSON.stringify(result.data, null, 2)).to.equal(`\
+{
+  "interface": null
+}`);
+});
