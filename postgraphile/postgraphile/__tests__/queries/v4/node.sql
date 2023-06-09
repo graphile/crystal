885f9c844dd146cf1694eca5eba1040c9f4d9a8f
Benjie Gillam <benjie@jemjie.com>
2023-06-09 12:14:24 +0100

    New resource names changed a few SQL snapshots

--- postgraphile/postgraphile/__tests__/queries/v4/node.sql
+++ postgraphile/postgraphile/__tests__/queries/v4/node.sql
@@ -91,37 +91,37 @@ lateral (
   order by __compound_key__."person_id_1" asc, __compound_key__."person_id_2" asc
 ) as __compound_key_result__;
 
-select __similar_table1_result__.*
-from (select 0 as idx, $1::"int4" as "id0") as __similar_table1_identifiers__,
+select __similar_table_1_result__.*
+from (select 0 as idx, $1::"int4" as "id0") as __similar_table_1_identifiers__,
 lateral (
   select
-    __similar_table1__."id"::text as "0",
-    __similar_table1__."col1"::text as "1",
-    __similar_table1__."col2"::text as "2",
-    __similar_table1__."col3"::text as "3",
-    __similar_table1_identifiers__.idx as "4"
-  from "a"."similar_table_1" as __similar_table1__
+    __similar_table_1__."id"::text as "0",
+    __similar_table_1__."col1"::text as "1",
+    __similar_table_1__."col2"::text as "2",
+    __similar_table_1__."col3"::text as "3",
+    __similar_table_1_identifiers__.idx as "4"
+  from "a"."similar_table_1" as __similar_table_1__
   where (
-    __similar_table1__."id" = __similar_table1_identifiers__."id0"
+    __similar_table_1__."id" = __similar_table_1_identifiers__."id0"
   )
-  order by __similar_table1__."id" asc
-) as __similar_table1_result__;
+  order by __similar_table_1__."id" asc
+) as __similar_table_1_result__;
 
-select __similar_table2_result__.*
-from (select 0 as idx, $1::"int4" as "id0") as __similar_table2_identifiers__,
+select __similar_table_2_result__.*
+from (select 0 as idx, $1::"int4" as "id0") as __similar_table_2_identifiers__,
 lateral (
   select
-    __similar_table2__."id"::text as "0",
-    __similar_table2__."col3"::text as "1",
-    __similar_table2__."col4"::text as "2",
-    __similar_table2__."col5"::text as "3",
-    __similar_table2_identifiers__.idx as "4"
-  from "a"."similar_table_2" as __similar_table2__
+    __similar_table_2__."id"::text as "0",
+    __similar_table_2__."col3"::text as "1",
+    __similar_table_2__."col4"::text as "2",
+    __similar_table_2__."col5"::text as "3",
+    __similar_table_2_identifiers__.idx as "4"
+  from "a"."similar_table_2" as __similar_table_2__
   where (
-    __similar_table2__."id" = __similar_table2_identifiers__."id0"
+    __similar_table_2__."id" = __similar_table_2_identifiers__."id0"
   )
-  order by __similar_table2__."id" asc
-) as __similar_table2_result__;
+  order by __similar_table_2__."id" asc
+) as __similar_table_2_result__;
 
 select
   __person__."id"::text as "0",
--- postgraphile/postgraphile/__tests__/queries/v4/orderByNullsLast.sql
+++ postgraphile/postgraphile/__tests__/queries/v4/orderByNullsLast.sql
@@ -1,11 +1,11 @@
 select
-  __similar_table1__."id"::text as "0",
-  __similar_table1__."col2"::text as "1"
-from "a"."similar_table_1" as __similar_table1__
-order by __similar_table1__."col2" asc nulls last, __similar_table1__."id" asc;
+  __similar_table_1__."id"::text as "0",
+  __similar_table_1__."col2"::text as "1"
+from "a"."similar_table_1" as __similar_table_1__
+order by __similar_table_1__."col2" asc nulls last, __similar_table_1__."id" asc;
 
 select
-  __similar_table1__."id"::text as "0",
-  __similar_table1__."col2"::text as "1"
-from "a"."similar_table_1" as __similar_table1__
-order by __similar_table1__."col2" desc nulls last, __similar_table1__."id" asc;
+  __similar_table_1__."id"::text as "0",
+  __similar_table_1__."col2"::text as "1"
+from "a"."similar_table_1" as __similar_table_1__
+order by __similar_table_1__."col2" desc nulls last, __similar_table_1__."id" asc;
