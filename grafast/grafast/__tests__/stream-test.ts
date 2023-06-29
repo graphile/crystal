73a11502e83dc663b894343e76ce1e193ac0b7e1
Benjie Gillam <benjie@jemjie.com>
2023-06-29 13:17:15 +0100

    Add test for streamable step

---
+++ grafast/grafast/__tests__/stream-test.ts
@@ -0,0 +1,131 @@
+/* eslint-disable graphile-export/exhaustive-deps, graphile-export/export-methods, graphile-export/export-instances, graphile-export/export-subclasses, graphile-export/no-nested */
+import { expect } from "chai";
+import type { AsyncExecutionResult } from "graphql";
+import { it } from "mocha";
+
+import type { PromiseOrDirect } from "../dist/index.js";
+import {
+  constant,
+  ExecutableStep,
+  grafast,
+  lambda,
+  makeGrafastSchema,
+} from "../dist/index.js";
+
+class SyncListCallbackStep<
+  TIn,
+  TOut extends any[],
+> extends ExecutableStep<TOut> {
+  isSyncAndSafe = false;
+  constructor(
+    $dep: ExecutableStep<TIn>,
+    private callback: (val: TIn) => PromiseOrDirect<TOut>,
+  ) {
+    super();
+    this.addDependency($dep);
+  }
+  execute(_count: number, [val]: [Array<TIn>]): Array<PromiseOrDirect<TOut>> {
+    return val.map((entry) => this.callback(entry));
+  }
+  stream(_count: number, [val]: [Array<TIn>]) {
+    const { callback } = this;
+    return val.map((entry) =>
+      (async function* () {
+        const data = await callback(entry);
+        yield* data;
+      })(),
+    );
+  }
+}
+
+const makeSchema = (useStreamableStep = false) => {
+  return makeGrafastSchema({
+    typeDefs: /* GraphQL */ `
+      type OtherThing {
+        id: Int
+      }
+      type Thing {
+        id: Int
+        anotherList: [OtherThing!]
+      }
+      type Query {
+        list: [Thing!]
+      }
+    `,
+    plans: {
+      Query: {
+        list() {
+          return constant([1, 2]);
+        },
+      },
+      Thing: {
+        id($i: ExecutableStep<number>) {
+          return $i;
+        },
+        anotherList($i: ExecutableStep<number>) {
+          const cb = (i: number) => [i + 0, i + 1, i + 2];
+          if (useStreamableStep) {
+            return new SyncListCallbackStep($i, cb);
+          } else {
+            return lambda($i, cb);
+          }
+        },
+      },
+      OtherThing: {
+        id($i: ExecutableStep<number>) {
+          return $i;
+        },
+      },
+    },
+    enableDeferStream: true,
+  });
+};
+
+it("streams with streamable step", async () => {
+  const source = /* GraphQL */ `
+    {
+      list {
+        id
+        anotherList @stream(initialCount: 2) {
+          id
+        }
+      }
+    }
+  `;
+  const schema = makeSchema(true);
+  const stream = (await grafast(
+    {
+      schema,
+      source,
+    },
+    {},
+    {},
+  )) as AsyncGenerator<AsyncExecutionResult>;
+  let payloads: AsyncExecutionResult[] = [];
+  for await (const payload of stream) {
+    payloads.push(payload);
+  }
+  expect(payloads).to.have.length(4);
+  expect(payloads[0]).to.deep.equal({
+    data: {
+      list: [
+        { id: 1, anotherList: [{ id: 1 }, { id: 2 }] },
+        { id: 2, anotherList: [{ id: 2 }, { id: 3 }] },
+      ],
+    },
+    hasNext: true,
+  });
+  expect(payloads[1]).to.deep.equal({
+    path: ["list", 0, "anotherList", 2],
+    data: { id: 3 },
+    hasNext: true,
+  });
+  expect(payloads[2]).to.deep.equal({
+    path: ["list", 1, "anotherList", 2],
+    data: { id: 4 },
+    hasNext: true,
+  });
+  expect(payloads[3]).to.deep.equal({
+    hasNext: false,
+  });
+});
