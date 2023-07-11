e0b39fea9499b8e8cf8492945312489f4e944c07
Benjie Gillam <benjie@jemjie.com>
2023-07-11 16:47:41 +0100

    Changeset should auto-commit but only add, not version

---
+++ .changeset/commit.js
@@ -0,0 +1,7 @@
+const commit = require("@changesets/cli/commit");
+
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.getAddMessage = commit.default.getAddMessage;
+exports.default = {
+  getAddMessage: exports.getAddMessage,
+};
