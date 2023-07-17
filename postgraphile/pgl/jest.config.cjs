5a7cead283c934980516e5b0773bc8a9080eef17
Benjie Gillam <benjie@jemjie.com>
2023-07-17 17:30:13 +0100

    Fix pgl and deps

--- postgraphile/pgl/jest.config.cjs
+++ postgraphile/pgl/jest.config.cjs
@@ -1,11 +1,1 @@
-const rootConfig = require("../../jest.config.base")(__dirname);
-
-module.exports = {
-  ...rootConfig,
-  testMatch: [...rootConfig.testMatch, "**/*.test.graphql"],
-  transform: {
-    ...rootConfig.transform,
-    "^.+\\.graphql$": "<rootDir>/__tests__/transform-graphql.mjs",
-  },
-  moduleFileExtensions: [...rootConfig.moduleFileExtensions, "graphql"],
-};
+module.exports = require("../../jest.config.base")(__dirname);
