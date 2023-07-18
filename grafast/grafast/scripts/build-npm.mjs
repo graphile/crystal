62c55ff71f9981d347c1d01a09302a8348aa0fba
Benjie Gillam <benjie@jemjie.com>
2023-07-18 17:52:08 +0100

    Fix grafast release command

--- grafast/grafast/scripts/build-npm.mjs
+++ grafast/grafast/scripts/build-npm.mjs
@@ -14,6 +14,9 @@ await $`tsc -b`;
 await $`webpack --mode=production`;
 await $`find dist -type d -exec mkdir -p release/{} \\;`;
 await $`find dist -type f -name '*.d.ts' -exec cp {} release/{} \\;`;
+await $`find fwd -type d -exec mkdir -p release/{} \\;`;
+await $`find fwd -type f -name '*.js' -exec cp {} release/{} \\;`;
+await $`find fwd -type f -name '*.d.ts' -exec cp {} release/{} \\;`;
 await $`cp src/.npmignore release/dist/.npmignore`;
 await $`cp README.npm.md release/README.md`;
 
