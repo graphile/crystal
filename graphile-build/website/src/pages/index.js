bfa59dbccd44db7eebd8f7f5cbabd02168859298
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2023-07-31 16:12:22 +0100

    Homepage theming

--- graphile-build/website/src/pages/index.js
+++ graphile-build/website/src/pages/index.js
@@ -4,23 +4,38 @@ import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import Layout from "@theme/Layout";
 import clsx from "clsx";
 import React from "react";
+import HeroImage from "@site/static/img/introspection.svg";
 
 import styles from "./index.module.css";
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header className={clsx(styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/graphile-build"
-          >
-            Graphile Build Overview - 5min ⏱️
-          </Link>
+        <div className={clsx("row", styles.heroRow)}>
+          <div className="col col--6">
+            <h1 className={clsx("padding-vert--md", styles.hero)}>
+              {siteConfig.tagline}
+            </h1>
+            <div className={styles.buttons}>
+              <Link
+                className={clsx(
+                  "button button--primary button--lg margin-left--none margin-right--md",
+                  styles.buttonHero,
+                )}
+                to="/graphile-build/"
+              >
+                Documentation
+              </Link>
+            </div>
+          </div>
+          <div className="col col--6">
+            <HeroImage
+              title="Coder uses magnifying glass to introspect his code on the monitor"
+              className={styles.heroImage}
+            />
+          </div>
         </div>
       </div>
     </header>
