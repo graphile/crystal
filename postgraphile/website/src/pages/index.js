3d9e138044ad737e8ba2bfcfbccdc893473c31b8
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2023-08-02 13:23:31 +0100

    v5 banner

--- postgraphile/website/src/pages/index.js
+++ postgraphile/website/src/pages/index.js
@@ -6,6 +6,7 @@ import HomepageTestimonials from "@site/src/components/HomepageTestimonials";
 import HomepageTools from "@site/src/components/HomepageTools";
 import SecondarySection from "@site/src/components/SecondarySection";
 import TertiarySection from "@site/src/components/TertiarySection";
+import WarningSection from "@site/src/components/WarningSection";
 import styles from "@site/src/pages/index.module.css";
 import HeroImage from "@site/static/img/homepage/coder.svg";
 import Layout from "@theme/Layout";
@@ -71,6 +72,14 @@ export default function Home() {
       title={`PostGraphile`}
       description="Extensible high-performance automatic GraphQL API for PostgresSQL"
     >
+      <WarningSection
+        title={`PostGraphile Version 5 early access!`}
+        body={`This website and the associated software is still a work-in-progress. Whilst suitable to run in production, we anticipate a small number of breaking changes over the coming weeks and the documentation is not yet complete. Please feel free to send pull requests or let us know of any gaps, we hope you have fun!`}
+        primaryLink={`https://graphile.org/sponsor/`}
+        primaryButtonText={`Beta Announcement`}
+        secondaryLink={`/postgraphile/next/migrating-from-v4/`}
+        secondaryButtonText={`V4 Migration Guide`}
+      />
       <HomepageHeader />
       <main>
         <HomepageTestimonials />
