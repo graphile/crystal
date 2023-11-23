021dd25f14ba0a7de598aa36774883c64c462806
Benjie Gillam <benjie@jemjie.com>
2023-11-23 10:12:20 +0000

    Fix loadOne ioEquivalence and add tests

--- grafast/grafast/__tests__/loadOne-test.ts
+++ grafast/grafast/__tests__/loadOne-test.ts
@@ -2,8 +2,14 @@ import { expect } from "chai";
 import type { ExecutionResult } from "graphql";
 import { it } from "mocha";
 
-import type { LoadOneCallback } from "../dist/index.js";
-import { grafast, loadOne, makeGrafastSchema } from "../dist/index.js";
+import type { ExecutableStep, LoadOneCallback } from "../dist/index.js";
+import {
+  grafast,
+  loadOne,
+  makeGrafastSchema,
+  object,
+  list,
+} from "../dist/index.js";
 
 interface Thing {
   id: number;
@@ -33,20 +39,44 @@ function pick<T extends object, K extends keyof T>(
 }
 
 let CALLS: {
-  ids: readonly number[];
+  specs: readonly (number | { identifier: number } | readonly number[])[];
   result: object;
   attributes: readonly (keyof Thing)[] | null;
   params: object;
 }[] = [];
 
 const loadThingByIds: LoadOneCallback<number, Thing, Record<string, never>> = (
-  ids,
+  specs,
   { attributes, params },
 ) => {
-  const result = ids
+  const result = specs
     .map((id) => THINGS.find((t) => t.id === id))
     .map((t) => (t && attributes ? pick(t, attributes) : t));
-  CALLS.push({ ids, result, attributes, params });
+  CALLS.push({ specs, result, attributes, params });
+  return result;
+};
+
+const loadThingByIdentifierObjs: LoadOneCallback<
+  { identifier: number },
+  Thing,
+  Record<string, never>
+> = (specs, { attributes, params }) => {
+  const result = specs
+    .map((spec) => THINGS.find((t) => t.id === spec.identifier))
+    .map((t) => (t && attributes ? pick(t, attributes) : t));
+  CALLS.push({ specs, result, attributes, params });
+  return result;
+};
+
+const loadThingByIdentifierLists: LoadOneCallback<
+  readonly [identifier: number],
+  Thing,
+  Record<string, never>
+> = (specs, { attributes, params }) => {
+  const result = specs
+    .map((spec) => THINGS.find((t) => t.id === spec[0]))
+    .map((t) => (t && attributes ? pick(t, attributes) : t));
+  CALLS.push({ specs, result, attributes, params });
   return result;
 };
 
@@ -60,12 +90,28 @@ const makeSchema = (useStreamableStep = false) => {
       }
       type Query {
         thingById(id: Int!): Thing
+        thingByIdObj(id: Int!): Thing
+        thingByIdList(id: Int!): Thing
       }
     `,
     plans: {
       Query: {
         thingById(_, { $id }) {
-          return loadOne($id, loadThingByIds);
+          return loadOne($id as ExecutableStep<number>, loadThingByIds);
+        },
+        thingByIdObj(_, { $id }) {
+          return loadOne(
+            object({ identifier: $id as ExecutableStep<number> }),
+            { identifier: "id" },
+            loadThingByIdentifierObjs,
+          );
+        },
+        thingByIdList(_, { $id }) {
+          return loadOne(
+            list([$id as ExecutableStep<number>]),
+            ["id"],
+            loadThingByIdentifierLists,
+          );
         },
       },
     },
@@ -117,6 +163,7 @@ it("batches across parallel trees with identical selection sets", async () => {
   expect(CALLS).to.have.length(1);
   expect(CALLS[0].attributes).to.deep.equal(["id", "name"]);
 });
+
 it("batches across parallel trees with non-identical selection sets", async () => {
   const source = /* GraphQL */ `
     {
@@ -158,4 +205,117 @@ it("batches across parallel trees with non-identical selection sets", async () =
   });
   expect(CALLS).to.have.length(1);
   expect(CALLS[0].attributes).to.deep.equal(["id", "name", "reallyLongBio"]);
+});
+
+it("skips call on pure ioEquivalence (obj)", async () => {
+  const source = /* GraphQL */ `
+    {
+      t1: thingByIdObj(id: 1) {
+        id
+      }
+    }
+  `;
+  const schema = makeSchema(false);
+
+  CALLS = [];
+  const result = (await grafast(
+    {
+      schema,
+      source,
+    },
+    {},
+    {},
+  )) as ExecutionResult;
+  expect(result).to.deep.equal({
+    data: {
+      t1: {
+        id: 1,
+      },
+    },
+  });
+  expect(CALLS).to.have.length(1);
+  expect(CALLS[0].attributes).to.have.length(0);
+});
+
+it("gets input step on pure ioEquivalence (list)", async () => {
+  const source = /* GraphQL */ `
+    {
+      t1: thingByIdList(id: 1) {
+        id
+      }
+    }
+  `;
+  const schema = makeSchema(false);
+
+  CALLS = [];
+  const result = (await grafast(
+    {
+      schema,
+      source,
+    },
+    {},
+    {},
+  )) as ExecutionResult;
+  expect(result).to.deep.equal({
+    data: {
+      t1: {
+        id: 1,
+      },
+    },
+  });
+  expect(CALLS).to.have.length(1);
+  expect(CALLS[0].attributes).to.have.length(0);
+});
+
+it("supports full call with ioEquivalence", async () => {
+  const source = /* GraphQL */ `
+    {
+      t1: thingById(id: 1) {
+        id
+        name
+      }
+      t2: thingByIdObj(id: 1) {
+        id
+        name
+      }
+      t3: thingByIdList(id: 1) {
+        id
+        name
+      }
+    }
+  `;
+  const schema = makeSchema(false);
+
+  CALLS = [];
+  const result = (await grafast(
+    {
+      schema,
+      source,
+    },
+    {},
+    {},
+  )) as ExecutionResult;
+  expect(result).to.deep.equal({
+    data: {
+      t1: {
+        id: 1,
+        name: "Eyedee Won",
+      },
+      t2: {
+        id: 1,
+        name: "Eyedee Won",
+      },
+      t3: {
+        id: 1,
+        name: "Eyedee Won",
+      },
+    },
+  });
+  expect(CALLS).to.have.length(3);
+  expect(CALLS[0].specs).to.deep.equal([1]);
+  expect(CALLS[0].attributes).to.deep.equal(["id", "name"]);
+  expect(CALLS[1].specs).to.deep.equal([{ identifier: 1 }]);
+  expect(CALLS[1].attributes).to.deep.equal(["name"]);
+  expect(CALLS[2].specs).to.deep.equal([[1]]);
+  expect(CALLS[2].attributes).to.deep.equal(["name"]);
 });
