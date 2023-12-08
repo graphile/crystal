991ae33c4e26b95905b5e534e96bec04546cbdfd
Benjie Gillam <benjie@jemjie.com>
2023-12-08 11:51:54 +0000

    Remove bundling of grafast

--- grafast/grafast/scripts/build-npm.mjs
+++ grafast/grafast/scripts/build-npm.mjs
@@ -7,13 +7,9 @@ import { esmHack, transformPackageJson } from "../../../scripts/build-core.mjs";
 cd(__dirname + "/..");
 await $`rm -Rf tsconfig.tsbuildinfo dist release`;
 await $`tsc -b`;
-await $`webpack --mode=production`;
 await $`find dist -type d -exec mkdir -p release/{} \\;`;
 await $`find dist -type f -name '*.js' -exec cp {} release/{} \\;`;
 await $`find dist -type f -name '*.d.ts' -exec cp {} release/{} \\;`;
-await $`find browser -type d -exec mkdir -p release/{} \\;`;
-await $`find browser -type f -name '*.js' -exec cp {} release/{} \\;`;
-await $`find browser -type f -name '*.d.ts' -exec cp {} release/{} \\;`;
 await $`find fwd -type d -exec mkdir -p release/{} \\;`;
 await $`find fwd -type f -name '*.js' -exec cp {} release/{} \\;`;
 await $`find fwd -type f -name '*.d.ts' -exec cp {} release/{} \\;`;
@@ -25,6 +21,9 @@ await transformPackageJson(
   __dirname + "/../package.json",
   __dirname + "/../release/package.json",
 );
+
+// TODO: force GRAPHILE_ENV="production" and eliminate all related dead branches
+
 //await esmHack(__dirname + "/../release/dist/index.js");
 //await esmHack(__dirname + "/../release/dist/envelop.js");
 //await esmHack(__dirname + "/../release/dist/mermaid.js");
