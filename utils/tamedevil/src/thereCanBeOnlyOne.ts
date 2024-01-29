8a0f1f05b9db7e2ba5c5c7fc459b477ba926bca8
Benjie Gillam <benjie@jemjie.com>
2024-01-29 17:31:11 +0000

    Change location of @internal in tamedevil APIs

--- utils/tamedevil/src/thereCanBeOnlyOne.ts
+++ utils/tamedevil/src/thereCanBeOnlyOne.ts
@@ -5,7 +5,5 @@ const revision = 1;
  * This is the secret to our safety; since this is a symbol it cannot be faked
  * in a JSON payload or other user-provided data, so external data cannot make
  * itself trusted.
- *
- * @internal
  */
 export const $$type: unique symbol = Symbol.for(`tamedevil-type-${revision}`);
