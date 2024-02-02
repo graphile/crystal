09193009dbed2ce979288c4e7bd26007068e7080
Benjie Gillam <benjie@jemjie.com>
2024-02-02 15:38:33 +0000

    Check exported schema matches pre-export schema

--- postgraphile/postgraphile/scripts/test-schema-exports.mjs
+++ postgraphile/postgraphile/scripts/test-schema-exports.mjs
@@ -1,5 +1,10 @@
-import { isSchema } from "grafast/graphql";
+import { readFile } from "fs/promises";
 import { glob } from "glob";
+import {
+  isSchema,
+  printSchema,
+  lexicographicSortSchema,
+} from "grafast/graphql";
 
 const TESTS = new URL("../__tests__", import.meta.url).pathname;
 
@@ -10,5 +15,15 @@ for (const exportPath of exportPaths) {
   const { schema } = module;
   if (!isSchema(schema)) {
     throw new Error(`Expected '${exportPath}' to export a schema`);
+  }
+  const sorted = lexicographicSortSchema(schema);
+  const printed = printSchema(schema).trim();
+  const sortedPrinted = printSchema(sorted).trim();
+  const schemaPath = exportPath.replace(/\.export\.mjs$/, ".graphql");
+  const schemaString = (
+    await readFile(`${TESTS}/${schemaPath}`, "utf8")
+  ).trim();
+  if (schemaString !== printed && schemaString !== sortedPrinted) {
+    throw new Error(`Schemas differ`);
   }
 }
