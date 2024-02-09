28d6bf0fd3ae3d5987d75c43c311d80e7c6d9893
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2024-02-09 15:54:15 +0000

    Table works, install font awesome

--- postgraphile/website/src/theme/MDXComponents.jsx
+++ postgraphile/website/src/theme/MDXComponents.jsx
@@ -1,7 +1,14 @@
 import Equivalent from "@site/src/components/Equivalent";
 import MDXComponents from "@theme-original/MDXComponents";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component.
+import { library } from "@fortawesome/fontawesome-svg-core"; // Import the library component.
+import { fab } from "@fortawesome/free-brands-svg-icons"; // Import all brands icons.
+import { fas } from "@fortawesome/free-solid-svg-icons"; // Import all solid icons.
+
+library.add(fab, fas);
 
 export default {
   ...MDXComponents,
   equivalent: Equivalent,
+  Icon: FontAwesomeIcon, // Make the FontAwesomeIcon component available in MDX as <icon />.
 };
