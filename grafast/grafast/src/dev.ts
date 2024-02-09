f610a94d024894a2a1b1cd0d1cd26326b8126ffd
Benjie Gillam <benjie@jemjie.com>
2024-02-09 12:14:50 +0000

    Encourage people to enable GRAPHILE_ENV envvar

--- grafast/grafast/src/dev.ts
+++ grafast/grafast/src/dev.ts
@@ -1,6 +1,16 @@
 /**
  * @internal
  */
-export const isDev =
-  typeof process !== "undefined" && process.env.GRAPHILE_ENV === "development";
+export const graphileEnv =
+  typeof process !== "undefined" ? process.env.GRAPHILE_ENV : undefined;
+/**
+ * @internal
+ */
+export const isDev = graphileEnv === "development";
 export function noop(): void {}
+
+if (typeof process !== "undefined" && typeof graphileEnv === "undefined") {
+  console.warn(
+    `The GRAPHILE_ENV environmental variable is not set; we will run in production mode. In your development environments, it's recommended that you set \`GRAPHILE_ENV=development\` to opt in to additional checks that will provide guidance and help you to catch issues in your code earlier.`,
+  );
+}
