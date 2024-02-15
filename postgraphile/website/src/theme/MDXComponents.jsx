77b64b557ccc1addfba605f02478e5dab3e914b9
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2024-02-15 17:36:35 +0000

    yarn lint fix

--- postgraphile/website/src/theme/MDXComponents.jsx
+++ postgraphile/website/src/theme/MDXComponents.jsx
@@ -1,9 +1,9 @@
-import Equivalent from "@site/src/components/Equivalent";
-import MDXComponents from "@theme-original/MDXComponents";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component.
 import { library } from "@fortawesome/fontawesome-svg-core"; // Import the library component.
 import { fab } from "@fortawesome/free-brands-svg-icons"; // Import all brands icons.
 import { fas } from "@fortawesome/free-solid-svg-icons"; // Import all solid icons.
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; // Import the FontAwesomeIcon component.
+import Equivalent from "@site/src/components/Equivalent";
+import MDXComponents from "@theme-original/MDXComponents";
 
 library.add(fab, fas);
 
