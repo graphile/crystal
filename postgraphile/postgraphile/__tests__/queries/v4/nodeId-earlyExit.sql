4b3827264c546d606e99ed8023fcd6ef20b6712d
Benjie Gillam <benjie@jemjie.com>
2024-04-09 11:11:03 +0100

    Fix test snapshots: remove final SQL query

--- postgraphile/postgraphile/__tests__/queries/v4/nodeId-earlyExit.sql
+++ postgraphile/postgraphile/__tests__/queries/v4/nodeId-earlyExit.sql
@@ -35,17 +35,3 @@ lateral (
   )
   order by __person__."id" asc
 ) as __person_result__;
-
-select __post_result__.*
-from (select 0 as idx, $1::"text" as "id0") as __post_identifiers__,
-lateral (
-  select
-    __post__."id"::text as "0",
-    __post__."headline" as "1",
-    __post_identifiers__.idx as "2"
-  from "a"."post" as __post__
-  where (
-    __post__."headline" = __post_identifiers__."id0"
-  )
-  order by __post__."id" asc
-) as __post_result__;
