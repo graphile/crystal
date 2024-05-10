4b2ff1b04d95b6da24460ff03c43439bcf81aa20
Benjie Gillam <benjie@jemjie.com>
2024-05-10 17:36:47 +0100

    Update snapshots now test passes

--- postgraphile/postgraphile/__tests__/mutations/v4/relay.updateLeftArm.sql
+++ postgraphile/postgraphile/__tests__/mutations/v4/relay.updateLeftArm.sql
@@ -1,4 +1,4 @@
-update "c"."left_arm" as __left_arm__ set "person_id" = $1::"int4", "length_in_metres" = $2::"float8" where (__left_arm__."id" = $3::"int4") returning
+update "c"."left_arm" as __left_arm__ set "length_in_metres" = $1::"float8", "person_id" = $2::"int4" where (__left_arm__."id" = $3::"int4") returning
   __left_arm__."id"::text as "0",
   __left_arm__."person_id"::text as "1",
   __left_arm__."length_in_metres"::text as "2",
