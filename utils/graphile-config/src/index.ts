d12ad71c6d8e0937eebc09c72df3a855a75926ad
Benjie Gillam <benjie@jemjie.com>
2024-05-17 11:57:00 +0100

    AsyncHooks can now handle callbacks to call after the given action completes

--- utils/graphile-config/src/index.ts
+++ utils/graphile-config/src/index.ts
@@ -4,7 +4,12 @@ import { sortWithBeforeAfterProvides } from "./sort.js";
 
 export { GraphileConfig };
 
-export { applyHooks, AsyncHooks, HookObject } from "./hooks.js";
+export {
+  applyHooks,
+  AsyncHooks,
+  HookObject,
+  AsyncHookResultHandler,
+} from "./hooks.js";
 export type { PluginHook, PluginHookObject } from "./interfaces.js";
 export { isResolvedPreset, resolvePresets } from "./resolvePresets.js";
 
--- utils/graphile-config/src/interfaces.ts
+++ utils/graphile-config/src/interfaces.ts
@@ -5,9 +5,11 @@ export type PluginHookObject<T extends (...args: any[]) => any> = {
   callback: T;
 };
 
-export type PluginHook<T extends (...args: any[]) => any> =
-  | T
-  | PluginHookObject<T>;
+export type PromiseOrDirect<T> = T | PromiseLike<T>;
+
+export type PluginHook<
+  T extends (...args: any[]) => PromiseOrDirect<PluginHookCallback<any> | void>,
+> = T | PluginHookObject<T>;
 
 export type PluginHookCallback<T extends PluginHook<(...args: any[]) => any>> =
   T extends PluginHook<infer U> ? U : never;
