e841251e7049ff51413fcc94746b105e31b70157
Benjie Gillam <benjie@jemjie.com>
2024-07-02 08:49:38 +0100

    Fix more type inference issues by not using never on execute

--- grafast/grafast/__tests__/types-test.ts
+++ grafast/grafast/__tests__/types-test.ts
@@ -2,15 +2,19 @@ import type { Equals } from "tsafe";
 import { assert } from "tsafe";
 
 import type {
+  __ValueStep,
   ConstantStep,
   DataFromObjectSteps,
+  ExecutableStep,
   ListStep,
 } from "../dist/index.js";
 
 type Expected = {
   test: number[];
 };
-
 type Test = DataFromObjectSteps<{ test: ListStep<ConstantStep<number>[]> }>;
-
 assert<Equals<Test, Expected>>();
+
+type TStep = __ValueStep<Grafast.Context>;
+type TCtx = TStep extends ExecutableStep<infer U> ? U : never;
+assert<Equals<TCtx, Grafast.Context>>();
