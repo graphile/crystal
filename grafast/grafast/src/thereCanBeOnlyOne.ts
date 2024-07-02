19fcfb432a2331bc0c9de7fcfcccc821c9ea8206
Benjie Gillam <benjie@jemjie.com>
2024-07-02 11:51:20 +0100

    Improve error message

--- grafast/grafast/src/thereCanBeOnlyOne.ts
+++ grafast/grafast/src/thereCanBeOnlyOne.ts
@@ -5,7 +5,9 @@ if (globalAny[$$grafast] === true) {
   // Already warned
 } else if (globalAny[$$grafast]) {
   console.trace(
-    `WARNING: more than one version of the 'grafast' module has been instantiated - you must ensure there is exactly one grafast (e.g. using yarn "resolutions" or similar techniques). Try clearing your package lockfile and reinstalling. ${globalAny[$$grafast].stack}`,
+    `WARNING: more than one version of the 'grafast' module has been instantiated - you must ensure there is exactly one grafast (e.g. using yarn "resolutions" or similar techniques). Try clearing your package lockfile and reinstalling.\n    ${String(
+      globalAny[$$grafast].stack,
+    ).replace(/\n/g, "\n    ")}`,
   );
   globalAny[$$grafast] = true;
 } else {
