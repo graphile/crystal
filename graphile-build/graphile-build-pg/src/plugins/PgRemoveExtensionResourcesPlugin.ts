2dd58e6c5f57ee64ccbe5f89a36afa89900fda8b
Benjie Gillam <benjie@jemjie.com>
2024-09-26 12:29:24 +0100

    Remove from final place

--- graphile-build/graphile-build-pg/src/plugins/PgRemoveExtensionResourcesPlugin.ts
+++ graphile-build/graphile-build-pg/src/plugins/PgRemoveExtensionResourcesPlugin.ts
@@ -1,6 +1,7 @@
 import "graphile-config";
 
 import { gatherConfig } from "graphile-build";
+import type { PgClass, PgProc } from "graphile-build-pg/pg-introspection";
 
 import { version } from "../version.js";
 
@@ -14,9 +15,19 @@ declare global {
       pgRemoveExtensionResources: Record<string, never>;
     }
   }
+  namespace DataplanPg {
+    interface PgResourceExtensions {
+      isFromExtension?: boolean;
+    }
+  }
 }
 
 const EMPTY_OBJECT = Object.freeze({});
+
+interface State {
+  extensionProcs: PgProc[];
+  extensionClasses: PgClass[];
+}
 
 export const PgRemoveExtensionResourcesPlugin: GraphileConfig.Plugin = {
   name: "PgRemoveExtensionResourcesPlugin",
@@ -29,8 +40,11 @@ export const PgRemoveExtensionResourcesPlugin: GraphileConfig.Plugin = {
     initialCache() {
       return EMPTY_OBJECT;
     },
-    initialState() {
-      return EMPTY_OBJECT;
+    initialState(): State {
+      return {
+        extensionProcs: [],
+        extensionClasses: [],
+      };
     },
     helpers: {},
     hooks: {
@@ -59,7 +73,7 @@ export const PgRemoveExtensionResourcesPlugin: GraphileConfig.Plugin = {
                   (p) => p._id === procId,
                 );
                 if (pgProc) {
-                  addBehaviorToTags(pgProc.getTags(), "-*");
+                  info.state.extensionProcs.push(pgProc);
                 }
                 // ...
                 break;
@@ -70,7 +84,7 @@ export const PgRemoveExtensionResourcesPlugin: GraphileConfig.Plugin = {
                   (p) => p._id === relId,
                 );
                 if (pgClass) {
-                  addBehaviorToTags(pgClass.getTags(), "-*");
+                  info.state.extensionClasses.push(pgClass);
                 }
                 // ...
                 break;
@@ -82,6 +96,38 @@ export const PgRemoveExtensionResourcesPlugin: GraphileConfig.Plugin = {
           }
         }
       },
+      async pgProcedures_PgResourceOptions(info, event) {
+        if (info.state.extensionProcs.includes(event.pgProc)) {
+          if (!event.resourceOptions.extensions) {
+            event.resourceOptions.extensions = Object.create(null);
+          }
+          event.resourceOptions.extensions!.isFromExtension = true;
+        }
+      },
+      async pgTables_PgResourceOptions(info, event) {
+        if (info.state.extensionClasses.includes(event.pgClass)) {
+          if (!event.resourceOptions.extensions) {
+            event.resourceOptions.extensions = Object.create(null);
+          }
+          event.resourceOptions.extensions!.isFromExtension = true;
+        }
+      },
     },
   }),
+  schema: {
+    entityBehavior: {
+      pgResource: {
+        inferred: {
+          after: ["inferred"],
+          provides: ["postInferred"],
+          callback(behavior, resource) {
+            if (resource.extensions?.isFromExtension) {
+              return [behavior, "-*"];
+            }
+            return behavior;
+          },
+        },
+      },
+    },
+  },
 };
