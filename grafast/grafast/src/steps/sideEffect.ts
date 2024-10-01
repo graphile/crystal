a19fa4b60bc28f93f73a37636a657a6466e3fd33
Benjie Gillam <benjie@jemjie.com>
2024-10-01 17:05:17 +0100

    Apply multistep to sideEffect too

--- grafast/grafast/src/steps/sideEffect.ts
+++ grafast/grafast/src/steps/sideEffect.ts
@@ -1,11 +1,11 @@
 import type {
   PromiseOrDirect,
   UnbatchedExecutionExtra,
-  UnwrapPlanTuple,
 } from "../interfaces.js";
+import type { Multistep, UnwrapMultistep } from "../multistep.js";
+import { multistep } from "../multistep.js";
 import type { ExecutableStep } from "../step.js";
 import { UnbatchedExecutableStep } from "../step.js";
-import { list } from "./list.js";
 
 /**
  * Calls the given callback function for each tuple
@@ -39,31 +39,26 @@ export class SideEffectStep<TIn, TOut> extends UnbatchedExecutableStep<TOut> {
     return this.fn(value);
   }
 }
+
 /**
  * A plan that takes the input `$plan` and feeds each value through the `fn`
  * callback. Note: if you need to pass more than one value, pass a `ListStep`
  * as the `$plan` argument.
  */
-function sideEffect<const TIn extends readonly ExecutableStep[], TOut>(
-  plans: TIn,
-  fn: (value: UnwrapPlanTuple<TIn>) => PromiseOrDirect<TOut>,
-): SideEffectStep<UnwrapPlanTuple<TIn>, TOut>;
-function sideEffect<const TIn, TOut>(
-  $plan: ExecutableStep<TIn> | null | undefined,
-  fn: (value: TIn) => PromiseOrDirect<TOut>,
-): SideEffectStep<TIn, TOut>;
-function sideEffect(
-  planOrPlans: ExecutableStep | ExecutableStep[] | null | undefined,
-  fn: (value: any) => any,
-): SideEffectStep<any, any> {
+function sideEffect<const TInMultistep extends Multistep, TOut>(
+  spec: TInMultistep,
+  fn: (value: UnwrapMultistep<TInMultistep>) => PromiseOrDirect<TOut>,
+): SideEffectStep<UnwrapMultistep<TInMultistep>, TOut> {
   if (fn.length > 1) {
     throw new Error(
       "sideEffect callback should accept one argument, perhaps you forgot to destructure the arguments?",
     );
   }
-  const $result = Array.isArray(planOrPlans)
-    ? new SideEffectStep<any, any>(list(planOrPlans), fn)
-    : new SideEffectStep<any, any>(planOrPlans, fn);
+  const $in = multistep(spec);
+  const $result = new SideEffectStep<UnwrapMultistep<TInMultistep>, TOut>(
+    $in,
+    fn,
+  );
   return $result;
 }
 
