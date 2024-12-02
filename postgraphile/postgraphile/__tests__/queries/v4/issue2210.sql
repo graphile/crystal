3988bb7f31ecb4c0f3faccc4e116cf1ecdf2628a
Benjie Gillam <benjie@jemjie.com>
2024-12-02 16:29:50 +0000

    Snapshots

---
+++ postgraphile/postgraphile/__tests__/queries/v4/issue2210.sql
@@ -0,0 +1,16 @@
+select __test_messages_result__.*
+from (select 0 as idx, $1::"uuid" as "id0") as __test_messages_identifiers__,
+lateral (
+  select
+    __test_messages__."id" as "0",
+    __test_messages__."message" as "1",
+    to_char(__test_messages__."created_at", 'YYYY-MM-DD"T"HH24:MI:SS.USTZH:TZM'::text) as "2",
+    __test_user__."test_account_id" as "3",
+    __test_user__."name" as "4",
+    (row_number() over (partition by 1))::text as "5",
+    __test_messages_identifiers__.idx as "6"
+  from "issue_2210"."test_messages"(__test_messages_identifiers__."id0") as __test_messages__
+  left outer join "issue_2210"."test_user" as __test_user__
+  on (__test_messages__."test_user_id"::"uuid" = __test_user__."test_account_id")
+  limit 51
+) as __test_messages_result__;
