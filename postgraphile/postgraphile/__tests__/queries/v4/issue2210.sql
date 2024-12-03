6041b6dc08cddb3051767ec3976c8d26eca5232a
Benjie Gillam <benjie@jemjie.com>
2024-12-03 14:16:28 +0000

    Actual implementation snapshot results

--- postgraphile/postgraphile/__tests__/queries/v4/issue2210.sql
+++ postgraphile/postgraphile/__tests__/queries/v4/issue2210.sql
@@ -7,11 +7,11 @@ lateral (
     to_char(__test_messages__."created_at", 'YYYY-MM-DD"T"HH24:MI:SS.USTZH:TZM'::text) as "2",
     __test_user__."test_account_id" as "3",
     __test_user__."name" as "4",
-    (row_number() over (partition by 1))::text as "5",
+    __test_messages__."ordinality"::text as "5",
     __test_messages_identifiers__.idx as "6"
-  from "issue_2210"."test_messages"(__test_messages_identifiers__."id0") with ordinal as __test_messages__
+  from "issue_2210"."test_messages"(__test_messages_identifiers__."id0") with ordinality as __test_messages__
   left outer join "issue_2210"."test_user" as __test_user__
   on (__test_messages__."test_user_id"::"uuid" = __test_user__."test_account_id")
-  order by __test_messages__.ordinal
+  order by __test_messages__."ordinality" asc
   limit 51
 ) as __test_messages_result__;
