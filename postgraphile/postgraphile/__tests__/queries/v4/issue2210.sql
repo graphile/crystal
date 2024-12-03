1149dcdf68d1e979a5a5924933c87ad205483855
Benjie Gillam <benjie@jemjie.com>
2024-12-03 15:39:27 +0000

    Separate queries

--- postgraphile/postgraphile/__tests__/queries/v4/issue2210.sql
+++ postgraphile/postgraphile/__tests__/queries/v4/issue2210.sql
@@ -5,13 +5,22 @@ lateral (
     __test_messages__."id" as "0",
     __test_messages__."message" as "1",
     to_char(__test_messages__."created_at", 'YYYY-MM-DD"T"HH24:MI:SS.USTZH:TZM'::text) as "2",
-    __test_user__."test_account_id" as "3",
-    __test_user__."name" as "4",
-    __test_messages__."ordinality"::text as "5",
-    __test_messages_identifiers__.idx as "6"
-  from "issue_2210"."test_messages"(__test_messages_identifiers__."id0") with ordinality as __test_messages__
-  left outer join "issue_2210"."test_user" as __test_user__
-  on (__test_messages__."test_user_id"::"uuid" = __test_user__."test_account_id")
-  order by __test_messages__."ordinality" asc
+    __test_messages__."test_user_id" as "3",
+    (row_number() over (partition by 1))::text as "4",
+    __test_messages_identifiers__.idx as "5"
+  from "issue_2210"."test_messages"(__test_messages_identifiers__."id0") as __test_messages__
   limit 51
 ) as __test_messages_result__;
+
+select __test_user_result__.*
+from (select ids.ordinality - 1 as idx, (ids.value->>0)::"uuid" as "id0" from json_array_elements($1::json) with ordinality as ids) as __test_user_identifiers__,
+lateral (
+  select
+    __test_user__."test_account_id" as "0",
+    __test_user__."name" as "1",
+    __test_user_identifiers__.idx as "2"
+  from "issue_2210"."test_user" as __test_user__
+  where (
+    __test_user__."test_account_id" = __test_user_identifiers__."id0"
+  )
+) as __test_user_result__;
