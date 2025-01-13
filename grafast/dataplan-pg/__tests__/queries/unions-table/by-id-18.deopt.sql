d1947405dae3d7712fed4fdc911af66873b9399c
Benjie Gillam <benjie@jemjie.com>
2025-01-13 13:44:10 +0000

    Remove unnecessary idx wrappers

--- grafast/dataplan-pg/__tests__/queries/unions-table/by-id-18.deopt.sql
+++ grafast/dataplan-pg/__tests__/queries/unions-table/by-id-18.deopt.sql
@@ -14,108 +14,104 @@ lateral (
     )
 ) as __union_items_result__;
 
-select __union_result__.*
-from (select ids.ordinality - 1 as idx from json_array_elements($2::json) with ordinality as ids) as __union_identifiers__,
-lateral (
-  select
-    __union__."0" as "0",
-    __union__."1"::text as "1",
-    __union_identifiers__.idx as "2"
-  from (
+select
+  __union__."0" as "0",
+  __union__."1"::text as "1"
+from (
+    select
+      __union_topics__."0",
+      __union_topics__."1",
+      "n"
+    from (
       select
-        __union_topics__."0",
-        __union_topics__."1",
-        "n"
-      from (
-        select
-          'UnionTopic' as "0",
-          json_build_array((__union_topics__."id")::text) as "1",
-          row_number() over (
-            order by
-              __union_topics__."id" asc
-          ) as "n"
-        from interfaces_and_unions.union_topics as __union_topics__
-        where __union_topics__.id = $1::"int4"
-        order by
-          __union_topics__."id" asc
-      ) as __union_topics__
-    union all
+        'UnionTopic' as "0",
+        json_build_array((__union_topics__."id")::text) as "1",
+        row_number() over (
+          order by
+            __union_topics__."id" asc
+        ) as "n"
+      from interfaces_and_unions.union_topics as __union_topics__
+      where __union_topics__.id = $1::"int4"
+      order by
+        __union_topics__."id" asc
+    ) as __union_topics__
+  union all
+    select
+      __union_posts__."0",
+      __union_posts__."1",
+      "n"
+    from (
       select
-        __union_posts__."0",
-        __union_posts__."1",
-        "n"
-      from (
-        select
-          'UnionPost' as "0",
-          json_build_array((__union_posts__."id")::text) as "1",
-          row_number() over (
-            order by
-              __union_posts__."id" asc
-          ) as "n"
-        from interfaces_and_unions.union_posts as __union_posts__
-        where __union_posts__.id = $1::"int4"
-        order by
-          __union_posts__."id" asc
-      ) as __union_posts__
-    union all
+        'UnionPost' as "0",
+        json_build_array((__union_posts__."id")::text) as "1",
+        row_number() over (
+          order by
+            __union_posts__."id" asc
+        ) as "n"
+      from interfaces_and_unions.union_posts as __union_posts__
+      where __union_posts__.id = $1::"int4"
+      order by
+        __union_posts__."id" asc
+    ) as __union_posts__
+  union all
+    select
+      __union_dividers__."0",
+      __union_dividers__."1",
+      "n"
+    from (
       select
-        __union_dividers__."0",
-        __union_dividers__."1",
-        "n"
-      from (
-        select
-          'UnionDivider' as "0",
-          json_build_array((__union_dividers__."id")::text) as "1",
-          row_number() over (
-            order by
-              __union_dividers__."id" asc
-          ) as "n"
-        from interfaces_and_unions.union_dividers as __union_dividers__
-        where __union_dividers__.id = $1::"int4"
-        order by
-          __union_dividers__."id" asc
-      ) as __union_dividers__
-    union all
+        'UnionDivider' as "0",
+        json_build_array((__union_dividers__."id")::text) as "1",
+        row_number() over (
+          order by
+            __union_dividers__."id" asc
+        ) as "n"
+      from interfaces_and_unions.union_dividers as __union_dividers__
+      where __union_dividers__.id = $1::"int4"
+      order by
+        __union_dividers__."id" asc
+    ) as __union_dividers__
+  union all
+    select
+      __union_checklists__."0",
+      __union_checklists__."1",
+      "n"
+    from (
       select
-        __union_checklists__."0",
-        __union_checklists__."1",
-        "n"
-      from (
-        select
-          'UnionChecklist' as "0",
-          json_build_array((__union_checklists__."id")::text) as "1",
-          row_number() over (
-            order by
-              __union_checklists__."id" asc
-          ) as "n"
-        from interfaces_and_unions.union_checklists as __union_checklists__
-        where __union_checklists__.id = $1::"int4"
-        order by
-          __union_checklists__."id" asc
-      ) as __union_checklists__
-    union all
+        'UnionChecklist' as "0",
+        json_build_array((__union_checklists__."id")::text) as "1",
+        row_number() over (
+          order by
+            __union_checklists__."id" asc
+        ) as "n"
+      from interfaces_and_unions.union_checklists as __union_checklists__
+      where __union_checklists__.id = $1::"int4"
+      order by
+        __union_checklists__."id" asc
+    ) as __union_checklists__
+  union all
+    select
+      __union_checklist_items__."0",
+      __union_checklist_items__."1",
+      "n"
+    from (
       select
-        __union_checklist_items__."0",
-        __union_checklist_items__."1",
-        "n"
-      from (
-        select
-          'UnionChecklistItem' as "0",
-          json_build_array((__union_checklist_items__."id")::text) as "1",
-          row_number() over (
-            order by
-              __union_checklist_items__."id" asc
-          ) as "n"
-        from interfaces_and_unions.union_checklist_items as __union_checklist_items__
-        where __union_checklist_items__.id = $1::"int4"
-        order by
-          __union_checklist_items__."id" asc
-      ) as __union_checklist_items__
-    order by
-      "0" asc,
-      "n" asc
-  ) __union__
-) as __union_result__;
+        'UnionChecklistItem' as "0",
+        json_build_array((__union_checklist_items__."id")::text) as "1",
+        row_number() over (
+          order by
+            __union_checklist_items__."id" asc
+        ) as "n"
+      from interfaces_and_unions.union_checklist_items as __union_checklist_items__
+      where __union_checklist_items__.id = $1::"int4"
+      order by
+        __union_checklist_items__."id" asc
+    ) as __union_checklist_items__
+  order by
+    "0" asc,
+    "n" asc
+) __union__
+
 
 select __union_checklist_items_result__.*
 from (select 0 as idx, $1::"int4" as "id0") as __union_checklist_items_identifiers__,
--- postgraphile/postgraphile/__tests__/queries/polymorphic/person-app-vulns.app-page-2.sql
+++ postgraphile/postgraphile/__tests__/queries/polymorphic/person-app-vulns.app-page-2.sql
@@ -159,65 +159,61 @@ lateral (
   ) __vulnerabilities__
 ) as __union_result__;
 
-select __union_result__.*
-from (select ids.ordinality - 1 as idx from json_array_elements($3::json) with ordinality as ids) as __union_identifiers__,
-lateral (
-  select
-    __vulnerabilities__."0" as "0",
-    __vulnerabilities__."1"::text as "1",
-    __union_identifiers__.idx as "2"
-  from (
+select
+  __vulnerabilities__."0" as "0",
+  __vulnerabilities__."1"::text as "1"
+from (
+    select
+      __first_party_vulnerabilities__."0",
+      __first_party_vulnerabilities__."1",
+      "n"
+    from (
       select
-        __first_party_vulnerabilities__."0",
-        __first_party_vulnerabilities__."1",
-        "n"
-      from (
-        select
-          'FirstPartyVulnerability' as "0",
-          json_build_array((__first_party_vulnerabilities__."id")::text) as "1",
-          row_number() over (
-            order by
-              __first_party_vulnerabilities__."id" asc
-          ) as "n"
-        from "polymorphic"."gcp_application_first_party_vulnerabilities" as __gcp_application_first_party_vulnerabilities__
-        inner join "polymorphic"."first_party_vulnerabilities" as __first_party_vulnerabilities__
-        on (
-          __first_party_vulnerabilities__."id" = __gcp_application_first_party_vulnerabilities__."first_party_vulnerability_id"
-        )
-        where __gcp_application_first_party_vulnerabilities__."gcp_application_id" = $1::"int4"
-        order by
-          __first_party_vulnerabilities__."id" asc
-        limit 1
-      ) as __first_party_vulnerabilities__
-    union all
+        'FirstPartyVulnerability' as "0",
+        json_build_array((__first_party_vulnerabilities__."id")::text) as "1",
+        row_number() over (
+          order by
+            __first_party_vulnerabilities__."id" asc
+        ) as "n"
+      from "polymorphic"."gcp_application_first_party_vulnerabilities" as __gcp_application_first_party_vulnerabilities__
+      inner join "polymorphic"."first_party_vulnerabilities" as __first_party_vulnerabilities__
+      on (
+        __first_party_vulnerabilities__."id" = __gcp_application_first_party_vulnerabilities__."first_party_vulnerability_id"
+      )
+      where __gcp_application_first_party_vulnerabilities__."gcp_application_id" = $1::"int4"
+      order by
+        __first_party_vulnerabilities__."id" asc
+      limit 1
+    ) as __first_party_vulnerabilities__
+  union all
+    select
+      __third_party_vulnerabilities__."0",
+      __third_party_vulnerabilities__."1",
+      "n"
+    from (
       select
-        __third_party_vulnerabilities__."0",
-        __third_party_vulnerabilities__."1",
-        "n"
-      from (
-        select
-          'ThirdPartyVulnerability' as "0",
-          json_build_array((__third_party_vulnerabilities__."id")::text) as "1",
-          row_number() over (
-            order by
-              __third_party_vulnerabilities__."id" asc
-          ) as "n"
-        from "polymorphic"."gcp_application_third_party_vulnerabilities" as __gcp_application_third_party_vulnerabilities__
-        inner join "polymorphic"."third_party_vulnerabilities" as __third_party_vulnerabilities__
-        on (
-          __third_party_vulnerabilities__."id" = __gcp_application_third_party_vulnerabilities__."third_party_vulnerability_id"
-        )
-        where __gcp_application_third_party_vulnerabilities__."gcp_application_id" = $2::"int4"
-        order by
-          __third_party_vulnerabilities__."id" asc
-        limit 1
-      ) as __third_party_vulnerabilities__
-    order by
-      "0" asc,
-      "n" asc
-    limit 1
-  ) __vulnerabilities__
-) as __union_result__;
+        'ThirdPartyVulnerability' as "0",
+        json_build_array((__third_party_vulnerabilities__."id")::text) as "1",
+        row_number() over (
+          order by
+            __third_party_vulnerabilities__."id" asc
+        ) as "n"
+      from "polymorphic"."gcp_application_third_party_vulnerabilities" as __gcp_application_third_party_vulnerabilities__
+      inner join "polymorphic"."third_party_vulnerabilities" as __third_party_vulnerabilities__
+      on (
+        __third_party_vulnerabilities__."id" = __gcp_application_third_party_vulnerabilities__."third_party_vulnerability_id"
+      )
+      where __gcp_application_third_party_vulnerabilities__."gcp_application_id" = $2::"int4"
+      order by
+        __third_party_vulnerabilities__."id" asc
+      limit 1
+    ) as __third_party_vulnerabilities__
+  order by
+    "0" asc,
+    "n" asc
+  limit 1
+) __vulnerabilities__
+
 
 select __first_party_vulnerabilities_result__.*
 from (select 0 as idx, $1::"int4" as "id0") as __first_party_vulnerabilities_identifiers__,
