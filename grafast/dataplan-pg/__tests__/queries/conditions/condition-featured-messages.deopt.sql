5f7dc200b3c26809fb98c4d1aecdf4769419c50a
Benjie Gillam <benjie@jemjie.com>
2025-01-14 12:03:29 +0000

    Reorder identifiers a little

--- grafast/dataplan-pg/__tests__/queries/conditions/condition-featured-messages.deopt.sql
+++ grafast/dataplan-pg/__tests__/queries/conditions/condition-featured-messages.deopt.sql
@@ -12,7 +12,7 @@ where
 order by __forums__."id" asc;
 
 select __messages_result__.*
-from (select ids.ordinality - 1 as idx, (ids.value->>0)::"uuid" as "id0", (ids.value->>1)::"timestamptz" as "id1" from json_array_elements($2::json) with ordinality as ids) as __messages_identifiers__,
+from (select ids.ordinality - 1 as idx, (ids.value->>0)::"timestamptz" as "id0", (ids.value->>1)::"uuid" as "id1" from json_array_elements($2::json) with ordinality as ids) as __messages_identifiers__,
 lateral (
   select
     __messages__."body" as "0",
@@ -24,16 +24,16 @@ lateral (
     (
       __messages__.featured = $1::"bool"
     ) and (
-      (__messages__.archived_at is null) = (__messages_identifiers__."id1" is null)
+      (__messages__.archived_at is null) = (__messages_identifiers__."id0" is null)
     ) and (
-      __messages__."forum_id" = __messages_identifiers__."id0"
+      __messages__."forum_id" = __messages_identifiers__."id1"
     )
   order by __messages__."id" asc
   limit 6
 ) as __messages_result__;
 
 select __messages_result__.*
-from (select ids.ordinality - 1 as idx, (ids.value->>0)::"uuid" as "id0", (ids.value->>1)::"timestamptz" as "id1" from json_array_elements($2::json) with ordinality as ids) as __messages_identifiers__,
+from (select ids.ordinality - 1 as idx, (ids.value->>0)::"timestamptz" as "id0", (ids.value->>1)::"uuid" as "id1" from json_array_elements($2::json) with ordinality as ids) as __messages_identifiers__,
 lateral (
   select
     (count(*))::text as "0",
@@ -43,9 +43,9 @@ lateral (
     (
       __messages__.featured = $1::"bool"
     ) and (
-      (__messages__.archived_at is null) = (__messages_identifiers__."id1" is null)
+      (__messages__.archived_at is null) = (__messages_identifiers__."id0" is null)
     ) and (
-      __messages__."forum_id" = __messages_identifiers__."id0"
+      __messages__."forum_id" = __messages_identifiers__."id1"
     )
 ) as __messages_result__;
 
