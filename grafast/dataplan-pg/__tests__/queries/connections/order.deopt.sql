afd767d22d89a645f7087eb17eeca78aa9fed3a6
Benjie Gillam <benjie@jemjie.com>
2025-01-14 16:13:36 +0000

    Reordered queries

--- grafast/dataplan-pg/__tests__/queries/connections/order.deopt.sql
+++ grafast/dataplan-pg/__tests__/queries/connections/order.deopt.sql
@@ -1,4 +1,16 @@
 select
+  (count(*))::text as "0"
+from app_public.messages as __messages__
+left outer join app_public.users as __author__
+on (__messages__."author_id" = __author__."id")
+where
+  (
+    __messages__.archived_at is null
+  ) and (
+    true /* authorization checks */
+  );
+
+select
   __author__.username as "0",
   __messages__.body as "1",
   __messages__."id" as "2",
@@ -15,18 +27,6 @@ where
   )
 order by __author__.username desc, __messages__.body asc, __messages__."id" asc
 limit 6;
-
-select
-  (count(*))::text as "0"
-from app_public.messages as __messages__
-left outer join app_public.users as __author__
-on (__messages__."author_id" = __author__."id")
-where
-  (
-    __messages__.archived_at is null
-  ) and (
-    true /* authorization checks */
-  );
 
 select __users_result__.*
 from (select ids.ordinality - 1 as idx, (ids.value->>0)::"uuid" as "id0" from json_array_elements($1::json) with ordinality as ids) as __users_identifiers__,
