6ad04e14f79b46461f47aec5f3aa55cde4fdd64f
Benjie Gillam <benjie@jemjie.com>
2025-01-14 11:23:56 +0000

    Lint

--- grafast/dataplan-pg/src/steps/pgUnionAll.ts
+++ grafast/dataplan-pg/src/steps/pgUnionAll.ts
@@ -1554,7 +1554,7 @@ and ${condition(i + 1)}`}
   }
 
   private buildTheQuery(executionDetails: ExecutionDetails): QueryBuildResult {
-    const { count, values } = executionDetails;
+    const { count } = executionDetails;
     const { typeIdx, reverse } = this;
 
     const memberCodecs = this.memberDigests.map(
