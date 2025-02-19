0b0bc88f1eb4f031cd60f9edff70e418ddddbbae
xanderg2 <XanderG2@users.noreply.github.com>
2025-02-19 16:47:12 +0000

    add ci check to ensure sql is in lowercase

---
+++ scripts/ci-check-to-ensure-sql-is-in-lowercase.mjs
@@ -0,0 +1,98 @@
+import fs from "fs";
+import path from "path";
+
+const regex = /```sql\n([\s\S]*?)\n```/g;
+const keywords = [
+  "SELECT",
+  "UPDATE",
+  "INSERT",
+  "DELETE",
+  "WITH",
+  "FROM",
+  "WHERE",
+  "ORDER BY",
+  "GROUP BY",
+  "HAVING",
+  "AS",
+  "VALUES",
+];
+
+const WEBSITE_FOLDERS = [
+  "../postgraphile/website",
+  "../grafast/website",
+  "../graphile-build/website",
+  "../utils/website",
+];
+
+async function walkDir(dir) {
+  const files = await fs.promises.readdir(dir, { withFileTypes: true });
+  let result = [];
+
+  for (let file of files) {
+    const fullPath = path.join(dir, file.name);
+    if (file.isDirectory()) {
+      result = result.concat(await walkDir(fullPath));
+    } else {
+      result.push(fullPath);
+    }
+  }
+  return result;
+}
+
+async function readFileAsync(filePath) {
+  try {
+    const data = await fs.promises.readFile(filePath, "utf-8");
+    return data;
+  } catch (error) {
+    console.error("Error reading file:", error);
+  }
+}
+
+function wrongSQLCheck(segment) {
+  if (keywords.some((keyword) => segment.includes(keyword))) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+(async () => {
+  let allFiles = [];
+  for (const directory of WEBSITE_FOLDERS) {
+    const fileStructure = await walkDir(directory);
+    const fileStructureFiltered = fileStructure.filter(
+      (x) => x.endsWith(".md") || x.endsWith(".mdx"),
+    );
+    allFiles = allFiles.concat(fileStructureFiltered);
+  }
+
+  let wrongSQL = {};
+  for (const file of allFiles) {
+    const data = await readFileAsync(file);
+    const matches = [...data.matchAll(regex)];
+
+    let sqlSegments = [];
+
+    matches.forEach((match) => {
+      sqlSegments.push(match[1]);
+    });
+    let localwrongSQL = [];
+    for (const segment of sqlSegments) {
+      if (wrongSQLCheck(segment)) {
+        localwrongSQL.push(segment);
+      }
+    }
+    if (localwrongSQL.length >= 1) {
+      wrongSQL[file] = localwrongSQL;
+    }
+  }
+
+  console.log(
+    Object.keys(wrongSQL),
+    `The above ${Object.keys(wrongSQL).length} files contain capital SQL.`,
+  );
+
+  if (wrongSQL.length !== 0) {
+    process.exitCode = 1;
+  }
+})();
