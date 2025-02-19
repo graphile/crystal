f06ac581bf9a79d4150e7dd8877758df62d88e1e
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2025-02-19 18:09:48 +0000

    Tests for cursor variables

---
+++ grafast/dataplan-pg/__tests__/queries/connections/pagination-before-last-pagination-only.variables.sql
@@ -0,0 +1,22 @@
+select /* NOTHING?! */
+from app_public.messages as __messages__
+where
+  (
+    __messages__.archived_at is null
+  ) and (
+    true /* authorization checks */
+  ) and (
+    __messages__."id" < $1::"uuid"
+  )
+order by __messages__."id" desc
+limit 4;
+
+select
+  (count(*))::text as "0"
+from app_public.messages as __messages__
+where
+  (
+    __messages__.archived_at is null
+  ) and (
+    true /* authorization checks */
+  );
---
+++ postgraphile/postgraphile/__tests__/queries/v4/connections.variables.sql
@@ -0,0 +1,340 @@
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+order by __person__."id" asc;
+
+select
+  (count(*))::text as "0"
+from "c"."person" as __person__;
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+order by __person__."id" asc
+limit 3;
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+order by __person__."id" desc
+limit 3;
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+order by __person__."person_full_name" asc, __person__."id" asc;
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+order by __person__."person_full_name" desc, __person__."id" asc;
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+where (
+  __person__."id" < $1::"int4"
+)
+order by __person__."id" asc;
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+where (
+  __person__."id" > $1::"int4"
+)
+order by __person__."id" asc;
+
+select
+  __updatable_view__."x"::text as "0",
+  __updatable_view__."name" as "1",
+  __updatable_view__."constant"::text as "2",
+  (not (__updatable_view__ is null))::text as "3"
+from "b"."updatable_view" as __updatable_view__
+order by __updatable_view__."x" asc;
+
+select
+  __updatable_view__."x"::text as "0",
+  __updatable_view__."name" as "1",
+  __updatable_view__."constant"::text as "2",
+  (not (__updatable_view__ is null))::text as "3"
+from "b"."updatable_view" as __updatable_view__
+order by __updatable_view__."constant" asc, __updatable_view__."x" asc;
+
+select
+  __post__."id"::text as "0",
+  __post__."headline" as "1",
+  __post__."author_id"::text as "2"
+from "a"."post" as __post__
+where (
+  __post__."author_id" = $1::"int4"
+)
+order by __post__."id" asc;
+
+select
+  (count(*))::text as "0"
+from "a"."post" as __post__
+where (
+  __post__."author_id" = $1::"int4"
+);
+
+select
+  __post__."id"::text as "0",
+  __post__."headline" as "1",
+  __post__."author_id"::text as "2"
+from "a"."post" as __post__
+where (
+  __post__."author_id" = $1::"int4"
+)
+order by __post__."id" asc
+limit 3;
+
+select
+  __post__."id"::text as "0",
+  __post__."headline" as "1",
+  __post__."author_id"::text as "2"
+from "a"."post" as __post__
+where (
+  __post__."author_id" = $1::"int4"
+)
+order by __post__."headline" desc, __post__."id" desc
+limit 2;
+
+select
+  (count(*))::text as "0"
+from "a"."post" as __post__
+where (
+  __post__."author_id" = $1::"int4"
+);
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+order by __person__."id" asc
+limit 4
+offset 1;
+
+select
+  __edge_case__."row_id"::text as "0",
+  __edge_case__."not_null_has_default"::text as "1"
+from "c"."edge_case" as __edge_case__
+where (
+  __edge_case__."row_id" = $1::"int4"
+);
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+where (
+  __person__."id" < $1::"int4"
+)
+order by __person__."id" desc
+limit 3;
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+where (
+  __person__."id" > $1::"int4"
+)
+order by __person__."id" asc
+limit 2;
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+where (
+  __person__."id" > $1::"int4"
+)
+order by __person__."id" desc
+limit 2;
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+where (
+  __person__."about" is null
+)
+order by __person__."id" asc;
+
+select
+  (count(*))::text as "0"
+from "c"."person" as __person__
+where (
+  __person__."about" is null
+);
+
+select
+  __post__."id"::text as "0",
+  __post__."headline" as "1",
+  __post__."author_id"::text as "2"
+from "a"."post" as __post__
+order by __post__."author_id" desc, __post__."headline" desc, __post__."id" asc
+limit 4;
+
+select
+  (count(*))::text as "0"
+from "a"."post" as __post__;
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+where (
+  __person__."last_login_from_ip" = $1::"inet"
+)
+order by __person__."id" asc;
+
+select
+  (count(*))::text as "0"
+from "c"."person" as __person__
+where (
+  __person__."last_login_from_ip" = $1::"inet"
+);
+
+select
+  __post__."id"::text as "0",
+  __post__."author_id"::text as "1",
+  __post__."headline" as "2"
+from "a"."post" as __post__
+order by __post__."id" asc
+limit 2;
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+where (
+  __person__."last_login_from_subnet" = $1::"cidr"
+)
+order by __person__."id" asc;
+
+select
+  (count(*))::text as "0"
+from "c"."person" as __person__
+where (
+  __person__."last_login_from_subnet" = $1::"cidr"
+);
+
+select
+  __person__."id"::text as "0",
+  __person__."person_full_name" as "1",
+  __person__."email" as "2",
+  __person__."config"::text as "3",
+  __person__."last_login_from_ip"::text as "4",
+  __person__."last_login_from_subnet"::text as "5",
+  __person__."user_mac"::text as "6"
+from "c"."person" as __person__
+where (
+  __person__."user_mac" = $1::"macaddr"
+)
+order by __person__."id" asc;
+
+select
+  (count(*))::text as "0"
+from "c"."person" as __person__
+where (
+  __person__."user_mac" = $1::"macaddr"
+);
+
+select
+  __null_test_record__."nullable_text" as "0",
+  __null_test_record__."nullable_int"::text as "1",
+  __null_test_record__."id"::text as "2"
+from "c"."null_test_record" as __null_test_record__
+order by __null_test_record__."id" asc;
+
+select __person_result__.*
+from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __person_identifiers__,
+lateral (
+  select
+    __person__."person_full_name" as "0",
+    __person__."id"::text as "1",
+    __person_identifiers__.idx as "2"
+  from "c"."person" as __person__
+  where (
+    __person__."id" = __person_identifiers__."id0"
+  )
+) as __person_result__;
