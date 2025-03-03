72adee894337054675ff9960f209aa7d6e23fa48
Benjie Gillam <benjie@jemjie.com>
2025-03-03 19:31:29 +0000

    Remove unnecessary object access

--- grafast/dataplan-pg/src/parseArray.ts
+++ grafast/dataplan-pg/src/parseArray.ts
@@ -2,14 +2,18 @@ const BACKSLASH = "\\";
 const DQUOT = '"';
 const LBRACE = "{";
 const RBRACE = "}";
+const LBRACKET = "[";
+const EQUALS = "=";
 
-const MODES = {
-  EXPECT_VALUE: 0,
-  IN_QUOTES: 1,
-  IN_VALUE: 2,
-  EXPECT_DELIM_AFTER_PROCESSED: 3,
-} as const;
-type Mode = (typeof MODES)[keyof typeof MODES];
+const EXPECT_VALUE = 0;
+const IN_QUOTES = 1;
+const IN_VALUE = 2;
+const EXPECT_DELIM_AFTER_PROCESSED = 3;
+type Mode =
+  | typeof EXPECT_VALUE
+  | typeof IN_QUOTES
+  | typeof IN_VALUE
+  | typeof EXPECT_DELIM_AFTER_PROCESSED;
 
 /**
  * Parses an array according to
@@ -18,17 +22,18 @@ type Mode = (typeof MODES)[keyof typeof MODES];
 export function parseArray(str: string, typdelim = ","): any[] {
   // If starts with `[`, it is specifying the index boundas. Skip past first `=`.
   let position = 0;
-  if (str[position] === "[") {
-    position = str.indexOf("=") + 1;
-    if (position === 0)
+  if (str[position] === LBRACKET) {
+    position = str.indexOf(EQUALS) + 1;
+    if (position === 0) {
       throw new Error(`Invalid array text, array indexes not understood`);
+    }
   }
 
-  if (str[position++] !== "{") {
+  if (str[position++] !== LBRACE) {
     throw new Error(`Invalid array text - must start with {`);
   }
   const rbraceIndex = str.length - 1;
-  if (str[rbraceIndex] !== "}") {
+  if (str[rbraceIndex] !== RBRACE) {
     throw new Error(`Invalid array text - must end with }`);
   }
   const output: any[] = [];
@@ -37,15 +42,15 @@ export function parseArray(str: string, typdelim = ","): any[] {
 
   let currentStringStart: number = position;
   let currentStringParts: string[] | null = null;
-  let mode: Mode = MODES.EXPECT_VALUE;
+  let mode: Mode = EXPECT_VALUE;
 
   function delim() {
-    if (mode === MODES.IN_VALUE) {
+    if (mode === IN_VALUE) {
       const part = str.slice(currentStringStart, position);
       current.push(part === "NULL" ? null : part);
       currentStringStart = position + 1;
     }
-    mode = MODES.EXPECT_DELIM_AFTER_PROCESSED;
+    mode = EXPECT_DELIM_AFTER_PROCESSED;
   }
 
   for (; position < rbraceIndex; position++) {
@@ -54,7 +59,7 @@ export function parseArray(str: string, typdelim = ","): any[] {
     // > they are empty strings, contain curly braces, delimiter characters, double
     // > quotes, backslashes, or white space, or match the word NULL. Double quotes
     // > and backslashes embedded in element values will be backslash-escaped.
-    if (mode === MODES.IN_QUOTES) {
+    if (mode === IN_QUOTES) {
       if (char === BACKSLASH) {
         // We contain escaping, so we have to do it the slow way
         const part = str.slice(currentStringStart, position);
@@ -72,13 +77,13 @@ export function parseArray(str: string, typdelim = ","): any[] {
         } else {
           current.push(part);
         }
-        mode = MODES.EXPECT_DELIM_AFTER_PROCESSED;
+        mode = EXPECT_DELIM_AFTER_PROCESSED;
       } else {
         continue;
       }
     } else if (char === DQUOT) {
       // It's escaped
-      mode = MODES.IN_QUOTES;
+      mode = IN_QUOTES;
       currentStringStart = position + 1;
     } else if (char === LBRACE) {
       const newArray: any[] = [];
@@ -86,24 +91,24 @@ export function parseArray(str: string, typdelim = ","): any[] {
       stack.push(current);
       current = newArray;
       currentStringStart = position + 1;
-      mode = MODES.EXPECT_VALUE;
+      mode = EXPECT_VALUE;
     } else if (char === typdelim) {
       delim();
-      mode = MODES.EXPECT_VALUE;
+      mode = EXPECT_VALUE;
     } else if (char === RBRACE) {
       delim();
-      mode = MODES.EXPECT_DELIM_AFTER_PROCESSED;
+      mode = EXPECT_DELIM_AFTER_PROCESSED;
       const arr = stack.pop();
       if (arr === undefined) {
         throw new Error(`Invalid array text - too many '}'`);
       }
       current = arr;
-    } else if (mode === MODES.EXPECT_VALUE) {
+    } else if (mode === EXPECT_VALUE) {
       currentStringStart = position;
-      mode = MODES.IN_VALUE;
-    } else if (mode === MODES.IN_VALUE) {
+      mode = IN_VALUE;
+    } else if (mode === IN_VALUE) {
       continue;
-    } else if (mode === MODES.EXPECT_DELIM_AFTER_PROCESSED) {
+    } else if (mode === EXPECT_DELIM_AFTER_PROCESSED) {
       throw new Error("Was expecting delimeter");
     } else {
       const never: never = mode;
