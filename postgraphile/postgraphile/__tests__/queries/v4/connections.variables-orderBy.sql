19a78951a9e5c19cc8ed6dd87572b530fe4629e8
Benjie Gillam <benjie@jemjie.com>
2025-03-07 11:50:32 +0000

    Update snapshots following the fix to default input value planning

---
+++ postgraphile/postgraphile/__tests__/queries/v4/connections.variables-orderBy.sql
@@ -0,0 +1,11 @@
+select
+  __post__."id"::text as "0",
+  __post__."headline" as "1",
+  __post__."author_id"::text as "2"
+from "a"."post" as __post__
+order by __post__."author_id" desc, __post__."headline" desc, __post__."id" asc
+limit 4;
+
+select
+  (count(*))::text as "0"
+from "a"."post" as __post__;
