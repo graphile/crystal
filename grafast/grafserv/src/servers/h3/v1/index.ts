58b92c0f0f51b808b51c4428a6372b8cf22f6a9a
Benjie Gillam <benjie@jemjie.com>
2025-03-12 18:15:04 +0000

    Upgrade graphql-http to fix type issue

--- grafast/grafserv/src/servers/h3/v1/index.ts
+++ grafast/grafserv/src/servers/h3/v1/index.ts
@@ -278,7 +278,7 @@ export class H3Grafserv extends GrafservBase {
           { socket: peer.websocket, request: peer.request },
         );
         client.closed = async (code, reason) => {
-          onClose(code as number, reason as string);
+          onClose(code, reason);
         };
       },
       message(peer, message) {
