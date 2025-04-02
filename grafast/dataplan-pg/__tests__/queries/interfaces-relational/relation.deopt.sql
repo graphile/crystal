8d64acc25347f14d9b48d50ce3c13a10ddf68083
Benjie Gillam <benjie@jemjie.com>
2025-04-02 09:22:23 +0100

    Plan changes

--- grafast/dataplan-pg/__tests__/queries/interfaces-relational/relation.deopt.sql
+++ grafast/dataplan-pg/__tests__/queries/interfaces-relational/relation.deopt.sql
@@ -11,6 +11,16 @@ where
   );
 
 select
+  __relational_posts__."id"::text as "0"
+from interfaces_and_unions.relational_posts as __relational_posts__
+where
+  (
+    __relational_posts__."id" = $1::"int4"
+  ) and (
+    true /* authorization checks */
+  );
+
+select
   __relational_items__."type"::text as "0",
   __relational_items__."id"::text as "1",
   __relational_items__."author_id"::text as "2"
@@ -18,16 +28,6 @@ from interfaces_and_unions.relational_items as __relational_items__
 where
   (
     __relational_items__."id" = $1::"int4"
-  ) and (
-    true /* authorization checks */
-  );
-
-select
-  __relational_posts__."id"::text as "0"
-from interfaces_and_unions.relational_posts as __relational_posts__
-where
-  (
-    __relational_posts__."id" = $1::"int4"
   ) and (
     true /* authorization checks */
   );
