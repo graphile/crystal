d1fe9f10de58bf20849c478bfb16f7b606797d4e
Benjie Gillam <benjie@jemjie.com>
2025-04-02 09:22:23 +0100

    Plan changes

--- grafast/dataplan-pg/__tests__/queries/interfaces-relational/nested-more.sql
+++ grafast/dataplan-pg/__tests__/queries/interfaces-relational/nested-more.sql
@@ -43,44 +43,6 @@ lateral (
     )
 ) as __relational_topics_result__;
 
-select __relational_items_result__.*
-from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __relational_items_identifiers__,
-lateral (
-  select
-    __relational_items__."type"::text as "0",
-    __relational_items__."id"::text as "1",
-    __relational_items__."type2"::text as "2",
-    __relational_items__."author_id"::text as "3",
-    __relational_items__."position"::text as "4",
-    to_char(__relational_items__."created_at", 'YYYY-MM-DD"T"HH24:MI:SS.USTZH:TZM'::text) as "5",
-    to_char(__relational_items__."updated_at", 'YYYY-MM-DD"T"HH24:MI:SS.USTZH:TZM'::text) as "6",
-    __relational_items__."is_explicitly_archived"::text as "7",
-    to_char(__relational_items__."archived_at", 'YYYY-MM-DD"T"HH24:MI:SS.USTZH:TZM'::text) as "8",
-    __relational_items_identifiers__.idx as "9"
-  from interfaces_and_unions.relational_items as __relational_items__
-  where
-    (
-      __relational_items__."id" = __relational_items_identifiers__."id0"
-    ) and (
-      true /* authorization checks */
-    )
-) as __relational_items_result__;
-
-select __people_result__.*
-from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __people_identifiers__,
-lateral (
-  select
-    __people__."username" as "0",
-    __people_identifiers__.idx as "1"
-  from interfaces_and_unions.people as __people__
-  where
-    (
-      __people__."person_id" = __people_identifiers__."id0"
-    ) and (
-      true /* authorization checks */
-    )
-) as __people_result__;
-
 select __relational_posts_result__.*
 from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __relational_posts_identifiers__,
 lateral (
@@ -135,6 +97,44 @@ lateral (
       true /* authorization checks */
     )
 ) as __relational_checklist_items_result__;
+
+select __relational_items_result__.*
+from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __relational_items_identifiers__,
+lateral (
+  select
+    __relational_items__."type"::text as "0",
+    __relational_items__."id"::text as "1",
+    __relational_items__."type2"::text as "2",
+    __relational_items__."author_id"::text as "3",
+    __relational_items__."position"::text as "4",
+    to_char(__relational_items__."created_at", 'YYYY-MM-DD"T"HH24:MI:SS.USTZH:TZM'::text) as "5",
+    to_char(__relational_items__."updated_at", 'YYYY-MM-DD"T"HH24:MI:SS.USTZH:TZM'::text) as "6",
+    __relational_items__."is_explicitly_archived"::text as "7",
+    to_char(__relational_items__."archived_at", 'YYYY-MM-DD"T"HH24:MI:SS.USTZH:TZM'::text) as "8",
+    __relational_items_identifiers__.idx as "9"
+  from interfaces_and_unions.relational_items as __relational_items__
+  where
+    (
+      __relational_items__."id" = __relational_items_identifiers__."id0"
+    ) and (
+      true /* authorization checks */
+    )
+) as __relational_items_result__;
+
+select __people_result__.*
+from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __people_identifiers__,
+lateral (
+  select
+    __people__."username" as "0",
+    __people_identifiers__.idx as "1"
+  from interfaces_and_unions.people as __people__
+  where
+    (
+      __people__."person_id" = __people_identifiers__."id0"
+    ) and (
+      true /* authorization checks */
+    )
+) as __people_result__;
 
 select
   __relational_posts__."id"::text as "0"
--- grafast/dataplan-pg/__tests__/queries/interfaces-relational/nested.sql
+++ grafast/dataplan-pg/__tests__/queries/interfaces-relational/nested.sql
@@ -37,23 +37,6 @@ lateral (
     )
 ) as __relational_topics_result__;
 
-select __relational_items_result__.*
-from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __relational_items_identifiers__,
-lateral (
-  select
-    __relational_items__."type"::text as "0",
-    __relational_items__."id"::text as "1",
-    __relational_items__."type2"::text as "2",
-    __relational_items_identifiers__.idx as "3"
-  from interfaces_and_unions.relational_items as __relational_items__
-  where
-    (
-      __relational_items__."id" = __relational_items_identifiers__."id0"
-    ) and (
-      true /* authorization checks */
-    )
-) as __relational_items_result__;
-
 select __relational_posts_result__.*
 from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __relational_posts_identifiers__,
 lateral (
@@ -108,6 +91,23 @@ lateral (
       true /* authorization checks */
     )
 ) as __relational_checklist_items_result__;
+
+select __relational_items_result__.*
+from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __relational_items_identifiers__,
+lateral (
+  select
+    __relational_items__."type"::text as "0",
+    __relational_items__."id"::text as "1",
+    __relational_items__."type2"::text as "2",
+    __relational_items_identifiers__.idx as "3"
+  from interfaces_and_unions.relational_items as __relational_items__
+  where
+    (
+      __relational_items__."id" = __relational_items_identifiers__."id0"
+    ) and (
+      true /* authorization checks */
+    )
+) as __relational_items_result__;
 
 select
   __relational_posts__."id"::text as "0"
