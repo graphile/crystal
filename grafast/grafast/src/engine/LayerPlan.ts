d5fc4379a787d64c8c3a1ac313f2fda32c0e6e7c
Benjie Gillam <benjie@jemjie.com>
2025-04-04 11:30:58 +0100

    Add missing id

--- grafast/grafast/src/engine/LayerPlan.ts
+++ grafast/grafast/src/engine/LayerPlan.ts
@@ -851,7 +851,7 @@ export class LayerPlan<TReason extends LayerPlanReason = LayerPlanReason> {
         // PERF: isDev
         if (layerPlanId !== this.reason.parentLayerPlans[sourceIndex]?.id) {
           throw new Error(
-            `GrafastInternalError<>: layer plans out of order in ${this}?!`,
+            `GrafastInternalError<9a03a1b9-7125-41bb-b99c-532ec05a3030>: layer plans out of order in ${this}?!`,
           );
         }
         const map = mapByParentLayerPlanId[layerPlanId]!;
