134abc808c8529c8053e281d79c4ab89c901e94a
Benjie Gillam <benjie@jemjie.com>
2025-04-04 15:07:23 +0100

    Naked GraphQLErrors are safe

--- grafast/grafserv/src/options.ts
+++ grafast/grafserv/src/options.ts
@@ -28,7 +28,10 @@ const randomString = (length = 10) => {
 export function defaultMaskError(
   error: graphql.GraphQLError,
 ): graphql.GraphQLError {
-  if (error.originalError instanceof GraphQLError) {
+  if (!error.originalError && error instanceof GraphQLError) {
+    // Things like 'Cannot return null for non-nullable field'
+    return error;
+  } else if (error.originalError instanceof GraphQLError) {
     return error;
   } else if (error.originalError != null && isSafeError(error.originalError)) {
     return new GraphQLError(
