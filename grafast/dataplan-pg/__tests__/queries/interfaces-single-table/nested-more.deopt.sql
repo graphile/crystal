812311e115bd3a06ea699431a77aa31a76465b4c
Benjie Gillam <benjie@jemjie.com>
2025-05-09 16:05:08 +0100

    Update tests

--- grafast/dataplan-pg/__tests__/queries/interfaces-single-table/nested-more.deopt.sql
+++ grafast/dataplan-pg/__tests__/queries/interfaces-single-table/nested-more.deopt.sql
@@ -32,21 +32,6 @@ lateral (
   order by __single_table_items__."id" asc
 ) as __single_table_items_result__;
 
-select __people_result__.*
-from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __people_identifiers__,
-lateral (
-  select
-    __people__."username" as "0",
-    __people_identifiers__.idx as "1"
-  from interfaces_and_unions.people as __people__
-  where
-    (
-      __people__."person_id" = __people_identifiers__."id0"
-    ) and (
-      true /* authorization checks */
-    )
-) as __people_result__;
-
 select __single_table_items_result__.*
 from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __single_table_items_identifiers__,
 lateral (
@@ -68,3 +53,18 @@ lateral (
       true /* authorization checks */
     )
 ) as __single_table_items_result__;
+
+select __people_result__.*
+from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __people_identifiers__,
+lateral (
+  select
+    __people__."username" as "0",
+    __people_identifiers__.idx as "1"
+  from interfaces_and_unions.people as __people__
+  where
+    (
+      __people__."person_id" = __people_identifiers__."id0"
+    ) and (
+      true /* authorization checks */
+    )
+) as __people_result__;
