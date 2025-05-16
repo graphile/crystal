cf756f0c6d7651ad12c59454b7531e02a52452d6
Benjie Gillam <benjie@jemjie.com>
2025-05-16 15:17:43 +0100

    Revert "Remove tamedevil from access step"
    This reverts commit a8e9465b2ec18c61e7b92570888ab61bea501ace.

--- grafast/grafast/src/steps/access.ts
+++ grafast/grafast/src/steps/access.ts
@@ -1,4 +1,6 @@
 import chalk from "chalk";
+import type { TE } from "tamedevil";
+import te from "tamedevil";
 
 import { inspect } from "../inspect.js";
 import type { ExecutionExtra, UnbatchedExecutionExtra } from "../interfaces.js";
@@ -18,18 +20,25 @@ function constructDestructureFunction(
   callback: (fn: (_extra: ExecutionExtra, value: any) => any) => void,
 ): void {
   const n = path.length;
+  /** 0 - slow mode; 1 - middle mode; 2 - turbo mode */
+  let mode: 0 | 1 | 2 = n > 50 || n < 1 ? 0 : n > 5 ? 1 : 2;
 
   for (let i = 0; i < n; i++) {
     const pathItem = path[i];
     const t = typeof pathItem;
     if (t === "symbol") {
-      // Fine
+      // Cannot use in superfast mode (because cannot create signature)
+      if (mode === 2) mode = 1;
     } else if (t === "string") {
-      // Fine
+      // Cannot use in superfast mode (because signature becomes ambiguous)
+      if (mode === 2 && (pathItem as string).includes("|")) mode = 1;
     } else if (t === "number") {
-      // Fine
+      if (!Number.isFinite(pathItem)) {
+        mode = 0;
+      }
     } else if (pathItem == null) {
-      // Fine
+      // Slow mode required
+      mode = 0;
     } else {
       throw new Error(
         `Invalid path item: ${inspect(pathItem)} in path '${JSON.stringify(
@@ -39,45 +48,66 @@ function constructDestructureFunction(
     }
   }
 
-  if (n === 0) {
-    if (fallback !== undefined) {
-      callback((_, v) => v ?? fallback);
-    } else {
-      callback((_, v) => v);
-    }
-  } else if (n === 1) {
-    const key0 = path[0];
-    if (fallback !== undefined) {
-      callback((_, v) => v?.[key0] ?? fallback);
-    } else {
-      callback((_, v) => v?.[key0]);
-    }
-  } else if (n === 2) {
-    const key0 = path[0];
-    const key1 = path[1];
-    if (fallback !== undefined) {
-      callback((_, v) => v?.[key0]?.[key1] ?? fallback);
-    } else {
-      callback((_, v) => v?.[key0]?.[key1]);
-    }
-  } else if (n === 3) {
-    const key0 = path[0];
-    const key1 = path[1];
-    const key2 = path[2];
-    if (fallback !== undefined) {
-      callback((_, v) => v?.[key0]?.[key1]?.[key2] ?? fallback);
-    } else {
-      callback((_, v) => v?.[key0]?.[key1]?.[key2]);
-    }
-  } else {
+  if (mode === 0) {
     // Slow mode
     callback(function slowlyExtractValueAtPath(_meta: any, value: any): any {
       let current = value;
-      for (let i = 0; i < n && current != null; i++) {
-        current = current[path[i]];
+      for (let i = 0, l = path.length; i < l && current != null; i++) {
+        const pathItem = path[i];
+        current = current[pathItem];
       }
       return current ?? fallback;
     });
+  } else {
+    const signature = (fallback !== undefined ? "f" : "n") + n;
+
+    const done =
+      mode === 2
+        ? (factory: Factory) => {
+            const fn = factory(fallback, ...path);
+            // ?.blah?.bog?.["!!!"]?.[0]
+            const expressionDetail = [path, fallback];
+            (fn as any)[expressionSymbol] = expressionDetail;
+            callback(fn);
+          }
+        : (factory: Factory) => callback(factory(fallback, ...path));
+
+    const fn = makeDestructureCache[signature];
+    if (fn !== undefined) {
+      done(fn);
+      return;
+    }
+    const making = makingDestructureCache[signature];
+    if (making !== undefined) {
+      making.push(done);
+      return;
+    }
+    const doneHandlers: Array<(fn: Factory) => void> = [done];
+    makingDestructureCache[signature] = doneHandlers;
+
+    // DO NOT REFERENCE 'path' BELOW HERE!
+
+    const names: TE[] = [];
+    const access: TE[] = [];
+    for (let i = 0; i < n; i++) {
+      const te_name = te.identifier(`p${i}`);
+      names.push(te_name);
+      access.push(te`[${te_name}]`);
+    }
+    te.runInBatch<Factory>(
+      te`function (fallback, ${te.join(names, ", ")}) {
+return (_meta, value) => value?.${te.join(access, "?.")}${
+        fallback === undefined ? te.blank : te.cache` ?? fallback`
+      };
+}`,
+      (factory) => {
+        makeDestructureCache[signature] = factory;
+        delete makingDestructureCache[signature];
+        for (const doneHandler of doneHandlers) {
+          doneHandler(factory);
+        }
+      },
+    );
   }
 }
 
