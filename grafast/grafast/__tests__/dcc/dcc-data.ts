ff0d8d6ce00a579d0c318cef2e17defed8beab1f
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2025-05-19 17:39:50 +0100

    overhaul item data

--- grafast/grafast/__tests__/dcc/dcc-data.ts
+++ grafast/grafast/__tests__/dcc/dcc-data.ts
@@ -2,12 +2,14 @@
 
 import type { LoadManyCallback, LoadOneCallback } from "../../dist";
 
+type ItemSpec = `${"Equipment" | "Consumable" | "MiscItem"}:${number}`;
+
 export interface CrawlerData {
   id: number;
   species: "Human" | "Cat" | "Crocodilian";
   name: string;
-  items?: number[];
-  favouriteItem?: number;
+  items?: ItemSpec[];
+  favouriteItem?: ItemSpec;
   friends?: number[];
   bestFriend?: number;
   crawlerNumber?: number;
@@ -22,22 +24,31 @@ export interface NpcData {
   exCrawler?: boolean;
   client?: number;
   clients?: number[];
-  items?: number[];
+  items?: ItemSpec[];
   friends?: number[];
 }
 
 export interface ItemData {
   id: number;
   name: string;
-  type: "Equipment" | "Consumable" | "Misc";
   creator?: number;
-  items?: number[];
+  items?: ItemSpec[];
+  /** @deprecated */
+  type?: string;
 }
+
+export interface EquipmentData extends ItemData {}
+
+export interface ConsumableData extends ItemData {}
+
+export interface MiscItemData extends ItemData {}
 
 export interface Database {
   crawlers: readonly CrawlerData[];
   npcs: readonly NpcData[];
-  items: readonly ItemData[];
+  equipment: readonly EquipmentData[];
+  consumables: readonly ConsumableData[];
+  miscItems: readonly MiscItemData[];
 }
 
 export function makeData(): Database {
@@ -47,8 +58,18 @@ export function makeData(): Database {
         id: 101,
         species: "Human",
         name: "Carl",
-        items: [201, 202, 203, 204, 211, 212, 213, 214, 215],
-        favouriteItem: 203,
+        items: [
+          "Equipment:201",
+          "Equipment:202",
+          "Equipment:203",
+          "Consumable:201",
+          "Consumable:202",
+          "Consumable:203",
+          "Consumable:204",
+          "Consumable:205",
+          "Consumable:206",
+        ],
+        favouriteItem: "Equipment:203",
         friends: [102, 103, 104, 105, 301],
         bestFriend: 102,
         crawlerNumber: 4122,
@@ -57,8 +78,16 @@ export function makeData(): Database {
         id: 102,
         species: "Cat",
         name: "Princess Donut",
-        items: [204, 206, 207, 211, 212, 213, 214],
-        favouriteItem: 205 /* Favourite item is not in inventory */,
+        items: [
+          "Equipment:204",
+          "Equipment:206",
+          "Consumable:201",
+          "Consumable:202",
+          "Consumable:203",
+          "Consumable:204",
+          "Consumable:205",
+        ],
+        favouriteItem: "Equipment:205" /* Favourite item is not in inventory */,
         friends: [101, 103, 104, 105, 301, 302, 303],
         bestFriend: 101,
         crawlerNumber: 4119,
@@ -67,8 +96,13 @@ export function makeData(): Database {
         id: 103,
         species: "Human",
         name: "Katia",
-        items: [208, 213, 214, 216],
-        favouriteItem: 216 /* Favourite item has a creator */,
+        items: [
+          "Equipment:207",
+          "Consumable:203",
+          "Consumable:204",
+          "Equipment:210",
+        ],
+        favouriteItem: "Equipment:210" /* Favourite item has a creator */,
         friends: [101, 102],
         bestFriend: 107 /* Best friend is deleted */,
         crawlerNumber: 9077265,
@@ -77,17 +111,28 @@ export function makeData(): Database {
         id: 104,
         species: "Human",
         name: "Imani",
-        items: [209, 210, 210, 212, 213],
+        items: [
+          "Equipment:208",
+          "Equipment:209",
+          "Consumable:204",
+          "Consumable:202",
+          "Consumable:203",
+        ],
         friends: [101, 102, 105],
       },
       {
         id: 105,
         species: "Human",
         name: "Elle",
-        items: [212, 213],
+        items: ["Consumable:202", "Consumable:203"],
         friends: [101, 102, 104],
       },
-      { id: 106, species: "Crocodilian", name: "Dolores", items: [214] },
+      {
+        id: 106,
+        species: "Crocodilian",
+        name: "Dolores",
+        items: ["Consumable:205"],
+      },
       { id: 107, species: "Human", name: "Hekla", deleted: true },
     ],
     npcs: [
@@ -98,7 +143,7 @@ export function makeData(): Database {
         name: "Mordecai",
         exCrawler: true,
         client: 102,
-        items: [212, 212, 212],
+        items: ["Consumable:203", "Consumable:203", "Consumable:203"],
         friends: [101, 102, 103],
       },
       {
@@ -127,52 +172,62 @@ export function makeData(): Database {
         client: 105,
       },
     ],
-    items: [
-      { id: 201, name: "Cloak of Stoutness", type: "Equipment" },
-      { id: 202, name: "Toe Ring of the Splatter Skunk", type: "Equipment" },
-      { id: 203, name: "Enchanted War Gauntlet", type: "Equipment" },
-      { id: 204, name: "Rev-Up Immunity Smoothie", type: "Consumable" },
-      { id: 205, name: "Enchanted Crown", type: "Equipment" },
-      { id: 206, name: "Enchanted Tiara of Mana", type: "Equipment" },
-      { id: 207, name: "Enchanted Anklet", type: "Equipment" },
-      { id: 208, name: "Enchanted Repeating Crossbow", type: "Equipment" },
-      { id: 209, name: "Longsword", type: "Equipment" },
-      { id: 210, name: "Enchanted Cloak", type: "Equipment" },
-      { id: 211, name: "Bandage", type: "Consumable" },
-      { id: 212, name: "Mana Potion", type: "Consumable" },
-      { id: 213, name: "Healing Potion", type: "Consumable" },
+    equipment: [
+      { id: 201, name: "Cloak of Stoutness" },
+      { id: 202, name: "Toe Ring of the Splatter Skunk" },
+      { id: 203, name: "Enchanted War Gauntlet" },
+      { id: 204, name: "Enchanted Crown" },
+      { id: 205, name: "Enchanted Tiara of Mana" },
+      { id: 206, name: "Enchanted Anklet" },
+      { id: 207, name: "Enchanted Repeating Crossbow" },
+      { id: 208, name: "Longsword" },
+      { id: 209, name: "Enchanted Cloak" },
       {
-        id: 214,
+        /* This item can have an inventory */
+        id: 210,
+        name: "Ugly Backpack With a Completely Useless Design",
+        creator: 101,
+        items: [
+          "MiscItem:201",
+          "MiscItem:201",
+          "MiscItem:202",
+          "MiscItem:202",
+          "MiscItem:203",
+          "MiscItem:203",
+        ],
+      },
+    ],
+    consumables: [
+      { id: 201, name: "Rev-Up Immunity Smoothie", type: "Consumable" },
+      { id: 202, name: "Bandage", type: "Consumable" },
+      { id: 203, name: "Mana Potion", type: "Consumable" },
+      { id: 204, name: "Healing Potion", type: "Consumable" },
+      {
+        id: 205,
         name: "Dolores Doesn't Splat Potion",
         type: "Consumable",
         creator: 106,
       },
       {
-        id: 215,
+        id: 206,
         name: "Carl's Jug O' Boom",
         type: "Consumable",
         creator: 101,
       },
+    ],
+    miscItems: [
       {
-        /* This item can have an inventory */
-        id: 216,
-        name: "Ugly Backpack With a Completely Useless Design",
-        type: "Equipment",
-        creator: 101,
-        items: [217, 217, 218, 218, 219, 219],
-      },
-      {
-        id: 217,
+        id: 201,
         name: "Scrap Metal",
         type: "Misc",
       },
       {
-        id: 218,
+        id: 202,
         name: "Scrap Metal Pole",
         type: "Misc",
       },
       {
-        id: 219,
+        id: 203,
         name: "Metal Bearing",
         type: "Misc",
       },
