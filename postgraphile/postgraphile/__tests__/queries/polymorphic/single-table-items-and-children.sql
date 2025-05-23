fd2fd6e4e080c01a46d1695a9255e8b93cc1e140
Benjie Gillam <benjie@jemjie.com>
2025-05-23 10:08:23 +0100

    Removing dataonly simplified this SQL

--- postgraphile/postgraphile/__tests__/queries/polymorphic/single-table-items-and-children.sql
+++ postgraphile/postgraphile/__tests__/queries/polymorphic/single-table-items-and-children.sql
@@ -1,20 +1,17 @@
 select
-  __single_table_items__."id"::text as "0",
-  __single_table_items__."type"::text as "1"
-from "polymorphic"."single_table_items" as __single_table_items__
-order by __single_table_items__."id" asc;
-
-select __single_table_items_result__.*
-from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __single_table_items_identifiers__,
-lateral (
-  select
-    __single_table_items__."id"::text as "0",
-    __single_table_items__."type"::text as "1",
-    __single_table_items__."position"::text as "2",
-    __single_table_items_identifiers__.idx as "3"
-  from "polymorphic"."single_table_items" as __single_table_items__
-  where (
-    __single_table_items__."parent_id" = __single_table_items_identifiers__."id0"
-  )
-  order by __single_table_items__."id" asc
-) as __single_table_items_result__;
+  __single_table_items_2."id"::text as "0",
+  __single_table_items_2."type"::text as "1",
+  array(
+    select array[
+      __single_table_items__."id"::text,
+      __single_table_items__."type"::text,
+      __single_table_items__."position"::text
+    ]::text[]
+    from "polymorphic"."single_table_items" as __single_table_items__
+    where (
+      __single_table_items__."parent_id" = __single_table_items_2."id"
+    )
+    order by __single_table_items__."id" asc
+  )::text as "2"
+from "polymorphic"."single_table_items" as __single_table_items_2
+order by __single_table_items_2."id" asc;
