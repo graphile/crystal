6d6eebd9ecca27140e19b8cc171e139b723a0d9c
Benjie Gillam <benjie@jemjie.com>
2025-05-24 13:15:46 +0100

    Remove duplication of steps

--- postgraphile/postgraphile/__tests__/queries/polymorphic/vulns.deep.sql
+++ postgraphile/postgraphile/__tests__/queries/polymorphic/vulns.deep.sql
@@ -282,62 +282,6 @@ from (
           order by
             __first_party_vulnerabilities__."id" asc
         ) as "n"
-      from "polymorphic"."aws_application_first_party_vulnerabilities" as __aws_application_first_party_vulnerabilities__
-      inner join "polymorphic"."first_party_vulnerabilities" as __first_party_vulnerabilities__
-      on (
-        __first_party_vulnerabilities__."id" = __aws_application_first_party_vulnerabilities__."first_party_vulnerability_id"
-      )
-      where __aws_application_first_party_vulnerabilities__."aws_application_id" = $1::"int4"
-      order by
-        __first_party_vulnerabilities__."id" asc
-      limit 2
-    ) as __first_party_vulnerabilities__
-  union all
-    select
-      __third_party_vulnerabilities__."0",
-      __third_party_vulnerabilities__."1",
-      "n"
-    from (
-      select
-        'ThirdPartyVulnerability' as "0",
-        json_build_array((__third_party_vulnerabilities__."id")::text) as "1",
-        row_number() over (
-          order by
-            __third_party_vulnerabilities__."id" asc
-        ) as "n"
-      from "polymorphic"."aws_application_third_party_vulnerabilities" as __aws_application_third_party_vulnerabilities__
-      inner join "polymorphic"."third_party_vulnerabilities" as __third_party_vulnerabilities__
-      on (
-        __third_party_vulnerabilities__."id" = __aws_application_third_party_vulnerabilities__."third_party_vulnerability_id"
-      )
-      where __aws_application_third_party_vulnerabilities__."aws_application_id" = $2::"int4"
-      order by
-        __third_party_vulnerabilities__."id" asc
-      limit 2
-    ) as __third_party_vulnerabilities__
-  order by
-    "0" asc,
-    "n" asc
-  limit 2
-) __vulnerabilities__
-
-
-select
-  __vulnerabilities__."0" as "0",
-  __vulnerabilities__."1"::text as "1"
-from (
-    select
-      __first_party_vulnerabilities__."0",
-      __first_party_vulnerabilities__."1",
-      "n"
-    from (
-      select
-        'FirstPartyVulnerability' as "0",
-        json_build_array((__first_party_vulnerabilities__."id")::text) as "1",
-        row_number() over (
-          order by
-            __first_party_vulnerabilities__."id" asc
-        ) as "n"
       from "polymorphic"."gcp_application_first_party_vulnerabilities" as __gcp_application_first_party_vulnerabilities__
       inner join "polymorphic"."first_party_vulnerabilities" as __first_party_vulnerabilities__
       on (
@@ -465,63 +409,6 @@ lateral (
   ) __applications__
 ) as __union_result__;
 
-select
-  __applications__."0" as "0",
-  __applications__."1"::text as "1"
-from (
-    select
-      __aws_applications__."0",
-      __aws_applications__."1",
-      "n"
-    from (
-      select
-        'AwsApplication' as "0",
-        json_build_array((__aws_applications__."id")::text) as "1",
-        row_number() over (
-          order by
-            __aws_applications__."id" asc
-        ) as "n"
-      from "polymorphic"."aws_application_first_party_vulnerabilities" as __aws_application_first_party_vulnerabilities__
-      inner join "polymorphic"."aws_applications" as __aws_applications__
-      on (
-        __aws_applications__."id" = __aws_application_first_party_vulnerabilities__."aws_application_id"
-      )
-      where __aws_application_first_party_vulnerabilities__."first_party_vulnerability_id" = $1::"int4"
-      order by
-        __aws_applications__."id" asc
-      limit 6
-    ) as __aws_applications__
-  union all
-    select
-      __gcp_applications__."0",
-      __gcp_applications__."1",
-      "n"
-    from (
-      select
-        'GcpApplication' as "0",
-        json_build_array((__gcp_applications__."id")::text) as "1",
-        row_number() over (
-          order by
-            __gcp_applications__."id" asc
-        ) as "n"
-      from "polymorphic"."gcp_application_first_party_vulnerabilities" as __gcp_application_first_party_vulnerabilities__
-      inner join "polymorphic"."gcp_applications" as __gcp_applications__
-      on (
-        __gcp_applications__."id" = __gcp_application_first_party_vulnerabilities__."gcp_application_id"
-      )
-      where __gcp_application_first_party_vulnerabilities__."first_party_vulnerability_id" = $2::"int4"
-      order by
-        __gcp_applications__."id" asc
-      limit 6
-    ) as __gcp_applications__
-  order by
-    "0" asc,
-    "n" asc
-  limit 2
-  offset 4
-) __applications__
-
-
 select __union_result__.*
 from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __union_identifiers__,
 lateral (
@@ -542,12 +429,12 @@ lateral (
             order by
               __aws_applications__."id" asc
           ) as "n"
-        from "polymorphic"."aws_application_first_party_vulnerabilities" as __aws_application_first_party_vulnerabilities__
+        from "polymorphic"."aws_application_third_party_vulnerabilities" as __aws_application_third_party_vulnerabilities__
         inner join "polymorphic"."aws_applications" as __aws_applications__
         on (
-          __aws_applications__."id" = __aws_application_first_party_vulnerabilities__."aws_application_id"
+          __aws_applications__."id" = __aws_application_third_party_vulnerabilities__."aws_application_id"
         )
-        where __aws_application_first_party_vulnerabilities__."first_party_vulnerability_id" = __union_identifiers__."id0"
+        where __aws_application_third_party_vulnerabilities__."third_party_vulnerability_id" = __union_identifiers__."id0"
         order by
           __aws_applications__."id" asc
         limit 6
@@ -565,12 +452,12 @@ lateral (
             order by
               __gcp_applications__."id" asc
           ) as "n"
-        from "polymorphic"."gcp_application_first_party_vulnerabilities" as __gcp_application_first_party_vulnerabilities__
+        from "polymorphic"."gcp_application_third_party_vulnerabilities" as __gcp_application_third_party_vulnerabilities__
         inner join "polymorphic"."gcp_applications" as __gcp_applications__
         on (
-          __gcp_applications__."id" = __gcp_application_first_party_vulnerabilities__."gcp_application_id"
+          __gcp_applications__."id" = __gcp_application_third_party_vulnerabilities__."gcp_application_id"
         )
-        where __gcp_application_first_party_vulnerabilities__."first_party_vulnerability_id" = __union_identifiers__."id0"
+        where __gcp_application_third_party_vulnerabilities__."third_party_vulnerability_id" = __union_identifiers__."id0"
         order by
           __gcp_applications__."id" asc
         limit 6
@@ -582,63 +469,6 @@ lateral (
     offset 4
   ) __applications__
 ) as __union_result__;
-
-select
-  __applications__."0" as "0",
-  __applications__."1"::text as "1"
-from (
-    select
-      __aws_applications__."0",
-      __aws_applications__."1",
-      "n"
-    from (
-      select
-        'AwsApplication' as "0",
-        json_build_array((__aws_applications__."id")::text) as "1",
-        row_number() over (
-          order by
-            __aws_applications__."id" asc
-        ) as "n"
-      from "polymorphic"."aws_application_third_party_vulnerabilities" as __aws_application_third_party_vulnerabilities__
-      inner join "polymorphic"."aws_applications" as __aws_applications__
-      on (
-        __aws_applications__."id" = __aws_application_third_party_vulnerabilities__."aws_application_id"
-      )
-      where __aws_application_third_party_vulnerabilities__."third_party_vulnerability_id" = $1::"int4"
-      order by
-        __aws_applications__."id" asc
-      limit 6
-    ) as __aws_applications__
-  union all
-    select
-      __gcp_applications__."0",
-      __gcp_applications__."1",
-      "n"
-    from (
-      select
-        'GcpApplication' as "0",
-        json_build_array((__gcp_applications__."id")::text) as "1",
-        row_number() over (
-          order by
-            __gcp_applications__."id" asc
-        ) as "n"
-      from "polymorphic"."gcp_application_third_party_vulnerabilities" as __gcp_application_third_party_vulnerabilities__
-      inner join "polymorphic"."gcp_applications" as __gcp_applications__
-      on (
-        __gcp_applications__."id" = __gcp_application_third_party_vulnerabilities__."gcp_application_id"
-      )
-      where __gcp_application_third_party_vulnerabilities__."third_party_vulnerability_id" = $2::"int4"
-      order by
-        __gcp_applications__."id" asc
-      limit 6
-    ) as __gcp_applications__
-  order by
-    "0" asc,
-    "n" asc
-  limit 2
-  offset 4
-) __applications__
-
 
 select __gcp_applications_result__.*
 from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __gcp_applications_identifiers__,
@@ -712,122 +542,6 @@ lateral (
     limit 3
   ) __vulnerabilities__
 ) as __union_result__;
-
-select __union_result__.*
-from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __union_identifiers__,
-lateral (
-  select
-    __vulnerabilities__."0" as "0",
-    __vulnerabilities__."1"::text as "1",
-    __union_identifiers__.idx as "2"
-  from (
-      select
-        __first_party_vulnerabilities__."0",
-        __first_party_vulnerabilities__."1",
-        "n"
-      from (
-        select
-          'FirstPartyVulnerability' as "0",
-          json_build_array((__first_party_vulnerabilities__."id")::text) as "1",
-          row_number() over (
-            order by
-              __first_party_vulnerabilities__."id" asc
-          ) as "n"
-        from "polymorphic"."gcp_application_first_party_vulnerabilities" as __gcp_application_first_party_vulnerabilities__
-        inner join "polymorphic"."first_party_vulnerabilities" as __first_party_vulnerabilities__
-        on (
-          __first_party_vulnerabilities__."id" = __gcp_application_first_party_vulnerabilities__."first_party_vulnerability_id"
-        )
-        where __gcp_application_first_party_vulnerabilities__."gcp_application_id" = __union_identifiers__."id0"
-        order by
-          __first_party_vulnerabilities__."id" asc
-        limit 3
-      ) as __first_party_vulnerabilities__
-    union all
-      select
-        __third_party_vulnerabilities__."0",
-        __third_party_vulnerabilities__."1",
-        "n"
-      from (
-        select
-          'ThirdPartyVulnerability' as "0",
-          json_build_array((__third_party_vulnerabilities__."id")::text) as "1",
-          row_number() over (
-            order by
-              __third_party_vulnerabilities__."id" asc
-          ) as "n"
-        from "polymorphic"."gcp_application_third_party_vulnerabilities" as __gcp_application_third_party_vulnerabilities__
-        inner join "polymorphic"."third_party_vulnerabilities" as __third_party_vulnerabilities__
-        on (
-          __third_party_vulnerabilities__."id" = __gcp_application_third_party_vulnerabilities__."third_party_vulnerability_id"
-        )
-        where __gcp_application_third_party_vulnerabilities__."gcp_application_id" = __union_identifiers__."id0"
-        order by
-          __third_party_vulnerabilities__."id" asc
-        limit 3
-      ) as __third_party_vulnerabilities__
-    order by
-      "0" asc,
-      "n" asc
-    limit 3
-  ) __vulnerabilities__
-) as __union_result__;
-
-select
-  __vulnerabilities__."0" as "0",
-  __vulnerabilities__."1"::text as "1"
-from (
-    select
-      __first_party_vulnerabilities__."0",
-      __first_party_vulnerabilities__."1",
-      "n"
-    from (
-      select
-        'FirstPartyVulnerability' as "0",
-        json_build_array((__first_party_vulnerabilities__."id")::text) as "1",
-        row_number() over (
-          order by
-            __first_party_vulnerabilities__."id" asc
-        ) as "n"
-      from "polymorphic"."gcp_application_first_party_vulnerabilities" as __gcp_application_first_party_vulnerabilities__
-      inner join "polymorphic"."first_party_vulnerabilities" as __first_party_vulnerabilities__
-      on (
-        __first_party_vulnerabilities__."id" = __gcp_application_first_party_vulnerabilities__."first_party_vulnerability_id"
-      )
-      where __gcp_application_first_party_vulnerabilities__."gcp_application_id" = $1::"int4"
-      order by
-        __first_party_vulnerabilities__."id" asc
-      limit 3
-    ) as __first_party_vulnerabilities__
-  union all
-    select
-      __third_party_vulnerabilities__."0",
-      __third_party_vulnerabilities__."1",
-      "n"
-    from (
-      select
-        'ThirdPartyVulnerability' as "0",
-        json_build_array((__third_party_vulnerabilities__."id")::text) as "1",
-        row_number() over (
-          order by
-            __third_party_vulnerabilities__."id" asc
-        ) as "n"
-      from "polymorphic"."gcp_application_third_party_vulnerabilities" as __gcp_application_third_party_vulnerabilities__
-      inner join "polymorphic"."third_party_vulnerabilities" as __third_party_vulnerabilities__
-      on (
-        __third_party_vulnerabilities__."id" = __gcp_application_third_party_vulnerabilities__."third_party_vulnerability_id"
-      )
-      where __gcp_application_third_party_vulnerabilities__."gcp_application_id" = $2::"int4"
-      order by
-        __third_party_vulnerabilities__."id" asc
-      limit 3
-    ) as __third_party_vulnerabilities__
-  order by
-    "0" asc,
-    "n" asc
-  limit 3
-) __vulnerabilities__
-
 
 select __first_party_vulnerabilities_result__.*
 from (select ids.ordinality - 1 as idx, (ids.value->>0)::"int4" as "id0" from json_array_elements($1::json) with ordinality as ids) as __first_party_vulnerabilities_identifiers__,
