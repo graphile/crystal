aa2ab4fec108eb55622fb196834f83de28644f46
Benjie Gillam <benjie@jemjie.com>
2025-05-24 16:58:16 +0100

    Add comment

--- grafast/grafast/__tests__/dcc/delegate.ts
+++ grafast/grafast/__tests__/dcc/delegate.ts
@@ -1,5 +1,13 @@
 import { get, Step } from "../../dist/index.js";
 
+/**
+ * Returns a "plantime only step" that will optimize away to be simply
+ * `$primary` at runtime; but during planning if you `.get(...)` any of the
+ * attributes listed in `delegatedAttributes` they will be sourced from
+ * `$secondary` rather than `$primary`. Useful if your data is split across two
+ * objects, e.g. a shared object for all "animal" types and then a specific
+ * object for the properties only relevant to a "cat".
+ */
 export function delegate(
   $primary: Step,
   delegatedAttributes: string[],
