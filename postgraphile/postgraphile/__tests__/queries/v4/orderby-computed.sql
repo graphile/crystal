1509dca96cd79a8205ea4bfe8d470bacfa78ff79
Benjie Gillam <benjie@jemjie.com>
2025-05-31 08:33:25 +0100

    Make test stable

--- postgraphile/postgraphile/__tests__/queries/v4/orderby-computed.sql
+++ postgraphile/postgraphile/__tests__/queries/v4/orderby-computed.sql
@@ -4,4 +4,4 @@ select
   __edge_case__."row_id"::text as "2",
   __edge_case__."not_null_has_default"::text as "3"
 from "c"."edge_case" as __edge_case__
-order by "c"."edge_case_computed"(__edge_case__) asc;
+order by "c"."edge_case_computed"(__edge_case__) asc, __edge_case__."row_id" asc;
