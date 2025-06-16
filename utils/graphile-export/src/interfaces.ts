060a25f5e358a91e4ab74a1539167f9acca181a1
Benjie Gillam <benjie@jemjie.com>
2025-06-16 13:57:49 +0100

    Give Graphile Export a way to disable optimize

--- utils/graphile-export/src/interfaces.ts
+++ utils/graphile-export/src/interfaces.ts
@@ -24,4 +24,15 @@ export interface ExportOptions {
    * Set 'true' if we should use prettier to format the exported code.
    */
   prettier?: boolean;
+
+  /**
+   * Set 'true' if you're facing memory exhaustion issues on large schemas and
+   * wish to skip optimizing the export.
+   *
+   * Optimizing the export can make the export smaller and more readable,
+   * involving fewer IIFEs and similar constructs used throughout EXPORTABLE
+   * expressions. It may also have a (positive, hopefully) impact on
+   * performance. In general it's recommended to leave optimize enabled.
+   */
+  disableOptimize?: boolean;
 }
