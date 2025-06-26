0a497dfc91658967922c19c3e229b267d871bd7c
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2025-06-26 16:04:35 +0100

    upgrade prism

--- grafast/website/docusaurus.config.js
+++ grafast/website/docusaurus.config.js
@@ -2,8 +2,9 @@
 // Note: type annotations allow type checking and IDEs autocompletion
 
 const TerserPlugin = require("terser-webpack-plugin");
-const lightCodeTheme = require("prism-react-renderer").themes.github;
-const darkCodeTheme = require("prism-react-renderer").themes.dracula;
+const { themes } = require("prism-react-renderer");
+const lightCodeTheme = themes.github;
+const darkCodeTheme = themes.dracula;
 
 const organizationName = "graphile";
 const projectName = "crystal";
