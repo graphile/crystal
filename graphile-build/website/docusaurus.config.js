ec3b5cc275686d94754240b65d14768c5ce83184
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2025-06-26 13:06:33 +0100

    Add same to utils and graphile-build sites

--- graphile-build/website/docusaurus.config.js
+++ graphile-build/website/docusaurus.config.js
@@ -241,6 +241,12 @@ const config = {
     }),
   trailingSlash: undefined,
   clientModules: [require.resolve("./docusaurus.client.js")],
+  markdown: {
+    mdx1Compat: {
+      comments: true,
+      admonitions: true,
+    },
+  },
 };
 
 module.exports = config;
