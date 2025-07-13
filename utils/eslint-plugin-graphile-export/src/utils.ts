046d81108918467f0da66e6b764266e262f3eb86
Benjie Gillam <benjie@jemjie.com>
2025-07-13 11:04:45 +0100

    Add support for build.EXPORTABLE linting

--- utils/eslint-plugin-graphile-export/src/utils.ts
+++ utils/eslint-plugin-graphile-export/src/utils.ts
@@ -1,10 +1,16 @@
 import type { CallExpression } from "@babel/types";
 
 export function isExportableCall(node: any): node is CallExpression {
-  return (
-    node.type === "CallExpression" &&
-    node.callee.type === "Identifier" &&
-    node.callee.name === "EXPORTABLE"
-  );
+  return node.type === "CallExpression" && isEXPORTABLE(node.callee);
 }
 
+export function isEXPORTABLE(node: any): boolean {
+  switch (node.type) {
+    case "Identifier":
+      return node.name === "EXPORTABLE";
+    case "MemberExpression":
+      return isEXPORTABLE(node.property);
+    default:
+      return false;
+  }
+}
