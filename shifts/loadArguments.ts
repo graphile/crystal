80fac1ce345ee656f8b959510b67d5568db6b139
Benjie Gillam <benjie@jemjie.com>
2025-07-23 18:58:21 +0100

    More type safety, filter out nulls

--- shifts/loadArguments.ts
+++ shifts/loadArguments.ts
@@ -20,31 +20,6 @@ export default function transformer(file: FileInfo, api: API) {
   const j: JSCodeshift = api.jscodeshift;
   const root = j(file.source);
 
-  function buildOptions(args: any[]) {
-    const [spec, a, b, c] = args;
-    const props = [j.objectProperty(j.identifier("lookup"), spec)];
-
-    if (args.length === 2) {
-      // (spec, loadCallback)
-      props.push(j.objectProperty(j.identifier("load"), a));
-    } else if (args.length === 3) {
-      // Ambiguous: could be (spec, ioEquivalence, loadCallback) OR (spec, unary, loadCallback)
-      props.push(
-        j.objectProperty(j.identifier("unaryOrIoEquivalencePleaseResolve"), a),
-      );
-      props.push(j.objectProperty(j.identifier("load"), b));
-    } else if (args.length === 4) {
-      // (spec, unary, ioEquivalence, loadCallback)
-      props.push(j.objectProperty(j.identifier("unary"), a));
-      props.push(j.objectProperty(j.identifier("ioEquivalence"), b));
-      props.push(j.objectProperty(j.identifier("load"), c));
-    } else {
-      return null;
-    }
-
-    return j.objectExpression(props);
-  }
-
   let changed = false;
   root
     .find(j.CallExpression, {
@@ -57,7 +32,43 @@ export default function transformer(file: FileInfo, api: API) {
       const args = path.node.arguments;
       if (args.length < 2 || args.length > 4) return;
 
-      const options = buildOptions(args);
+      function buildOptions() {
+        const [spec, a, b, c] = args.filter(
+          (arg) =>
+            arg.type !== "NullLiteral" &&
+            (arg.type !== "Identifier" || arg.name !== "undefined"),
+        );
+        if (spec.type === "SpreadElement") throw new Error("Spread forbidden");
+        if (a.type === "SpreadElement") throw new Error("Spread forbidden");
+        if (b.type === "SpreadElement") throw new Error("Spread forbidden");
+        if (c.type === "SpreadElement") throw new Error("Spread forbidden");
+        const props = [j.objectProperty(j.identifier("lookup"), spec)];
+
+        if (args.length === 2) {
+          // (spec, loadCallback)
+          props.push(j.objectProperty(j.identifier("load"), a));
+        } else if (args.length === 3) {
+          // Ambiguous: could be (spec, ioEquivalence, loadCallback) OR (spec, unary, loadCallback)
+          props.push(
+            j.objectProperty(
+              j.identifier("unaryOrIoEquivalencePleaseResolve"),
+              a,
+            ),
+          );
+          props.push(j.objectProperty(j.identifier("load"), b));
+        } else if (args.length === 4) {
+          // (spec, unary, ioEquivalence, loadCallback)
+          props.push(j.objectProperty(j.identifier("unary"), a));
+          props.push(j.objectProperty(j.identifier("ioEquivalence"), b));
+          props.push(j.objectProperty(j.identifier("load"), c));
+        } else {
+          return null;
+        }
+
+        return j.objectExpression(props);
+      }
+
+      const options = buildOptions();
       if (!options) return;
       changed = true;
 
