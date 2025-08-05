061de13b701ed0b82f291ea8e496ec41518d9389
Benjie Gillam <benjie@jemjie.com>
2025-08-05 07:27:23 +0100

    Switch to new loadOne/loadMany shape

--- grafast/website/examples/users-and-friends/plans.mjs
+++ grafast/website/examples/users-and-friends/plans.mjs
@@ -11,26 +11,34 @@ const userByIdCallback = (ids, { attributes }) =>
   getUsersByIds(ids, { columns: attributes });
 userByIdCallback.displayName = "userById";
 export function userById($id) {
-  return loadOne($id, "id", userByIdCallback);
+  return loadOne({ lookup: $id, ioEquivalence: "id", load: userByIdCallback });
 }
 
 const friendshipsByUserIdsCallback = (ids, { attributes }) =>
   getFriendshipsByUserIds(ids, { columns: attributes });
 friendshipsByUserIdsCallback.displayName = "friendshipsByUserId";
 export function friendshipsByUserId($id) {
-  return loadMany($id, "user_id", friendshipsByUserIdsCallback);
+  return loadMany({
+    lookup: $id,
+    ioEquivalence: "user_id",
+    load: friendshipsByUserIdsCallback,
+  });
 }
 
 const postByIdCallback = (ids, { attributes }) =>
   getPostsByIds(ids, { columns: attributes });
 postByIdCallback.displayName = "postById";
 export function postById($id) {
-  return loadOne($id, "id", postByIdCallback);
+  return loadOne({ lookup: $id, ioEquivalence: "id", load: postByIdCallback });
 }
 
 const postsByAuthorIdsCallback = (ids, { attributes }) =>
   getPostsByAuthorIds(ids, { columns: attributes });
 postsByAuthorIdsCallback.displayName = "postsByAuthorId";
 export function postsByAuthorId($id) {
-  return loadMany($id, "user_id", postsByAuthorIdsCallback);
+  return loadMany({
+    lookup: $id,
+    ioEquivalence: "user_id",
+    load: postsByAuthorIdsCallback,
+  });
 }
