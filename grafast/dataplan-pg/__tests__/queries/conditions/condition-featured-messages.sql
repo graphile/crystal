47376c3d5114f8261dd09eacf100dbc2ef69ec57
Benjie Gillam <benjie@jemjie.com>
2025-08-05 12:12:38 +0100

    Update SQL

--- grafast/dataplan-pg/__tests__/queries/conditions/condition-featured-messages.sql
+++ grafast/dataplan-pg/__tests__/queries/conditions/condition-featured-messages.sql
@@ -3,11 +3,11 @@ select
   array(
     select array[
       __messages__."body",
-      __messages__."id",
       __users__."username",
       __users__."gravatar_url",
       __users_2."username",
-      __users_2."gravatar_url"
+      __users_2."gravatar_url",
+      __messages__."id"
     ]::text[]
     from app_public.messages as __messages__
     left outer join app_public.users as __users__
