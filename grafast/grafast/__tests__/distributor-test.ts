57fb43291ab1875e37105fe51eee7bbad6111502
Benjie Gillam <benjie@jemjie.com>
2025-08-07 12:59:21 +0100

    Start writing tests for distributor

---
+++ grafast/grafast/__tests__/distributor-test.ts
@@ -0,0 +1,228 @@
+/* eslint-disable graphile-export/exhaustive-deps, graphile-export/export-methods, graphile-export/export-instances, graphile-export/export-subclasses, graphile-export/no-nested */
+import { expect } from "chai";
+import { resolvePreset } from "graphile-config";
+import { type AsyncExecutionResult } from "graphql";
+import { it } from "mocha";
+
+import {
+  connection,
+  grafast,
+  loadMany,
+  loadManyCallback,
+  makeGrafastSchema,
+  Step,
+} from "../dist/index.js";
+import { resolveStreamDefer, streamToArray } from "./incrementalUtils.js";
+
+const resolvedPreset = resolvePreset({});
+const requestContext = {};
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const schema = makeGrafastSchema({
+  typeDefs: /* GraphQL */ `
+    type Query {
+      connection(max: Int! = 100): FibConnection
+    }
+    type FibConnection {
+      edges: [FibEdge]
+      nodes: [Fib]
+      pageInfo: PageInfo!
+    }
+    type FibEdge {
+      cursor: String!
+      node: Fib
+    }
+    type PageInfo {
+      hasNextPage: Boolean
+      hasPreviousPage: Boolean
+      startCursor: String
+      endCursor: String
+    }
+    type Fib {
+      index: Int
+      value: Int
+    }
+  `,
+  objects: {
+    Query: {
+      plans: {
+        connection(_, { $max }) {
+          const $fib = fib($max);
+          return connection($fib);
+        },
+      },
+    },
+  },
+  enableDeferStream: true,
+});
+
+function fib($max: Step<number>) {
+  return loadMany($max, fibonacciLoader);
+}
+
+const fibonacciLoader = loadManyCallback((maxes: readonly number[]) => {
+  return maxes.map((max) => fibonacci(max));
+});
+
+let startCount = 0;
+let endCount = 0;
+
+async function* fibonacci(max: number) {
+  startCount++;
+  try {
+    let index = 0;
+    let a = 0;
+    let b = 1;
+    while (b < max) {
+      await sleep(1);
+      yield { index: index++, value: b };
+      const c = a + b;
+      a = b;
+      b = c;
+    }
+  } finally {
+    endCount++;
+  }
+}
+
+function throwOnUnhandledRejections(callback: () => Promise<void>) {
+  return async () => {
+    let failed: Error | undefined;
+    function fail(e: Error) {
+      console.error(`UNHANDLED PROMISE REJECTION: ${e}`);
+      failed = e;
+    }
+    process.on("unhandledRejection", fail);
+    try {
+      return await callback();
+    } finally {
+      process.off("unhandledRejection", fail);
+      if (failed) {
+        failed = undefined;
+        // eslint-disable-next-line no-unsafe-finally
+        throw new Error(`Unhandled promise rejection occurred`);
+      }
+    }
+  };
+}
+
+beforeEach(() => {
+  startCount = 0;
+  endCount = 0;
+});
+
+it(
+  "happy path",
+  throwOnUnhandledRejections(async () => {
+    const source = /* GraphQL */ `
+      {
+        connection {
+          edges @stream(initialCount: 1) {
+            cursor
+            node {
+              index
+              value
+            }
+          }
+          nodes @stream(initialCount: 2) {
+            index
+            value
+          }
+          pageInfo {
+            ... @defer {
+              hasNextPage
+            }
+            ... @defer {
+              hasPreviousPage
+            }
+            ... @defer {
+              startCursor
+            }
+            ... @defer {
+              endCursor
+            }
+          }
+        }
+      }
+    `;
+    const stream = await grafast({
+      schema,
+      requestContext,
+      resolvedPreset,
+
+      source,
+    });
+    if ("errors" in stream) {
+      console.dir(stream.errors);
+      throw stream.errors![0];
+    }
+    assertIterable(stream);
+    const payloads = (await streamToArray(stream)) as AsyncExecutionResult[];
+    expect(payloads[0].errors).not.to.exist;
+    expect(payloads[0].data).to.deep.equal({
+      connection: {
+        edges: [
+          {
+            cursor: Buffer.from("0").toString("base64"),
+            node: {
+              index: 0,
+              value: 1,
+            },
+          },
+        ],
+        nodes: [
+          {
+            index: 0,
+            value: 1,
+          },
+          {
+            index: 1,
+            value: 1,
+          },
+        ],
+        pageInfo: {},
+      },
+    });
+    const finalData = resolveStreamDefer(payloads);
+    const expectedNodes = [
+      { index: 0, value: 1 },
+      { index: 1, value: 1 },
+      { index: 2, value: 2 },
+      { index: 3, value: 3 },
+      { index: 4, value: 5 },
+      { index: 5, value: 8 },
+      { index: 6, value: 13 },
+      { index: 7, value: 21 },
+      { index: 8, value: 34 },
+      { index: 9, value: 55 },
+      { index: 10, value: 89 },
+    ];
+    expect(finalData.errors).not.to.exist;
+    expect(finalData.data).to.deep.equal({
+      connection: {
+        edges: expectedNodes.map((node) => ({
+          cursor: Buffer.from(String(node.index)).toString("base64"),
+          node,
+        })),
+        nodes: expectedNodes,
+        pageInfo: {
+          hasNextPage: false,
+          hasPreviousPage: false,
+          startCursor: Buffer.from("0").toString("base64"),
+          endCursor: Buffer.from("10").toString("base64"),
+        },
+      },
+    });
+    expect(startCount).to.equal(1);
+    expect(endCount).to.equal(1);
+  }),
+);
+
+function assertIterable<T, TReturn, TNext>(
+  stream: AsyncIterable<T, TReturn, TNext> | unknown,
+): asserts stream is AsyncIterable<T, TReturn, TNext> {
+  expect(stream).to.have.property(Symbol.asyncIterator).that.is.a("function");
+}
+
+it("throws an error when initialCount > limit");
