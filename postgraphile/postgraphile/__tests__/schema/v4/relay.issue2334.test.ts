be588c69016dee381fb74a7bd9d9cc6b65f7556e
Benjie Gillam <benjie@jemjie.com>
2025-08-26 18:58:51 +0100

    Add assertion

--- postgraphile/postgraphile/__tests__/schema/v4/relay.issue2334.test.ts
+++ postgraphile/postgraphile/__tests__/schema/v4/relay.issue2334.test.ts
@@ -1,3 +1,5 @@
+import { assertInputObjectType } from "graphql";
+
 import { PostGraphileRelayPreset } from "../../../src/presets/relay.js";
 import * as core from "./core.js";
 
@@ -8,7 +10,13 @@ test(
     ["issue_2334"],
     { ignoreRBAC: false },
     (client) => client.query("set role postgraphile_test_authenticator"),
-    undefined,
+    (schema) => {
+      const t = assertInputObjectType(schema.getType("BarPatch"));
+      const f = t.getFields();
+      // Explicitly this must not contain `fooByRowId`, `fooById`, `foo` or any
+      // other related thing - permissions forbid it!
+      expect(Object.keys(f)).toEqual(["col"]);
+    },
     undefined,
     { extends: [PostGraphileRelayPreset] },
   ),
