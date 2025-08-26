511bb5740b291fa833c5e10fcf92b148dc86229e
Benjie Gillam <benjie@jemjie.com>
2025-08-26 16:28:56 +0100

    Update snapshots to be a single select

--- postgraphile/postgraphile/__tests__/mutations/v4/custom-mutation-with-computed-column.sql
+++ postgraphile/postgraphile/__tests__/mutations/v4/custom-mutation-with-computed-column.sql
@@ -13,21 +13,13 @@ select
     __upsert_setting__,
     $1::"text"
   ) as "0",
-  __upsert_setting__."id"::text as "1"
-from (select ($2::"issue_2287"."users").*) as __upsert_setting__;
-
-select
   "issue_2287"."users_setting"(
     __upsert_setting__,
-    $1::"text"
-  ) as "0",
-  __upsert_setting__."id"::text as "1"
-from (select ($2::"issue_2287"."users").*) as __upsert_setting__;
-
-select
+    $2::"text"
+  ) as "1",
   "issue_2287"."users_setting"(
     __upsert_setting__,
-    $1::"text"
-  ) as "0",
-  __upsert_setting__."id"::text as "1"
-from (select ($2::"issue_2287"."users").*) as __upsert_setting__;
+    $3::"text"
+  ) as "2",
+  __upsert_setting__."id"::text as "3"
+from (select ($4::"issue_2287"."users").*) as __upsert_setting__;
