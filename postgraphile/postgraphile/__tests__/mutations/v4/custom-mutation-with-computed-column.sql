331caec0bc25f65d42f7fa3b17d9d0667a889bb1
Benjie Gillam <benjie@jemjie.com>
2025-08-26 11:09:40 +0100

    Update plan and SQL

--- postgraphile/postgraphile/__tests__/mutations/v4/custom-mutation-with-computed-column.sql
+++ postgraphile/postgraphile/__tests__/mutations/v4/custom-mutation-with-computed-column.sql
@@ -1,20 +1,33 @@
 select
   __upsert_setting__."id"::text as "0",
   __upsert_setting__."username" as "1",
+  case when (__upsert_setting__) is not distinct from null then null::text else json_build_array((((__upsert_setting__)."id"))::text, ((__upsert_setting__)."username"))::text end as "2"
+from "issue_2287"."upsert_setting"(
+  $1::"int4",
+  $2::"text",
+  $3::"text"
+) as __upsert_setting__;
+
+select
   "issue_2287"."users_setting"(
     __upsert_setting__,
     $1::"text"
-  ) as "2",
+  ) as "0",
+  __upsert_setting__."id"::text as "1"
+from (select ($2::"issue_2287"."users").*) as __upsert_setting__;
+
+select
   "issue_2287"."users_setting"(
     __upsert_setting__,
-    $2::"text"
-  ) as "3",
+    $1::"text"
+  ) as "0",
+  __upsert_setting__."id"::text as "1"
+from (select ($2::"issue_2287"."users").*) as __upsert_setting__;
+
+select
   "issue_2287"."users_setting"(
     __upsert_setting__,
-    $3::"text"
-  ) as "4"
-from "issue_2287"."upsert_setting"(
-  $4::"int4",
-  $5::"text",
-  $6::"text"
-) as __upsert_setting__;
+    $1::"text"
+  ) as "0",
+  __upsert_setting__."id"::text as "1"
+from (select ($2::"issue_2287"."users").*) as __upsert_setting__;
