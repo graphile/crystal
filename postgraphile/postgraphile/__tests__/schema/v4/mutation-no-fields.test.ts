ebb5df70d74e39d1cd1ab439eaa85f31930cbe5c
Benjie Gillam <benjie@jemjie.com>
2025-09-17 16:53:36 +0100

    Prettier

--- postgraphile/postgraphile/__tests__/schema/v4/mutation-no-fields.test.ts
+++ postgraphile/postgraphile/__tests__/schema/v4/mutation-no-fields.test.ts
@@ -1,5 +1,6 @@
 import * as core from "./core.js";
 
-test("prints a schema from a table with no mutable fields",
-  core.test(__filename, "no_fields"
-  ));
+test(
+  "prints a schema from a table with no mutable fields",
+  core.test(__filename, "no_fields"),
+);
