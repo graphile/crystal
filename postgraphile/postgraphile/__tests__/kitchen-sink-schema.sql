3345e34d1ee5b9429e8d68524dce9d61917eecb8
Benjie Gillam <benjie@jemjie.com>
2025-09-22 15:38:12 +0100

    Add tests for querying the data

--- postgraphile/postgraphile/__tests__/kitchen-sink-schema.sql
+++ postgraphile/postgraphile/__tests__/kitchen-sink-schema.sql
@@ -1917,7 +1917,8 @@ create table partitions.measurements_y2024 partition of partitions.measurements 
 
 create table partitions.entity_kinds (kind text primary key);
 comment on table partitions.entity_kinds is '@enum';
-insert into partitions.entity_kinds (kind) values ('photos'), ('locations'), ('profiles');
+insert into partitions.entity_kinds (kind)
+  values ('photos'), ('locations'), ('profiles');
 
 create table partitions.photos (
   id uuid primary key
@@ -1955,29 +1956,6 @@ create table partitions.profile_tags
 alter table partitions.profile_tags
   add constraint profile_tags_entity_fk
   foreign key (entity_id) references partitions.profiles(id) on delete cascade;
-
-insert into partitions.photos (id) values 
-  ('28be1007-e624-46c8-bfa0-6b7e5a2c630e'),
-  ('36a9cc35-f158-4648-83f4-d8fa04ee7527');
-insert into partitions.locations (id) values 
-  ('039b863d-73c6-44f7-bef3-2e0058a85552'),
-  ('4b7c1188-94e9-4ae1-849c-e088964a9911');
-insert into partitions.profiles (id) values 
-  ('8cc170ed-7869-4cd9-ae4b-8d3b528de606'),
-  ('e3f89f32-e963-425e-b08f-a4c6be2161b6');
-insert into partitions.tags (entity_kind, entity_id, tag) values 
-  ('photos', '28be1007-e624-46c8-bfa0-6b7e5a2c630e', 'sunny'),
-  ('photos', '28be1007-e624-46c8-bfa0-6b7e5a2c630e', 'beach'),
-  ('photos', '28be1007-e624-46c8-bfa0-6b7e5a2c630e', 'spf50'),
-  ('photos', '28be1007-e624-46c8-bfa0-6b7e5a2c630e', 'holiday'),
-  ('photos', '36a9cc35-f158-4648-83f4-d8fa04ee7527', 'yummy'),
-  ('photos', '36a9cc35-f158-4648-83f4-d8fa04ee7527', 'sundae'),
-  ('photos', '36a9cc35-f158-4648-83f4-d8fa04ee7527', 'dessert'),
-  ('locations', '039b863d-73c6-44f7-bef3-2e0058a85552', 'canal'),
-  ('locations', '039b863d-73c6-44f7-bef3-2e0058a85552', 'boats'),
-  ('locations', '4b7c1188-94e9-4ae1-849c-e088964a9911', 'mountain'),
-  ('profiles', '8cc170ed-7869-4cd9-ae4b-8d3b528de606', 'admin'),
-  ('profiles', 'e3f89f32-e963-425e-b08f-a4c6be2161b6', 'moderator');
 
 --------------------------------------------------------------------------------
 
--- postgraphile/postgraphile/__tests__/queries/v4/partitions.sql
+++ postgraphile/postgraphile/__tests__/queries/v4/partitions.sql
@@ -13,5 +13,89 @@ on (
 order by __measurements__."timestamp" asc, __measurements__."key" asc;
 
 select
+  __location_tags__."entity_kind"::text as "0",
+  __location_tags__."entity_id" as "1",
+  __location_tags__."tag" as "2",
+  __locations__."id" as "3"
+from "partitions"."location_tags" as __location_tags__
+left outer join "partitions"."locations" as __locations__
+on (
+/* WHERE becoming ON */ (
+  __locations__."id" = __location_tags__."entity_id"
+))
+order by __location_tags__."entity_kind" asc, __location_tags__."entity_id" asc, __location_tags__."tag" asc;
+
+select
+  __locations__."id" as "0",
+  array(
+    select array[
+      __location_tags__."tag"
+    ]::text[]
+    from "partitions"."location_tags" as __location_tags__
+    where (
+      __location_tags__."entity_id" = __locations__."id"
+    )
+    order by __location_tags__."entity_kind" asc, __location_tags__."entity_id" asc, __location_tags__."tag" asc
+  )::text as "1"
+from "partitions"."locations" as __locations__
+order by __locations__."id" asc;
+
+select
+  __photo_tags__."entity_kind"::text as "0",
+  __photo_tags__."entity_id" as "1",
+  __photo_tags__."tag" as "2",
+  __photos__."id" as "3"
+from "partitions"."photo_tags" as __photo_tags__
+left outer join "partitions"."photos" as __photos__
+on (
+/* WHERE becoming ON */ (
+  __photos__."id" = __photo_tags__."entity_id"
+))
+order by __photo_tags__."entity_kind" asc, __photo_tags__."entity_id" asc, __photo_tags__."tag" asc;
+
+select
+  __photos__."id" as "0",
+  array(
+    select array[
+      __photo_tags__."tag"
+    ]::text[]
+    from "partitions"."photo_tags" as __photo_tags__
+    where (
+      __photo_tags__."entity_id" = __photos__."id"
+    )
+    order by __photo_tags__."entity_kind" asc, __photo_tags__."entity_id" asc, __photo_tags__."tag" asc
+  )::text as "1"
+from "partitions"."photos" as __photos__
+order by __photos__."id" asc;
+
+select
+  __profile_tags__."entity_kind"::text as "0",
+  __profile_tags__."entity_id" as "1",
+  __profile_tags__."tag" as "2",
+  __profiles__."id" as "3"
+from "partitions"."profile_tags" as __profile_tags__
+left outer join "partitions"."profiles" as __profiles__
+on (
+/* WHERE becoming ON */ (
+  __profiles__."id" = __profile_tags__."entity_id"
+))
+order by __profile_tags__."entity_kind" asc, __profile_tags__."entity_id" asc, __profile_tags__."tag" asc;
+
+select
+  __profiles__."id" as "0",
+  array(
+    select array[
+      __profile_tags__."tag"
+    ]::text[]
+    from "partitions"."profile_tags" as __profile_tags__
+    where (
+      __profile_tags__."entity_id" = __profiles__."id"
+    )
+    order by __profile_tags__."entity_kind" asc, __profile_tags__."entity_id" asc, __profile_tags__."tag" asc
+  )::text as "1"
+from "partitions"."profiles" as __profiles__
+order by __profiles__."id" asc;
+
+select
   (count(*))::text as "0"
 from "partitions"."measurements" as __measurements__;
