959cbb8ef85b5a15880487b0e5979d1d5f74c3f1
Jem Gillam <6413628+jemgillam@users.noreply.github.com>
2025-10-29 12:47:49 +0000

    Change menu to show RC status

--- graphile-build/website/docusaurus.config.js
+++ graphile-build/website/docusaurus.config.js
@@ -50,7 +50,7 @@ const config = {
           versions: {
             latest: { label: "Current" },
             4: { label: "v4 (current)", banner: "none" },
-            5: { label: "v5 (beta)", banner: "unreleased" },
+            5: { label: "v5 (rc)", banner: "unreleased" },
             current: { label: "Draft" },
           },
         },
@@ -76,7 +76,7 @@ const config = {
         versions: {
           latest: { label: "Current" },
           4: { label: "v4 (current)", banner: "none" },
-          5: { label: "v5 (beta)", banner: "unreleased" },
+          5: { label: "v5 (rc)", banner: "unreleased" },
           current: { label: "Draft" },
         },
       },
