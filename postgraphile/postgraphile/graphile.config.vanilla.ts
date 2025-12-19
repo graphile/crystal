9fba7361b6bc9cc7011c419951bb555e05319cd3
Benjie Gillam <benjie@jemjie.com>
2025-12-19 14:32:07 +0000

    Add more presets for more options

--- postgraphile/postgraphile/graphile.config.vanilla.ts
+++ postgraphile/postgraphile/graphile.config.vanilla.ts
@@ -1,4 +1,9 @@
 import "postgraphile";
 import "postgraphile/adaptors/pg";
+import "postgraphile/utils";
+import "postgraphile/presets/amber";
+import "postgraphile/presets/v4";
+import "postgraphile/presets/relay";
+import "postgraphile/presets/lazy-jwt";
 const preset: GraphileConfig.Preset = {};
 export default preset;
