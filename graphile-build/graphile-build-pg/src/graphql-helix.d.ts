e6c0e6368474cff5254433c60de537fd28722011
Benjie Gillam <benjie@jemjie.com>
2026-01-27 17:36:21 +0000

    Remove file LLM created

--- graphile-build/graphile-build-pg/src/graphql-helix.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "graphql-helix" {
-  export const getGraphQLParameters: (...args: any[]) => any;
-  export const processRequest: (...args: any[]) => any;
-  export const sendResult: (...args: any[]) => any;
-}
