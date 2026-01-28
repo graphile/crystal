fae4142815b0de2a87fb59be0449bd5362d90b2e
Benjie Gillam <benjie@jemjie.com>
2026-01-28 15:37:20 +0000

    Fix browser render

--- grafast/website/src/components/UrlParam.jsx
+++ grafast/website/src/components/UrlParam.jsx
@@ -1,5 +1,10 @@
+/* global window */
+import useIsBrowser from "@docusaurus/useIsBrowser";
+
 export default function UrlParam({ param, fallback }) {
-  const hash = typeof window !== "undefined" ? window.location.hash : null;
+  const isBrowser = useIsBrowser();
+  const hash =
+    isBrowser && typeof window !== "undefined" ? window.location.hash : null;
   const qs = hash ? hash.replace(/^#+/, "") : null;
   const params = qs ? new URLSearchParams(qs) : null;
   const val = params ? params.get(param) : null;
