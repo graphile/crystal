402ff34c16191358352664b27703e346a7463ac3
BenjAIe <code+ai@benjiegillam.com>
2026-02-23 12:46:25 +0000

    AI elimination of unneeded params

--- utils/graphile-export/src/optimize/index.ts
+++ utils/graphile-export/src/optimize/index.ts
@@ -235,6 +235,15 @@ export const optimize = (inAst: t.File, runs = 1): t.File => {
         // Make sure our scope information is up to date!
         exitPath.scope.crawl();
 
+        // If a local top-level function is only ever called directly, and all
+        // calls pass the same leading identifier arguments, inline those
+        // identifiers into the function body and remove the redundant
+        // parameters/arguments.
+        exitPath.traverse({
+          VariableDeclarator: optimizeCommonLeadingCallArgs,
+          FunctionDeclaration: optimizeCommonLeadingCallArgs,
+        });
+
         // Replace all things that are only referenced once.
         // This nested traversal approach inspired by https://github.com/babel/babel/issues/15544#issuecomment-1540542863
         exitPath.traverse({
@@ -316,6 +325,165 @@ export const optimize = (inAst: t.File, runs = 1): t.File => {
           targetPath.stop();
           scope.removeBinding(bindingName);
           path.remove();
+        }
+
+        function optimizeCommonLeadingCallArgs(
+          path:
+            | NodePath<t.VariableDeclarator>
+            | NodePath<t.FunctionDeclaration>,
+        ) {
+          let bindingName: string | null = null;
+          let functionPath:
+            | NodePath<t.FunctionDeclaration>
+            | NodePath<t.FunctionExpression | t.ArrowFunctionExpression>
+            | null = null;
+
+          if (path.isFunctionDeclaration()) {
+            if (!path.node.id) {
+              return;
+            }
+            bindingName = path.node.id.name;
+            functionPath = path;
+          } else {
+            if (!t.isIdentifier(path.node.id)) {
+              return;
+            }
+            const init = path.get("init");
+            if (
+              !init ||
+              (!init.isFunctionExpression() && !init.isArrowFunctionExpression())
+            ) {
+              return;
+            }
+            bindingName = path.node.id.name;
+            functionPath = init as NodePath<
+              t.FunctionExpression | t.ArrowFunctionExpression
+            >;
+          }
+
+          const scope = path.isFunctionDeclaration() ? path.scope.parent : path.scope;
+          if (scope !== exitPath.scope) {
+            return;
+          }
+          if (!bindingName) {
+            return;
+          }
+
+          const statementPath = path.getStatementParent();
+          if (
+            !statementPath ||
+            t.isExportNamedDeclaration(statementPath.node) ||
+            t.isExportDefaultDeclaration(statementPath.node)
+          ) {
+            return;
+          }
+
+          const binding = scope.bindings[bindingName];
+          if (!binding || binding.referencePaths.length === 0) {
+            return;
+          }
+
+          const params = functionPath.node.params;
+          if (params.length === 0 || !params.every(isSimpleParam)) {
+            return;
+          }
+
+          const callPaths: NodePath<t.CallExpression>[] = [];
+          for (const refPath of binding.referencePaths) {
+            const parentPath = refPath.parentPath;
+            if (
+              !parentPath?.isCallExpression() ||
+              parentPath.node.callee !== refPath.node
+            ) {
+              return;
+            }
+            callPaths.push(parentPath);
+          }
+          if (callPaths.length === 0) {
+            return;
+          }
+
+          let prefixLength = 0;
+          const maxPrefix = Math.min(
+            params.length,
+            ...callPaths.map((callPath) => callPath.node.arguments.length),
+          );
+          for (let i = 0; i < maxPrefix; i++) {
+            const param = functionPath.node.params[i];
+            if (!t.isIdentifier(param)) {
+              break;
+            }
+            const firstArg = callPaths[0].node.arguments[i];
+            if (!t.isIdentifier(firstArg)) {
+              break;
+            }
+            const firstArgBinding = callPaths[0].scope.getBinding(firstArg.name);
+            if (!firstArgBinding || firstArgBinding.scope !== exitPath.scope) {
+              break;
+            }
+            if (firstArg.name === param.name) {
+              let allMatch = true;
+              for (let j = 1; j < callPaths.length; j++) {
+                const arg = callPaths[j].node.arguments[i];
+                if (!t.isIdentifier(arg) || arg.name !== firstArg.name) {
+                  allMatch = false;
+                  break;
+                }
+                const argBinding = callPaths[j].scope.getBinding(arg.name);
+                if (!argBinding || argBinding.scope !== exitPath.scope) {
+                  allMatch = false;
+                  break;
+                }
+              }
+              if (!allMatch) {
+                break;
+              }
+              prefixLength++;
+              continue;
+            }
+            if (functionPath.scope.hasOwnBinding(firstArg.name)) {
+              break;
+            }
+            let allMatch = true;
+            for (let j = 1; j < callPaths.length; j++) {
+              const arg = callPaths[j].node.arguments[i];
+              if (!t.isIdentifier(arg) || arg.name !== firstArg.name) {
+                allMatch = false;
+                break;
+              }
+              const argBinding = callPaths[j].scope.getBinding(arg.name);
+              if (!argBinding || argBinding.scope !== exitPath.scope) {
+                allMatch = false;
+                break;
+              }
+            }
+            if (!allMatch) {
+              break;
+            }
+            prefixLength++;
+          }
+
+          if (prefixLength === 0) {
+            return;
+          }
+
+          for (let i = 0; i < prefixLength; i++) {
+            const param = functionPath.node.params[i];
+            const firstArg = callPaths[0].node.arguments[i];
+            if (!t.isIdentifier(param) || !t.isIdentifier(firstArg)) {
+              return;
+            }
+            if (param.name !== firstArg.name) {
+              functionPath.scope.rename(param.name, firstArg.name);
+            }
+          }
+
+          for (let i = prefixLength - 1; i >= 0; i--) {
+            functionPath.node.params.splice(i, 1);
+            for (const callPath of callPaths) {
+              callPath.node.arguments.splice(i, 1);
+            }
+          }
         }
       },
     },
