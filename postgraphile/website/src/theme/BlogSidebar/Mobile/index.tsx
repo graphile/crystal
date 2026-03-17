8dac4bd714e4d830f3626880f42c5be0cccc26fc
Benjie Gillam <benjie@jemjie.com>
2026-03-17 16:53:15 +0000

    Swizzle sidebar

---
+++ postgraphile/website/src/theme/BlogSidebar/Mobile/index.tsx
@@ -0,0 +1,45 @@
+import React, {memo, type ReactNode} from 'react';
+import {
+  useVisibleBlogSidebarItems,
+  BlogSidebarItemList,
+} from '@docusaurus/plugin-content-blog/client';
+import {NavbarSecondaryMenuFiller} from '@docusaurus/theme-common';
+import BlogSidebarContent from '@theme/BlogSidebar/Content';
+import type {Props} from '@theme/BlogSidebar/Mobile';
+import type {Props as BlogSidebarContentProps} from '@theme/BlogSidebar/Content';
+
+import styles from './styles.module.css';
+
+const ListComponent: BlogSidebarContentProps['ListComponent'] = ({items}) => {
+  return (
+    <BlogSidebarItemList
+      items={items}
+      ulClassName="menu__list"
+      liClassName="menu__list-item"
+      linkClassName="menu__link"
+      linkActiveClassName="menu__link--active"
+    />
+  );
+};
+
+function BlogSidebarMobileSecondaryMenu({sidebar}: Props): ReactNode {
+  const items = useVisibleBlogSidebarItems(sidebar.items);
+  return (
+    <BlogSidebarContent
+      items={items}
+      ListComponent={ListComponent}
+      yearGroupHeadingClassName={styles.yearGroupHeading}
+    />
+  );
+}
+
+function BlogSidebarMobile(props: Props): ReactNode {
+  return (
+    <NavbarSecondaryMenuFiller
+      component={BlogSidebarMobileSecondaryMenu}
+      props={props}
+    />
+  );
+}
+
+export default memo(BlogSidebarMobile);
