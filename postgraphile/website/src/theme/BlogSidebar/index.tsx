78d8b47cb87c9ecb7cdb48c616f79c025fcf2b70
Benjie Gillam <benjie@jemjie.com>
2026-03-17 16:53:15 +0000

    Swizzle sidebar

---
+++ postgraphile/website/src/theme/BlogSidebar/index.tsx
@@ -0,0 +1,17 @@
+import React, {type ReactNode} from 'react';
+import {useWindowSize} from '@docusaurus/theme-common';
+import BlogSidebarDesktop from '@theme/BlogSidebar/Desktop';
+import BlogSidebarMobile from '@theme/BlogSidebar/Mobile';
+import type {Props} from '@theme/BlogSidebar';
+
+export default function BlogSidebar({sidebar}: Props): ReactNode {
+  const windowSize = useWindowSize();
+  if (!sidebar?.items.length) {
+    return null;
+  }
+  // Mobile sidebar doesn't need to be server-rendered
+  if (windowSize === 'mobile') {
+    return <BlogSidebarMobile sidebar={sidebar} />;
+  }
+  return <BlogSidebarDesktop sidebar={sidebar} />;
+}
