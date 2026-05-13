84524cc76242f715b3bbcd840168e2690f27ac39
Benjie Gillam <benjie@jemjie.com>
2026-05-13 20:12:46 +0100

    Rudimentary functioning

--- grafast/dataplan-graphql/src/steps/graphqlSelectionSet.ts
+++ grafast/dataplan-graphql/src/steps/graphqlSelectionSet.ts
@@ -17,6 +17,10 @@ type SelectionDataParent<TSchema, TOperationType extends OperationType> =
   | __ItemStep<any>
   | AccessStep<any>;
 
+interface FieldDetails {
+  args?: Record<string, Step>;
+  selections?: SelectionNode[];
+}
 export class GraphQLSelectionSetStep<
   TSchema,
   TOperationType extends OperationType,
@@ -88,6 +92,82 @@ export class GraphQLSelectionSetStep<
       : never
   ) {
     return new GraphQLSelectFieldStep(this, ...args);
+  }
+
+  /**
+   * Finds the selection set closes to the root without passing through a field
+   * selection. Used to ensure field names are uniquely selected.
+   */
+  owningSelectionSet() {
+    let $owning: GraphQLSelectionSetStep<TSchema, TOperationType> = this;
+    for (let i = 0; i < 100; i++) {
+      const $parent = $owning.getGraphQLParent();
+      if ($parent instanceof GraphQLSelectionSetStep) {
+        $owning = $parent;
+      } else {
+        return $owning;
+      }
+    }
+    throw new Error(`Walked too far`);
+  }
+
+  selectField(fieldName: string, details: FieldDetails = {}): string {
+    const { args, selections } = details;
+    const $op = this.getOperation();
+    const $owningSS = this.owningSelectionSet();
+    const alias = $owningSS.getFieldAlias(fieldName, details);
+    this.selections.push({
+      kind: Kind.FIELD,
+      alias:
+        alias === fieldName ? undefined : { kind: Kind.NAME, value: alias },
+      name: { kind: Kind.NAME, value: fieldName },
+      arguments:
+        args && Object.keys(args).length > 0
+          ? Object.entries(args).map(([argName, $step]) => ({
+              kind: Kind.ARGUMENT,
+              name: {
+                kind: Kind.NAME,
+                value: argName,
+              },
+              value: {
+                kind: Kind.VARIABLE,
+                name: {
+                  kind: Kind.NAME,
+                  value: $op.getVariableName($step, argName),
+                },
+              },
+            }))
+          : undefined,
+      selectionSet: selections
+        ? {
+            kind: Kind.SELECTION_SET,
+            selections,
+          }
+        : undefined,
+    });
+    return alias;
+  }
+
+  usedAliases = new Set<string>();
+  fieldAliases = new Map<string, string>();
+  protected getFieldAlias(fieldName: string, { args }: FieldDetails) {
+    const signature = args
+      ? `${fieldName}|${Object.entries(args)
+          .map(([argName, $step]) => `${argName}:${$step.id}`)
+          .join(",")}`
+      : fieldName;
+    const existing = this.fieldAliases.get(signature);
+    if (existing) {
+      return existing;
+    }
+    let alias = fieldName;
+    let counter = 1;
+    while (this.usedAliases.has(alias)) {
+      alias = `${fieldName}${++counter}`;
+    }
+    this.usedAliases.add(alias);
+    this.fieldAliases.set(signature, alias);
+    return alias;
   }
 
   addSelection(selection: SelectionNode) {
