d6b370bec01282aa826e1065c64963dd8e88385f
Benjie Gillam <benjie@jemjie.com>
2026-07-01 11:15:46 +0100

    Move log for missing type

--- graphile-build/graphile-build-pg/src/plugins/PgCodecsPlugin.ts
+++ graphile-build/graphile-build-pg/src/plugins/PgCodecsPlugin.ts
@@ -521,13 +521,13 @@ export const PgCodecsPlugin: GraphileConfig.Plugin = {
             });
             return codec;
           } else {
-            console.dir(event);
             console.warn(
               `Could not build PgCodec for '${
                 type.getNamespace()?.nspname ?? "??"
               }.${
                 type.typname
               }'; maybe you need a plugin implementing gather.hooks.pgCodecs_findPgCodec to add support.`,
+              event,
             );
             return null;
           }
