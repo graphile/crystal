c7fa05babf8e3a9492f6466e0fcd8b8ae5d5dab1
Benjie Gillam <benjie@jemjie.com>
2026-07-13 11:34:12 +0100

    Optimize coalesce to eliminate looping for small sizes of dependencies

--- grafast/grafast/src/steps/coalesce.ts
+++ grafast/grafast/src/steps/coalesce.ts
@@ -2,6 +2,18 @@ import { FLAG_INHIBITED, FLAG_NULL } from "../constants.ts";
 import type { Maybe, UnbatchedExecutionExtra } from "../interfaces.ts";
 import type { Step } from "../step.ts";
 import { UnbatchedStep } from "../step.ts";
+import { constant } from "./constant.ts";
+
+type UBE = (
+  _info: UnbatchedExecutionExtra,
+  ...values: ReadonlyArray<any>
+) => any;
+
+const ube0: UBE = () => null;
+const ube1: UBE = (_info, a) => a ?? null;
+const ube2: UBE = (_info, a, b) => a ?? b ?? null;
+const ube3: UBE = (_info, a, b, c) => a ?? b ?? c ?? null;
+const ube4: UBE = (_info, a, b, c, d) => a ?? b ?? c ?? d ?? null;
 
 export class CoalesceStep<T> extends UnbatchedStep<T | null> {
   static $$export = {
@@ -21,6 +33,19 @@ export class CoalesceStep<T> extends UnbatchedStep<T | null> {
     }
   }
 
+  finalize() {
+    // Use optimal callback functions rather than looping
+    const l = this.dependencies.length;
+    if (l === 0) this.unbatchedExecute = ube0;
+    else if (l === 1) this.unbatchedExecute = ube1;
+    else if (l === 2) this.unbatchedExecute = ube2;
+    else if (l === 3) this.unbatchedExecute = ube3;
+    else if (l === 4) this.unbatchedExecute = ube4;
+    else {
+      // Keep default
+    }
+  }
+
   unbatchedExecute(
     _info: UnbatchedExecutionExtra,
     ...values: ReadonlyArray<T | null>
@@ -37,10 +62,14 @@ export function coalesce<T>(
 ): CoalesceStep<T>;
 export function coalesce<T>(
   ...input: ReadonlyArray<Step<Maybe<T>>> | [ReadonlyArray<Step<Maybe<T>>>]
-): CoalesceStep<T> {
-  if (Array.isArray(input[0])) {
-    return new CoalesceStep(input[0]);
-  } else {
-    return new CoalesceStep(input as ReadonlyArray<Step<Maybe<T>>>);
+): Step<T | null> {
+  const list = Array.isArray(input[0])
+    ? input[0]
+    : (input as ReadonlyArray<Step<Maybe<T>>>);
+
+  if (list.length === 0) {
+    return constant(null);
   }
+
+  return new CoalesceStep(list);
 }
