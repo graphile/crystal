3be0b58e1e277f9898fe0279f02c7b1c872c8838
Benjie Gillam <benjie@jemjie.com>
2026-08-07 14:03:23 +0100

    Update snapshots with planning error

--- grafast/dataplan-pg/__tests__/queries/trap/trapped-empty-pg-select.deopt.sql
+++ grafast/dataplan-pg/__tests__/queries/trap/trapped-empty-pg-select.deopt.sql
@@ -8,28 +8,3 @@ where
     true /* authorization checks */
   )
 order by __messages__."id" asc;
-
-select
-  __messages__."id" as "0"
-from app_public.messages as __messages__
-where
-  (
-    true /* connection */
-  ) and (
-    __messages__.id = any($1::"uuid"[])
-  ) and (
-    true /* authorization checks */
-  )
-order by __messages__."id" asc;
-
-select
-  (count(*))::text as "0"
-from app_public.messages as __messages__
-where
-  (
-    true /* connection */
-  ) and (
-    __messages__.id = any($1::"uuid"[])
-  ) and (
-    true /* authorization checks */
-  );
