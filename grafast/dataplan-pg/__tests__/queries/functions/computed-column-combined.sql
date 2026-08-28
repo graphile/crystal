e16be9d16f410e0638254bdf6a8a3f7b61297646
Benjie Gillam <benjie@jemjie.com>
2026-08-28 22:57:45 +0100

    Apply forced another left join into a subquery

--- grafast/dataplan-pg/__tests__/queries/functions/computed-column-combined.sql
+++ grafast/dataplan-pg/__tests__/queries/functions/computed-column-combined.sql
@@ -1,34 +1,37 @@
 select
   __forums__."id" as "0",
-  __forums_random_user__."username" as "1",
-  __forums_random_user__."gravatar_url" as "2",
   (
     select array[
-      __users_most_recent_forum__."id",
-      __forums_unique_author_count__.v::text,
-      array(
+      __forums_random_user__."username",
+      __forums_random_user__."gravatar_url",
+      (
         select array[
-          __forums_featured_messages__."body"
+          __users_most_recent_forum__."id",
+          __forums_unique_author_count__.v::text,
+          array(
+            select array[
+              __forums_featured_messages__."body"
+            ]::text[]
+            from app_public.forums_featured_messages(__users_most_recent_forum__) as __forums_featured_messages__
+            where (
+              true /* authorization checks */
+            )
+          )::text
         ]::text[]
-        from app_public.forums_featured_messages(__users_most_recent_forum__) as __forums_featured_messages__
-        where (
+        from app_public.users_most_recent_forum(__forums_random_user__) as __users_most_recent_forum__
+        left outer join app_public.forums_unique_author_count(__users_most_recent_forum__) as __forums_unique_author_count__(v)
+        on (
+        /* WHERE becoming ON */ (
           true /* authorization checks */
-        )
+        ))
       )::text
     ]::text[]
-    from app_public.users_most_recent_forum(__forums_random_user__) as __users_most_recent_forum__
-    left outer join app_public.forums_unique_author_count(__users_most_recent_forum__) as __forums_unique_author_count__(v)
-    on (
-    /* WHERE becoming ON */ (
+    from app_public.forums_random_user(__forums__) as __forums_random_user__
+    where (
       true /* authorization checks */
-    ))
-  )::text as "3"
+    )
+  )::text as "1"
 from app_public.forums as __forums__
-left outer join app_public.forums_random_user(__forums__) as __forums_random_user__
-on (
-/* WHERE becoming ON */ (
-  true /* authorization checks */
-))
 where
   (
     __forums__."id" = $1::"uuid"
