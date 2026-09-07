ae73cc06f8cd2904191406990e4f39263ebda72e
BenjAIe <code+ai@benjiegillam.com>
2026-09-07 13:20:13 +0100

    Expand test

--- grafast/grafast/__tests__/types-test.ts
+++ grafast/grafast/__tests__/types-test.ts
@@ -21,13 +21,17 @@ type TStep = __ValueStep<Grafast.Context>;
 type TCtx = TStep extends Step<infer U> ? U : never;
 assert<Equals<TCtx, Grafast.Context>>();
 
+type StepValue<T extends Step> = T extends Step<infer U> ? U : never;
 const myPlan: FieldPlanResolver = () => {
   const $dependency = constant(1);
   const $record = loadOne($dependency, (dependencies) =>
     dependencies.map(() => ({ a: 1, b: 2, c: 3 })),
   );
   const $a1 = get($record, "a");
+  assert<Equals<StepValue<typeof $a1>, number>>();
   const $a2 = $record.get("a");
+  assert<Equals<StepValue<typeof $a2>, number>>();
   const $a3 = access($record, "a");
+  assert<Equals<StepValue<typeof $a3>, number>>();
   return constant(null);
 };
