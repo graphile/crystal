07337da8ed4d3842636d85b27063705ad8bcaf85
BenjAIe <code+ai@benjiegillam.com>
2026-09-19 17:54:52 +0100

    Update snapshots

--- postgraphile/postgraphile/__tests__/mutations/plugins/mutation-wrappers.sql
+++ postgraphile/postgraphile/__tests__/mutations/plugins/mutation-wrappers.sql
@@ -1,19 +1,39 @@
+begin; /*fake*/
+
 insert into "c"."person" as __person__ ("id", "person_full_name", "email") values ($1::"int4", $2::"varchar", $3::"b"."email") returning
   __person__."id" as "0",
   __person__."person_full_name" as "1",
-  __person__."id"::text as "2";
+  __person__."about" as "2",
+  __person__."id"::text as "3";
+
+commit; /*fake*/
+
+begin; /*fake*/
 
 update "c"."person" as __person__ set "person_full_name" = $1::"varchar" where (__person__."id" = $2::"int4") returning
   __person__."id" as "0",
   __person__."person_full_name" as "1",
-  __person__."id"::text as "2";
+  __person__."about" as "2",
+  __person__."id"::text as "3";
+
+commit; /*fake*/
+
+begin; /*fake*/
 
 delete from "c"."person" as __person__ where (__person__."id" = $1::"int4") returning
   __person__."id" as "0",
   __person__."person_full_name" as "1",
-  __person__."id"::text as "2";
+  __person__."about" as "2",
+  __person__."id"::text as "3";
+
+commit; /*fake*/
+
+begin; /*fake*/
 
 insert into "c"."person" as __person__ ("id", "person_full_name", "email") values ($1::"int4", $2::"varchar", $3::"b"."email") returning
   __person__."id" as "0",
   __person__."person_full_name" as "1",
-  __person__."id"::text as "2";
+  __person__."about" as "2",
+  __person__."id"::text as "3";
+
+commit; /*fake*/
