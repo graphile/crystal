d0d9a35e08b8105b0ec0450bef42c348b6fc4500
BenjAIe <code+ai@benjiegillam.com>
2026-09-19 18:37:44 +0100

    Undo optional params

--- grafast/dataplan-pg/src/executor.ts
+++ grafast/dataplan-pg/src/executor.ts
@@ -165,11 +165,6 @@ export type PgExecutorMutationOptions = {
   context: PgExecutorContext;
   /** @defaultValue `true` */
   useTransaction?: boolean;
-
-  /** @deprecated Pass a callback instead. */
-  text?: string;
-  /** @deprecated Pass a callback instead. */
-  values?: ReadonlyArray<SQLRawValue>;
 };
 type DeprecatedPgExecutorMutationOptions = PgExecutorMutationOptions & {
   /** @deprecated Pass a callback instead. */
