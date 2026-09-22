db9ee52b099b0a9fdd5f3302ca35f506614e32ec
Benjie Gillam <benjie@jemjie.com>
2026-09-22 23:10:35 +0100

    Update test with more assertions

--- grafast/grafast/__tests__/prepare-test.ts
+++ grafast/grafast/__tests__/prepare-test.ts
@@ -11,6 +11,7 @@ import {
 } from "../dist/index.js";
 
 it("prepares an operation without executing it", async () => {
+  let queryHelloPlans = 0;
   let executions = 0;
   const schema = makeGrafastSchema({
     typeDefs: /* GraphQL */ `
@@ -22,6 +23,7 @@ it("prepares an operation without executing it", async () => {
       Query: {
         plans: {
           hello() {
+            queryHelloPlans++;
             return sideEffect(null, () => {
               executions++;
               return "world";
@@ -34,15 +36,22 @@ it("prepares an operation without executing it", async () => {
   });
   const args = { schema, document: parse("query { hello }") };
 
+  expect(executions).to.equal(0);
+  expect(queryHelloPlans).to.equal(0);
+
   const prepared = prepare(args);
   if (prepared.errors) throw prepared.errors[0];
+  expect(queryHelloPlans).to.equal(1);
   expect(executions).to.equal(0);
 
   const result = await execute(args);
   expect(result).to.deep.equal({ data: { hello: "world" } });
+  expect(queryHelloPlans).to.equal(1);
   expect(executions).to.equal(1);
 
   const preparedAgain = prepare(args);
   if (preparedAgain.errors) throw preparedAgain.errors[0];
   expect(preparedAgain.operationPlan).to.equal(prepared.operationPlan);
+  expect(queryHelloPlans).to.equal(1);
+  expect(executions).to.equal(1);
 });
